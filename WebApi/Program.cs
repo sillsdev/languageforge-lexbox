fa8629ee1474ae53d7ddcd076b77176b1b33ddbd
Kevin Hahn <hahn.kev@gmail.com>
2023-02-10 14:30:36 +0700

    add reverse proxy

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,11 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorizationBuilder()
+    .AddPolicy("HgAuth",
+        policyBuilder =>
+        {
+            policyBuilder.RequireAuthenticatedUser();
+        });
 
 var app = builder.Build();
 
