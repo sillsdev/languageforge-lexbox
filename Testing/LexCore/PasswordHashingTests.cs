2d16f038e3bf5b47edd607aec7bc71beab68580e
Kevin Hahn <hahn.kev@gmail.com>
2023-02-14 14:27:27 +0700

    add another password for redmine password hashing.

--- Testing/LexCore/PasswordHashingTests.cs
+++ Testing/LexCore/PasswordHashingTests.cs
@@ -6,6 +6,7 @@ namespace Testing.LexCore;
 public class PasswordHashingTests
 {
     [InlineData("h0vJVjQUOfxaEC3Uddz", "266d7346dbf074a28ab29ef730191feb", "ba205a1131e34030fa1507a77cb9d1bd0c5fb800")]
+    [InlineData("5!2j6cwau2ZqUXL", "57b7216f93b66d8dfa6cc0dad910025d", "f17f1754dfcf346298e2154071fc077203900a28")]
     [Theory]
     public void CanHashPassword(string pw, string salt, string hash)
     {
