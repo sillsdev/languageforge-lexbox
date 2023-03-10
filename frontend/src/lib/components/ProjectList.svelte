1a05b5009a8dbf0387f7887773cd78e503e5bc66
Kevin Hahn <hahn.kev@gmail.com>
2023-03-10 10:09:14 +0700

    make the project list card size consistent.

--- frontend/src/lib/components/ProjectList.svelte
+++ frontend/src/lib/components/ProjectList.svelte
@@ -5,11 +5,11 @@
     export let projects: Project[] = [];
 </script>
 
-<div class="grid grid-cols-3 gap-4 justify-items-center">
+<div class="grid grid-cols-3 gap-4">
     {#each projects as project}
-        <div class="card w-50 bg-base-200">
+        <div class="card bg-base-200">
             <div class="card-body">
-                <h2 class="card-title">{project.name}</h2>
+                <h2 class="card-title overflow-hidden text-ellipsis" title={project.name}>{project.name}</h2>
                 <p>{project.code}</p>
             </div>
         </div>
