02393232081da6140a4e4ab639bfcdf7d9a33620
billy clark <billy_clark@sil.org>
2023-03-10 12:38:49 -0500

    themed ui

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -5,4 +5,22 @@ module.exports = {
 		require('@tailwindcss/typography'),
 		require('daisyui'),
 	],
+	daisyui: {
+		themes: [
+			{
+				light: {
+					...require('daisyui/src/colors/themes')['[data-theme=light]'],
+					primary: '#104060',
+					secondary: '#0a2440',
+				}
+			},
+			{
+				dark: {
+					...require('daisyui/src/colors/themes')['[data-theme=dark]'],
+					primary: '#104060',
+					secondary: '#0a2440',
+				}
+			},
+		 ]
+	}
 }
