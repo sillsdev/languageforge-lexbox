448a4e74529b99b2a61dcc36d63fafcbe8862e8a
Kevin Hahn <hahn.kev@gmail.com>
2023-03-15 12:35:36 +0700

    introduce svg icons with tailwind css.

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,9 +1,13 @@
+const { iconsPlugin, getIconCollections } = require("@egoist/tailwindcss-icons")
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{svelte,ts}'],
 	plugins: [
 		require('@tailwindcss/typography'),
 		require('daisyui'),
+		iconsPlugin({
+			collections: getIconCollections(['mdi'])
+		})
 	],
 	daisyui: {
 		themes: [
