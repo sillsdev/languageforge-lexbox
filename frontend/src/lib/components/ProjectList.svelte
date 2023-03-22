6926beac6635ec92787155592e1b124e9812d82c
Kevin Hahn <hahn.kev@gmail.com>
2023-03-22 11:14:07 +0700

    user dashboard, display `Project Empty` when there's no last change date.

--- frontend/src/lib/components/ProjectList.svelte
+++ frontend/src/lib/components/ProjectList.svelte
@@ -1,32 +1,36 @@
 ﻿<script lang="ts">
-	import t from '$lib/i18n';
-	import type { Project } from '$lib/project';
-	import FormatDate from './FormatDate.svelte';
+    import t from '$lib/i18n';
+    import type {Project} from '$lib/project';
 
-	export let projects: Project[] = [];
+    export let projects: Project[] = [];
 </script>
 
 <div class="grid grid-cols-3 gap-4">
-	{#each projects as project}
-		<div class="card bg-base-200">
-			<div class="card-body">
-				<h2 class="card-title overflow-hidden text-ellipsis" title={project.name}>
-					<a class="link" href={`/project/${project.code}`}>
-						{project.name}
-					</a>
-				</h2>
+    {#each projects as project}
+        <div class="card bg-base-200">
+            <div class="card-body">
+                <h2 class="card-title overflow-hidden text-ellipsis" title={project.name}>
+                    <a class="link" href={`/project/${project.code}`}>
+                        {project.name}
+                    </a>
+                </h2>
 
-				<p>{project.code}</p>
+                <p>{project.code}</p>
 
-				{#if project.userCount > 1}
-					<p>{t('projectlist.shared_with', {memberCount: project.userCount})}</p>
-				{/if}
+                {#if project.userCount > 1}
+                    <p>{t('projectlist.shared_with', {memberCount: project.userCount})}</p>
+                {/if}
 
-				<p>
-					{t('projectlist.last_change')}
-					<FormatDate date={project.lastCommit} nullLabel={t('unknown')} />
-				</p>
-			</div>
-		</div>
-	{/each}
+                <p>
+                    {#if project.lastCommit}
+                        {t('projectlist.last_change', {lastChange: new Date(project.lastCommit)})}
+                    {:else}
+                        <div class="badge badge-info">
+                            {t('projectlist.no_changes')}
+                        </div>
+                    {/if}
+                </p>
+            </div>
+        </div>
+    {/each}
 </div>
