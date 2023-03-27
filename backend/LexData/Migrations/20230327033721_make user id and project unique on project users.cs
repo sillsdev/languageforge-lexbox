eb4054ce7de8d2543fc9a288a4173f0ee2a050f5
Kevin Hahn <hahn.kev@gmail.com>
2023-03-27 10:54:17 +0700

    add unique constraint on project user to prevent duplicate entries per user.

---
+++ backend/LexData/Migrations/20230327033721_make user id and project unique on project users.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LexData.Migrations
+{
+    /// <inheritdoc />
+    public partial class makeuseridandprojectuniqueonprojectusers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ProjectUsers_UserId",
+                table: "ProjectUsers");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectUsers_UserId_ProjectId",
+                table: "ProjectUsers",
+                columns: new[] { "UserId", "ProjectId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ProjectUsers_UserId_ProjectId",
+                table: "ProjectUsers");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectUsers_UserId",
+                table: "ProjectUsers",
+                column: "UserId");
+        }
+    }
+}
