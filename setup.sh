e3438bc9610a9c75030824ffe2ee0ea553107a43
Chris Hirt <chris@hirtfamily.net>
2023-04-20 22:12:30 +0700

    update setup scripts with new hgweb path
    The new path is "hgweb", changed from "hg-web"

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 ﻿#!/bin/sh
 
-rm -rf hg-web/repos/sena-3
+rm -rf hgweb/repos/sena-3
 wget -O sena-3.zip 'https://drive.google.com/uc?export=download&id=1I-hwc0RHoQqW774gbS5qR-GHa1E7BlsS'
-unzip -q sena-3.zip -d hg-web/repos/
+unzip -q sena-3.zip -d hgweb/repos/
 rm sena-3.zip
