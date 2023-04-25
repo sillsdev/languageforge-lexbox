4c3a52513c8af0f9e8231cfdc7353c7b076c13aa
Kevin Hahn <hahn.kev@gmail.com>
2023-04-25 11:32:07 +0700

    add email rendering in svelte via mjml, expose for preview via a route, and as an api for dotnet.

---
+++ frontend/src/routes/email/tester/+page.ts
@@ -0,0 +1,6 @@
+﻿
+import type {PageLoadEvent} from "./$types";
+// stub, just return data loaded on the server since we can't do that client side.
+export function load(event: PageLoadEvent) {
+    return event.data;
+}
