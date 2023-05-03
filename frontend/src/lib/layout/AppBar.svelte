c20a3ac5e706905fcec08ae209c5576c26380516
Kevin Hahn <hahn.kev@gmail.com>
2023-05-03 10:50:23 +0700

    add environment warning for non prod environments.

--- frontend/src/lib/layout/AppBar.svelte
+++ frontend/src/lib/layout/AppBar.svelte
@@ -1,26 +1,36 @@
-<script lang=ts>
-	import t from '$lib/i18n'
-	import { AuthenticatedUserIcon, UserAddOutline } from '$lib/icons'
-	import { user } from '$lib/user'
-	import { createEventDispatcher } from 'svelte'
+<script lang="ts">
+	import { env } from '$env/dynamic/public';
+	import t from '$lib/i18n';
+	import { AuthenticatedUserIcon, UserAddOutline } from '$lib/icons';
+	import { user } from '$lib/user';
+	import { createEventDispatcher } from 'svelte';
 
-	const dispatch = createEventDispatcher()
+	let environmentName = env.PUBLIC_ENV_NAME;
+	const dispatch = createEventDispatcher();
 </script>
 
 <!-- https://daisyui.com/components/navbar -->
-<header class='navbar bg-primary text-primary-content'>
-	<span class='navbar-start ml-4 text-lg md:text-3xl tracking-wider'>
-		{ $t('appbar.app_name') }
+<header class="navbar bg-primary text-primary-content">
+	<span class="navbar-start ml-4 text-lg md:text-3xl tracking-wider">
+		{$t('appbar.app_name')}
 	</span>
-
-	<div class=navbar-end>
+	{#if environmentName !== 'production'}
+		<div class="alert alert-warning">
+			<div class="items-center">
+				<span>
+					{ $t('environment-warning', {environmentName}) }
+				</span>
+			</div>
+		</div>
+	{/if}
+	<div class="navbar-end">
 		{#if $user}
-			<button on:click={ () => dispatch('menuopen') } class='btn btn-primary btn-circle'>
+			<button on:click={() => dispatch('menuopen')} class="btn btn-primary btn-circle">
 				<AuthenticatedUserIcon />
 			</button>
 		{:else}
-			<div class='tooltip tooltip-left' data-tip='Create a new account'>
-				<a href=/register class='btn btn-primary btn-circle'>
+			<div class="tooltip tooltip-left" data-tip="Create a new account">
+				<a href="/register" class="btn btn-primary btn-circle">
 					<UserAddOutline />
 				</a>
 			</div>
