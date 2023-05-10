6f93a07d32414c086070b99bc1ac757a424aee72
Kevin Hahn <hahn.kev@gmail.com>
2023-05-10 10:30:51 +0700

    update form usage to use submitting status properly.

--- frontend/src/lib/forms/index.ts
+++ frontend/src/lib/forms/index.ts
@@ -4,16 +4,27 @@ import ProtectedForm, { type Token } from './ProtectedForm.svelte'
 import Input from './Input.svelte'
 import Select from "./Select.svelte";
 import { superForm, type SuperForm, type FormOptions } from 'sveltekit-superforms/client';
-import type { AnyZodObject, z } from 'zod';
+import type { AnyZodObject } from 'zod';
 import {superValidate} from "sveltekit-superforms/server";
+import type { Validation, ZodValidation } from 'sveltekit-superforms/index';
+import type { ActionResult } from '@sveltejs/kit';
 
 //we've got to wrap this in our own version because we're not using the server side component, which this expects
-export function lexSuperForm<S extends AnyZodObject>(schema: S, options: Omit<FormOptions<S, string>, 'validators'> = {}): SuperForm<S, string> {
-	return superForm(undefined, {
-		validators: schema,
+export function lexSuperForm<S extends ZodValidation<AnyZodObject>>(schema: S, onSubmit: NonNullable<FormOptions<S, string>["onUpdate"]>, options: Omit<FormOptions<S, string>, 'validators'> = {}): SuperForm<S, string> {
+	return superForm<S>(undefined, {
+		validators: schema as any,
 		dataType: 'json',
 		SPA: true,
-		 ...options});
+		invalidateAll: false,
+		 ...options,
+		 onResult: async (event) => {
+			await options.onResult?.(event);
+			const result = event.result as ActionResult<{form: Validation<S>}>;
+			if (result.type == "success" && result.data) {
+				await onSubmit({form: result.data.form, cancel: event.cancel});
+			}
+		 },
+	});
 }
 
 //again not using the server side component, so we have to wrap this
--- frontend/src/routes/forgotPassword/+page.svelte
+++ frontend/src/routes/forgotPassword/+page.svelte
@@ -1,36 +1,32 @@
 ﻿<script lang="ts">
-    import Page from "$lib/layout/Page.svelte";
-    import {z} from "zod";
-    import t from '$lib/i18n';
-    import {lexSuperForm, lexSuperValidate} from "$lib/forms";
-    import {Form, Input} from "$lib/forms";
-    import Button from "$lib/forms/Button.svelte";
-    import {goto} from "$app/navigation";
+	import Page from '$lib/layout/Page.svelte';
+	import { z } from 'zod';
+	import t from '$lib/i18n';
+	import { lexSuperForm } from '$lib/forms';
+	import { Form, Input } from '$lib/forms';
+	import Button from '$lib/forms/Button.svelte';
+	import { goto } from '$app/navigation';
 
-    const formSchema = z.object({
-        email: z.string().email($t('register.email')),
-    });
-    let {form, errors, valid, update, enhance} = lexSuperForm(formSchema);
-
-    async function submit() {
-        await lexSuperValidate($form, formSchema, update);
-        if (!$valid) return;
-        await fetch(`api/login/forgotPassword?email=${$form.email}`, {method: 'POST'});
-        goto('/forgotPassword/emailSent');
-    }
+	const formSchema = z.object({
+		email: z.string().email($t('register.email')),
+	});
+	let { form, errors, enhance, submitting } = lexSuperForm(formSchema, async ({ form }) => {
+		await fetch(`api/login/forgotPassword?email=${$form.email}`, { method: 'POST' });
+		await goto('/forgotPassword/emailSent');
+	});
 </script>
 
 <Page>
-    <Form {enhance} on:submit={submit}>
-        <Input
-            id="email"
-            label={$t('register.label_email')}
-            autofocus
-            type="email"
-            required
-            bind:value={$form.email}
-            error={$errors.email}
-    />
-        <Button>{$t('forgot_password.send_email')}</Button>
-    </Form>
+	<Form {enhance}>
+		<Input
+			id="email"
+			label={$t('register.label_email')}
+			autofocus
+			type="email"
+			required
+			bind:value={$form.email}
+			error={$errors.email}
+		/>
+		<Button loading={$submitting}>{$t('forgot_password.send_email')}</Button>
+	</Form>
 </Page>
