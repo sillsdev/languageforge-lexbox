fd1acc3b864f1f0df3dc4903c320db4564f1e8bc
Robin Munn <rmunn@pobox.com>
2023-05-29 12:31:14 +0700

    Fix FW model version in S/R
    Because of the way the model version checks go in FLExBridge, we need to
    specify only the second half (after the period) of the FLEx model
    version as a parameter, not the full two-part version.

--- backend/Testing/Services/SendReceiveService.cs
+++ backend/Testing/Services/SendReceiveService.cs
@@ -12,7 +12,7 @@ namespace Testing.Services;
 public class SendReceiveService
 {
     private readonly IOptions<HgConfig> _hgOptions;
-    private const string fdoDataModelVersion = "7500002.7000072";
+    private const string fdoDataModelVersion = "7000072";
     private readonly IProgress _progress;
 
     public SendReceiveService(IProgress progress, IOptions<HgConfig> hgOptions)
