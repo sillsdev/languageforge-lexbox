4a741c14168d55bd91799359fbee1dbe9d1200b1
Tim Haasdyk <tim_haasdyk@sil.org>
2023-06-02 17:40:44 +0200

    Fix no user race condition on projects dashboard.

--- frontend/src/routes/+layout.ts
+++ frontend/src/routes/+layout.ts
@@ -4,7 +4,8 @@ import { user } from '$lib/user'
 import {browser} from "$app/environment";
 
 export function load(event: LayoutLoadEvent)  {
-    if (browser) initClient(event);
-    user.set(event.data.user);
-    return { traceParent: event.data.traceParent };
+  if (browser) initClient(event);
+  const _user = event.data.user;
+  user.set(_user);
+  return { traceParent: event.data.traceParent, userId: _user?.id };
 }
