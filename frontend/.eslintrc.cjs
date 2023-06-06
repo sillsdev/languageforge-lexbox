dd3d42f475923a771aaa4d1f6dbe93eb517fbae0
Tim Haasdyk <tim_haasdyk@sil.org>
2023-06-06 10:21:07 +0200

    Remove deprecated eslint-plugin-svelte3

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -2,12 +2,8 @@ module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
 	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-	plugins: ['svelte3', '@typescript-eslint'],
+	plugins: ['@typescript-eslint'],
 	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	settings: {
-		'svelte3/typescript': () => require('typescript')
-	},
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2020
