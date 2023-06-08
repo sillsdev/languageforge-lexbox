50e0ea64a303e76459856a5b7e5a9f227825e677
Kevin Hahn <hahn.kev@gmail.com>
2023-06-08 14:00:03 +0700

    update readme with taskfile usage, add some missing commands

--- setup.bat
+++
@@ -1,5 +0,0 @@
-echo off
-rmdir /s /q hgweb\repos\sena-3
-powershell -Command "Invoke-WebRequest 'https://drive.google.com/uc?export=download&id=1I-hwc0RHoQqW774gbS5qR-GHa1E7BlsS' -OutFile sena-3.zip"
-powershell -Command "Expand-Archive sena-3.zip -DestinationPath hgweb\repos"
-del sena-3.zip
