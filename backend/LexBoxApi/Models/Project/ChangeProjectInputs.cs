3de11892325090b182af0cbf80b0e090b822d4f0
dadukhankevin <danieljlosey@gmail.com>
2023-06-18 22:44:47 -0500

    fixed input

--- backend/LexBoxApi/Models/Project/ChangeProjectInputs.cs
+++ backend/LexBoxApi/Models/Project/ChangeProjectInputs.cs
@@ -4,4 +4,4 @@ public record ChangeProjectNameInput(Guid ProjectId, string Name);
 
 public record ChangeProjectDescriptionInput(Guid ProjectId, string Description);
 
-public record ChangeUserAccountDataInput(Guid UserId, string Username, string Email, string Name);
+public record ChangeUserAccountDataInput(Guid UserId, string Email, string Name);
