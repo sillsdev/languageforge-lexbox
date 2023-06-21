25e2a39039f6de381d3cd19d3054f0b3d2ba6624
Robin Munn <rmunn@pobox.com>
2023-06-21 15:11:48 +0700

    Address review comments

--- frontend/src/lib/user.ts
+++ frontend/src/lib/user.ts
@@ -116,23 +116,12 @@ function clear(): void {
 export async function hash(password: string): Promise<string> {
   const msgUint8 = new TextEncoder().encode(password) // encode as (utf-8) Uint8Array
   let hashBuffer: ArrayBuffer;
-  // const c = crypto ? crypto : await import('node:crypto');
-  if (crypto && crypto.subtle) {
-    hashBuffer = await crypto.subtle.digest('SHA-1', msgUint8) // hash the message
+  const c = crypto ? crypto : await import('node:crypto');
+  if (c && c.subtle) {
+    hashBuffer = await c.subtle.digest('SHA-1', msgUint8) // hash the message
   } else {
-    // Running server-side
-    try {
-      const crypto = await import('node:crypto');
-      if (crypto.subtle)
-        hashBuffer = await crypto.subtle.digest('SHA-1', msgUint8) // hash the message
-      else {
-        console.log('crypto is', crypto);
-        throw new Error('crypto found but crypto.subtle not there');
-      }
-    } catch (err) {
-      console.log('ERROR: crypto module not available -- are we running on an old version of Node? Error was', err);
-      hashBuffer = new ArrayBuffer(0); // Should cause the hash comparison to fail
-    }
+      console.log('crypto.subtle not found; cryptop module was', c);
+      throw new Error('crypto.subtle not found -- are we running on an old version of Node?');
   }
   const hashArray = Array.from(new Uint8Array(hashBuffer)) // convert buffer to byte array
   const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('') // convert bytes to hex string
