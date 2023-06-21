af8ba6c7dbcaa7df948b3620df935761cb831379
Robin Munn <rmunn@pobox.com>
2023-06-21 17:02:26 +0700

    Allow refreshing user data in pages if needed
    Any page needing to refresh user data can call invalidate('user:(id)')
    and that will trigger this load function to re-run, keeping the page
    data up-to-date. This will be useful later, such as once the "edit user
    data" PR gets merged in.

--- frontend/src/routes/+layout.server.ts
+++ frontend/src/routes/+layout.server.ts
@@ -1,9 +1,11 @@
 import type { LayoutServerLoadEvent } from './$types'
 import { getRootTraceparent } from '$lib/otel/server'
 
-export function load({ locals }: LayoutServerLoadEvent) {
+export function load({ locals, depends }: LayoutServerLoadEvent) {
   const user = locals.getUser();
   const traceParent = getRootTraceparent()
+
+  if (user) depends(`user:${user.id}`);
 
   return {
     user,
