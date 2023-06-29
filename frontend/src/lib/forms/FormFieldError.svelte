c08c998c7aff979b88d9dd48e051658d54d70369
Tim Haasdyk <tim_haasdyk@sil.org>
2023-06-29 14:56:52 +0200

    Fix required description error, standardize forms

---
+++ frontend/src/lib/forms/FormFieldError.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let error: string | string[] | undefined;
+  export let id: string;
+</script>
+
+{#if error}
+  <label for={id} class="label">
+    <span class="label-text-alt text-error">{typeof error === 'string' ? error : error.join(', ')}</span>
+  </label>
+{/if}
