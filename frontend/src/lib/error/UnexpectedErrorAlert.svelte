6a884f8da8cb606efa91f94ecb25bd09bd8d8e84
Tim Haasdyk <tim_haasdyk@sil.org>
2023-06-29 17:06:58 +0200

    Make UnexpectedErrorAlert a dialog element

--- frontend/src/lib/error/UnexpectedErrorAlert.svelte
+++ frontend/src/lib/error/UnexpectedErrorAlert.svelte
@@ -5,31 +5,38 @@
   import { t } from 'svelte-intl-precompile';
   import UnexpectedError from './UnexpectedError.svelte';
 
-  let alertContainerElem: HTMLElement;
+  let dialog: HTMLDialogElement;
 
   beforeNavigate(dismiss);
 
   onDestroy(
     error.subscribe((e) => {
-      alertContainerElem?.classList.toggle('hidden', !e);
+      if (!dialog) return;
+      (e) ? open() : close();
     })
   );
 
   function dismissClick(): void {
-    alertContainerElem.classList.add('hidden');
+    close();
     dismiss();
+  }
+
+  function open(): void {
+    dialog.showModal();
+    dialog.classList.add('modal-open');
+  }
+
+  function close(): void {
+    dialog.close();
+    dialog.classList.remove('modal-open');
   }
 </script>
 
-<!-- https://daisyui.com/components/alert -->
-<aside
-  bind:this={alertContainerElem}
-  class:hidden={!$error}
-  class="block alert alert-error shadow-lg w-auto fixed bottom-4 z-10 left-1/2 -translate-x-1/2 max-w-[95vw] max-h-[95vh] overflow-y-auto"
->
-  <UnexpectedError />
-
-  <div class="flex justify-end">
-    <button on:click={dismissClick} class="btn btn-ghost self-end">{$t('modal.dismiss')}</button>
+<dialog bind:this={dialog} class="modal modal-bottom alert-error">
+  <div class="modal-box alert-error">
+    <UnexpectedError />
+    <div class="flex justify-end modal-action">
+      <button on:click={dismissClick} class="btn btn-ghost self-end">{$t('modal.dismiss')}</button>
+    </div>
   </div>
-</aside>
+</dialog>
