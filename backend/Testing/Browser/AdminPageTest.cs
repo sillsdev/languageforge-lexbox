17d74f4d6d1f236bc385789a8ad69098907216fa
Kevin Hahn <hahn.kev@gmail.com>
2023-07-06 10:50:04 +0700

    Refactor tests and add ManagerPageTest.cs

--- backend/Testing/Browser/AdminPageTest.cs
+++ backend/Testing/Browser/AdminPageTest.cs
@@ -7,26 +7,20 @@ namespace Testing.Browser;
 [Trait("Category", "Integration")]
 public class AdminPageTest: PageTest
 {
+    private string _host = TestingEnvironmentVariables.ServerHostname;
     [Fact]
     public async Task CanNavigateToProjectPage()
     {
-        await Page.GotoAsync($"https://{TestingEnvironmentVariables.ServerHostname}/admin");
-
-        await Page.GetByLabel("Email (or Send/Receive username)").ClickAsync();
+        await Page.GotoAsync($"https://{_host}/admin");
 
         await Page.GetByLabel("Email (or Send/Receive username)").FillAsync("admin");
-
-        await Page.GetByLabel("Email (or Send/Receive username)").PressAsync("Tab");
-
         await Page.GetByLabel("Password").FillAsync("pass");
-
         await Page.GetByLabel("Password").PressAsync("Enter");
 
-        await Expect(Page).ToHaveURLAsync($"https://{TestingEnvironmentVariables.ServerHostname}/admin");
+        await Expect(Page).ToHaveURLAsync($"https://{_host}/admin");
 
         await Page.GetByRole(AriaRole.Link, new() { Name = "Sena 3" }).ClickAsync();
 
-        await Expect(Page).ToHaveURLAsync($"https://{TestingEnvironmentVariables.ServerHostname}/project/sena-3");
-
+        await Expect(Page).ToHaveURLAsync($"https://{_host}/project/sena-3");
     }
 }
