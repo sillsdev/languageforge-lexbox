01f49e315c2c92386fb29b335971750571a661ec
dadukhankevin <danieljlosey@gmail.com>
2023-07-07 01:31:11 -0500

    hypothetically persistant notifications

--- frontend/src/lib/notify/notify.svelte
+++ frontend/src/lib/notify/notify.svelte
@@ -1,63 +1,52 @@
 <script lang="ts">
-    import { writable, type Writable } from 'svelte/store';
-    interface Notification {
-      id: string;
-      message: string;
-      category: string;
-      duration: number;
-    }
+  import { writable, type Writable } from 'svelte/store';
 
-    // create a store to hold the notifications
-    export const notifications: Writable<Notification[]> = writable([]);
+  interface Notification {
+    id: string;
+    message: string;
+    category: string;
+    duration: number;
+  }
 
-    // create a function to add a notification
-    export function add(
-      message = 'Success',
-      category = 'Success',
-      duration = 1000,
-    ): void {
-      // generate a unique id for the notification
-      const id: string = Math.random().toString(36).substr(2, 9);
+  // Retrieve notifications from localStorage or use an empty array
+  const initialNotifications: Notification[] = JSON.parse(localStorage.getItem('notifications')) || [];
 
-      // push the notification to the store
-      notifications.update((n: Notification[]) => [
-        ...n,
-        { id, message, category, duration },
-      ]);
+  // create a writable store to hold the notifications
+  export const notifications: Writable<Notification[]> = writable(initialNotifications);
 
-      // remove the notification after the duration
-      setTimeout(() => {
-        remove(id);
-      }, duration * 1000);
-    }
+  // create a function to add a notification
+  export function add(
+    message = 'Success',
+    category = 'Success',
+    duration = 1000,
+  ): void {
+    // generate a unique id for the notification
+    const id: string = Math.random().toString(36).substr(2, 9);
 
-    export function remove(id: string): void {
-      notifications.update((n: Notification[]) =>
-        n.filter((x: Notification) => x.id !== id)
-      );
-    }
-    export function removeAll(): void {
+    // push the notification to the store
+    notifications.update((n: Notification[]) => [
+      ...n,
+      { id, message, category, duration },
+    ]);
+
+    // remove the notification after the duration
+    setTimeout(() => {
+      remove(id);
+    }, duration * 1000);
+  }
+
+  export function remove(id: string): void {
+    notifications.update((n: Notification[]) =>
+      n.filter((x: Notification) => x.id !== id)
+    );
+  }
+
+  export function removeAll(): void {
     notifications.set([]);
   }
-  </script>
 
-<div class="toast toast-center w-96">
-  {#if $notifications.length > 1}
-  <div class="mt-2">
-    <button class="btn btn-ghost" on:click={removeAll}>Close All ✕</button>
-  </div>
-  {/if}
-  {#each $notifications as { id, message, category }}
-  <div class={`alert alert-${category}`}>
-    {message}
-    <span class="btn btn-ghost btn-sm float-right pd-0" on:click={() => remove(id)}>
-      ✕
-    </span>
-  </div>
-  {/each}
-  <!--force these classes to be loaded-->
-  <div class="alert-success" hidden></div>
-  <div class="alert-error" hidden></div>
-  <div class="alert-warning" hidden></div>
-</div>
-
+  // Persist notifications in localStorage whenever the store changes
+  notifications.subscribe((value: Notification[]) => {
+    localStorage.setItem('notifications', JSON.stringify(value));
+  });
+</script>
