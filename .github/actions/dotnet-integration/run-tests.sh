75412e562495a93b33f35c6381b17cdd298a070d
Kevin Hahn <hahn.kev@gmail.com>
2023-07-12 14:20:15 +0700

    change dotnet integration to run off built runner image instead of building each CI run. Don't install playwright deps speed up test run

--- .github/actions/dotnet-integration/run-tests.sh
+++ .github/actions/dotnet-integration/run-tests.sh
@@ -1,4 +1,4 @@
 ﻿dotnet restore
 dotnet build
-pwsh backend/Testing/bin/Debug/net7.0/playwright.ps1 install --with-deps
+pwsh backend/Testing/bin/Debug/net7.0/playwright.ps1 install
 dotnet test --logger trx --results-directory ./testresults --filter Category=Integration
