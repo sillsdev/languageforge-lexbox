1af36f411fa178595880b2f35e42f6b18c99a708
Kevin Hahn <hahn.kev@gmail.com>
2023-07-12 11:51:03 +0700

    configure integration action to run commands in a script

---
+++ .github/actions/dotnet-integration/run-tests.sh
@@ -0,0 +1,4 @@
+﻿dotnet restore
+dotnet build
+pwsh backend/Testing/bin/Debug/net7.0/playwright.ps1 install --with-deps
+dotnet test --logger trx --results-directory ./testresults --filter Category=Integration
