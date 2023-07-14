dd5d289736d07507819e116691a7c5729951ac31
Kevin Hahn <hahn.kev@gmail.com>
2023-07-14 12:40:51 +0700

    refactor S&R integration tests

--- backend/Testing/Services/TestingEnvironmentVariables.cs
+++ backend/Testing/Services/TestingEnvironmentVariables.cs
@@ -5,4 +5,20 @@ public static class TestingEnvironmentVariables
     public static string ServerHostname = Environment.GetEnvironmentVariable("TEST_SERVER_HOSTNAME") ?? "localhost:5158";
     public static string StandardHgHostname = Environment.GetEnvironmentVariable("TEST_STANDARD_HG_HOSTNAME") ?? "hg.localhost";
     public static string ResumableHgHostname = Environment.GetEnvironmentVariable("TEST_RESUMABLE_HG_HOSTNAME") ?? "resumable.localhost";
+
+    public static string GetTestHostName(this HgProtocol protocol)
+    {
+        return protocol switch
+        {
+            HgProtocol.Hgweb => StandardHgHostname,
+            HgProtocol.Resumable => ResumableHgHostname,
+            _ => throw new ArgumentOutOfRangeException(nameof(protocol), protocol, null)
+        };
+    }
+}
+
+public enum HgProtocol
+{
+    Hgweb,
+    Resumable
 }
--- backend/Testing/SyncReverseProxy/SendReceiveServiceTests.cs
+++ backend/Testing/SyncReverseProxy/SendReceiveServiceTests.cs
@@ -1,8 +1,6 @@
+using System.Runtime.CompilerServices;
 using Chorus.VcsDrivers.Mercurial;
-using Microsoft.Extensions.Hosting;
 using Shouldly;
-using SIL.Progress;
-using Testing.Logging;
 using Testing.Services;
 using Xunit.Abstractions;
 
@@ -11,12 +9,20 @@ namespace Testing.SyncReverseProxy;
 [Trait("Category", "Integration")]
 public class SendReceiveServiceTests
 {
+    public SendReceiveAuth ManagerAuth = new("manager", "pass");
+    public SendReceiveAuth AdminAuth = new("admin", "pass");
+    public SendReceiveAuth InvalidPass = new("manager", "incorrect_pass");
+    public SendReceiveAuth InvalidUser = new("invalid_user", "pass");
+    public SendReceiveAuth UnauthorizedUser = new("user", "pass");
+
     private readonly ITestOutputHelper _output;
     private string _basePath = Path.Join(Path.GetTempPath(), "SendReceiveTests");
+    private SendReceiveService _sendReceiveService;
 
     public SendReceiveServiceTests(ITestOutputHelper output)
     {
         _output = output;
+        _sendReceiveService = new SendReceiveService(_output);
         CleanUpTempDir();
     }
 
@@ -33,15 +39,38 @@ public class SendReceiveServiceTests
         }
     }
 
+    private static int _folderIndex = 1;
+
+    private (string projectDir, string fwDataFile) GetProjectDir(string projectCode,
+        string? identifier = null,
+        [CallerMemberName] string testName = "")
+    {
+        var projectDir = Path.Join(_basePath, testName);
+        if (identifier is not null) projectDir = Path.Join(projectDir, identifier);
+        //fwdata file containing folder name will be the same as the file name
+        projectDir = Path.Join(projectDir, _folderIndex++.ToString(), projectCode);
+        var fwDataFile = Path.Join(projectDir, $"{projectCode}.fwdata");
+        return (projectDir, fwDataFile);
+    }
+
+    private SendReceiveParams GetParams(HgProtocol protocol,
+        string projectCode = TestData.ProjectCode,
+        [CallerMemberName] string testName = "")
+    {
+        var (projectDir, _) = GetProjectDir(projectCode, testName: testName);
+        var sendReceiveParams = new SendReceiveParams(projectCode, protocol.GetTestHostName(), projectDir);
+        return sendReceiveParams;
+    }
+
     [Fact]
     public async Task VerifyHgWorking()
     {
-        var srService = new SendReceiveService(_progress);
-        string version = await srService.GetHgVersion();
+        string version = await _sendReceiveService.GetHgVersion();
         version.ShouldStartWith("Mercurial Distributed SCM");
     }
 
-    private static readonly (string host, string type)[] HostsAndTypes = {
+    private static readonly (string host, string type)[] HostsAndTypes =
+    {
         (host: $"http://{TestingEnvironmentVariables.StandardHgHostname}", type: "normal"),
         (host: $"http://{TestingEnvironmentVariables.ResumableHgHostname}", type: "resumable")
     };
@@ -67,143 +96,142 @@ public class SendReceiveServiceTests
         {
             foreach (var (user, pass, valid) in Credentials)
             {
-                yield return new[]
-                {
-                    new SendReceiveTestData(projectCode, host, type, user, pass, valid)
-                };
+                yield return new[] { new SendReceiveTestData(projectCode, host, type, user, pass, valid) };
             }
         }
     }
 
     [Fact(
-        Skip = "Just for testing, comment out to run"
-        )]
+        // Skip = "Just for testing, comment out to run"
+    )]
     public void CloneForDev()
     {
-        CloneProjectAndSendReceive(GetTestDataForSR("sena-3").First().OfType<SendReceiveTestData>().First());
+        var sendReceiveTestData = GetTestDataForSR("sena-3").ElementAt(1)
+            .OfType<SendReceiveTestData>().First();
+        _output.WriteLine("Test run: {0}", sendReceiveTestData);
+        // CloneProjectAndSendReceive(sendReceiveTestData);
     }
 
     [Theory]
-    [MemberData(nameof(GetTestDataForSR), "sena-3")]
-    public void CloneProjectAndSendReceive(SendReceiveTestData data)
+    [InlineData(HgProtocol.Hgweb, "admin")]
+    [InlineData(HgProtocol.Hgweb, "manager")]
+    [InlineData(HgProtocol.Resumable, "admin")]
+    [InlineData(HgProtocol.Resumable, "manager")]
+    public void CanCloneSendReceive(HgProtocol hgProtocol, string user)
     {
-        var srService = new SendReceiveService(_output, data.Host);
+        var (projectDir, fwDataFile) = GetProjectDir(TestData.ProjectCode, Path.Join(hgProtocol.ToString(), user));
+        var auth = new SendReceiveAuth(user, "pass");
+        var sendReceiveParams = new SendReceiveParams(TestData.ProjectCode, hgProtocol.GetTestHostName(), projectDir);
 
-        string projectDir = Path.Join(_basePath, data.HostType, data.ProjectCode);
-        string fwdataFile = Path.Join(projectDir, $"{data.ProjectCode}.fwdata");
-        long oldLength = 0;
-        var fileInfo = new FileInfo(fwdataFile);
-        try
-        {
-            string result = srService.CloneProject(data.ProjectCode, projectDir, data.Username, data.Password);
-            if (data.ShouldPass)
-            {
-                result.ShouldNotContain("abort");
-                result.ShouldNotContain("error");
-                fileInfo.Directory?.Exists.ShouldBeTrue("directory " + fileInfo.DirectoryName + " not found. Clone response: " + result);
-                fileInfo.Directory!.EnumerateFiles().ShouldContain(child => child.Name == fileInfo.Name);
-                fwdataFile.ShouldSatisfyAllConditions(
-                    () => fileInfo.Exists.ShouldBeTrue(),
-                    () => fileInfo.Length.ShouldBeGreaterThan(0)
-                );
-                oldLength = fileInfo.Length;
-            }
-            else
-            {
-                result.ShouldMatch("abort: authorization failed|Server Response 'Unauthorized'");
-            }
-        }
-        catch (RepositoryAuthorizationException) when (!data.ShouldPass)
-        {
-            // This is a successful test, because the repo rejected the invalid password as it should
-        }
-        catch (UnauthorizedAccessException) when (!data.ShouldPass)
-        {
-            // This is a successful test, because the repo rejected the invalid password as it should
-        }
+        // Clone
+        var cloneResult = _sendReceiveService.CloneProject(sendReceiveParams, auth);
+        cloneResult.ShouldNotContain("abort");
+        cloneResult.ShouldNotContain("error");
+        Directory.Exists(projectDir).ShouldBeTrue($"Directory {projectDir} not found. Clone response: {cloneResult}");
+        Directory.EnumerateFiles(projectDir).ShouldContain(fwDataFile);
+        var fwDataFileInfo = new FileInfo(fwDataFile);
+        fwDataFileInfo.Length.ShouldBeGreaterThan(0);
+        var fwDataFileOriginalLength = fwDataFileInfo.Length;
 
-        // Now do a Send/Receive which should get no changes
-        // Running in same test because it's dependent on CloneProject happening first
-        try
-        {
-            string result2 = srService.SendReceiveProject(data.ProjectCode, projectDir, data.Username, data.Password);
-            if (data.ShouldPass)
-            {
-                result2.ShouldNotContain("abort");
-                result2.ShouldNotContain("error");
-                result2.ShouldContain("no changes from others");
-                fwdataFile.ShouldSatisfyAllConditions(
-                    () => fileInfo.Exists.ShouldBeTrue(),
-                    () => fileInfo.Length.ShouldBe(oldLength)
-                );
-            }
-            else
-            {
-                result2.ShouldMatch("abort: authorization failed|Server Response 'Unauthorized'");
-            }
-        }
-        catch (Chorus.VcsDrivers.Mercurial.RepositoryAuthorizationException)
-        {
-            if (data.ShouldPass)
-            {
-                throw;
-            }
-            else
-            {
-                // This is a successful test, because the repo rejected the invalid password as it should
-            }
-
-            ;
-        }
-        catch (System.UnauthorizedAccessException)
-        {
-            if (data.ShouldPass)
-            {
-                throw;
-            }
-            else
-            {
-                // This is a successful test, because the repo rejected the invalid password as it should
-            }
-
-            ;
-        }
+        // SendReceive
+        var srResult = _sendReceiveService.SendReceiveProject(sendReceiveParams, auth);
+        srResult.ShouldNotContain("abort");
+        srResult.ShouldNotContain("error");
+        srResult.ShouldContain("no changes from others");
+        fwDataFileInfo.Refresh();
+        fwDataFileInfo.Exists.ShouldBeTrue();
+        fwDataFileInfo.Length.ShouldBe(fwDataFileOriginalLength);
     }
 
-    [Theory]
-    [InlineData("non-existent-project", "manager", "pass")]
-    [InlineData("non-existent-project", "admin", "pass")]
-    // NOTE: resumable failing because can't read sena-3 repo, because owned by UID 82 (Alpine www-data) instead of 33 (Debian www-data)
-    public void TestInvalidProject(string projectCode, string username, string password)
+    [Fact]
+    public void InvalidPassOnCloneHgWeb()
     {
-        foreach (var (host, type) in HostsAndTypes)
-        {
-            if (type == "resumable")
-            {
-                continue; // Skip resumable as Chorus just retries 4xx errors constantly (because it assumes they're caused by a flaky net connection)
-            }
+        var sendReceiveParams = GetParams(HgProtocol.Hgweb);
+        var act = () => _sendReceiveService.CloneProject(sendReceiveParams, InvalidPass);
 
-            var _srService = new SendReceiveService(_output, host);
-            string projectDir = Path.Join(_basePath, type, projectCode);
-            string fwdataFile = Path.Join(projectDir, $"{projectCode}.fwdata");
-            try
-            {
-                string result = _srService.CloneProject(projectCode, projectDir, username, password);
-                throw new Exception(
-                    "Clone should have thrown an exception but didn't; if we reach this point, this is a failed test");
-            }
-            catch (Chorus.VcsDrivers.Mercurial.ProjectLabelErrorException)
-            {
-                // Expected failure - this is what admin sees
-            }
-            catch (Chorus.VcsDrivers.Mercurial.RepositoryAuthorizationException)
-            {
-                // Expected failure - this is what manager user sees
-            }
+        act.ShouldThrow<RepositoryAuthorizationException>();
+    }
 
-            fwdataFile.ShouldSatisfyAllConditions(
-                () => new FileInfo(fwdataFile).Exists.ShouldBeFalse()
-            );
-        }
+    [Fact]
+    public void InvalidPassOnCloneHgResumable()
+    {
+        var sendReceiveParams = GetParams(HgProtocol.Resumable);
+        var act = () => _sendReceiveService.CloneProject(sendReceiveParams, InvalidPass);
+
+        act.ShouldThrow<UnauthorizedAccessException>();
+    }
+
+    [Fact]
+    public void InvalidPassOnSendReceiveHgWeb()
+    {
+        var sendReceiveParams = GetParams(HgProtocol.Hgweb);
+        _sendReceiveService.CloneProject(sendReceiveParams, ManagerAuth);
+
+        var act = () => _sendReceiveService.SendReceiveProject(sendReceiveParams, InvalidPass);
+        act.ShouldThrow<RepositoryAuthorizationException>();
+    }
+
+    [Fact]
+    public void InvalidPassOnSendReceiveHgResumable()
+    {
+        var sendReceiveParams = GetParams(HgProtocol.Resumable);
+        _sendReceiveService.CloneProject(sendReceiveParams, ManagerAuth);
+
+        var act = () => _sendReceiveService.SendReceiveProject(sendReceiveParams, InvalidPass);
+        act.ShouldThrow<UnauthorizedAccessException>();
+    }
+
+    [Fact]
+    public void InvalidUserCloneHgWeb()
+    {
+        var sendReceiveParams = GetParams(HgProtocol.Hgweb);
+        var act = () => _sendReceiveService.CloneProject(sendReceiveParams, InvalidUser);
+        act.ShouldThrow<RepositoryAuthorizationException>();
+    }
+
+    [Fact]
+    public void InvalidUserCloneHgResumable()
+    {
+        var sendReceiveParams = GetParams(HgProtocol.Resumable);
+        var act = () => _sendReceiveService.CloneProject(sendReceiveParams, InvalidUser);
+        act.ShouldThrow<UnauthorizedAccessException>();
+    }
+
+    [Fact]
+    public void InvalidProjectAdminLogin()
+    {
+        var sendReceiveParams = GetParams(HgProtocol.Hgweb, "non-existent-project");
+        var act = () => _sendReceiveService.CloneProject(sendReceiveParams, AdminAuth);
+
+        act.ShouldThrow<ProjectLabelErrorException>();
+        Directory.GetFiles(sendReceiveParams.DestDir).ShouldBeEmpty();
+    }
+
+    [Fact]
+    public void InvalidProjectManagerLogin()
+    {
+        var sendReceiveParams = GetParams(HgProtocol.Hgweb, "non-existent-project");
+        var act = () => _sendReceiveService.CloneProject(sendReceiveParams, ManagerAuth);
+
+        act.ShouldThrow<RepositoryAuthorizationException>();
+        Directory.GetFiles(sendReceiveParams.DestDir).ShouldBeEmpty();
+    }
+
+    [Fact]
+    public void UnauthorizedUserCloneHgWeb()
+    {
+        var sendReceiveParams = GetParams(HgProtocol.Hgweb);
+
+        var act = () => _sendReceiveService.CloneProject(sendReceiveParams, UnauthorizedUser);
+        act.ShouldThrow<RepositoryAuthorizationException>();
+    }
+
+    [Fact]
+    public void UnauthorizedUserCloneHgResumable()
+    {
+        var sendReceiveParams = GetParams(HgProtocol.Resumable);
+
+        var act = () => _sendReceiveService.CloneProject(sendReceiveParams, UnauthorizedUser);
+        act.ShouldThrow<UnauthorizedAccessException>();
     }
 }
