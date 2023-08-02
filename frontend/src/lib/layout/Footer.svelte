ebd83ff5e69e80b27c1bbb82bc18f472c7e40ebf
Tim Haasdyk <tim_haasdyk@sil.org>
2023-08-02 11:02:00 +0200

    Add footer with logos, copyright, contact, and legal links

---
+++ frontend/src/lib/layout/Footer.svelte
@@ -0,0 +1,29 @@
+<script>
+  import t from '$lib/i18n';
+</script>
+
+<footer class="bg-neutral text-neutral-content py-2 px-2 md:py-4 md:px-8 w-screen">
+  <div class="flex justify-between items-start max-w-2xl m-auto">
+    <div class="space-y-2">
+      <div class="flex gap-4 items-center">
+        <a href="https://www.sil.org/">
+          <!-- https://www.sil.org/about/media -->
+          <img class="h-12" src="./sil_logo_glyph.png" alt="SIL International logo" />
+        </a>
+        <a href="https://ic.payap.ac.th/">
+          <!-- http://info.payap.ac.th/info/link_download/ -->
+          <img class="h-11" src="./payap-logo.png" alt="Payap University logo" />
+        </a>
+      </div>
+      <div>
+        © {new Date().getFullYear()}
+        <a class="hover:link" href="https://www.sil.org/">SIL International</a>
+      </div>
+    </div>
+    <div class="flex flex-col text-right">
+      <a class="hover:link" href="mailto:admin@languagedepot.org" target="_blank">{$t('footer.contact')}</a>
+      <a class="hover:link" href="https://software.sil.org/language-software-terms-of-use/">{$t('footer.terms_of_use')}</a>
+      <a class="hover:link" href="https://software.sil.org/language-software-privacy-policy/">{$t('footer.privacy_policy')}</a>
+    </div>
+  </div>
+</footer>
