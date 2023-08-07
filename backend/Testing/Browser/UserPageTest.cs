43f09b483e27c033e1a86117edbd46b845759312
Tim Haasdyk <tim_haasdyk@sil.org>
2023-08-07 15:07:44 +0200

    Refactor Playwright tests to use page objects

--- backend/Testing/Browser/UserPageTest.cs
+++ backend/Testing/Browser/UserPageTest.cs
@@ -1,54 +1,54 @@
-using Microsoft.Playwright;
 using Testing.Browser.Base;
-using Testing.Services;
+using Testing.Browser.Page;
 
 namespace Testing.Browser;
 
 [Trait("Category", "Integration")]
 public class UserPageTest : PageTest
 {
-    private string _host = TestingEnvironmentVariables.ServerHostname;
-
     public override async Task InitializeAsync()
     {
         await base.InitializeAsync();
-
-        await LoginAs("admin", "pass");
     }
 
-    [Fact(Skip = "we need to create a user for testing that we can change the email of, maybe do that in a dedicated test")]
+    [Fact]
     public async Task CanUpdateAccountInfo()
     {
-        await Page.GotoAsync($"http://{_host}/user");
-        await Page.GetByLabel("Display name").ClickAsync();
-        await Page.GetByLabel("Display name").FillAsync("Test Admin Changed");
-        await Page.GetByLabel("Email").ClickAsync();
-        await Page.GetByLabel("Email").FillAsync("admin_new@test.com");
-        // Submit the form
-        await Page.ClickAsync("text=Update account info");
-        await Page.WaitForLoadStateAsync(LoadState.DOMContentLoaded);
-        var successMessage = await Page.QuerySelectorAsync("text=Your account has been updated.");
-        Assert.NotNull(successMessage);
+        await using var userDashboardPage = await RegisterUser("Test: Edit account - update", $"{Guid.NewGuid()}@mailinator.com", "test_edit_account_update");
+        var userPage = await new UserAccountSettingsPage(Page).Goto();
+        await userPage.FillDisplayName("Test: Edit account - update - changed");
+        await userPage.FillEmail($"{Guid.NewGuid()}_changed@test.com");
+        await userPage.ClickSave();
+        await Page.GetByText("Your account has been updated.").WaitForAsync();
+        await userPage.EmailVerificationAlert.AssertRequestedToChange();
     }
 
     [Fact]
     public async Task DisplaysFormErrorsOnInvalidData()
     {
-        //need to use network idle, otherwise the inputs might get reset by hydration
-        await Page.GotoAsync($"http://{_host}/user", new (){WaitUntil = WaitUntilState.NetworkIdle});
-        await Page.FillAsync("#email", "");
-        await Page.FillAsync("#name", "");
-        await Page.ClickAsync("text=Update account info");
+        await using var userDashboardPage = await RegisterUser("Test: Edit account - errors", $"{Guid.NewGuid()}@mailinator.com", "test_edit_account_errors");
+        var userPage = await new UserAccountSettingsPage(Page).Goto();
+        await userPage.FillDisplayName("");
+        await userPage.FillEmail("");
+        await userPage.ClickSave();
         await Expect(Page.GetByText("Invalid email")).ToBeVisibleAsync();
     }
 
     [Fact]
     public async Task CanResetPassword()
     {
-        await Page.GotoAsync($"http://{_host}/user");
-        await Expect(Page).ToHaveURLAsync($"http://{_host}/user");
-        await Page.ClickAsync("text=Reset your password instead?");
-        await Expect(Page).ToHaveURLAsync($"http://{_host}/resetPassword");
+        await using var userDashboardPage = await RegisterUser("Test: Edit account - reset password", $"{Guid.NewGuid()}@mailinator.com", "test_edit_account_reset_password");
+        var user = userDashboardPage.User;
+        var userPage = await new UserAccountSettingsPage(Page).Goto();
+        var resetPasswordPage = await userPage.ClickResetPassword();
+        var newPassword = "test_edit_account_reset_password_changed";
+        await resetPasswordPage.FillForm(newPassword);
+        await resetPasswordPage.Submit();
+
+        var loginPage = await Logout();
+        await loginPage.FillForm(userDashboardPage.User.Email, newPassword);
+        await Task.WhenAll(
+            loginPage.Submit(),
+            new UserDashboardPage(Page).WaitFor());
     }
 }
-
