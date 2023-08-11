9d383cef4a6f9c3b206f6ebf8db0000c3a6d6ee1
Tim Haasdyk <tim_haasdyk@sil.org>
2023-08-11 16:39:14 +0200

    Prevent 'Cannot subscribe to 'page' store on the server outside of a Svelte component'

--- frontend/src/routes/+error.svelte
+++ frontend/src/routes/+error.svelte
@@ -1,6 +1,19 @@
 <script lang="ts">
+  import { browser } from '$app/environment';
+  import { page } from '$app/stores';
+  import { error } from '$lib/error';
   import UnexpectedError from '$lib/error/UnexpectedError.svelte';
   import { Content, Page } from '$lib/layout';
+  import { onDestroy } from 'svelte';
+
+  
+  if (browser) {
+    onDestroy(
+      page.subscribe((p) => {
+        error.set(p.error);
+      })
+    );
+  }
 </script>
 
 <Content>
