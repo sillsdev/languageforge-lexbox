8070fb8176cd72b029b713efb34e408098c98b41
Kevin Hahn <hahn.kev@gmail.com>
2023-08-21 11:45:45 +0700

    allow using projection with the mutation convention attribute, gql.

--- backend/LexBoxApi/GraphQL/GraphQlSetupKernel.cs
+++ backend/LexBoxApi/GraphQL/GraphQlSetupKernel.cs
@@ -25,6 +25,7 @@ public static class GraphQlSetupKernel
             .RegisterService<LoggedInContext>()
             .RegisterService<EmailService>()
             .RegisterService<LexAuthService>()
+            //for now this is an inlined version to work around this issue: https://github.com/fiakkasa/DataAnnotatedModelValidations/issues/13
             .AddDataAnnotationsValidator()
             .AddSorting(descriptor =>
             {
