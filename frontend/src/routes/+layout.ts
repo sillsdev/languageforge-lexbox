be3c9707f177a8a5dcc00bb3e2a4563b871a6102
Robin Munn <rmunn@pobox.com>
2023-09-04 16:52:50 +0700

    Remove do-nothing load() in root layout
    The load() function in the root layout.ts file now does absolutely
    nothing; it takes the data from the parent layout and passes it along
    completely unchanged. Let's get rid of it and simplify our code.

--- frontend/src/routes/+layout.ts
+++ frontend/src/routes/+layout.ts
@@ -1,7 +1,2 @@
-import type { LayoutLoadEvent } from './$types';
-
 //setting this to false can help diagnose requests to the api as you can see them in the browser instead of sveltekit
 export const ssr = true;
-export function load(event: LayoutLoadEvent) {
-  return { ...event.data, traceParent: event.data.traceParent };
-}
