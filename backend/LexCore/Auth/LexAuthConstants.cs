f97933ce2c7950216ebecf16b0367998e792a4c0
Kevin Hahn <kevin_hahn@sil.org>
2023-09-06 10:52:52 +0700

    change can create project claim type
    Co-authored-by: Tim Haasdyk <myieye@gmail.com>

--- backend/LexCore/Auth/LexAuthConstants.cs
+++ backend/LexCore/Auth/LexAuthConstants.cs
@@ -8,7 +8,7 @@ public static class LexAuthConstants
     public const string IdClaimType = "sub";
     public const string ProjectsClaimType = "proj";
     public const string EmailUnverifiedClaimType = "unver";
-    public const string CanCreateProjectClaimType = "mkProject";
+    public const string CanCreateProjectClaimType = "mkproj";
 }
 
 /// <summary>
