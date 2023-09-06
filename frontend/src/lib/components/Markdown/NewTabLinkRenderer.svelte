89e6f3283f0328ca312d50a513401657f0142c0c
Tim Haasdyk <tim_haasdyk@sil.org>
2023-09-06 14:57:54 +0700

    Make form input description links open in new tab

---
+++ frontend/src/lib/components/Markdown/NewTabLinkRenderer.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  export let href: string;
+  export let title: string | undefined = undefined;
+  export let text: string;
+</script>
+
+<a {href} {title} target="_blank" class="inline-flex items-end gap-0.5">
+  {text}<span class="i-mdi-open-in-new" />
+</a>
---
+++ frontend/src/lib/components/Markdown/index.ts
@@ -0,0 +1,1 @@
+export { default as NewTabLinkRenderer } from './NewTabLinkRenderer.svelte';
