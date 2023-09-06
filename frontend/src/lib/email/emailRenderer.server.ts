5f26cb8813fbae556165622fbccee6fffe3ecc09
Kevin Hahn <hahn.kev@gmail.com>
2023-09-06 15:31:49 +0700

    update to svelte 4 and typescript 5

--- frontend/src/lib/email/emailRenderer.server.ts
+++ frontend/src/lib/email/emailRenderer.server.ts
@@ -1,4 +1,4 @@
-import { parse, walk } from 'svelte/compiler';
+import { parse, walk, type Node } from 'svelte/compiler';
 
 import type { ComponentType } from 'svelte';
 import type { TemplateNode } from 'svelte/types/compiler/interfaces';
@@ -11,9 +11,10 @@ export type RenderEmailResult = { subject: string, html: string };
 function getSubject(head: string): string {
   // using the Subject.svelte component a title should have been placed in the head.
   // we need to parse the head and find the title and extract the text
-  const headAst = parse(head, { filename: 'file.html' });
+  const {html} = parse(head, { filename: 'file.html' });
   let subject: string | undefined;
-  walk(headAst.html, {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  walk(html as Node, {
     enter(node: TemplateNode, ..._) {
       if (node.type === 'Element' && node.name === 'title') {
         subject = node.children?.[0].data as string;
@@ -27,8 +28,12 @@ function getSubject(head: string): string {
 export function render(emailComponent: ComponentType, props: Omit<EmailTemplateProps, 'template'>): RenderEmailResult {
   // eslint-disable-next-line
   const result: RenderResult = (emailComponent as any).render(props) as RenderResult;
+  const mjmlResult = mjml2html(result.html, { validationLevel: 'soft' });
+  if (mjmlResult.errors) {
+    console.error(mjmlResult.errors);
+  }
   return {
-    html: mjml2html(result.html, { validationLevel: 'strict' }).html,
+    html: mjmlResult.html,
     subject: getSubject(result.head)
   };
 }
