92786bcb038c3dbe576e755b09e5dec77f283328
Robin Munn <rmunn@pobox.com>
2023-09-07 11:20:07 +0700

    Edit Checkbox component to have label on right
    https://github.com/sillsdev/languageforge-lexbox/pull/211#issuecomment-1675984794

--- frontend/src/lib/forms/Checkbox.svelte
+++ frontend/src/lib/forms/Checkbox.svelte
@@ -8,7 +8,7 @@
 
 <div class="form-control w-full">
   <label class="label cursor-pointer">
-    <span class="label-text">{label}</span>
     <input {id} type="checkbox" bind:checked={value} class="checkbox" />
+    <span class="label-text">{label}</span>
   </label>
 </div>
