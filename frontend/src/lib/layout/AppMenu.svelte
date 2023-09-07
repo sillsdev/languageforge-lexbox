b2c778b0cbb3d2ea33485d605f5050b1e9635b4c
Kevin Hahn <hahn.kev@gmail.com>
2023-09-07 15:02:45 +0700

    don't close sidebar unless navigation or clicking outside. Change my projects in breadcrumbs to 'Home' #219

--- frontend/src/lib/layout/AppMenu.svelte
+++ frontend/src/lib/layout/AppMenu.svelte
@@ -1,68 +1,63 @@
 <script lang="ts">
   import t from '$lib/i18n';
-  import { AdminIcon, AuthenticatedUserIcon, HomeIcon, LogoutIcon } from '$lib/icons';
+  import {AdminIcon, AuthenticatedUserIcon, HomeIcon, LogoutIcon} from '$lib/icons';
   import AdminContent from './AdminContent.svelte';
   import Badge from '$lib/components/Badges/Badge.svelte';
-  import { APP_VERSION } from '$lib/util/verstion';
-  import type { LexAuthUser } from '$lib/user';
+  import {APP_VERSION} from '$lib/util/verstion';
+  import type {LexAuthUser} from '$lib/user';
+
   export let serverVersion: string;
   export let apiVersion: string | null;
   export let user: LexAuthUser;
 </script>
 
-<!-- https://daisyui.com/components/drawer -->
-<div class="drawer-side z-10" on:click on:keydown>
-  <div class="drawer-overlay" />
+<!-- https://daisyui.com/components/menu  -->
+<ul class="menu bg-base-100 min-w-[33%] flex-nowrap overflow-y-auto">
+  <header class="prose flex flex-col items-end p-4 mb-4">
+    <h2 class="mb-0">{user.name}</h2>
+    <span class="font-light">{user.email}</span>
+  </header>
 
-  <!-- https://daisyui.com/components/menu  -->
-  <ul class="menu bg-base-100 min-w-[33%] flex-nowrap overflow-y-auto">
-    <header class="prose flex flex-col items-end p-4 mb-4">
-      <h2 class="mb-0">{user.name}</h2>
-      <span class="font-light">{user.email}</span>
-    </header>
+  <li>
+    <a href="/logout" data-sveltekit-preload-data="tap">
+      {$t('appmenu.log_out')}
+      <LogoutIcon/>
+    </a>
+  </li>
 
+  <div class="divider"/>
+
+  <AdminContent>
     <li>
-      <a href="/logout" data-sveltekit-preload-data="tap">
-        {$t('appmenu.log_out')}
-        <LogoutIcon />
+      <a href="/admin" class="text-accent" data-sveltekit-preload-data="tap">
+        {$t('admin_dashboard.title')}
+        <AdminIcon/>
       </a>
     </li>
+  </AdminContent>
 
-    <div class="divider" />
+  <li>
+    <a href="/" data-sveltekit-preload-data="tap">
+      {$t('user_dashboard.title')}
+      <HomeIcon/>
+    </a>
+  </li>
 
-    <AdminContent>
-      <li>
-        <a href="/admin" class="text-accent" data-sveltekit-preload-data="tap">
-          {$t('admin_dashboard.title')}
-          <AdminIcon />
-        </a>
-      </li>
-    </AdminContent>
+  <li>
+    <a href="/user" data-sveltekit-preload-data="tap">
+      {$t('account_settings.title')}
+      <AuthenticatedUserIcon/>
+    </a>
+  </li>
 
-    <li>
-      <a href="/" data-sveltekit-preload-data="tap">
-        {$t('user_dashboard.title')}
-        <HomeIcon />
-      </a>
-    </li>
-
-    <li>
-      <a href="/user" data-sveltekit-preload-data="tap">
-        {$t('account_settings.title')}
-        <AuthenticatedUserIcon />
-      </a>
-    </li>
-
-    <div class="divider" />
-    <div class="grow" />
-    <!-- svelte-ignore a11y-click-events-have-key-events -->
-    <div class="flex flex-col items-end gap-1" on:click|stopPropagation={() => null}>
-      <Badge>Client Version: {APP_VERSION}</Badge>
-      <Badge>Server Version: {serverVersion}</Badge>
-      <Badge>API Version: {apiVersion}</Badge>
-    </div>
-  </ul>
-</div>
+  <div class="divider"/>
+  <div class="grow"/>
+  <div class="flex flex-col items-end gap-1">
+    <Badge>Client Version: {APP_VERSION}</Badge>
+    <Badge>Server Version: {serverVersion}</Badge>
+    <Badge>API Version: {apiVersion}</Badge>
+  </div>
+</ul>
 
 <style>
   a {
