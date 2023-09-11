e182e634e43908e1063b7aa5130c8519908d0806
Kevin Hahn <hahn.kev@gmail.com>
2023-09-11 15:30:07 +0700

    fix BadgeButton.svelte icon type

--- frontend/src/lib/components/Badges/BadgeButton.svelte
+++ frontend/src/lib/components/Badges/BadgeButton.svelte
@@ -4,7 +4,7 @@
   // Add more as necessary. Should be as limited as possible to maximize consistency.
   type BadgeButtonType = 'badge-success' | '';
   export let type: BadgeButtonType = '';
-  export let icon = '';
+  export let icon: `i-mdi-${string}` | undefined = undefined;
   export let disabled = false;
 
   $: _type = type as unknown as BadgeType;
