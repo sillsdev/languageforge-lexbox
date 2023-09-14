5e9da4de0111a46cfb108b79b4f9df0be52de0ed
Kevin Hahn <hahn.kev@gmail.com>
2023-09-14 11:59:19 +0700

    unit test DetermineProjectUrlPrefix

---
+++ backend/Testing/LexCore/Services/LexProxyServiceTests.cs
@@ -0,0 +1,50 @@
+﻿using LexBoxApi.Services;
+using LexCore.Config;
+using LexCore.Entities;
+using LexCore.Exceptions;
+using LexSyncReverseProxy;
+using Shouldly;
+using Testing.Fixtures;
+
+namespace Testing.LexCore.Services;
+
+public class LexProxyServiceTests
+{
+    private const string LexboxHgWeb = "https://hg.lexbox";
+    private const string LexboxResumable = "https://resumable.lexbox";
+    private const string RedmineResumable = "https://resumable.redmine";
+    private const string RedminePublic = "https://public.redmine";
+    private const string RedminePrivate = "https://private.redmine";
+    private HgConfig _hgConfig = new HgConfig
+    {
+        RepoPath = "nothing",
+        HgWebUrl = LexboxHgWeb,
+        HgResumableUrl = LexboxResumable,
+        PublicRedmineHgWebUrl = RedminePublic,
+        PrivateRedmineHgWebUrl = RedminePrivate,
+        RedmineHgResumableUrl = RedmineResumable
+    };
+
+    [Theory]
+    //lexbox
+    [InlineData(HgType.hgWeb, ProjectMigrationStatus.Migrated, LexboxHgWeb)]
+    [InlineData(HgType.resumable, ProjectMigrationStatus.Migrated, LexboxResumable)]
+    //redmine
+    [InlineData(HgType.hgWeb, ProjectMigrationStatus.PublicRedmine, RedminePublic)]
+    [InlineData(HgType.hgWeb, ProjectMigrationStatus.PrivateRedmine, RedminePrivate)]
+    [InlineData(HgType.resumable, ProjectMigrationStatus.PublicRedmine, RedmineResumable)]
+    [InlineData(HgType.resumable, ProjectMigrationStatus.PrivateRedmine, RedmineResumable)]
+    public void DetermineProjectPrefixWorks(HgType type, ProjectMigrationStatus status, string expectedUrl)
+    {
+        LexProxyService.DetermineProjectUrlPrefix(type, "test", status, _hgConfig).ShouldBe(expectedUrl);
+    }
+
+    [Theory]
+    [InlineData(HgType.hgWeb)]
+    [InlineData(HgType.resumable)]
+    public void ThrowsIfMigrating(HgType type)
+    {
+        var act = () => LexProxyService.DetermineProjectUrlPrefix(type, "test", ProjectMigrationStatus.Migrating, _hgConfig);
+        act.ShouldThrow<ProjectMigratingException>();
+    }
+}
