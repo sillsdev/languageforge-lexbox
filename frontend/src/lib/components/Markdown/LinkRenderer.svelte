58cead7fc07fce3041d20cc41f4e0a08e8f698f2
Tim Haasdyk <tim_haasdyk@sil.org>
2023-10-10 13:56:08 +0200

    Add 'go home' button to all error pages

--- frontend/src/lib/components/Markdown/LinkRenderer.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  export let href: string;
-  export let title: string | undefined = undefined;
-  export let text: string;
-</script>
-
-<a {href} {title} class="link">
-  {text}
-</a>
