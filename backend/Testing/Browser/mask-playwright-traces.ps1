3a5bf92a99ca2e9799c6ea09d9abe2aa0f51a8a4
Tim Haasdyk <tim_haasdyk@sil.org>
2023-10-12 12:30:37 +0200

    Tidy up names in playwright-masker

--- backend/Testing/Browser/mask-playwright-traces.ps1
+++ backend/Testing/Browser/mask-playwright-traces.ps1
@@ -1,6 +1,6 @@
 param(
-    [Parameter(Mandatory)][string] $dir,
-    [Parameter(Mandatory)][string[]] $values
+    [Parameter(Mandatory)][string] $traceDir,
+    [Parameter(Mandatory)][string[]] $secrets
 )
 
 Add-Type -Assembly  System.IO.Compression.FileSystem
@@ -8,23 +8,23 @@ Add-Type -Assembly  System.IO.Compression.FileSystem
 # trace = Core trace info
 # network = Network log
 # json = Network log payloads
-$filesToEdit = "\.(json|trace|network)$"
-$files = Get-ChildItem -path $dir -Filter *.zip
+$filesToMask = "\.(json|trace|network)$"
+$files = Get-ChildItem -path $traceDir -Filter *.zip
 
-Write-Output "Masking $($values.Count) values in $($files.Count) traces.";
+Write-Output "Masking $($secrets.Count) secrets in $($files.Count) traces.";
 
 foreach ($file in $files) {
     try {
         Write-Output "Masking $file"
         $zip = [System.IO.Compression.ZipFile]::Open($file.FullName, "Update")
-        $entries = $zip.Entries.Where({ $_.Name -match $filesToEdit })
+        $entries = $zip.Entries.Where({ $_.Name -match $filesToMask })
         $traceMaskCount = 0;
         foreach ($entry in $entries) {
             $reader = [System.IO.StreamReader]::new($entry.Open())
             $content = $reader.ReadToEnd()
             $entryMaskCount = 0;
-            foreach ($value in $values) {
-                $pieces = $content -split "\b$value\b"
+            foreach ($secret in $secrets) {
+                $pieces = $content -split "\b$secret\b"
                 $entryMaskCount += $pieces.Count - 1;
                 $content = $pieces -join $pieces, "*******"
             }
