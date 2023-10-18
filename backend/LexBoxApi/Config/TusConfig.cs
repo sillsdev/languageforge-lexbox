d4387800dda2bd5c7c5c263c523f054920d437a4
Kevin Hahn <hahn.kev@gmail.com>
2023-10-18 11:56:15 +0700

    add tus upload support with basic example

---
+++ backend/LexBoxApi/Config/TusConfig.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LexBoxApi.Config;
+
+public class TusConfig
+{
+    [Required(AllowEmptyStrings = false)]
+    public required string TestUploadPath { get; set; }
+    [Required(AllowEmptyStrings = false)]
+    public required string ResetUploadPath { get; set; }
+}
