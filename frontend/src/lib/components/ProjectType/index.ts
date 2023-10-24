50e7a6d440334f431aa11a9648f9d504aeb6b0fc
Tim Haasdyk <tim_haasdyk@sil.org>
2023-10-24 14:40:40 +0200

    Use larger project-type icon as background on user dashboard

--- frontend/src/lib/components/ProjectType/index.ts
+++ frontend/src/lib/components/ProjectType/index.ts
@@ -1,3 +1,3 @@
 export { default as ProjectTypeBadge } from './ProjectTypeBadge.svelte';
-export { default as ProjectTypeIcon } from './ProjectTypeIcon.svelte';
+export { default as ProjectTypeIcon, getProjectTypeIcon } from './ProjectTypeIcon.svelte';
 export { default as FormatProjectType, getProjectTypeI18nKey } from './FormatProjectType.svelte';
