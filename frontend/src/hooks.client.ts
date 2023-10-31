def31a58407613170b295d95ef53411b866d940e
Tim Haasdyk <tim_haasdyk@sil.org>
2023-10-31 11:34:47 +0100

    Show error dialog for 500 featch responses

--- frontend/src/hooks.client.ts
+++ frontend/src/hooks.client.ts
@@ -63,11 +63,18 @@ function shouldTryAutoReload(updateDetected: boolean): boolean {
  */
 handleFetch(async ({ fetch, args }) => {
   const response = await traceFetch(() => fetch(...args));
+
   if (response.status === 401 && location.pathname !== '/login') {
     throw redirect(307, '/logout');
   }
+
+  if (response.status >= 500) {
+    throw new Error(`Unexpected response: ${response.statusText} (${response.status}). URL: ${response.url}.`);
+  }
+
   if (response.headers.get('lexbox-refresh-jwt') == 'true') {
     await invalidate(USER_LOAD_KEY);
   }
+
   return response;
 });
