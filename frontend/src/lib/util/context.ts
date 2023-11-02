7f8de7992aa513030ac558ea9f12e3a8579d2f40
Tim Haasdyk <tim_haasdyk@sil.org>
2023-11-02 16:56:36 +0100

    Use streamlined context-api for errors

--- frontend/src/lib/util/context.ts
+++ frontend/src/lib/util/context.ts
@@ -5,13 +5,14 @@ interface ContextDefinition<T> {
   init: (value: T) => T;
 }
 
-export function defineContext<T>(key: string | symbol = Symbol()): ContextDefinition<T> {
+export function defineContext<T>(key: string | symbol = Symbol(), onInit?: (value: T) => void): ContextDefinition<T> {
   return {
     use(): T {
       return getContext<T>(key);
     },
     init(value: T): T {
       setContext(key, value);
+      onInit?.(value);
       return value;
     }
   }
