9313a58d4343705f0508f91dac337a3767dd20d5
Kevin Hahn <hahn.kev@gmail.com>
2023-12-05 16:56:15 +0700

    send user to login with an error when using a jwt with a date that doesn't match the user in the DB

---
+++ backend/LexBoxApi/Auth/Attributes/RequireCurrentUserInfoAttribute.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace LexBoxApi.Auth.Attributes;
+
+/// <summary>
+/// validates the updated date of the jwt against the database, should be used to make a jwt expire if the user is updated
+/// </summary>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+public class RequireCurrentUserInfoAttribute: Attribute, IAuthorizationRequirement, IAuthorizationRequirementData
+{
+    public IEnumerable<IAuthorizationRequirement> GetRequirements()
+    {
+        yield return this;
+    }
+}
