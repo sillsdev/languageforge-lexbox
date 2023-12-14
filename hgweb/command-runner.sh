b275adf2f923fe20a7ffb2c1ecd9a299818a957d
Kevin Hahn <hahn.kev@gmail.com>
2023-12-14 15:56:35 +0700

    first pass at command server

---
+++ hgweb/command-runner.sh
@@ -0,0 +1,33 @@
+﻿#!/bin/bash
+
+# Define the list of allowed commands
+allowed_commands=("verify")
+
+# Get the project code and command name from the URL
+IFS='/' read -ra PATH_INFO <<< "$PATH_INFO"
+project_code="${PATH_INFO[1]}"
+command_name="${PATH_INFO[2]}"
+
+# Ensure the project code and command name are safe to use in a shell command
+if [[ ! $project_code =~ ^[a-zA-Z0-9]+$ ]] || [[ ! $command_name =~ ^[a-zA-Z0-9]+$ ]]; then
+    echo "Content-type: text/plain"
+    echo ""
+    echo "Invalid project code or command name."
+    exit 1
+fi
+
+# Check if the command is in the list of allowed commands
+if [[ ! " ${allowed_commands[@]} " =~ " ${command_name} " ]]; then
+    echo "Content-type: text/plain"
+    echo ""
+    echo "Invalid command. Allowed commands are: ${allowed_commands[*]}"
+    exit 1
+fi
+
+# Run the hg command
+command_output=$(hg $command_name $project_code)
+
+# Output the result
+echo "Content-type: text/plain"
+echo ""
+echo "$command_output"
