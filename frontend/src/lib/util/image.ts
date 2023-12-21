feb155d6e0c9185e224ec7ee86db626c190ddcd4
Tim Haasdyk <tim_haasdyk@sil.org>
2023-12-21 16:34:07 +0100

    Preload project-type icons to minimize delay

---
+++ frontend/src/lib/util/image.ts
@@ -0,0 +1,7 @@
+export function preloadImage(src: string): Promise<void> {
+  return new Promise((resolve) => {
+    const img = new Image();
+    img.src = src;
+    img.onload = () => resolve();
+  });
+}
