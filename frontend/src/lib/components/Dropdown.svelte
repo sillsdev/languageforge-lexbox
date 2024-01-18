e615aadbd0c750989a8929b0bed712040a7c4b8a
Tim Haasdyk <tim_haasdyk@sil.org>
2024-01-18 14:39:10 +0100

    Use grid for project member list

--- frontend/src/lib/components/Dropdown.svelte
+++ frontend/src/lib/components/Dropdown.svelte
@@ -4,9 +4,16 @@
   export let disabled = false;
 </script>
 
-<div class="inline-grid" use:overlay={{disabled}}>
+<div class="dropdown-container inline-flex" use:overlay={{ disabled }}>
   <slot />
   <div class="overlay-content">
     <slot name="content" />
   </div>
 </div>
+
+<style>
+  :global(.dropdown-container > *) {
+    max-width: 100%;
+    flex-grow: 1;
+  }
+</style>
