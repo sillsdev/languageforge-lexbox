356191792263701e0ab7993006d68b9f6b973c10
Tim Haasdyk <tim_haasdyk@sil.org>
2024-01-18 15:44:02 +0100

    Sort and truncate member list

--- frontend/src/lib/forms/Button.svelte
+++ frontend/src/lib/forms/Button.svelte
@@ -2,13 +2,14 @@
   import Loader from '$lib/components/Loader.svelte';
 
   export let loading = false;
-  export let style: 'btn-primary' | 'btn-success' | 'btn-error' | undefined = undefined;
+  export let style: 'btn-primary' | 'btn-success' | 'btn-error' | 'btn-outline' | undefined = undefined;
   export let type: undefined | 'submit' = undefined;
+  export let size: undefined | 'btn-sm' = undefined;
   export let disabled = false;
 </script>
 
 <!-- https://daisyui.com/components/button -->
-<button on:click {...$$restProps} class="btn whitespace-nowrap {style ?? ''} {$$restProps.class ?? ''}" {type} {disabled}>
+<button on:click {...$$restProps} class="btn whitespace-nowrap {style ?? ''} {$$restProps.class ?? ''} {size ?? ''}" {type} {disabled}>
   <Loader {loading} />
   <slot />
 </button>
