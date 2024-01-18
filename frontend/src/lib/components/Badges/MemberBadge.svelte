6959bc496fff7fc6e2fec9a31c88dfa598bf1dd0
Tim Haasdyk <tim_haasdyk@sil.org>
2024-01-18 14:41:30 +0100

    Make manager and editor members different colours

--- frontend/src/lib/components/Badges/MemberBadge.svelte
+++ frontend/src/lib/components/Badges/MemberBadge.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
-  import type { ProjectRole } from '$lib/gql/types';
+  import { ProjectRole } from '$lib/gql/types';
   import FormatUserProjectRole from '../FormatUserProjectRole.svelte';
   import ActionBadge from './ActionBadge.svelte';
   import Badge from './Badge.svelte';
   export let member: { name: string; role: ProjectRole };
   export let canManage = false;
+
+  let variant = member.role === ProjectRole.Manager ? 'btn-primary' as const : 'btn-secondary' as const;
 </script>
 
-<ActionBadge actionIcon="i-mdi-dots-vertical" variant="btn-primary" disabled={!canManage}>
+<ActionBadge actionIcon="i-mdi-dots-vertical" {variant} disabled={!canManage}>
   <span class="pr-3 whitespace-nowrap overflow-ellipsis overflow-hidden" title={member.name}>
     {member.name}
   </span>
