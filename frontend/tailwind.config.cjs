ee7220fddf15b597644d344ff1038bc06ea65af5
Tim Haasdyk <tim_haasdyk@sil.org>
2024-01-19 14:24:41 +0100

    Use base-200 and base-300 from daisy 3 for dark theme

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -12,7 +12,14 @@ module.exports = {
   daisyui: {
     themes: [
       'winter',
-      'business',
+      {
+        'business': {
+          ...require("daisyui/src/theming/themes")["business"],
+          // base colors we had in Daisy 3
+          "base-200": "#0F0F0F",
+          "base-300": "#000",
+        },
+      },
     ],
     darkTheme: 'business',
     logs: false,
