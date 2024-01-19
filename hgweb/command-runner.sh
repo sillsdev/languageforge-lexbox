5244fdd0cf33b4c6d06466cdb7d10f5787356862
Robin Munn <rmunn@pobox.com>
2024-01-19 14:04:59 +0700

    Use chg for greater speed at the cost of some RAM
    The chg command is implemented in C for faster startup speed (no need to
    start a Python interpreter each time). It starts up a Python hg process
    and keeps it running as a daemon, then talks to it over a Unix socket.
    So the second and subsequent times you run chg, you don't have to pay
    the Python startup cost. Worth it if we'll run lexentrycount often.

--- hgweb/command-runner.sh
+++ hgweb/command-runner.sh
@@ -33,11 +33,11 @@ case $command_name in
 
     lexentrycount)
         # The \b for word boundary is necessary to distinguish LexEntry from LexEntryType and similar
-        command_output=$(hg cat -r tip Linguistics/Lexicon/Lexicon_{01,02,03,04,05,06,07,08,09,10}.lexdb | grep -c '<LexEntry\b')
+        command_output=$(chg cat -r tip Linguistics/Lexicon/Lexicon_{01,02,03,04,05,06,07,08,09,10}.lexdb | grep -c '<LexEntry\b')
         ;;
 
     *)
-        command_output=$(hg $command_name)
+        command_output=$(chg $command_name)
         ;;
 esac
 
