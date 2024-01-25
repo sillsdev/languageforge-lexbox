81944a709f6e69a743f15d32892fdea81ff2e8d6
Tim Haasdyk <tim_haasdyk@sil.org>
2024-01-25 15:11:07 +0100

    Add icon to home breadcrumb

--- frontend/src/lib/layout/Breadcrumbs/index.ts
+++ frontend/src/lib/layout/Breadcrumbs/index.ts
@@ -1,2 +1,3 @@
 export { default as Breadcrumbs } from './Breadcrumbs.svelte';
 export { default as PageBreadcrumb } from './PageBreadcrumb.svelte';
+export { default as HomeBreadcrumb } from './HomeBreadcrumb.svelte';
