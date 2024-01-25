56d071a8026e51b73ba032111cbd740338c7cfa1
Robin Munn <rmunn@pobox.com>
2024-01-25 09:06:17 +0700

    Remove int Id from FlexProjectMetadata, key on ProjectId
    No need for a separate Id field in a one-to-one table, since its
    ProjectId foreign key can also safely serve as its primary key.

--- backend/LexCore/Entities/FlexProjectMetadata.cs
+++ backend/LexCore/Entities/FlexProjectMetadata.cs
@@ -3,8 +3,6 @@ namespace LexCore.Entities;
 // Note that we do *not* derive from EntityBase since we don't need Guid IDs or created/modified dates
 public class FlexProjectMetadata
 {
-    public int Id { get; init; }
-
     public Guid ProjectId { get; set; }
     public Project Project { get; set; }
     public int? LexEntryCount { get; set; }
