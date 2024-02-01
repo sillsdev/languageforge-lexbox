ba56071aaf986e99b26afd9967d637d465a22e7a
Robin Munn <rmunn@pobox.com>
2024-02-01 10:12:08 +0700

    Actually install correct Playwright version
    An earlier commit checked in package.json changes, but not the
    corresponding pnpm-lock.yaml changes, so Playwright was looking for the
    wrong browser versions.

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -13,8 +13,6 @@ import { defineConfig, devices } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
   testDir: './tests',
-  /* Default regex looks for *.test.[tj]s, but we want it to look for test*.[tj]s instead */
-  testMatch: /\/test.*\.[tj]s/,
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
