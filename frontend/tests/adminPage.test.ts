478e702ba7bb140f8b7e225f9458608a61ebac91
Robin Munn <rmunn@pobox.com>
2024-02-02 09:38:57 +0700

    Fix types after BasePage improvement

--- frontend/tests/adminPage.test.ts
+++ frontend/tests/adminPage.test.ts
@@ -4,7 +4,7 @@ import { defaultPassword } from './envVars';
 import { AdminDashboardPage } from './pages/adminDashboardPage';
 
 test('can navigate to project page', async ({ page }) => {
-  const loginPage = await new LoginPage(page).goto<LoginPage>();
+  const loginPage = await new LoginPage(page).goto();
   await loginPage.fillForm('admin', defaultPassword);
   await loginPage.submit();
   const adminPage = await new AdminDashboardPage(page).waitFor();
