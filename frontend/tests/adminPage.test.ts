0f00c6975b2a2fd483ac212aed89d92bbcc4184e
Robin Munn <rmunn@pobox.com>
2024-02-02 09:36:35 +0700

    Remove completed TODO comment

--- frontend/tests/adminPage.test.ts
+++ frontend/tests/adminPage.test.ts
@@ -7,31 +7,6 @@ test('can navigate to project page', async ({ page }) => {
   const loginPage = await new LoginPage(page).goto<LoginPage>();
   await loginPage.fillForm('admin', defaultPassword);
   await loginPage.submit();
-  // TODO: Port admin page and finish this test
   const adminPage = await new AdminDashboardPage(page).waitFor();
   await adminPage.openProject('Sena 3', 'sena-3');
 });
-
-/*
-using Testing.Browser.Base;
-using Testing.Browser.Page;
-using Testing.Services;
-
-namespace Testing.Browser;
-
-[Trait("Category", "Integration")]
-public class AdminPageTest : PageTest
-{
-    [Fact]
-    public async Task CanNavigateToProjectPage()
-    {
-        var loginPage = await new LoginPage(Page).Goto();
-        await loginPage.FillForm("admin", TestingEnvironmentVariables.DefaultPassword);
-
-        await loginPage.Submit();
-        var adminPage = await new AdminDashboardPage(Page).WaitFor();
-
-        await adminPage.OpenProject("Sena 3", "sena-3");
-    }
-}
-*/
