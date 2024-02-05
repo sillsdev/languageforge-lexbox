9329a6f043197ba7b70fb38514bcee6df77bf6a2
Robin Munn <rmunn@pobox.com>
2024-02-05 13:03:29 +0700

    Port login page tests

---
+++ frontend/tests/loginPage.test.ts
@@ -0,0 +1,37 @@
+import { expect, test } from '@playwright/test';
+import { LoginPage } from './pages/loginPage';
+import { defaultPassword } from './envVars';
+import { AdminDashboardPage } from './pages/adminDashboardPage';
+
+test('can log in', async ({ page }) => {
+  const loginPage = await new LoginPage(page).goto();
+  await loginPage.fillForm('admin', defaultPassword);
+  await loginPage.submit();
+  await new AdminDashboardPage(page).waitFor();
+});
+
+test('show error withour username', async ({ page }) => {
+  const loginPage = await new LoginPage(page).goto();
+  await loginPage.fillForm('', defaultPassword);
+  await loginPage.submit();
+  await loginPage.waitFor();
+
+  await expect(page.getByText('User info missing')).toBeVisible();
+});
+
+test('can log in after error', async ({ page }) => {
+  const loginPage = await new LoginPage(page).goto();
+  expect(defaultPassword).not.toBe('bad');
+
+  // Bad password is bad
+  await loginPage.fillForm('admin', 'bad');
+  await loginPage.submit();
+  await loginPage.waitFor();
+
+  await expect(page.getByText('Something went wrong, please make sure you have used the correct account informa')).toBeVisible();
+
+  // Correct password still works
+  await loginPage.fillForm('admin', defaultPassword);
+  await loginPage.submit();
+  await new AdminDashboardPage(page).waitFor();
+});
