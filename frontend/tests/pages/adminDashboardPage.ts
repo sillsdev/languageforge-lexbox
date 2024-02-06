fe6ee058223c7118a2db0fc1d21c3827d954c83e
Robin Munn <rmunn@pobox.com>
2024-02-06 10:00:06 +0700

    Fix clicking project not in top 10 on admin dashboard
    These changes are from commit 3af52c79ddb3581a1512778a70524545a59c83e4

--- frontend/tests/pages/adminDashboardPage.ts
+++ frontend/tests/pages/adminDashboardPage.ts
@@ -1,8 +1,10 @@
-import type { Page } from '@playwright/test';
+import type { Locator, Page } from '@playwright/test';
 import { AuthenticatedBasePage } from './authenticatedBasePage';
 import { ProjectPage } from './projectPage';
 
 export class AdminDashboardPage extends AuthenticatedBasePage {
+  get projectFilterBarInput(): Locator { return this.page.locator('.filter-bar').nth(0).getByRole('textbox'); }
+
   constructor(page: Page) {
     super(page, page.locator(`.breadcrumbs :text('Admin Dashboard')`), `/admin`);
   }
@@ -12,7 +14,8 @@ export class AdminDashboardPage extends AuthenticatedBasePage {
     await new ProjectPage(this.page, projectName, projectCode).waitFor();
   }
 
-  clickProject(projectName: string): Promise<void> {
+  async clickProject(projectName: string): Promise<void> {
+    await this.projectFilterBarInput.fill(projectName); // make sure project is visible
     const table = this.page.locator('table').nth(0);
     return table.getByRole('link', {name: projectName, exact: true}).click();
   }
