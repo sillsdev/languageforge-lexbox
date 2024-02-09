6ba9f615dc908c5421fcf1d0468f442e8ac57c06
Robin Munn <rmunn@pobox.com>
2024-02-09 15:08:58 +0700

    Greatly improve reset-project Playwright test
    Greatly expanded the reset-project Playwright test:
    
    - It uses a temp project now, whose name is based on a hash of the test
      title and browser settings, so Chromium and Firefox will use different
      project names. This will allow the test to run in parallel with itself.
    - It uploads a known initial state of a Mercurial repo, which is a tiny
      zip file containing just one commit.
    - It checks that the project-reset modal dialog can reset the project to
      an empty state or upload from a zip file correctly.
    
    Combined with the .NET test that confirms that a project can still be
    used with Send/Receive after resetting to an empty state, that should
    cover just about everything.

--- frontend/tests/resetProject.test.ts
+++ frontend/tests/resetProject.test.ts
@@ -7,29 +7,61 @@ import { ProjectPage } from './pages/projectPage';
 import { ResetProjectModal } from './components/resetProjectModal';
 
 test('reset project and upload .zip file', async ({ page, tempProject }) => {
-  console.log(tempProject.code);
-  expect(1).toBe(2); // Fail on purpose to check project is automatically deleted by fixture
+  // Step 1: Populate project with known initial state
   await loginAs(page.request, 'admin', testEnv.defaultPassword);
-  await page.goto('http://localhost/hg/sena-3/file/tip');
-  const fileCountBeforeReset = await page.locator('tr.fileline').count()
-  const h3BeforeReset = await page.locator('.main h3').innerText();
   const adminDashboardPage = await new AdminDashboardPage(page).goto();
-  await adminDashboardPage.clickProject('Sena 3');
-  const projectPage = await new ProjectPage(page, 'Sena 3', 'sena-3').waitFor();
+  await adminDashboardPage.clickProject(tempProject.name);
+  const projectPage = await new ProjectPage(page, tempProject.name, tempProject.code).waitFor();
   await projectPage.clickResetProject();
   const resetProjectModel = await new ResetProjectModal(page).waitFor();
-  const download = await resetProjectModel.downloadProjectBackup();
-  await download.saveAs('sena-3.zip'); // TODO: Create fixture to use temporary directory and tear it down after test, as of right now this pollutes current working directory
   await resetProjectModel.clickNextStepButton('I have a working backup');
-  await resetProjectModel.confirmProjectBackupReceived('sena-3');
+  await resetProjectModel.confirmProjectBackupReceived(tempProject.code);
   await resetProjectModel.clickNextStepButton('Reset project');
-  await resetProjectModel.uploadProjectZipFile('sena-3.zip');
-  // Should go to next step automatically if successful, no clickNextStepButton needed this time
+  await resetProjectModel.uploadProjectZipFile('tests/data/test-project-one-commit.zip');
   await expect(page.getByText('Project successfully reset')).toBeVisible();
   await page.getByRole('button', { name: 'Close' }).click();
   await resetProjectModel.assertGone();
-  await page.goto('http://localhost/hg/sena-3/file/tip');
-  const fileCountAfterReset = await page.locator('tr.fileline').count()
-  expect(fileCountAfterReset).toBe(fileCountBeforeReset);
+
+  // Step 2: Get tip hash and file list from hgweb, check some known values
+  await page.goto(`http://localhost/hg/${tempProject.code}/file/tip`);
+  await expect(page.locator('tr.fileline')).toHaveCount(1);
+  await expect(page.locator('tr.fileline').first()).toHaveText(/hello\.txt/);
+  const h3BeforeReset = await page.locator('.main h3').innerText();
+
+  // Step 3: reset project, do not upload zip file
+  await projectPage.goto();
+  await projectPage.clickResetProject();
+  await resetProjectModel.waitFor();
+  const download = await resetProjectModel.downloadProjectBackup();
+  // TODO: Create fixture to use temporary directory and tear it down after test, as of right now this pollutes current working directory
+  await download.saveAs('tests/data/reset-project-test-step-1.zip');
+  await resetProjectModel.clickNextStepButton('I have a working backup');
+  await resetProjectModel.confirmProjectBackupReceived(tempProject.code);
+  await resetProjectModel.clickNextStepButton('Reset project');
+  await resetProjectModel.clickNextStepButton('Leave project empty');
+  await page.getByRole('button', { name: 'Close' }).click();
+  await resetProjectModel.assertGone();
+
+  // Step 4: confirm it's empty now
+  await page.goto(`http://localhost/hg/${tempProject.code}/file/tip`);
+  await expect(page.locator('tr.fileline')).toHaveCount(0);
+  await expect(page.locator('.main h3')).not.toHaveText(h3BeforeReset);
+
+  // Step 5: reset project again, uploading zip file downloaded from step 1
+  await projectPage.goto();
+  await projectPage.clickResetProject();
+  await resetProjectModel.waitFor();
+  await resetProjectModel.clickNextStepButton('I have a working backup');
+  await resetProjectModel.confirmProjectBackupReceived(tempProject.code);
+  await resetProjectModel.clickNextStepButton('Reset project');
+  await resetProjectModel.uploadProjectZipFile('tests/data/reset-project-test-step-1.zip');
+  await expect(page.getByText('Project successfully reset')).toBeVisible();
+  await page.getByRole('button', { name: 'Close' }).click();
+  await resetProjectModel.assertGone();
+
+  // Step 6: confirm hash in <h3> element is same as before reset
+  await page.goto(`http://localhost/hg/${tempProject.code}/file/tip`);
+  await expect(page.locator('tr.fileline')).toHaveCount(1);
+  await expect(page.locator('tr.fileline').first()).toHaveText(/hello\.txt/);
   await expect(page.locator('.main h3')).toHaveText(h3BeforeReset);
 });
