5e1446e21eefe4c5c8f116915ff1f94ad4ad9694
Tim Haasdyk <tim_haasdyk@sil.org>
2024-02-20 16:05:57 +0100

    Add SSR GQL-500 to sandbox

--- frontend/src/routes/(unauthenticated)/sandbox/+page.ts
+++ frontend/src/routes/(unauthenticated)/sandbox/+page.ts
@@ -1,11 +1,20 @@
 ﻿import {getClient, graphql} from '$lib/gql';
-import type {SandboxQueryMyselfQuery} from '$lib/gql/generated/graphql';
-import type {Readable} from 'svelte/store';
 
-export async function _gqlThrows500(): Promise<Readable<SandboxQueryMyselfQuery['testingThrowsError']>> {
+import type { PageLoadEvent } from './$types';
+import type {Readable} from 'svelte/store';
+import type {SandboxQueryMyselfQuery} from '$lib/gql/generated/graphql';
+import { browser } from '$app/environment';
+
+export async function load(event: PageLoadEvent) {
+  if (!browser && event.url.searchParams.has('gql-500')) {
+    await _gqlThrows500(event.fetch);
+  }
+}
+
+export async function _gqlThrows500(argFetch?: Fetch): Promise<Readable<SandboxQueryMyselfQuery['testingThrowsError']>> {
   const client = getClient();
   //language=GraphQL
-  const results = await client.awaitedQueryStore(fetch, graphql(`
+  const results = await client.awaitedQueryStore(argFetch ?? fetch, graphql(`
     query sandboxQueryMyself {
       testingThrowsError {
         id
