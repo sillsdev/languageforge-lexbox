057a27ae861699a70ace3576bd38b6cd8ca1f8a8
Robin Munn <rmunn@pobox.com>
2024-02-21 16:03:27 +0700

    First attempt at a show-password icon
    CSS is wrong, and button isn't hooked up to anything yet

--- frontend/src/lib/forms/Input.svelte
+++ frontend/src/lib/forms/Input.svelte
@@ -2,6 +2,7 @@
   import { randomFormId } from './utils';
   import PlainInput from './PlainInput.svelte';
   import FormField from './FormField.svelte';
+  import Icon from '$lib/icons/Icon.svelte';
 
   export let id = randomFormId();
   export let label: string;
@@ -15,8 +16,29 @@
   // Despite the compatibility table, 'new-password' seems to work well in Chrome, Edge & Firefox
   // https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/autocomplete#browser_compatibility
   export let autocomplete: 'new-password' | 'current-password' | undefined = undefined;
+
+  let currentType = type;
+  if (type == 'password') console.log('Password field displaying');
+
 </script>
 
 <FormField {id} {error} {label} {autofocus} {description}>
-  <PlainInput {id} bind:value {type} {autofocus} {readonly} {error} {placeholder} {autocomplete} />
+  {#if (type == 'password')}
+  <div class="container">
+    <PlainInput {id} bind:value type={currentType} {autofocus} {readonly} {error} {placeholder} {autocomplete} />
+    <span class="eye"><Icon icon="i-mdi-eye" size="text-md"></Icon></span>
+  </div>
+  {:else}
+    <PlainInput {id} bind:value {type} {autofocus} {readonly} {error} {placeholder} {autocomplete} />
+  {/if}
 </FormField>
+
+<style>
+  .eye {
+    position: absolute;
+    right: 1px;
+  }
+  .container {
+    position: relative;
+  }
+</style>
