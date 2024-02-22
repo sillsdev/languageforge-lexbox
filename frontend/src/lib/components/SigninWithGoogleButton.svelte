da09bfa96cf561a1d7653794dd9cff99666e1024
Robin Munn <rmunn@pobox.com>
2024-02-22 09:35:32 +0700

    Sign in with Google button component, first draft
    This was copied directly from the HTML that Google first injects into
    your page, meaningless CSS class names and all. I copied it exactly
    as-is so that we could render their logo just as they want it rendered.
    I'll tweak it to give the CSS classes more meaningful names, and to
    remove any redundancies, but I'll leave it mostly unchanged apart from
    renaming CSS classes.

---
+++ frontend/src/lib/components/SigninWithGoogleButton.svelte
@@ -0,0 +1,173 @@
+<script>
+</script>
+
+<div class="g_id_signin s-JDfLTrt3Oc2b" data-type="standard" data-shape="rectangular" data-theme="filled_blue" data-text="signin_with" data-size="large" data-logo_alignment="left" data-svelte-h="svelte-1b6glw7">
+  <div class="S9gUrf-YoZ4jf" style="position: relative;">
+  <div>
+  <div tabindex="0" role="button" aria-labelledby="button-label" class="nsm7Bb-HzV7m-LgbsSe  hJDwNd-SxQuSe MFS4be-v3pZbf-Ia7Qfc MFS4be-Ia7Qfc uaxL4e-RbRzK">
+  <div class="nsm7Bb-HzV7m-LgbsSe-MJoBVe">
+
+</div>
+<div class="nsm7Bb-HzV7m-LgbsSe-bN97Pc-sM5MNb ">
+  <div class="nsm7Bb-HzV7m-LgbsSe-Bz112c-haAclf">
+  <div class="nsm7Bb-HzV7m-LgbsSe-Bz112c">
+  <!-- Official Google logo, do not modify -->
+  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
+  <g>
+  <path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z" />
+  <path fill="#4285F4" d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z" />
+  <path fill="#FBBC05" d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z" />
+  <path fill="#34A853" d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z" />
+  <path fill="none" d="M0 0h48v48H0z" />
+  </g>
+  </svg>
+</div>
+</div>
+<span class="nsm7Bb-HzV7m-LgbsSe-BPrWId">
+  Sign in with Google</span>
+<span class="L6cTce" id="button-label">
+  Sign in with Google</span>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<style>
+  .S9gUrf-YoZ4jf, .S9gUrf-YoZ4jf * {
+    border: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  .nsm7Bb-HzV7m-LgbsSe .nsm7Bb-HzV7m-LgbsSe-Bz112c-haAclf .nsm7Bb-HzV7m-LgbsSe-Bz112c, .nsm7Bb-HzV7m-LgbsSe.Bz112c-LgbsSe .nsm7Bb-HzV7m-LgbsSe-Bz112c-haAclf .nsm7Bb-HzV7m-LgbsSe-Bz112c {
+    margin: 0;
+    padding: 0;
+  }
+
+  .nsm7Bb-HzV7m-LgbsSe.MFS4be-v3pZbf-Ia7Qfc {
+    background-color: #1a73e8;
+  }
+
+  .nsm7Bb-HzV7m-LgbsSe.MFS4be-Ia7Qfc {
+    border: none;
+    color: #fff;
+  }
+
+  .nsm7Bb-HzV7m-LgbsSe.MFS4be-Ia7Qfc:hover .nsm7Bb-HzV7m-LgbsSe-MJoBVe,
+  .nsm7Bb-HzV7m-LgbsSe.MFS4be-Ia7Qfc:focus .nsm7Bb-HzV7m-LgbsSe-MJoBVe {
+    background: rgba(255,255,255,.24);
+  }
+
+  .nsm7Bb-HzV7m-LgbsSe.MFS4be-Ia7Qfc:active .nsm7Bb-HzV7m-LgbsSe-MJoBVe {
+    background: rgba(255,255,255,.32);
+  }
+
+  .nsm7Bb-HzV7m-LgbsSe:hover,
+  .nsm7Bb-HzV7m-LgbsSe:focus {
+    box-shadow: none;
+    border-color: #d2e3fc;
+    outline: none;
+  }
+  .nsm7Bb-HzV7m-LgbsSe:hover .nsm7Bb-HzV7m-LgbsSe-MJoBVe,
+  .nsm7Bb-HzV7m-LgbsSe:focus .nsm7Bb-HzV7m-LgbsSe-MJoBVe {
+    background: rgba(66,133,244,.04);
+  }
+  .nsm7Bb-HzV7m-LgbsSe:active .nsm7Bb-HzV7m-LgbsSe-MJoBVe {
+    background: rgba(66,133,244,.1);
+  }
+
+  .nsm7Bb-HzV7m-LgbsSe {
+    border-radius: 4px;
+    box-sizing: border-box;
+    transition: background-color .218s,border-color .218s;
+    -moz-user-select: none;
+    user-select: none;
+    background-color: #fff;
+    background-image: none;
+    border: 1px solid #dadce0;
+    color: #3c4043;
+    cursor: pointer;
+    font-family: "Google Sans",arial,sans-serif;
+    font-size: 14px;
+    height: 40px;
+    letter-spacing: 0.25px;
+    outline: none;
+    overflow: hidden;
+    padding: 0 12px;
+    position: relative;
+    text-align: center;
+    vertical-align: middle;
+    white-space: nowrap;
+    width: auto;
+  }
+
+  @media screen and (-ms-high-contrast:active) {
+    .nsm7Bb-HzV7m-LgbsSe {
+      border: 2px solid windowText;
+      color: windowText;
+    }
+  }
+
+  .nsm7Bb-HzV7m-LgbsSe .nsm7Bb-HzV7m-LgbsSe-MJoBVe {
+    transition: background-color .218s;
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+  }
+
+  .nsm7Bb-HzV7m-LgbsSe .nsm7Bb-HzV7m-LgbsSe-bN97Pc-sM5MNb {
+    display: -webkit-box;
+    display: -moz-box;
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    display: flex;
+    position: relative;
+    align-items: center;
+    flex-direction: row;
+    justify-content: space-between;
+    flex-wrap: nowrap;
+    height: 100%;
+    width: 100%;
+  }
+
+  .nsm7Bb-HzV7m-LgbsSe .nsm7Bb-HzV7m-LgbsSe-Bz112c-haAclf {
+    border-top-left-radius: 3px;
+    border-bottom-left-radius: 3px;
+    display: -webkit-box;
+    display: -moz-box;
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #fff;
+    height: 36px;
+    margin-left: -10px;
+    margin-right: 12px;
+    min-width: 36px;
+    width: 36px;
+  }
+
+  .nsm7Bb-HzV7m-LgbsSe .nsm7Bb-HzV7m-LgbsSe-Bz112c {
+    height: 18px;
+    margin-right: 8px;
+    min-width: 18px;
+    width: 18px;
+  }
+
+  .nsm7Bb-HzV7m-LgbsSe .nsm7Bb-HzV7m-LgbsSe-BPrWId {
+    flex-grow: 1;
+    font-family: "Google Sans",arial,sans-serif;
+    font-weight: 500;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    vertical-align: top;
+  }
+
+  .L6cTce {
+    display: none;
+  }
+</style>
