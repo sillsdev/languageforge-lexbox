154689975cb01516ab1fc8af09407e51a3667aee
Tim Haasdyk <tim_haasdyk@sil.org>
2024-03-04 14:26:49 +0100

    Remove redmine migration scripts folder

--- redmineMigration/compareFolders/compareFolders.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ssh languagedepot ls -1 /var/vcs/public | sort > public.folders
-ssh languagedepot ls -1 /var/vcs/private | sort > private.folders
-cat public.folders private.folders | sort > all.folders.sorted
-cat all.folders.sorted | uniq > all.folders.sorted.uniq
-diff all.folders.sorted all.folders.sorted.uniq | grep "^<" | sed "s/^< //" | tee folders.inboth
-echo There are $(wc folders.inboth|awk '{print $1}') folders that occur in both public and private
--- redmineMigration/loadSql/loadSql.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-docker stop mariadb-server || true
-docker rm mariadb-server || true
-docker run --name mariadb-server --detach -e MARIADB_ALLOW_EMPTY_ROOT_PASSWORD=yes -dp 3306:3306 mariadb:latest
-sleep 10
-docker exec -i mariadb-server mariadb -w -uroot < dump.sql
