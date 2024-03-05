77752272b1d38d89b92887e7ce37e6f63bebeb26
Robin Munn <rmunn@pobox.com>
2024-03-05 16:27:54 +0700

    And also delete the now-unused input records

--- backend/LexBoxApi/Models/Project/ChangeProjectInputs.cs
+++ backend/LexBoxApi/Models/Project/ChangeProjectInputs.cs
@@ -4,10 +4,6 @@ public record ChangeProjectNameInput(Guid ProjectId, string Name);
 
 public record ChangeProjectDescriptionInput(Guid ProjectId, string Description);
 
-public record ChangeDraftProjectNameInput(Guid ProjectId, string Name);
-
-public record ChangeDraftProjectDescriptionInput(Guid ProjectId, string Description);
-
 public record DeleteUserByAdminOrSelfInput(Guid UserId);
 
 public record ResetProjectByAdminInput(string Code);
