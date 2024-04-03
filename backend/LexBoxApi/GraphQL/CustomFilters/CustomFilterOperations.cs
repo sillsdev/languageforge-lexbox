6ce44a34df886002e8a7864c0eace38d5a211cc9
Robin Munn <rmunn@pobox.com>
2024-04-03 14:14:17 +0700

    Add custom ieq operation, like eq but case-insensitive

--- backend/LexBoxApi/GraphQL/CustomFilters/CustomFilterOperations.cs
+++ backend/LexBoxApi/GraphQL/CustomFilters/CustomFilterOperations.cs
@@ -8,6 +8,7 @@ public static class CustomFilterOperations
     // Our custom operation IDs should be above 1024:
     // https://chillicream.com/docs/hotchocolate/v13/api-reference/extending-filtering#operation
     public const int IContains = 1025;
+    public const int IEq = 1026;
 
     public static IFilterConventionDescriptor AddDeterministicInvariantContainsFilter(this IFilterConventionDescriptor descriptor)
     {
@@ -17,6 +18,14 @@ public static class CustomFilterOperations
             x => x.Operation(IContains).Type<StringType>());
         descriptor.AddProviderExtension(new QueryableFilterProviderExtension(y => y
                     .AddFieldHandler<QueryableStringDeterministicInvariantContainsHandler>()));
+
+        descriptor.Operation(IEq)
+            .Name("ieq");
+        descriptor.Configure<StringOperationFilterInputType>(
+            x => x.Operation(IEq).Type<StringType>());
+        descriptor.AddProviderExtension(new QueryableFilterProviderExtension(y => y
+                    .AddFieldHandler<QueryableStringDeterministicInvariantEqualsHandler>()));
+
         return descriptor;
     }
 }
