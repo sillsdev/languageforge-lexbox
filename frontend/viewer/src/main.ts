070e3e3db1223ea4b096c7bf1fe619070502a246
Tim Haasdyk <tim_haasdyk@sil.org>
2024-04-05 14:54:23 +0200

    Revert to standalone app using normal Svelte component by default This has much better DX

--- frontend/viewer/src/main.ts
+++ frontend/viewer/src/main.ts
@@ -1,8 +1,21 @@
-// import App from './App.svelte'
+//*// v1 Run with normal Svelte App (advantages: tailwind classes stay up to date)
+
+import './app.postcss';
+
+import App from './App.svelte'
+
+new App({
+  target: document.getElementById('app')!,
+});
+
+/*/// v2 Run with web-component in shadow dom
+
+import './web-component';
+
+//*/
+
 // import { HubConnectionBuilder } from '@microsoft/signalr';
 // import { SetupSignalR } from './lib/services/service-provider-signalr';
-
-
 
 // const connection = new HubConnectionBuilder()
 //     .withUrl("/api/hub/project")
@@ -10,11 +23,3 @@
 //     .build();
 // await connection.start();
 // SetupSignalR(connection);
-
-// const app = new WebComponent({
-//   target: document.getElementById('app')!,
-// });
-
-// export default app
-
-import './web-component';
--- frontend/viewer/tailwind.config.cjs
+++ frontend/viewer/tailwind.config.cjs
@@ -16,7 +16,7 @@ module.exports = {
     svelteUx({ colorSpace: 'oklch' }),
   ],
   ux: {
-    themeRoot: ':host',
+    themeRoot: ':root, :host',
     themes: {
       "light": {
         "color-scheme": "light",
