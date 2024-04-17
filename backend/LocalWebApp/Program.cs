55c8783db1ccbb0615d5215098d9dbca7414a57b
Kevin Hahn <kevin_hahn@sil.org>
2024-04-17 16:41:41 -0700

    setup LocalWebApp.csproj to run itself

--- backend/LocalWebApp/Program.cs
+++ backend/LocalWebApp/Program.cs
@@ -1,5 +1,9 @@
-var builder = WebApplication.CreateBuilder(args);
+using System.Diagnostics;
+using Microsoft.AspNetCore.StaticFiles.Infrastructure;
+using Microsoft.Extensions.FileProviders;
 
+var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseUrls("http://[::1]:0");
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -14,7 +18,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+var sharedOptions = new SharedOptions() { FileProvider = new ManifestEmbeddedFileProvider(typeof(Program).Assembly) };
+app.UseDefaultFiles(new DefaultFilesOptions(sharedOptions));
+app.UseStaticFiles(new StaticFileOptions(sharedOptions));
 
-
-app.Run();
+await using (app)
+{
+    await app.StartAsync();
+    Process.Start(new ProcessStartInfo(app.Urls.First()) { UseShellExecute = true });
+    await app.WaitForShutdownAsync();
+}
