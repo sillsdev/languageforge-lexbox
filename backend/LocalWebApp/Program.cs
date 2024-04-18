fccaf28f116be7051d1d933bc8436fde4a83d1c9
Kevin Hahn <kevin_hahn@sil.org>
2024-04-17 17:04:52 -0700

    auto launch browser on startup. Enabled published trimmed to reduce size

--- backend/LocalWebApp/Program.cs
+++ backend/LocalWebApp/Program.cs
@@ -1,9 +1,10 @@
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 using Microsoft.AspNetCore.StaticFiles.Infrastructure;
 using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.WebHost.UseUrls("http://[::1]:0");
+builder.WebHost.UseUrls("http://127.0.0.1:0");
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -25,6 +26,20 @@ app.UseStaticFiles(new StaticFileOptions(sharedOptions));
 await using (app)
 {
     await app.StartAsync();
-    Process.Start(new ProcessStartInfo(app.Urls.First()) { UseShellExecute = true });
+
+    var url = app.Urls.First();
+    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+    {
+        Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+    }
+    else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+    {
+        Process.Start(new ProcessStartInfo("xdg-open", url) { UseShellExecute = true });
+    }
+    else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+    {
+        Process.Start("open", url);
+    }
+
     await app.WaitForShutdownAsync();
 }
