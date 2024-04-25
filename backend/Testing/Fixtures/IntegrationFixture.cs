0434a8e47663ad469b74b2917451000d872a037e
Robin Munn <rmunn@pobox.com>
2024-04-25 12:09:53 +0700

    Keep test repo zip in Git instead of downloading it
    This saves seeral seconds of test setup time.

--- backend/Testing/Fixtures/IntegrationFixture.cs
+++ backend/Testing/Fixtures/IntegrationFixture.cs
@@ -1,11 +1,9 @@
 using System.IO.Compression;
 using System.Net;
+using System.Reflection;
 using System.Runtime.CompilerServices;
-using Chorus.VcsDrivers.Mercurial;
 using LexCore.Utils;
 using Shouldly;
-using SIL.Linq;
-using SIL.Progress;
 using Testing.ApiTests;
 using Testing.Services;
 using TusDotNetClient;
@@ -15,6 +13,7 @@ namespace Testing.Fixtures;
 
 public class IntegrationFixture : IAsyncLifetime
 {
+    private readonly string _templateRepoName = "test-template-repo.zip";
     private readonly FileInfo _templateRepoZip = new(Path.Join(BasePath, "_template-repo_.zip"));
     private readonly DirectoryInfo _templateRepo = new(Path.Join(BasePath, "_template-repo_"));
     public ApiTestBase AdminApiTester { get; } = new();
@@ -24,7 +23,7 @@ public class IntegrationFixture : IAsyncLifetime
     {
         DeletePreviousTestFiles();
         Directory.CreateDirectory(BasePath);
-        await DownloadTemplateRepo();
+        InitTemplateRepo();
         AdminJwt = await AdminApiTester.LoginAs(AdminAuth.Username, AdminAuth.Password);
     }
 
@@ -38,13 +37,13 @@ public class IntegrationFixture : IAsyncLifetime
         if (Directory.Exists(BasePath)) Directory.Delete(BasePath, true);
     }
 
-    private async Task DownloadTemplateRepo()
+    private void InitTemplateRepo()
     {
-        await using var stream = await AdminApiTester.HttpClient.GetStreamAsync("https://drive.google.com/uc?export=download&id=1w357T1Ti7bDwEof4HPBUZ5gB7WSKA5O2");
-        using var zip = new ZipArchive(stream);
-        zip.ExtractToDirectory(_templateRepo.FullName);
-        InitRepoInDirectory(_templateRepo.FullName);
-        ZipFile.CreateFromDirectory(_templateRepo.FullName, _templateRepoZip.FullName);
+        var assembly = Assembly.GetExecutingAssembly();
+        var templatePath = Path.Join(Path.GetDirectoryName(assembly.Location), _templateRepoName);
+        File.Copy(templatePath, _templateRepoZip.FullName);
+        using var stream = _templateRepoZip.OpenRead();
+        ZipFile.ExtractToDirectory(stream, _templateRepo.FullName);
     }
 
     public ProjectConfig InitLocalFlexProjectWithRepo(HgProtocol? protocol = null, [CallerMemberName] string projectName = "")
@@ -74,14 +73,5 @@ public class IntegrationFixture : IAsyncLifetime
         var fileUrl = await client.CreateAsync($"{AdminApiTester.BaseUrl}/api/project/upload-zip/{projectCode}", repo.Length, [("filetype", "application/zip")]);
         var responses = await client.UploadAsync(fileUrl, repo, chunkSize: 20);
         responses.ShouldAllBe(r => r.StatusCode.ToString() == nameof(HttpStatusCode.NoContent));
-    }
-
-    private static void InitRepoInDirectory(string projectDir)
-    {
-        var progress = new NullProgress();
-        HgRunner.Run("hg init", projectDir, 5, progress);
-        HgRunner.Run("hg branch 7500002.7000072", projectDir, 5, progress);
-        HgRunner.Run($"hg add Lexicon.fwstub", projectDir, 5, progress);
-        HgRunner.Run("""hg commit -m "first commit" """, projectDir, 5, progress);
     }
 }
