a3e2e5dd49f93d7c4c0c52adf11b069944bfc683
Robin Munn <rmunn@pobox.com>
2024-05-13 13:05:05 +0700

    Set up Create User modal help for translation
    Implements the TODO comment I had added earlier.

--- frontend/src/lib/components/Users/CreateUserModal.svelte
+++ frontend/src/lib/components/Users/CreateUserModal.svelte
@@ -1,8 +1,10 @@
 ﻿<script lang="ts">
   import { Modal } from '$lib/components/modals';
-  // import t from '$lib/i18n'; // TODO
+  import t from '$lib/i18n';
   import { helpLinks } from '$lib/components/help';
   import CreateUser from '$lib/components/Users/CreateUser.svelte';
+  import Markdown from 'svelte-exmarkdown';
+  import { NewTabLinkRenderer } from '$lib/components/Markdown';
 
   let createUserModal: Modal;
 
@@ -12,13 +14,7 @@
 </script>
 
 <Modal bind:this={createUserModal} bottom>
-  <!-- TODO: Move to en.json and use $t -->
-  <p>
-    Did you know you can also <a class="link" rel="external" href={helpLinks.addProjectMember}>create users on a project page</a>? That also allows you to make those users members of the project in one step;
-    users created here will not be members of any project and will need a second step to add them to a project.
-  </p>
-  <p>
-    If you want to create multiple users at once, the best way is probably to use the <a class="link" rel="external" href={helpLinks.bulkAddCreate}>Bulk Add/Create Project Members</a> tool.
-  </p>
+  <Markdown md={$t('admin_dashboard.create_user_modal.help_create_single_guest_user', helpLinks)} plugins={[{ renderer: { a: NewTabLinkRenderer } }]} />
+  <Markdown md={$t('admin_dashboard.create_user_modal.help_create_bulk_guest_users', helpLinks)} plugins={[{ renderer: { a: NewTabLinkRenderer } }]} />
   <CreateUser autoLogin={false} onSubmit={() => createUserModal.submitModal()} />
 </Modal>
