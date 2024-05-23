62c170a4bcac9d955e6161284af2b3cb2a0f8fa1
Kevin Hahn <kevin_hahn@sil.org>
2024-05-23 10:43:57 -0600

    add health check for hgweb to match api version (#679)
    * use standard otel environment variables to configure service.version and if it's enabled or not
    * add a health check in dotnet to require hgweb be the same version
    * validate status code during deploy check
    * allow versions of hg container and api to not match in develop environment

---
+++ backend/LexBoxApi/Services/HgWebHealthCheck.cs
@@ -0,0 +1,25 @@
+﻿using LexCore.Config;
+using LexCore.ServiceInterfaces;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Options;
+
+namespace LexBoxApi.Services;
+
+public class HgWebHealthCheck(IHgService hgService, IOptions<HgConfig> hgOptions) : IHealthCheck
+{
+    public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context,
+        CancellationToken cancellationToken = new())
+    {
+        var version = await hgService.HgCommandHealth();
+        if (string.IsNullOrEmpty(version))
+        {
+            return HealthCheckResult.Unhealthy();
+        }
+        if (hgOptions.Value.RequireContainerVersionMatch && version != AppVersionService.Version)
+        {
+            return HealthCheckResult.Degraded(
+                $"api version: '{AppVersionService.Version}' hg version: '{version}' mismatch");
+        }
+        return HealthCheckResult.Healthy();
+    }
+}
