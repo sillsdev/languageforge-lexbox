b83c78b3d2a4da8f00081c8e871c301795107cd9
Robin Munn <rmunn@pobox.com>
2024-05-27 13:57:22 +0700

    Use Mongo auth for Lf Classic connections (#817)
    The username and password supplied in the k8s secrets are for an account
    with read-only access to any non-system database.

--- backend/LfClassicData/LfClassicConfig.cs
+++ backend/LfClassicData/LfClassicConfig.cs
@@ -6,4 +6,9 @@ public class LfClassicConfig
 {
     [Required]
     public required string ConnectionString { get; set; }
+
+    public string? AuthSource { get; set; }
+    public string? Username { get; set; }
+    public string? Password { get; set; }
+    public bool HasCredentials => AuthSource is not null && Username is not null && Password is not null;
 }
