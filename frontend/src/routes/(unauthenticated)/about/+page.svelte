45accf7eff0bc4a74e1d97499afd7b29ee36cda9
SeongHo Park <nws0078@gmail.com>
2024-06-05 15:11:59 +0700

    created about page

---
+++ frontend/src/routes/(unauthenticated)/about/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { HeaderPage, HomeBreadcrumb } from '$lib/layout';
+  import t from '$lib/i18n';
+</script>
+
+<HomeBreadcrumb />
+<HeaderPage wide title={$t('about.title')}>
+</HeaderPage>
