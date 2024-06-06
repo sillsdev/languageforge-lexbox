5d820e102dccb5cb23af3889e7e6666a7fce0b0b
Robin Munn <rmunn@pobox.com>
2024-06-06 08:29:17 +0700

    Remove label from OrgRoleSelect
    Looks better in tables this way

--- frontend/src/lib/forms/OrgRoleSelect.svelte
+++ frontend/src/lib/forms/OrgRoleSelect.svelte
@@ -1,19 +1,17 @@
 <script lang="ts">
   import { OrgRole } from '$lib/gql/types';
   import t from '$lib/i18n';
-  import Select from './Select.svelte';
 
   export let id = 'role';
   export let value: OrgRole;
   export let verbose = false;
-  export let error: string | string[] | undefined = undefined;
 </script>
 
-<Select {id} bind:value label={$t('org_role.label')} {error} on:change>
+<select {id} bind:value class="select select-bordered" on:change>
   <option value={OrgRole.User}>
     {$t(verbose ? 'org_role.user_description' : 'org_role.user')}
   </option>
   <option value={OrgRole.Admin}>
     {$t(verbose ? 'org_role.admin_description' : 'org_role.admin')}
   </option>
-</Select>
+</select>
