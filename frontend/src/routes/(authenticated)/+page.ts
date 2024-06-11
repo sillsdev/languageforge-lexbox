712ac6fdc15c4ba452e2e68457ec0d70f5fe4af2
SeongHo Park <seongho_park@sil.org>
2024-06-11 09:43:28 +0700

    modified to query all the data in one request

--- frontend/src/routes/(authenticated)/+page.ts
+++ frontend/src/routes/(authenticated)/+page.ts
@@ -6,7 +6,7 @@ import {getViewMode} from './shared';
 export async function load(event: PageLoadEvent) {
   const client = getClient();
   //language=GraphQL
-  const myProjectPromise = await client.awaitedQueryStore(event.fetch, graphql(`
+  const results = await client.awaitedQueryStore(event.fetch, graphql(`
         query loadProjects {
             myProjects(orderBy: [
                 {code: ASC }
@@ -19,11 +19,7 @@ export async function load(event: PageLoadEvent) {
                 type
                 isConfidential
             }
-        }
-  `), {});
 
-  const myDraftProjectPromise = await client.awaitedQueryStore(event.fetch, graphql(`
-        query loadDraftProjects {
             myDraftProjects(orderBy: [
                 {code: ASC }
             ]) {
@@ -40,13 +36,11 @@ export async function load(event: PageLoadEvent) {
         }
   `), {});
 
-  const [myProjectResults, myDraftProjectResults] = await Promise.all([myProjectPromise, myDraftProjectPromise]);
-
   const projectViewMode = getViewMode(event);
 
   return {
-    projects: myProjectResults.myProjects,
-    draftProjects: myDraftProjectResults.myDraftProjects,
+    projects: results.myProjects,
+    draftProjects: results.myDraftProjects,
     projectViewMode,
   }
 }
