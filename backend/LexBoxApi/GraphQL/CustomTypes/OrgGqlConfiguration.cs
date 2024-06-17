1a934c0c226d237dd0c9c194282fe05e81e58368
Robin Munn <rmunn@pobox.com>
2024-06-17 12:34:50 +0700

    Only admins and org members can see members list

--- backend/LexBoxApi/GraphQL/CustomTypes/OrgGqlConfiguration.cs
+++ backend/LexBoxApi/GraphQL/CustomTypes/OrgGqlConfiguration.cs
@@ -8,5 +8,20 @@ public class OrgGqlConfiguration : ObjectType<Organization>
     protected override void Configure(IObjectTypeDescriptor<Organization> descriptor)
     {
         descriptor.Field(o => o.CreatedDate).IsProjected();
+        descriptor.Field(o => o.Members).Use(next => async context =>
+        {
+            await next(context);
+            var result = context.Result;
+            if (result is List<OrgMember> members)
+            {
+                // We want to throw if not logged in, so use User rather than MaybeUser below
+                var user = context.Service<LexBoxApi.Auth.LoggedInContext>().User;
+                if (user.IsAdmin || members.Any(om => om.UserId == user.Id))
+                {
+                    return;
+                }
+            }
+            throw new UnauthorizedAccessException("Must be org member");
+        });
     }
 }
