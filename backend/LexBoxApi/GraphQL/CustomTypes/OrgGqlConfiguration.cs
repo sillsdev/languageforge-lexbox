5c81fedea77ab2c45e5359793d9c829a4ca88e33
Robin Munn <rmunn@pobox.com>
2024-06-18 10:58:02 +0700

    Adjust o.Projects GraphQL logic
    This allows org admins to see all projects, but org members just get to
    see public projects the same way that non-members do (we might adjust
    that in the future).

--- backend/LexBoxApi/GraphQL/CustomTypes/OrgGqlConfiguration.cs
+++ backend/LexBoxApi/GraphQL/CustomTypes/OrgGqlConfiguration.cs
@@ -8,6 +8,7 @@ public class OrgGqlConfiguration : ObjectType<Organization>
     protected override void Configure(IObjectTypeDescriptor<Organization> descriptor)
     {
         descriptor.Field(o => o.CreatedDate).IsProjected();
+        descriptor.Field(o => o.Id).IsProjected(); // Needed for logic below
         descriptor.Field(o => o.Id).Use<RefreshJwtOrgMembershipMiddleware>();
         // Must be listed *before* RefreshJwtOrgMembershipMiddleware
         descriptor.Field(o => o.Members).Use(next => async context =>
@@ -36,8 +37,26 @@ public class OrgGqlConfiguration : ObjectType<Organization>
         //     await next(context);
         //     if (context.Result is List<Project> projects)
         //     {
+        //         var org = context.Parent<Organization>();
+        //         var orgId = org?.Id ?? default;
+        //         if (orgId == default)
+        //         {
+        //             throw new LexCore.Exceptions.NotFoundException("Org ID not found in GraphQL - shouldn't happen", nameof(Organization));
+        //         }
         //         var user = context.Service<LexBoxApi.Auth.LoggedInContext>().MaybeUser;
-        //         context.Result = projects.Where(p => p.IsConfidential == false || (user is not null && user.Projects.Any(up => up.ProjectId == p.Id)));
+        //         if (user is null)
+        //         {
+        //             // Anonymous sessions can only see public projects
+        //             context.Result = projects.Where(p => p.IsConfidential == false);
+        //             return;
+        //         }
+        //         if (user.Orgs.Any(o => o.OrgId == orgId && o.Role == OrgRole.Admin))
+        //         {
+        //             // Org admins can see all projects
+        //             return;
+        //         }
+        //         // Anyone else can only see public projects; org membership makes no difference
+        //         context.Result = projects.Where(p => p.IsConfidential == false || user.Projects.Any(up => up.ProjectId == p.Id)).ToList();
         //     }
         // });
     }
