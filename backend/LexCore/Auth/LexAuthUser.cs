96889fea1e7bbf58d9d7898256cb7d0df99a21f6
Robin Munn <rmunn@pobox.com>
2024-06-18 08:47:30 +0700

    No need to handle orgs claims specially
    We handle project claims specially because some users can be in many,
    many projects, and so we need to squeeze every byte that we can. But for
    orgs, most users will be in just one, and those who are in more than one
    are usually going to be in just two or three. So we can just do a simple
    array and let the standard JSON serialization handle it.

--- backend/LexCore/Auth/LexAuthUser.cs
+++ backend/LexCore/Auth/LexAuthUser.cs
@@ -131,7 +131,7 @@ public record LexAuthUser
     [JsonIgnore]
     public AuthUserProject[] Projects { get; set; } = Array.Empty<AuthUserProject>();
 
-    [JsonIgnore]
+    [JsonPropertyName(LexAuthConstants.OrgsClaimType)]
     public AuthUserOrg[] Orgs { get; set; } = Array.Empty<AuthUserOrg>();
 
     [JsonPropertyName(LexAuthConstants.ProjectsClaimType)]
@@ -169,45 +169,6 @@ public record LexAuthUser
                     _ => ProjectRole.Unknown
                 };
                 return p[2..].Split("|").Select(Guid.Parse).Select(pId => new AuthUserProject(role, pId));
-            }).ToArray();
-        }
-    }
-
-    [JsonPropertyName(LexAuthConstants.OrgsClaimType)]
-    public string OrgsJson
-    {
-        get =>
-            string.Join(",",
-                Orgs.GroupBy(p => p.Role).Select(roleGroup =>
-                {
-                    var orgRole = roleGroup.Key switch
-                    {
-                        OrgRole.Admin => "a",
-                        OrgRole.User => "u",
-                        _ => "x"
-                    };
-
-                    var orgIdString = string.Join("|", roleGroup.Select(p => p.OrgId.ToString("N")));
-                    return $"{orgRole}:{orgIdString}";
-                }));
-        set
-        {
-            //will be empty for admins
-            if (string.IsNullOrEmpty(value))
-            {
-                Orgs = Array.Empty<AuthUserOrg>();
-                return;
-            }
-            Orgs = value.Split(",").SelectMany(p =>
-            {
-                if (string.IsNullOrEmpty(p)) return Array.Empty<AuthUserOrg>();
-                var role = p[0] switch
-                {
-                    'a' => OrgRole.Admin,
-                    'u' => OrgRole.User,
-                    _ => OrgRole.Unknown
-                };
-                return p[2..].Split("|").Select(Guid.Parse).Select(pId => new AuthUserOrg(role, pId));
             }).ToArray();
         }
     }
