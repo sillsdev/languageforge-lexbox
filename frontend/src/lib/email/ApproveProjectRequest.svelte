fb947345db433716c93f50c7bd0480e41a482a64
SeongHo Park <seongho_park@sil.org>
2024-06-18 09:16:11 +0700

    add translation to email

--- frontend/src/lib/email/ApproveProjectRequest.svelte
+++ frontend/src/lib/email/ApproveProjectRequest.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
   import Email from '$lib/email/Email.svelte';
-  // import t from '$lib/i18n';
+  import t from '$lib/i18n';
 
   export let name: string;
 </script>
 
-<!-- add $t() -->
-<Email subject={'Your project has been approved.'} {name}>
-  <mj-text>Your project has been approved.</mj-text>
+<Email subject={$t('emails.approve_project_request_email.subject')} {name}>
+  <mj-text>$t('emails.approve_project_request_email.heading')</mj-text>
 </Email>
