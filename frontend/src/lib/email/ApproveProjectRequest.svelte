b2d0766dd12055b1942452b3d74700421106f22e
SeongHo Park <seongho_park@sil.org>
2024-06-18 16:13:30 +0700

    added button in ApproveProjectRequest email (still needs url)

--- frontend/src/lib/email/ApproveProjectRequest.svelte
+++ frontend/src/lib/email/ApproveProjectRequest.svelte
@@ -3,8 +3,11 @@
   import t from '$lib/i18n';
 
   export let name: string;
+  export let projectName: string;
+  export let projectUrl: string;
 </script>
 
-<Email subject={$t('emails.approve_project_request_email.subject')} {name}>
-  <mj-text>{$t('emails.approve_project_request_email.heading')}</mj-text>
+<Email subject={$t('emails.approve_project_request_email.subject', {projectName})} {name}>
+  <mj-text>{$t('emails.approve_project_request_email.heading', {projectName})}</mj-text>
+  <mj-button href={projectUrl}>{$t('emails.approve_project_request_email.view_button')}</mj-button>
 </Email>
