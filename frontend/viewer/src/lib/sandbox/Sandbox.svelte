9834ea57c1d63fe6ded5ec13717b9dc93a58977d
Tim Haasdyk <tim_haasdyk@sil.org>
2024-06-20 15:02:47 +0200

    Add Sandbox to reproduce open bug

---
+++ frontend/viewer/src/lib/sandbox/Sandbox.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { MultiSelectField, } from 'svelte-ux';
+
+  const options = [
+    { name: 'One', value: 1 },
+    { name: 'Two', value: 2 },
+    { name: 'Three', value: 3 },
+    { name: 'Four', value: 4 },
+  ];
+
+  let value = [3];
+</script>
+
+<MultiSelectField {options} {value} on:change={(e) => (value = e.detail.value)} />
+
+<div class="bg-green-400 h-[3000px]"></div>
