641c2b11f216254ae01c46db2e66c69e01aae3d1
SeongHo Park <seongho_park@sil.org>
2024-06-28 09:45:52 +0700

    add file search & fix type errors

--- frontend/src/lib/i18n/locales/update_tracker.ts
+++ frontend/src/lib/i18n/locales/update_tracker.ts
@@ -1,41 +1,46 @@
-// CL to run the script:
+// Command line to run the script:
 // pnpm exec tsc src/lib/i18n/locales/update_tracker.ts && mv update_tracker.js update_tracker.cjs && node update_tracker.cjs
-// Adding --json argument will spit out a json
-import { readFileSync } from 'fs'
+import * as fs from 'fs'
+import * as path from 'path'
 import { parse } from 'json5'
 
-const enJson = parse(readFileSync('en.json', 'utf-8')) as object;
-const locales = ['fr', 'es'];
+interface Langdef {
+  [key: string]: string | Langdef
+}
+
+const files = fs.readdirSync(__dirname);
+const locales = files
+    .filter(file => path.extname(file).toLowerCase() === '.json')
+    .map(file => path.basename(file, '.json'))
+    .filter(file => file !== 'en');
+
+const enJson: Langdef = parse(fs.readFileSync('en.json', 'utf-8'));
 const enAllKeys: string[] = [];
 compileKeys(enJson, '', enAllKeys);
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-function compileKeys(obj: object, prefix: string = '', keys: string[] = []) {
+function compileKeys(obj: Langdef, prefix: string = '', keys: string[] = []): void {
   for (const key in obj) {
     const fullKey = prefix ? `${prefix}.${key}` : key;
     keys.push(fullKey);
-    if (typeof obj[key] === 'object') {
-      compileKeys(obj[key], fullKey, keys);
+    const subKey = obj[key]
+    if (typeof subKey !== 'string') {
+      compileKeys(subKey, fullKey, keys);
     }
   }
 }
 
 function getDiffKeys(stdKeys: string[], lang: string): string[] {
-  const langJson = parse(readFileSync(`${lang}.json`, 'utf-8')) as object;
+  const langJson: Langdef = parse(fs.readFileSync(`${lang}.json`, 'utf-8'));
   const langKeys: string[] = [];
   compileKeys(langJson, '', langKeys);
   const diff = stdKeys.filter(x => !langKeys.includes(x));
   return diff;
 }
 
-if (process.argv.includes('--json')) {
-  locales.forEach(lang => {
-    const diff = getDiffKeys(enAllKeys, lang);
-    console.log(JSON.stringify(diff));
-  });
-} else {
-  locales.forEach(lang => {
-    const diff = getDiffKeys(enAllKeys, lang);
-    console.log(diff);
-  });
-}
+const output: Record<string, string[]> = {};
+locales.forEach(lang => {
+  const diff = getDiffKeys(enAllKeys, lang);
+  output[lang] = diff;
+});
+
+console.log(output);
