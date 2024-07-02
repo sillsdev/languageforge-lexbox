405b03034a17b1df4a31ac818a0d2531156a998e
SeongHo Park <seongho_park@sil.org>
2024-07-02 10:46:04 +0700

    create main function

--- frontend/src/lib/i18n/locales/update_tracker.mjs
+++ frontend/src/lib/i18n/locales/update_tracker.mjs
@@ -2,18 +2,6 @@ import * as fs from 'fs';
 import * as path from 'path';
 import json5 from 'json5';
 
-const dirname = import.meta.dirname;
-
-const files = fs.readdirSync(dirname);
-const locales = files
-    .filter(file => path.extname(file).toLowerCase() === '.json')
-    .map(file => path.basename(file, '.json'))
-    .filter(file => file !== 'en');
-
-const enJson = json5.parse(fs.readFileSync('en.json', 'utf-8'));
-const enAllKeys = [];
-compileKeys(enJson, '', enAllKeys);
-
 function compileKeys(obj, prefix = '', keys = []) {
     for (const key in obj) {
         const fullKey = prefix ? `${prefix}.${key}` : key;
@@ -33,9 +21,24 @@ function getDiffKeys(stdKeys, lang) {
     return diff;
 }
 
-const output = {};
-locales.forEach(lang => {
-    const diff = getDiffKeys(enAllKeys, lang);
-    output[lang] = diff;
-});
-console.log(output);
+function main() {
+  const dirname = import.meta.dirname;
+  const files = fs.readdirSync(dirname);
+  const locales = files
+      .filter(file => path.extname(file).toLowerCase() === '.json')
+      .map(file => path.basename(file, '.json'))
+      .filter(file => file !== 'en');
+
+  const enJson = json5.parse(fs.readFileSync('en.json', 'utf-8'));
+  const enAllKeys = [];
+  compileKeys(enJson, '', enAllKeys);
+
+  const output = {};
+  locales.forEach(lang => {
+      const diff = getDiffKeys(enAllKeys, lang);
+      output[lang] = diff;
+  });
+  console.log(output);
+}
+
+main();
