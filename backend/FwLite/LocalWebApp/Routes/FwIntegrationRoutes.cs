541226f619aefe7535672dc933deaec47ecea7cf
Kevin Hahn <kevin_hahn@sil.org>
2024-07-11 08:44:27 +0700

    use strongly typed hub when opening a project in flex

--- backend/FwLite/LocalWebApp/Routes/FwIntegrationRoutes.cs
+++ backend/FwLite/LocalWebApp/Routes/FwIntegrationRoutes.cs
@@ -19,10 +19,10 @@ public static class FwIntegrationRoutes
                 return operation;
             });
         group.MapGet("/open/entry/{id}",
-            async (FwDataProjectContext context, IHubContext<FwDataMiniLcmHub> hubContext, FwDataFactory factory, Guid id) =>
+            async (FwDataProjectContext context, IHubContext<FwDataMiniLcmHub, ILexboxHubClient> hubContext, FwDataFactory factory, Guid id) =>
             {
                 if (context.Project is null) return Results.BadRequest("No project is set in the context");
-                await hubContext.Clients.Group(context.Project.Name).SendCoreAsync(nameof(ILexboxHubClient.OnProjectClosed), [CloseReason.Locked]);
+                await hubContext.Clients.Group(context.Project.Name).OnProjectClosed(CloseReason.Locked);
                 factory.CloseCurrentProject();
                 //need to use redirect as a way to not trigger flex until after we have closed the project
                 return Results.Redirect($"silfw://localhost/link?database={context.Project.Name}&tool=lexiconEdit&guid={id}");
