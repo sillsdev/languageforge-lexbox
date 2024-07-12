dd2ffa7c42e2a34aa02fdd170b2c5158ca4e7911
Kevin Hahn <kevin_hahn@sil.org>
2024-07-12 15:34:59 +0700

    write test for writing language tags into flex project metadata

--- backend/Testing/LexCore/Services/ProjectServiceTest.cs
+++ backend/Testing/LexCore/Services/ProjectServiceTest.cs
@@ -2,6 +2,7 @@ using LexBoxApi.Services;
 using LexBoxApi.Services.Email;
 using LexCore.Entities;
 using LexCore.ServiceInterfaces;
+using LexData;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.DependencyInjection;
@@ -16,17 +17,33 @@ namespace Testing.LexCore.Services;
 public class ProjectServiceTest
 {
     private readonly ProjectService _projectService;
+    private readonly ProjectWritingSystems _writingSystems = new()
+    {
+        VernacularWss = new List<FLExWsId>()
+        {
+            new() { Tag = "en", IsActive = true },
+            new() { Tag = "fr", IsActive = false }
+        },
+        AnalysisWss = new List<FLExWsId>()
+        {
+            new() { Tag = "en", IsActive = true },
+            new() { Tag = "fr", IsActive = false }
+        }
+    };
+
+    private LexBoxDbContext _lexBoxDbContext;
 
     public ProjectServiceTest(TestingServicesFixture testing)
     {
         var serviceProvider = testing.ConfigureServices(s =>
         {
-            s.AddScoped<IHgService>(_ => Mock.Of<IHgService>());
+            s.AddScoped<IHgService>(_ => Mock.Of<IHgService>(service => service.GetProjectWritingSystems(It.IsAny<ProjectCode>(), It.IsAny<CancellationToken>()) == Task.FromResult(_writingSystems)));
             s.AddScoped<IEmailService>(_ => Mock.Of<IEmailService>());
             s.AddSingleton<IMemoryCache>(_ => Mock.Of<IMemoryCache>());
             s.AddScoped<ProjectService>();
         });
         _projectService = serviceProvider.GetRequiredService<ProjectService>();
+        _lexBoxDbContext = serviceProvider.GetRequiredService<LexBoxDbContext>();
     }
 
     [Fact]
@@ -35,6 +52,17 @@ public class ProjectServiceTest
         var projectId = await _projectService.CreateProject(
             new(null, "TestProject", "Test", "test", ProjectType.FLEx, RetentionPolicy.Test, false, null, null));
         projectId.ShouldNotBe(default);
+    }
+
+    [Fact]
+    public async Task CanUpdateProjectLangTags()
+    {
+        var projectId = await _projectService.CreateProject(
+            new(null, "TestProject", "Test", "test", ProjectType.FLEx, RetentionPolicy.Test, false, null, null));
+        await _projectService.UpdateProjectLangTags(projectId);
+        var project = await _lexBoxDbContext.Projects.Include(p => p.FlexProjectMetadata).SingleAsync(p => p.Id == projectId);
+        project.FlexProjectMetadata.ShouldNotBeNull();
+        project.FlexProjectMetadata.WritingSystems.ShouldBeEquivalentTo(_writingSystems);
     }
 
     [Fact]
