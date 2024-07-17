1dfbb48d075c14605ac668ef6b26c510d1d42f5a
Kevin Hahn <kevin_hahn@sil.org>
2024-07-17 16:27:17 +0700

    introduce some navigation retry logic as sometimes the first navigation after we get the server url fails.

--- backend/FwLite/FwLiteDesktop/MainPage.xaml.cs
+++ backend/FwLite/FwLiteDesktop/MainPage.xaml.cs
@@ -17,6 +17,29 @@ public partial class MainPage : ContentPage
         var url = options.CurrentValue.Url;
         webView.Source = url;
         logger.LogInformation("Main page initialized, url: {Url}", url);
+        webView.Navigating += (sender, args) =>
+        {
+            logger.LogInformation("Web view navigating to {Url}", args.Url);
+        };
+        int navigatedCount = 0;
+        int maxNavigatedCount = 10;
+        webView.Navigated += (sender, args) =>
+        {
+            logger.LogInformation("Web view navigated to {Url}, result: {Result}", args.Url, args.Result);
+            if (args.Url.StartsWith("https://appdir") && options.CurrentValue.Url is not null)
+            {
+                if (++navigatedCount <= maxNavigatedCount)
+                {
+                    logger.LogInformation("Navigating to {Url}", options.CurrentValue.Url);
+                    webView.Dispatcher.DispatchDelayed(TimeSpan.FromMilliseconds(100 * navigatedCount),
+                        () => webView.Source = options.CurrentValue.Url);
+                }
+                else
+                {
+                    logger.LogWarning("Too many navigations, stopping");
+                }
+            }
+        };
     }
 }
 
