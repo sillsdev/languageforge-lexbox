3ba2a0deac5f314bb1ad329f74d682b8a184dcd8
SeongHo Park <seongho_park@sil.org>
2024-07-19 11:34:33 +0700

    add dropdown to org badge

--- frontend/src/routes/(authenticated)/project/[project_code]/OrgList.svelte
+++ frontend/src/routes/(authenticated)/project/[project_code]/OrgList.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
   import t from '$lib/i18n';
-  import { Badge, BadgeList } from '$lib/components/Badges';
+  import { BadgeList } from '$lib/components/Badges';
   import type { Organization } from '$lib/gql/types';
+  import { createEventDispatcher } from 'svelte';
+  import Dropdown from '$lib/components/Dropdown.svelte';
+  import { TrashIcon } from '$lib/icons';
+  import ActionBadge from '$lib/components/Badges/ActionBadge.svelte';
 
   type Org = Pick<Organization, 'id' | 'name'>;
   export let organizations: Org[] = [];
+
+  const dispatch = createEventDispatcher<{
+    removeFromOrg: string;
+  }>();
 
   const TRUNCATED_MEMBER_COUNT = 5;
 </script>
@@ -23,9 +31,26 @@
       </div>
     {/if}
     {#each organizations as org (org.id)}
-      <Badge>
-        {org.name}
-      </Badge>
+      <Dropdown>
+        <ActionBadge actionIcon="i-mdi-dots-vertical" on:action>
+          <span class="pr-3 whitespace-nowrap overflow-ellipsis overflow-x-clip" title={org.name}>
+            {org.name}
+          </span>
+        </ActionBadge>
+        <ul slot="content" class="menu">
+          <li>
+            <button>
+              Link to Org
+            </button>
+          </li>
+          <li>
+            <button class="text-error" on:click={() => dispatch('removeFromOrg', org.id)}>
+              <TrashIcon />
+              {$t('project_page.remove_user')}
+            </button>
+          </li>
+        </ul>
+      </Dropdown>
     {/each}
   </BadgeList>
 </div>
