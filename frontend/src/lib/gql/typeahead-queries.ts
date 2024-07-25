c2e272b869a769f7ef7447345b74d76b5666e2a6
SeongHo Park <seongho_park@sil.org>
2024-07-25 15:25:47 +0700

    scaffolding for typeahead query

--- frontend/src/lib/gql/typeahead-queries.ts
+++ frontend/src/lib/gql/typeahead-queries.ts
@@ -48,3 +48,43 @@ export async function _userTypeaheadSearch(userSearch: string, limit = 10): Prom
 
   return users;
 }
+
+// export function orgMemberFilter(userSearch: string): OrgMemberFilterInput {
+//   return {
+//     or: [
+//       {name: {icontains: userSearch}},
+//       {email: {icontains: userSearch}},
+//       {username: {icontains: userSearch}}
+//     ]
+//   };
+// }
+
+// export async function _orgMemberTypeaheadSearch(orgMemberSearch: string, limit = 10): Promise<OrgMemberTypeaheadResult> {
+//   if (!orgMemberSearch) return Promise.resolve([]);
+//   const client = getClient();
+//   const result = client.query(graphql(`
+//     query loadOrgMembersTypeahead($filter: UserFilterInput, $take: Int!) {
+//       usersInMyOrgs(where: $filter, orderBy: {name: ASC}, take: $take) {
+//         user {
+//           id
+//           name
+//           email
+//           username
+//         }
+//       }
+//     }
+//   `), { filter: orgMemberFilter(orgMemberSearch), take: limit });
+
+//   const users = result.then(members => {
+//     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+//     const count = members.data?.members.length ?? 0;
+//     if (0 < count && count <= limit) {
+//       // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+//       return members.data?.members?.user ?? [];
+//     } else {
+//       return [];
+//     }
+//   });
+
+//   return users;
+// }
