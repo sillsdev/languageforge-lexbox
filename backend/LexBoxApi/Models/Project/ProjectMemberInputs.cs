70a4dc6c1645119d70fc421fcaa2fa3a0f5a15ec
SeongHo Park <seongho_park@sil.org>
2024-07-30 11:11:15 +0700

    add userId in schema

--- backend/LexBoxApi/Models/Project/ProjectMemberInputs.cs
+++ backend/LexBoxApi/Models/Project/ProjectMemberInputs.cs
@@ -3,7 +3,7 @@ using LexCore.Entities;
 
 namespace LexBoxApi.Models.Project;
 
-public record AddProjectMemberInput(Guid ProjectId, string UsernameOrEmail, ProjectRole Role, bool canInvite);
+public record AddProjectMemberInput(Guid ProjectId, string UsernameOrEmail, Guid? UserId, ProjectRole Role, bool canInvite);
 
 public record BulkAddProjectMembersInput(Guid? ProjectId, string[] Usernames, ProjectRole Role, string PasswordHash);
 
