29185fb1cc4bdcac8ccc7b9a73233ac7c0ff54dc
Kevin Hahn <kevin_hahn@sil.org>
2024-08-15 10:58:53 +0700

    remove `CanConnectAsync` as that only works if the database has already been created, use Migrate and wrap in a try catch instead.

---
+++ backend/Testing/Fixtures/Tests/ServicesFixtureTests.cs
@@ -0,0 +1,18 @@
+﻿using Shouldly;
+
+namespace Testing.Fixtures.Tests;
+
+public class ServicesFixtureTests
+{
+    [Fact]
+    public async Task CanSetupServices()
+    {
+        var fixture = new TestingServicesFixture();
+        var act = async () =>
+        {
+            await fixture.InitializeAsync();
+            await fixture.DisposeAsync();
+        };
+        Should.CompleteIn(act, TimeSpan.FromSeconds(10));
+    }
+}
