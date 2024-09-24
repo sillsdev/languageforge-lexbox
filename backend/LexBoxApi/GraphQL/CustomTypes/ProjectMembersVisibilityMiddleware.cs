078fc9c3b45fc7c476545dfcd8ad82bc2a95ba02
Tim Haasdyk <tim_haasdyk@sil.org>
2024-09-24 15:17:27 +0200

    Show current user and confidential info

--- backend/LexBoxApi/GraphQL/CustomTypes/ProjectMembersVisibilityMiddleware.cs
+++ backend/LexBoxApi/GraphQL/CustomTypes/ProjectMembersVisibilityMiddleware.cs
@@ -1,4 +1,5 @@
 using HotChocolate.Resolvers;
+using LexBoxApi.Auth;
 using LexCore.Entities;
 using LexCore.Exceptions;
 using LexCore.ServiceInterfaces;
@@ -7,17 +8,17 @@ namespace LexBoxApi.GraphQL.CustomTypes;
 
 public class ProjectMembersVisibilityMiddleware(FieldDelegate next)
 {
-    public async Task InvokeAsync(IMiddlewareContext context, IPermissionService permissionService)
+    public async Task InvokeAsync(IMiddlewareContext context, IPermissionService permissionService, LoggedInContext loggedInContext)
     {
         await next(context);
-        if (context.Result is IEnumerable<ProjectUsers>)
+        if (context.Result is IEnumerable<ProjectUsers> projectUsers)
         {
             var contextProject = context.Parent<Project>();
             var projId = contextProject?.Id ?? throw new RequiredException("Must include project ID in query if querying users");
             if (!await permissionService.CanViewProjectMembers(projId))
             {
-                // Confidential project, and user doesn't have permission to see its users, so hide the users list
-                context.Result = new List<ProjectUsers>();
+                // Confidential project, and user doesn't have permission to see its users, so only show the current user's membership
+                context.Result = projectUsers.Where(pu => pu.User?.Id == loggedInContext.MaybeUser?.Id);
             }
         }
     }
