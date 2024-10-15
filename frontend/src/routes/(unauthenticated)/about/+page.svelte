98214ca47d9213750860101c225ee3c094e21b1e
Tim Haasdyk <tim_haasdyk@sil.org>
2024-10-15 15:53:25 +0200

    Simplify text and add About footer-link

--- frontend/src/routes/(unauthenticated)/about/+page.svelte
+++ frontend/src/routes/(unauthenticated)/about/+page.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
-  import { HeaderPage, HomeBreadcrumb } from '$lib/layout';
+  import { TitlePage, HomeBreadcrumb } from '$lib/layout';
   import t from '$lib/i18n';
   import Markdown from 'svelte-exmarkdown';
-  import AppLogo from '$lib/icons/AppLogo.svelte';
 </script>
 
 <HomeBreadcrumb/>
-<HeaderPage wide title={$t('about.title')}>
+<TitlePage title={$t('about.title')}>
   <div class="prose text-lg">
     <Markdown md={$t('login.welcome')}/>
   </div>
-</HeaderPage>
+</TitlePage>
