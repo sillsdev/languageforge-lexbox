1dd8664a6089569d0df8d96bed183890559160f4
Kevin Hahn <kevin_hahn@sil.org>
2024-10-15 13:59:14 +0700

    update about to use the login welcome, mention lexbox being free and open source in the welcome text

--- frontend/src/routes/(unauthenticated)/about/+page.svelte
+++ frontend/src/routes/(unauthenticated)/about/+page.svelte
@@ -1,25 +1,13 @@
 <script lang="ts">
   import { HeaderPage, HomeBreadcrumb } from '$lib/layout';
   import t from '$lib/i18n';
+  import Markdown from 'svelte-exmarkdown';
+  import AppLogo from '$lib/icons/AppLogo.svelte';
 </script>
 
-<HomeBreadcrumb />
+<HomeBreadcrumb/>
 <HeaderPage wide title={$t('about.title')}>
-  <div class="prose">
-    <p>Lexbox is a SIL Lexical Tools project.</p>
-    <p>
-      We host project data for
-      <a href="https://software.sil.org/fieldworks/">FieldWorks</a>,
-      <a href="https://languageforge.org/">Language Forge</a>, and
-      <a href="https://software.sil.org/onestoryeditor/">OneStory Editor</a>.
-      It is provided as a free service to language communities by
-      <a href="https://software.sil.org/">SIL Language Technology</a> and
-      <a href="https://li.payap.ac.th/">Linguistics Institute at Payap University</a>
-      in Chiang Mai, Thailand.
-    </p>
-    <p>
-      Lexbox is a free and open source project, and is licensed under the
-      <a href="https://github.com/sillsdev/languageforge-lexbox/blob/main/LICENSE">MIT license</a>.
-    </p>
+  <div class="prose text-lg">
+    <Markdown md={$t('login.welcome')}/>
   </div>
 </HeaderPage>
