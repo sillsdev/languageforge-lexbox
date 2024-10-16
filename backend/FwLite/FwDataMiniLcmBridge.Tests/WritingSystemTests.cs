e9717532d987bac371e0930a1fb8786a6cc727d4
Kevin Hahn <kevin_hahn@sil.org>
2024-10-16 11:12:50 +0700

    move entry creation into fw data test case

--- backend/FwLite/FwDataMiniLcmBridge.Tests/WritingSystemTests.cs
+++ backend/FwLite/FwDataMiniLcmBridge.Tests/WritingSystemTests.cs
@@ -1,5 +1,6 @@
 ﻿using FwDataMiniLcmBridge.Tests.Fixtures;
 using MiniLcm;
+using MiniLcm.Models;
 using MiniLcm.Tests;
 
 namespace FwDataMiniLcmBridge.Tests;
@@ -7,8 +8,17 @@ namespace FwDataMiniLcmBridge.Tests;
 [Collection(ProjectLoaderFixture.Name)]
 public class WritingSystemTests(ProjectLoaderFixture fixture) : WritingSystemTestsBase
 {
-    protected override Task<IMiniLcmApi> NewApi()
+    protected override async Task<IMiniLcmApi> NewApi()
     {
-        return Task.FromResult<IMiniLcmApi>(fixture.NewProjectApi("ws-test", "en", "en"));
+        var api = fixture.NewProjectApi("ws-test", "en", "en");
+
+        //exemplars come from entries so we need to create one
+        await api.CreateEntry(new Entry()
+        {
+            Id = Guid.NewGuid(),
+            LexemeForm = { { "en", "new-lexeme-form" } },
+            Senses = new List<Sense>() { new Sense() { Gloss = { { "en", "new-sense-gloss" } } } }
+        });
+        return api;
     }
 }
--- backend/FwLite/MiniLcm.Tests/WritingSystemTestsBase.cs
+++ backend/FwLite/MiniLcm.Tests/WritingSystemTestsBase.cs
@@ -4,16 +4,6 @@ namespace MiniLcm.Tests;
 
 public abstract class WritingSystemTestsBase : MiniLcmTestBase
 {
-    public override async Task InitializeAsync()
-    {
-        await base.InitializeAsync();
-        await Api.CreateEntry(new Entry()
-        {
-            Id = Guid.NewGuid(),
-            LexemeForm = { { "en", "new-lexeme-form" } },
-            Senses = new List<Sense>() { new Sense() { Gloss = { { "en", "new-sense-gloss" } } } }
-        });
-    }
 
     [Fact]
     public async Task GetWritingSystems_DoesNotReturnNullOrEmpty()
