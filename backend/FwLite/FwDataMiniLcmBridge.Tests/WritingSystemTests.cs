3c936549284b79fb1d0d1ad6472785b7fa297e1f
Kevin Hahn <kevin_hahn@sil.org>
2024-10-16 11:02:22 +0700

    move writing system tests into MiniLcm.Tests to make them reusable

--- backend/FwLite/FwDataMiniLcmBridge.Tests/WritingSystemTests.cs
+++ backend/FwLite/FwDataMiniLcmBridge.Tests/WritingSystemTests.cs
@@ -1,48 +1,14 @@
-﻿using FwDataMiniLcmBridge.Api;
-using FwDataMiniLcmBridge.Tests.Fixtures;
-using MiniLcm.Models;
+﻿using FwDataMiniLcmBridge.Tests.Fixtures;
+using MiniLcm;
+using MiniLcm.Tests;
 
 namespace FwDataMiniLcmBridge.Tests;
 
 [Collection(ProjectLoaderFixture.Name)]
-public class WritingSystemTests(ProjectLoaderFixture fixture) : IAsyncLifetime
+public class WritingSystemTests(ProjectLoaderFixture fixture) : WritingSystemTestsBase
 {
-    private FwDataMiniLcmApi _api = null!;
-
-    public async Task InitializeAsync()
+    protected override Task<IMiniLcmApi> NewApi()
     {
-        var projectName = "ws-test_" + Guid.NewGuid();
-        fixture.MockFwProjectLoader.NewProject(projectName, "en", "en");
-        _api = fixture.CreateApi(projectName);
-        _api.Should().NotBeNull();
-
-
-        await _api.CreateEntry(new Entry()
-        {
-            Id = Guid.NewGuid(),
-            LexemeForm = { { "en", "new-lexeme-form" } },
-            Senses = new List<Sense>() { new Sense() { Gloss = { { "en", "new-sense-gloss" } } } }
-        });
-    }
-
-    public Task DisposeAsync()
-    {
-        _api.Dispose();
-        return Task.CompletedTask;
-    }
-
-    [Fact]
-    public async Task GetWritingSystems_DoesNotReturnNullOrEmpty()
-    {
-        var writingSystems = await _api.GetWritingSystems();
-        writingSystems.Vernacular.Should().NotBeNullOrEmpty();
-        writingSystems.Analysis.Should().NotBeNullOrEmpty();
-    }
-
-    [Fact]
-    public async Task GetWritingSystems_ReturnsExemplars()
-    {
-        var writingSystems = await _api.GetWritingSystems();
-        writingSystems.Vernacular.Should().Contain(ws => ws.Exemplars.Any());
+        return Task.FromResult<IMiniLcmApi>(fixture.NewProjectApi("ws-test", "en", "en"));
     }
 }
---
+++ backend/FwLite/MiniLcm.Tests/WritingSystemTestsBase.cs
@@ -0,0 +1,32 @@
+﻿using MiniLcm.Models;
+
+namespace MiniLcm.Tests;
+
+public abstract class WritingSystemTestsBase : MiniLcmTestBase
+{
+    public override async Task InitializeAsync()
+    {
+        await base.InitializeAsync();
+        await Api.CreateEntry(new Entry()
+        {
+            Id = Guid.NewGuid(),
+            LexemeForm = { { "en", "new-lexeme-form" } },
+            Senses = new List<Sense>() { new Sense() { Gloss = { { "en", "new-sense-gloss" } } } }
+        });
+    }
+
+    [Fact]
+    public async Task GetWritingSystems_DoesNotReturnNullOrEmpty()
+    {
+        var writingSystems = await Api.GetWritingSystems();
+        writingSystems.Vernacular.Should().NotBeNullOrEmpty();
+        writingSystems.Analysis.Should().NotBeNullOrEmpty();
+    }
+
+    [Fact]
+    public async Task GetWritingSystems_ReturnsExemplars()
+    {
+        var writingSystems = await Api.GetWritingSystems();
+        writingSystems.Vernacular.Should().Contain(ws => ws.Exemplars.Any());
+    }
+}
