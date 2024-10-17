a79b8a96e87b5fa387dc004aed708dd9b3344947
Robin Munn <rmunn@pobox.com>
2024-10-17 14:15:52 +0700

    Improve UserProjects component
    Now takes list of projects and roles, and doesn't need to do any GQL
    queries of its own.

--- frontend/src/lib/components/Users/UserProjects.svelte
+++ frontend/src/lib/components/Users/UserProjects.svelte
@@ -1,33 +1,43 @@
 <script  context="module" lang="ts">
-  import type { User as AdminDashboardUser } from '../../../routes/(authenticated)/admin/+page';
-
-  // We define User type that we'll want in here, and export it so that callers can know they're passing in the right type
-  export type User = Pick<AdminDashboardUser, 'id' | 'projects'> & {
-    projects: {
-      name: string
-      code: string
-      role: string
-    }
+  // We define the Project type that we'll want in here, and export it so that callers can know they're passing in the right type
+  export type Project = {
+    id: string
+    name: string
+    code: string
+    role: ProjectRole
   };
-  // TODO: That's not the correct way to expand the projects type in AdminDashboardUser. Figure out correct Typescripty way to do it.
-
 </script>
 
 <script lang="ts">
-  // List of projects that a given user is member of
-  // Shows "manager" badge if user is manager of project
-  // Includes confidential projects only if currently logged in user is site admin OR is same user as the one whose project we're looking at
-  // Has checkbox beside each project so they can be selected
-  // Projects the given user is managing are automatically pre-selected
-  // Exposes bindable prop with list of project codes (or project objects) that are selected
-  // TODO: Determine if list of project objects or project codes would be more useful / easier to implement
+  import { ProjectRole } from '$lib/gql/types';
+  import { writable } from 'svelte/store';
+  import Badge from '../Badges/Badge.svelte';
+  import { onMount } from 'svelte';
 
-  // export let userId: string; // GUID
-  export let user: User;
+  export let projects: Project[] = [];
+  export let selectedProjects: string[] = [];
+
+  function isManager(proj: Project): boolean {
+    return proj.role === ProjectRole.Manager;
+  }
+
+  // Projects managed by the given user come pre-checked, to save time in typical uses of this component
+  $: projectsStore = writable(projects);
+  onMount(() => projectsStore.subscribe(projects => {
+    if (projects && projects.length > 0) {
+      selectedProjects = [... projects.filter(isManager).map(proj => proj.code)];
+    }
+  }));
 </script>
 
 <ul>
-  {#each user.projects as proj}
-    <li>{proj.name} (proj.code)</li>
+  {#each projects as proj}
+    <li>
+      <input type="checkbox" bind:group={selectedProjects} value={proj.code} />
+      {proj.name}
+      {#if proj.role == ProjectRole.Manager}
+      <Badge variant="badge-info">Manager</Badge>
+      {/if}
+    </li>
   {/each}
 </ul>
