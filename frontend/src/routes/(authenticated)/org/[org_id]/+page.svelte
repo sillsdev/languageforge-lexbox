652640ae38bf264756518e7d6ee4b325e7989de9
Robin Munn <rmunn@pobox.com>
2024-10-18 16:40:45 +0700

    Fix typo that was producing incorrect HTML (#1134)

--- frontend/src/routes/(authenticated)/org/[org_id]/+page.svelte
+++ frontend/src/routes/(authenticated)/org/[org_id]/+page.svelte
@@ -156,7 +156,7 @@
               </ul>
             </Dropdown>
           {/if}
-        <td/>
+        </td>
       </ProjectTable>
       <DeleteModal
         bind:this={removeProjectFromOrgModal}
