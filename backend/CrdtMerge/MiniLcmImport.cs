961cd2496264982e0813839f972ed4cfecdaf31f
Robin Munn <rmunn@pobox.com>
2024-10-21 12:44:06 +0700

    Copy implementation from FwLiteProjectSync
    This is copied nearly as-is, but with an ASP.NET Core API wrapped around
    it instead of a System.CommandLine set of arguments and options. Next
    step would be to move the implementing classes into a CrdtMerge.Core
    library so that FwLiteProjectSync can reference them and we DRY
    ourselves out again.

---
+++ backend/CrdtMerge/MiniLcmImport.cs
@@ -0,0 +1,63 @@
+﻿using LcmCrdt;
+using Microsoft.Extensions.Logging;
+using MiniLcm;
+using MiniLcm.Models;
+
+public class MiniLcmImport(ILogger<MiniLcmImport> logger)
+{
+    public async Task ImportProject(IMiniLcmApi importTo, IMiniLcmApi importFrom, int entryCount)
+    {
+        var writingSystems = await importFrom.GetWritingSystems();
+        foreach (var ws in writingSystems.Analysis)
+        {
+            await importTo.CreateWritingSystem(WritingSystemType.Analysis, ws);
+            logger.LogInformation("Imported ws {WsId}", ws.WsId);
+        }
+
+        foreach (var ws in writingSystems.Vernacular)
+        {
+            await importTo.CreateWritingSystem(WritingSystemType.Vernacular, ws);
+            logger.LogInformation("Imported ws {WsId}", ws.WsId);
+        }
+
+        await foreach (var partOfSpeech in importFrom.GetPartsOfSpeech())
+        {
+            await importTo.CreatePartOfSpeech(partOfSpeech);
+            logger.LogInformation("Imported part of speech {Id}", partOfSpeech.Id);
+        }
+
+        await foreach (var complexFormType in importFrom.GetComplexFormTypes())
+        {
+            await importTo.CreateComplexFormType(complexFormType);
+            logger.LogInformation("Imported complex form type {Id}", complexFormType.Id);
+        }
+
+
+        var semanticDomains = importFrom.GetSemanticDomains();
+        var entries = importFrom.GetEntries(new QueryOptions(Count: 100_000, Offset: 0));
+        if (importTo is CrdtMiniLcmApi crdtLexboxApi)
+        {
+            logger.LogInformation("Importing semantic domains");
+            await crdtLexboxApi.BulkImportSemanticDomains(semanticDomains.ToBlockingEnumerable());
+            logger.LogInformation("Importing {Count} entries", entryCount);
+            await crdtLexboxApi.BulkCreateEntries(entries);
+        }
+        else
+        {
+            await foreach (var semanticDomain in semanticDomains)
+            {
+                await importTo.CreateSemanticDomain(semanticDomain);
+                logger.LogTrace("Imported semantic domain {Id}", semanticDomain.Id);
+            }
+
+            var index = 0;
+            await foreach (var entry in entries)
+            {
+                await importTo.CreateEntry(entry);
+                logger.LogTrace("Imported entry, {Index} of {Count} {Id}", index++, entryCount, entry.Id);
+            }
+        }
+
+        logger.LogInformation("Imported {Count} entries", entryCount);
+    }
+}
--- backend/CrdtMerge/Program.cs
+++ backend/CrdtMerge/Program.cs
@@ -1,3 +1,10 @@
+using FwDataMiniLcmBridge;
+using LcmCrdt;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MiniLcm;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -14,11 +21,70 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapPost("/sync", (string code) =>
+app.MapPost("/sync", async (string crdtFile, string fwDataFile, bool createCrdtDir, bool dryRun = false) =>
 {
-    return new { Result = $"TODO: Would sync project {code}" };
+    await ExecuteMergeRequest(crdtFile, fwDataFile, createCrdtDir, dryRun);
 });
 
 app.Run();
 
-record ExampleRequest(string ProjectCode);
+async Task ExecuteMergeRequest(string crdtFile, string fwDataFile, bool createCrdtDir, bool dryRun = false)
+{
+    Console.WriteLine($"crdtFile: {crdtFile}");
+    Console.WriteLine($"fwDataFile: {fwDataFile}");
+    var fwProjectName = Path.GetFileNameWithoutExtension(fwDataFile);
+    var crdtProjectName = Path.GetFileNameWithoutExtension(crdtFile);
+
+    // TODO: Move this service root into the builder up above so we're not creating the services every time
+    await using var serviceRoot = SyncServices(crdtFile, fwDataFile, createCrdtDir);
+    await using var scope = serviceRoot.CreateAsyncScope();
+    var services = scope.ServiceProvider;
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    var fwdataApi = services.GetRequiredService<FwDataFactory>().GetFwDataMiniLcmApi(fwProjectName, true);
+    var projectsService = services.GetRequiredService<ProjectsService>();
+    var crdtProject = projectsService.GetProject(crdtProjectName);
+    if (crdtProject is null)
+    {
+        crdtProject = await projectsService.CreateProject(new(crdtProjectName, fwdataApi.ProjectId, SeedNewProjectData: false));
+    }
+    projectsService.SetProjectScope(crdtProject);
+    await services.GetRequiredService<CurrentProjectService>().PopulateProjectDataCache();
+    var syncService = services.GetRequiredService<CrdtFwdataProjectSyncService>();
+
+    var result = await syncService.Sync(services.GetRequiredService<IMiniLcmApi>(), fwdataApi, dryRun);
+    logger.LogInformation("Sync result, CrdtChanges: {CrdtChanges}, FwdataChanges: {FwdataChanges}", result.CrdtChanges, result.FwdataChanges);
+};
+
+static ServiceProvider SyncServices(string crdtFile, string fwDataFile, bool createCrdtDir)
+{
+    if (!File.Exists(fwDataFile)) throw new InvalidOperationException("Could not find fwdata file " + fwDataFile);
+
+    var fwDataProjectsFolder = Directory.GetParent(fwDataFile)?.Parent;
+    if (fwDataProjectsFolder is null) throw new InvalidOperationException("Could not find parent folder of fwdata dir " + fwDataFile);
+    var crdtFolder = Directory.GetParent(crdtFile);
+    if (crdtFolder is null) throw new InvalidOperationException("Could not find parent folder of crdt file " + crdtFile);
+    if (!crdtFolder.Exists && createCrdtDir)
+    {
+        crdtFolder.Create();
+    } else if (!crdtFolder.Exists)
+    {
+        throw new InvalidOperationException("Could not find crdt folder " + crdtFolder);
+    }
+
+    var crdtServices = new ServiceCollection()
+        .AddLcmCrdtClient()
+        .AddFwDataBridge()
+        .AddSingleton<CrdtFwdataProjectSyncService>()
+        .AddSingleton<MiniLcmImport>()
+        .Configure<FwDataBridgeConfig>(c => c.ProjectsFolder = fwDataProjectsFolder.FullName)
+        .Configure<LcmCrdtConfig>(c => c.ProjectPath = crdtFolder.FullName)
+        .AddLogging(builder => builder.AddConsole().AddDebug().AddConfiguration(new ConfigurationManager().AddInMemoryCollection(new Dictionary<string, string?>
+        {
+            ["Logging:LogLevel:Microsoft.EntityFrameworkCore"] = "Warning"
+        }).Build()))
+        .BuildServiceProvider(true);
+    return crdtServices;
+}
+
+// TODO: Change the MapPost to take this type instead:
+record CrdtMergeRequest(string crdtFile, string fwDataFile, bool createCrdtDir, bool dryRun = false);
