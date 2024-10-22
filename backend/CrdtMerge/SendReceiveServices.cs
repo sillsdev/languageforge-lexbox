0db98e821139d1cbbf0ee472883150d9d4c8b792
Robin Munn <rmunn@pobox.com>
2024-10-22 14:00:43 +0700

    Simplify filename / directory code in SendReceive

--- backend/CrdtMerge/SendReceiveServices.cs
+++ backend/CrdtMerge/SendReceiveServices.cs
@@ -42,7 +42,8 @@ static class SendReceiveServices
     public static LfMergeBridgeResult SendReceive(string fwdataPath, string commitMessage, SendReceiveAuth? auth = null, string? projectCode = null)
     {
         // If projectCode not given, calculate it from the fwdataPath
-        var fwdataInfo = LocateFwDataFile(fwdataPath);
+        var fwdataInfo = new FileInfo(fwdataPath);
+        if (fwdataInfo.Directory is null) throw new ArgumentException("Not allowed to Send/Receive root-level directories like C:\\", nameof(fwdataPath));
         projectCode ??= fwdataInfo.Name.EndsWith(".fwdata") ? fwdataInfo.Name[..^".fwdata".Length] : fwdataInfo.Name;
 
         var hostname = "localhost";
@@ -53,7 +54,7 @@ static class SendReceiveServices
         var fdoDataModelVersion = "7000072"; // TODO: Determine
         var flexBridgeOptions = new Dictionary<string, string>
         {
-            { "fullPathToProject", fwdataInfo.Directory?.FullName ?? "" },
+            { "fullPathToProject", fwdataInfo.Directory.FullName },
             { "fwdataFilename", fwdataInfo.Name },
             { "fdoDataModelVersion", fdoDataModelVersion },
             { "languageDepotRepoName", "LexBox" },
@@ -68,7 +69,8 @@ static class SendReceiveServices
     public static LfMergeBridgeResult CloneProject(string fwdataPath, SendReceiveAuth? auth = null, string? projectCode = null)
     {
         // If projectCode not given, calculate it from the fwdataPath
-        var fwdataInfo = LocateFwDataFile(fwdataPath);
+        var fwdataInfo = new FileInfo(fwdataPath);
+        if (fwdataInfo.Directory is null) throw new ArgumentException("Not allowed to Send/Receive root-level directories like C:\\", nameof(fwdataPath));
         projectCode ??= fwdataInfo.Name.EndsWith(".fwdata") ? fwdataInfo.Name[..^".fwdata".Length] : fwdataInfo.Name;
 
         var hostname = "localhost";
@@ -79,41 +81,12 @@ static class SendReceiveServices
         var fdoDataModelVersion = "7000072"; // TODO: Determine
         var flexBridgeOptions = new Dictionary<string, string>
         {
-            { "fullPathToProject", fwdataInfo.Directory?.FullName ?? "" },
+            { "fullPathToProject", fwdataInfo.Directory.FullName },
             { "fdoDataModelVersion", fdoDataModelVersion },
             { "languageDepotRepoName", "LexBox" },
             { "languageDepotRepoUri", repoUrl.ToString() },
             { "deleteRepoIfNoSuchBranch", "false" },
         };
         return CallLfMergeBridge("Language_Forge_Clone", flexBridgeOptions);
-    }
-
-    // TODO: Simplify, we don't need all this logic since the API is going to know where the fwdata file is
-    private static FileInfo? MaybeLocateFwDataFile(string input)
-    {
-        Console.WriteLine("Locating {0} ...", input);
-        if (Directory.Exists(input)) {
-            var dirInfo = new DirectoryInfo(input);
-            var fname = dirInfo.Name + ".fwdata";
-            Console.WriteLine("Directory exists, creating file {0}", fname);
-            return new FileInfo(Path.Join(input, fname));
-        } else if (File.Exists(input)) {
-            Console.WriteLine("File exists, returning as is");
-            return new FileInfo(input);
-        } else if (File.Exists(input + ".fwdata")) {
-            Console.WriteLine("File exists with .fwdata extension added");
-            return new FileInfo(input + ".fwdata");
-        } else {
-            return null;
-        }
-    }
-
-    private static FileInfo LocateFwDataFile(string input)
-    {
-        var result = MaybeLocateFwDataFile(input);
-        if (result != null) return result;
-        Console.WriteLine("File not found, returning {0} with maybe a .fwdata extension", input);
-        if (input.EndsWith(".fwdata")) return new FileInfo(input);
-        return new FileInfo(input + ".fwdata");
     }
 }
