39de4beca77baf827557fe71e9d90ac6947226ac
Tim Haasdyk <tim_haasdyk@sil.org>
2024-10-31 11:52:06 +0100

    Add more OTEL context to GQL resolver errors

--- backend/LexBoxApi/GraphQL/ErrorLoggingDiagnosticsEventListener.cs
+++ backend/LexBoxApi/GraphQL/ErrorLoggingDiagnosticsEventListener.cs
@@ -7,16 +7,8 @@ using HotChocolate.Resolvers;
 
 namespace LexBoxApi.GraphQL;
 
-public class ErrorLoggingDiagnosticsEventListener : ExecutionDiagnosticEventListener
+public class ErrorLoggingDiagnosticsEventListener(ILogger<ErrorLoggingDiagnosticsEventListener> log) : ExecutionDiagnosticEventListener
 {
-    private readonly ILogger<ErrorLoggingDiagnosticsEventListener> log;
-
-    public ErrorLoggingDiagnosticsEventListener(
-        ILogger<ErrorLoggingDiagnosticsEventListener> log)
-    {
-        this.log = log;
-    }
-
     public override void ResolverError(
         IMiddlewareContext context,
         IError error)
@@ -67,13 +59,17 @@ public class ErrorLoggingDiagnosticsEventListener : ExecutionDiagnosticEventList
 
     public override void ResolverError(IRequestContext context, ISelection selection, IError error)
     {
-        LogError(error);
+        LogError(error, extraTags: [
+            new("graphql.selection.field_name", selection.Field.Name),
+            new("graphql.selection.field_declaring_type", selection.DeclaringType),
+            new("graphql.selection.field_response_name", selection.ResponseName),
+        ]);
     }
 
-    private void LogError(IError error, [CallerMemberName] string source = "")
+    private void LogError(IError error, [CallerMemberName] string source = "", params KeyValuePair<string, object?>[] extraTags)
     {
         log.LogError(error.Exception, "{Source}: {Message}", source, error.Message);
-        TraceError(error, source);
+        TraceError(error, source, extraTags);
     }
 
     private void LogException(Exception exception, [CallerMemberName] string source = "")
@@ -82,35 +78,45 @@ public class ErrorLoggingDiagnosticsEventListener : ExecutionDiagnosticEventList
         TraceException(exception, source);
     }
 
-    private void TraceError(IError error, string source)
+    private void TraceError(IError error, string source, params KeyValuePair<string, object?>[] extraTags)
     {
         if (error.Exception != null)
         {
-            TraceException(error.Exception, source);
+            TraceException(error.Exception, source, extraTags);
         }
         else
         {
-            Activity.Current?.AddEvent(new(source, tags: new()
+            Activity.Current?.AddEvent(new(source, tags: AddTags(new()
             {
                 ["error.message"] = error.Message,
                 ["error.code"] = error.Code,
-            }));
+            }, extraTags)));
         }
     }
 
-    private void TraceException(Exception exception, string source)
+    private void TraceException(Exception exception, string source, params KeyValuePair<string, object?>[] extraTags)
     {
         Activity.Current?
             .SetStatus(ActivityStatusCode.Error)
-            .AddEvent(new(source, tags: new()
+            .AddEvent(new(source, tags: AddTags(new()
             {
                 ["exception.message"] = exception.Message,
                 ["exception.stacktrace"] = exception.StackTrace,
                 ["exception.source"] = exception.Source,
-            }));
+            }, extraTags)));
         if (exception.InnerException != null)
         {
             TraceException(exception.InnerException, $"{source} - Inner");
         }
+    }
+
+    private static ActivityTagsCollection AddTags(ActivityTagsCollection tags, KeyValuePair<string, object?>[] moreTags)
+    {
+        foreach (var kvp in moreTags)
+        {
+            tags.Add(kvp.Key, kvp.Value);
+        }
+
+        return tags;
     }
 }
