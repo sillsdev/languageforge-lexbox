b8391eb43febea4d0aaa4bfd73e39007248b1c7d
Kevin Hahn <kevin_hahn@sil.org>
2024-11-05 16:28:06 +0700

    bring over complex form tests

---
+++ backend/FwLite/FwDataMiniLcmBridge.Tests/MiniLcmTests/ComplexFormComponentTests.cs
@@ -0,0 +1,12 @@
+﻿using FwDataMiniLcmBridge.Tests.Fixtures;
+
+namespace FwDataMiniLcmBridge.Tests.MiniLcmTests;
+
+[Collection(ProjectLoaderFixture.Name)]
+public class ComplexFormComponentTests(ProjectLoaderFixture fixture): ComplexFormComponentTestsBase
+{
+    protected override Task<IMiniLcmApi> NewApi()
+    {
+        return Task.FromResult<IMiniLcmApi>(fixture.NewProjectApi("complex-form-component-test", "en", "en"));
+    }
+}
