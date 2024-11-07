f0f0440eba6ed507ebb8584baf61c5f9cedc9861
Robin Munn <rmunn@pobox.com>
2024-11-07 16:17:05 +0700

    Get sync test passing

--- backend/FwLite/FwDataMiniLcmBridge/Api/FwDataMiniLcmApi.cs
+++ backend/FwLite/FwDataMiniLcmBridge/Api/FwDataMiniLcmApi.cs
@@ -253,6 +253,7 @@ public class FwDataMiniLcmApi(Lazy<LcmCache> cacheLazy, bool onCloseSave, ILogge
             Id = semanticDomain.Guid,
             Name = FromLcmMultiString(semanticDomain.Name),
             Code = semanticDomain.Abbreviation.UiString ?? "",
+            Predefined = true, // TODO: Look up in a GUID list of predefined data
         };
     }
 
