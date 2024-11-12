9a315be5809e0ecb4170213597762112f76c39a0
Kevin Hahn <kevin_hahn@sil.org>
2024-11-12 09:57:27 +0700

    exclude Semantic domain predefined due to it always being true in fwdata

--- backend/FwLite/MiniLcm.Tests/CreateEntryTestsBase.cs
+++ backend/FwLite/MiniLcm.Tests/CreateEntryTestsBase.cs
@@ -26,7 +26,9 @@ public abstract class CreateEntryTestsBase : MiniLcmTestBase
         var createdEntry = await Api.CreateEntry(entry);
         createdEntry.Should().BeEquivalentTo(entry, options => options
             .For(e => e.Components).Exclude(e => e.Id)
-            .For(e => e.ComplexForms).Exclude(e => e.Id));
+            .For(e => e.ComplexForms).Exclude(e => e.Id)
+            //predefined is always true in fwdata bridge, so we need to exclude it for now
+            .For(e => e.Senses).For(s => s.SemanticDomains).Exclude(s => s.Predefined));
     }
 
     [Fact]
