ed2ccb4db4437c6145ab85a1faea9e2741456ad8
Kevin Hahn <kevin_hahn@sil.org>
2024-11-13 10:18:11 +0700

    upgrade packages to latest versions

--- backend/FwLite/FwDataMiniLcmBridge.Tests/Fixtures/MockFwProjectList.cs
+++ backend/FwLite/FwDataMiniLcmBridge.Tests/Fixtures/MockFwProjectList.cs
@@ -7,7 +7,7 @@ public class MockFwProjectList(IOptions<FwDataBridgeConfig> config, MockFwProjec
 {
     public override IEnumerable<IProjectIdentifier> EnumerateProjects()
     {
-        return loader.Projects.Keys.Select(k => new FwDataProject(k, config.Value.ProjectsFolder));
+        return loader.Projects.Keys.Select(k => new FwDataProject(k, _config.Value.ProjectsFolder));
     }
 
     public override FwDataProject? GetProject(string name)
