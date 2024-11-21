b8dade440b215b5b6e960628dc5cacd607beecd2
Tim Haasdyk <tim_haasdyk@sil.org>
2024-11-21 13:12:01 +0100

    Make logout endpoint unauthenticated This prevents us using /logout as a returnTo URL

--- frontend/src/routes/(authenticated)/logout/+server.ts
+++
@@ -1,9 +0,0 @@
-import type {RequestEvent} from './$types'
-import { logout } from '$lib/user'
-import { redirect } from '@sveltejs/kit'
-
-export function GET({cookies}: RequestEvent) : void {
-  logout(cookies)
-
-  redirect(303, '/login');
-}
