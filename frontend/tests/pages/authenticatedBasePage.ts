e70a0f2641f4a586ad8feb7c62d4c1c90b3e4b21
Tim Haasdyk <tim_haasdyk@sil.org>
2024-11-21 11:38:15 +0100

    Add failing test

--- frontend/tests/pages/authenticatedBasePage.ts
+++ frontend/tests/pages/authenticatedBasePage.ts
@@ -1,21 +1,32 @@
-import type { Locator, Page } from '@playwright/test';
-import { BasePage } from './basePage';
-import { EmailVerificationAlert } from '../components/emailVerificationAlert';
+import type {Locator, Page} from '@playwright/test';
+
+import {AuthenticatedDrawer} from '../components/authenticatedDrawer';
+import {BasePage} from './basePage';
+import {EmailVerificationAlert} from '../components/emailVerificationAlert';
 
 export class AuthenticatedBasePage extends BasePage {
   readonly emailVerificationAlert: EmailVerificationAlert;
 
+  private drawerToggle: Locator;
+
   constructor(page: Page, locator: Locator | Locator[], url?: string) {
+    const drawerToggle = page.locator('label .i-mdi-account-circle');
     if (Array.isArray(locator)) {
-      locator = [page.locator('label .i-mdi-account-circle'), ...locator];
+      locator = [drawerToggle, ...locator];
     } else {
-      locator = [page.locator('label .i-mdi-account-circle'), locator];
+      locator = [drawerToggle, locator];
     }
     super(page, locator, url);
+    this.drawerToggle = drawerToggle;
     this.emailVerificationAlert = new EmailVerificationAlert(page);
   }
 
   clickHome(): Promise<void> {
     return this.page.locator('.breadcrumbs').getByRole('link', {name: 'Home'}).click();
+  }
+
+  async openDrawer(): Promise<AuthenticatedDrawer> {
+    await this.drawerToggle.click();
+    return new AuthenticatedDrawer(this.page).waitFor();
   }
 }
