e89a5cee71ed638f53d80fd4c10fb3f0783bc152
Tim Haasdyk <tim_haasdyk@sil.org>
2024-11-25 10:46:59 +0100

    Skip back button test in Firefox

--- frontend/tests/logout.test.ts
+++ frontend/tests/logout.test.ts
@@ -2,7 +2,8 @@ import {AdminDashboardPage} from './pages/adminDashboardPage';
 import {loginAs} from './utils/authHelpers';
 import {test} from './fixtures';
 
-test('Back button after logout redirects back to login page', async ({page}) => {
+test('Back button after logout redirects back to login page', async ({page, browserName}) => {
+  test.skip(browserName === 'firefox', 'Support for Clear-Site-Data: "cache" was removed and is WIP (https://bugzilla.mozilla.org/show_bug.cgi?id=1838506)');
   await loginAs(page.request, 'admin');
   const adminPage = await new AdminDashboardPage(page).goto();
   const drawer = await adminPage.openDrawer();
