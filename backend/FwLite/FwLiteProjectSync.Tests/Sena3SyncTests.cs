10b0994beeae9e27847b3c4a6b8c4b270a498bbb
Kevin Hahn <kevin_hahn@sil.org>
2024-11-26 16:35:29 +0700

    improve how entry assertions are executed to make it simpler to find the data causing the problem

--- backend/FwLite/FwLiteProjectSync.Tests/Sena3SyncTests.cs
+++ backend/FwLite/FwLiteProjectSync.Tests/Sena3SyncTests.cs
@@ -1,4 +1,5 @@
 ﻿using FluentAssertions.Equivalency;
+using FluentAssertions.Execution;
 using FwLiteProjectSync.Tests.Fixtures;
 using LcmCrdt;
 using Microsoft.EntityFrameworkCore;
@@ -35,11 +36,20 @@ public class Sena3SyncTests : IClassFixture<Sena3Fixture>, IAsyncLifetime
         var fwdataApi = _fixture.FwDataApi;
         await _syncService.Sync(crdtApi, fwdataApi);
 
-        var crdtEntries = await crdtApi.GetEntries().ToArrayAsync();
-        var fwdataEntries = await fwdataApi.GetEntries().ToArrayAsync();
-        crdtEntries.Should().BeEquivalentTo(fwdataEntries,
-            options => options.For(e => e.Components).Exclude(c => c.Id)
-                              .For(e => e.ComplexForms).Exclude(c => c.Id));
+        var crdtEntries = await crdtApi.GetEntries().ToDictionaryAsync(e => e.Id);
+        var fwdataEntries = await fwdataApi.GetEntries().ToDictionaryAsync(e => e.Id);
+        crdtEntries.Keys.Should().BeEquivalentTo(fwdataEntries.Keys);
+        using (new AssertionScope())
+        {
+            foreach (var crdtEntry in crdtEntries.Values)
+            {
+                var fwdataEntry = fwdataEntries[crdtEntry.Id];
+                crdtEntry.Should().BeEquivalentTo(fwdataEntry,
+                    options => options
+                        .For(e => e.Components).Exclude(c => c.Id)
+                        .For(e => e.ComplexForms).Exclude(c => c.Id), $"entry {crdtEntry.Id} should include fw data");
+            }
+        }
     }
 
     [Fact]
