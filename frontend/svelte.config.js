11bc58c164652f71ef95415cd8232b46a5f7d73a
Robin Munn <rmunn@pobox.com>
2024-11-26 13:47:03 +0700

    Stop Svelte warning about self-closing HTML tags
    We'll fix the self-closing tags when we migrate to Svelte 5; in the
    meantime, we don't want these warnings to flood the compilation results
    and end up hiding other, more important, warnings.

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -4,6 +4,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   compilerOptions: {
+    warningFilter: (warning) => warning.code != 'element_invalid_self_closing_tag',
     enableSourcemap: true
   },
   kit: {
