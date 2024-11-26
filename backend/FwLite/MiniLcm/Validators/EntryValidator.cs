42d1ea6c8e4bf1d7a0aa89ecfcb59d916a784ae6
Kevin Hahn <kevin_hahn@sil.org>
2024-11-26 14:56:06 +0700

    add basic entry validator as an example of validating dependent properties like EntryId on sense

---
+++ backend/FwLite/MiniLcm/Validators/EntryValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using MiniLcm.Models;
+
+namespace MiniLcm.Validators;
+
+public class EntryValidator : AbstractValidator<Entry>
+{
+    public EntryValidator()
+    {
+        RuleForEach(e => e.Senses).SetValidator(entry => new SenseValidator(entry));
+        //todo just a stub as an example for senses
+    }
+}
---
+++ backend/FwLite/MiniLcm/Validators/SenseValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using MiniLcm.Models;
+
+namespace MiniLcm.Validators;
+
+public class SenseValidator : AbstractValidator<Sense>
+{
+    public SenseValidator()
+    {
+        //todo add validation for the other properties
+    }
+
+    public SenseValidator(Entry entry): this()
+    {
+        //it's ok if senses EntryId is an Empty guid
+        RuleFor(s => s.EntryId).Equal(entry.Id).When(s => s.EntryId != Guid.Empty);
+    }
+}
