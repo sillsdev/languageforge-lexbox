e00fcfc5b115fd5ceba1660b91c79c6c567cae9e
Kevin Hahn <kevin_hahn@sil.org>
2024-11-28 12:21:17 +0700

    use HgRunner instead of trying to locate mercurial manually

--- backend/FwLite/FwLiteProjectSync.Tests/Fixtures/MercurialTestHelper.cs
+++ backend/FwLite/FwLiteProjectSync.Tests/Fixtures/MercurialTestHelper.cs
@@ -1,3 +1,4 @@
+using Chorus.VcsDrivers.Mercurial;
 using SIL.CommandLineProcessing;
 using SIL.PlatformUtilities;
 using SIL.Progress;
@@ -6,12 +7,11 @@ namespace FwLiteProjectSync.Tests.Fixtures;
 
 public static class MercurialTestHelper
 {
-    public static string HgCommand =>
-        Path.Combine("Mercurial", Platform.IsWindows ? "hg.exe" : "hg");
+    private static readonly NullProgress NullProgress = new NullProgress();
 
     private static string RunHgCommand(string repoPath, string args)
     {
-        var result = CommandLineRunner.Run(HgCommand, args, repoPath, 120, new NullProgress());
+        var result = HgRunner.Run(args, repoPath, 120, NullProgress);
         if (result.ExitCode == 0) return result.StandardOutput;
         throw new Exception(
             $"hg {args} failed.\nStdOut: {result.StandardOutput}\nStdErr: {result.StandardError}");
