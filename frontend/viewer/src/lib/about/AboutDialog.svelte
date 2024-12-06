fd453fbf231158d0eb1b55bcac348ca468fe8014
Robin Munn <rmunn@pobox.com>
2024-12-06 10:25:03 +0700

    Add custom component for markdown with new-tab links
    That way instead of having to do a Typescript cast every time we want to
    render Markdown that opens links in a new tab, we can do the cast once
    and just call a different component. We've done this already for the
    lexbox frontend code in src/, so we'll do the same thing for the viewer.

--- frontend/viewer/src/lib/about/AboutDialog.svelte
+++ frontend/viewer/src/lib/about/AboutDialog.svelte
@@ -2,9 +2,8 @@
   import { mdiInformationVariantCircle } from '@mdi/js';
   import { Button, Dialog, Toggle } from 'svelte-ux';
   import type { Readable } from 'svelte/store';
-  import Markdown from 'svelte-exmarkdown';
-  import NewTabLinkRenderer from './NewTabLinkRenderer.svelte';
-  import { onMount, type Component } from 'svelte';
+  import NewTabLinkMarkdown from '../markdown/NewTabLinKMarkdown.svelte';
+  import { onMount } from 'svelte';
 
   export let text: Readable<string>;
 
@@ -26,7 +25,7 @@
   </Button>
   <Dialog {open} on:close={toggleOff} class="w-[700px]">
     <div class="m-6 prose">
-      <Markdown md={$text} plugins={[{ renderer: { a: NewTabLinkRenderer as unknown as Component } }]} />
+      <NewTabLinkMarkdown md={$text} />
     </div>
     <div class="flex-grow"></div>
     <div slot="actions">
