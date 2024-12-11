b178fd01240a9a49c993885a3b14946592ff9a80
Kevin Hahn <kevin_hahn@sil.org>
2024-12-11 10:46:40 +0700

    use faker to generate changes

--- backend/FwLite/LcmCrdt.Tests/Changes/ChangeSerializationTests.cs
+++ backend/FwLite/LcmCrdt.Tests/Changes/ChangeSerializationTests.cs
@@ -1,82 +1,54 @@
-﻿using System.Text.Json;
+﻿using System.Reflection;
+using System.Text.Json;
 using FluentAssertions.Execution;
 using LcmCrdt.Changes;
 using LcmCrdt.Changes.Entries;
+using MiniLcm.Tests.AutoFakerHelpers;
 using SIL.Harmony.Changes;
+using Soenneker.Utils.AutoBogus;
 using SystemTextJsonPatch;
 
 namespace LcmCrdt.Tests.Changes;
 
 public class ChangeSerializationTests
 {
+    private static readonly AutoFaker Faker = new()
+    {
+        Config =
+        {
+            Overrides = [new WritingSystemIdOverride()]
+        }
+    };
+
     public static IEnumerable<object[]> Changes()
     {
-        yield return
-        [
-            new AddEntryComponentChange(ComplexFormComponent.FromEntries(
-                new Entry() { Id = Guid.NewGuid() },
-                new Entry() { Id = Guid.NewGuid() }))
-        ];
-        yield return
-        [
-            new CreateEntryChange(new Entry() { Id = Guid.NewGuid() })
-        ];
-        yield return [
-            new CreateSenseChange(new Sense() { Id = Guid.NewGuid() }, Guid.NewGuid())
-        ];
-        yield return [
-            new CreateExampleSentenceChange(new ExampleSentence() { Id = Guid.NewGuid() }, Guid.NewGuid())
-        ];
-        yield return
-        [
-            new CreatePartOfSpeechChange(Guid.NewGuid(), new MultiString())
-        ];
-        yield return
-        [
-            new CreateSemanticDomainChange(Guid.NewGuid(), new MultiString(), "1")
-        ];
-        yield return
-        [
-            new CreateWritingSystemChange(new WritingSystem()
+        foreach (var type in LcmCrdtKernel.AllChangeTypes())
+        {
+            //can't generate this type because there's no public constructor, so its specified below
+            if (type == typeof(SetComplexFormComponentChange)) continue;
+
+            object change;
+            if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(JsonPatchChange<>))
             {
-                Id = Guid.NewGuid(),
-                WsId = "en",
-                Name = "en",
-                Abbreviation = "en",
-                Font = "Arial",
-                Exemplars = ["en"],
-                Type = WritingSystemType.Analysis,
-                Order = 1,
-            }, WritingSystemType.Analysis, Guid.NewGuid(), 1)
-        ];
-        yield return [
-            new SetPartOfSpeechChange(Guid.NewGuid(), Guid.NewGuid())
-        ];
-        yield return [new AddSemanticDomainChange(new SemanticDomain() { Id = Guid.NewGuid() }, Guid.NewGuid())];
-        yield return [new RemoveSemanticDomainChange(Guid.NewGuid(), Guid.NewGuid())];
-        yield return [new ReplaceSemanticDomainChange(Guid.NewGuid(), new SemanticDomain() { Id = Guid.NewGuid() }, Guid.NewGuid())];
-        yield return [new RemoveComplexFormTypeChange(Guid.NewGuid(), Guid.NewGuid())];
-        yield return [new CreateComplexFormType(Guid.NewGuid(), new MultiString())];
-        yield return [new AddComplexFormTypeChange(Guid.NewGuid(), new() { Id = Guid.NewGuid(), Name = new()})];
+                change = PatchMethod.MakeGenericMethod(type.GenericTypeArguments[0]).Invoke(null, null)!;
+            }
+            else
+            {
+                change = Faker.Generate(type);
+            }
+            change.Should().NotBeNull($"change type {type.Name} should have been generated");
+            yield return [change];
+        }
         yield return [SetComplexFormComponentChange.NewComplexForm(Guid.NewGuid(), Guid.NewGuid())];
         yield return [SetComplexFormComponentChange.NewComponent(Guid.NewGuid(), Guid.NewGuid())];
         yield return [SetComplexFormComponentChange.NewComponentSense(Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid())];
+    }
 
-        yield return [new JsonPatchChange<Entry>(Guid.NewGuid(), new JsonPatchDocument<Entry>())];
-        yield return [new JsonPatchChange<Sense>(Guid.NewGuid(), new JsonPatchDocument<Sense>())];
-        yield return [new JsonPatchChange<ExampleSentence>(Guid.NewGuid(), new JsonPatchDocument<ExampleSentence>())];
-        yield return [new JsonPatchChange<WritingSystem>(Guid.NewGuid(), new JsonPatchDocument<WritingSystem>())];
-        yield return [new JsonPatchChange<PartOfSpeech>(Guid.NewGuid(), new JsonPatchDocument<PartOfSpeech>())];
-        yield return [new JsonPatchChange<SemanticDomain>(Guid.NewGuid(), new JsonPatchDocument<SemanticDomain>())];
-        yield return [new JsonPatchChange<ComplexFormType>(Guid.NewGuid(), new JsonPatchDocument<ComplexFormType>())];
-        yield return [new DeleteChange<Entry>(Guid.NewGuid())];
-        yield return [new DeleteChange<Sense>(Guid.NewGuid())];
-        yield return [new DeleteChange<ExampleSentence>(Guid.NewGuid())];
-        yield return [new DeleteChange<WritingSystem>(Guid.NewGuid())];
-        yield return [new DeleteChange<PartOfSpeech>(Guid.NewGuid())];
-        yield return [new DeleteChange<SemanticDomain>(Guid.NewGuid())];
-        yield return [new DeleteChange<ComplexFormType>(Guid.NewGuid())];
-        yield return [new DeleteChange<ComplexFormComponent>(Guid.NewGuid())];
+    private static readonly MethodInfo PatchMethod = new Func<IChange>(Patch<Entry>).Method.GetGenericMethodDefinition();
+
+    private static IChange Patch<T>() where T : class
+    {
+        return new JsonPatchChange<T>(Guid.NewGuid(), new JsonPatchDocument<T>());
     }
 
     [Theory]
@@ -85,6 +57,8 @@ public class ChangeSerializationTests
     {
         var config = new CrdtConfig();
         LcmCrdtKernel.ConfigureCrdt(config);
+        //commit id is not serialized
+        change.CommitId = Guid.Empty;
         var type = change.GetType();
         var json = JsonSerializer.Serialize(change, config.JsonSerializerOptions);
         var newChange = JsonSerializer.Deserialize(json, type, config.JsonSerializerOptions);
