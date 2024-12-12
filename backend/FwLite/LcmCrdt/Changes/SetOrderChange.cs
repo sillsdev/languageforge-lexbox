eabc7a7efb2820d8036ffa069979fadad7f5ef62
Tim Haasdyk <tim_haasdyk@sil.org>
2024-12-12 15:57:52 +0100

    Fix SetOrderChange type discriminator(s)

--- backend/FwLite/LcmCrdt/Changes/SetOrderChange.cs
+++ backend/FwLite/LcmCrdt/Changes/SetOrderChange.cs
@@ -3,9 +3,11 @@ using SIL.Harmony.Entities;
 
 namespace LcmCrdt.Changes;
 
-public class SetOrderChange<T>(Guid entityId, double order) : EditChange<T>(entityId), ISelfNamedType<SetOrderChange<T>>
+public class SetOrderChange<T>(Guid entityId, double order) : EditChange<T>(entityId), IPolyType
     where T : class, IOrderable
 {
+    public static string TypeName => $"{nameof(SetOrderChange<T>)}:" + typeof(T).Name;
+
     public double Order { get; init; } = order;
 
     public override ValueTask ApplyChange(T entity, ChangeContext context)
