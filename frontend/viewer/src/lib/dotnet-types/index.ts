4288963c1ee2ef078069c39a3fa70c413c690492
Kevin Hahn <kevin_hahn@sil.org>
2024-12-17 17:08:00 +0700

    remove deleted generated file from index

--- frontend/viewer/src/lib/dotnet-types/index.ts
+++ frontend/viewer/src/lib/dotnet-types/index.ts
@@ -4,5 +4,4 @@ export * from './generated-types/FwLiteShared/Auth/IServerStatus';
 export * from './generated-types/FwLiteShared/Projects/ICombinedProjectsService';
 export * from './generated-types/FwLiteShared/Projects/IProjectModel';
 export * from './generated-types/FwLiteShared/Projects/IServerProjects';
-export * from './generated-types/FwLiteShared/Services/IFwLiteProvider';
 export * from './generated-types/FwLiteShared/Services/DotnetService';
