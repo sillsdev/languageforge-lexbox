62e07a2b0dd820d37230c2a8790923a83aa593bd
Tim Haasdyk <tim_haasdyk@sil.org>
2024-12-20 11:02:49 +0100

    Try to dispose service provider on shutdown

--- backend/FwLite/FwLiteDesktop/MauiProgram.cs
+++ backend/FwLite/FwLiteDesktop/MauiProgram.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.Logging;
+using Microsoft.Maui.LifecycleEvents;
 
 namespace FwLiteDesktop;
 
@@ -7,11 +8,35 @@ public static class MauiProgram
     private record AppHolder(MauiApp? App)
     {
         public MauiApp? App { get; set; } = App;
+
+        public void Shutdown()
+        {
+            var thread = new Thread(() =>
+            {
+                if (App == null) return;
+
+                var logger = App.Services.GetRequiredService<ILogger<MauiApp>>();
+                try
+                {
+                    logger.LogInformation("Disposing app");
+                    App.DisposeAsync().GetAwaiter().GetResult();
+                }
+                catch (Exception e)
+                {
+                    logger.LogError(e, "Failed to dispose app");
+                    throw;
+                }
+            })
+            {
+                IsBackground = false
+            };
+            thread.Start();
+        }
     }
 
     public static MauiApp CreateMauiApp()
     {
-        // AppHolder holder = new AppHolder(null);
+        AppHolder holder = new AppHolder(null);
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
@@ -26,7 +51,24 @@ public static class MauiProgram
         });
         builder.Services.AddFwLiteDesktopServices(builder.Configuration, builder.Logging);
 
+        builder.ConfigureLifecycleEvents(events =>
+        {
+#if ANDROID
+            events.AddAndroid(android => android
+                .OnDestroy((activity) => holder.Shutdown()));
+#endif
+#if IOS || MACCATALYST
+            events.AddiOS(ios => ios
+                .WillTerminate((app) => holder.Shutdown()));
+#endif
+#if WINDOWS
+            events.AddWindows(windows => windows
+                .OnClosed((window, args) => holder.Shutdown()));
+#endif
+        });
+
         var app = builder.Build();
+        holder.App = app;
         var logger = app.Services.GetRequiredService<ILogger<MauiApp>>();
         logger.LogInformation("App started");
         AppDomain.CurrentDomain.UnhandledException += (sender, e) =>
