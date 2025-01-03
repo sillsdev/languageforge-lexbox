f5ed6edcfe3dc322ed6fdc55c8acf81089191573
Kevin Hahn <kevin_hahn@sil.org>
2025-01-03 15:34:14 +0700

    create a delete dialog in the project root to reuse across components

---
+++ frontend/viewer/src/lib/entry-editor/DeleteDialog.svelte
@@ -0,0 +1,44 @@
+﻿<script lang="ts">
+  import {Button, Dialog} from 'svelte-ux';
+  let subject: string;
+  let open = false;
+  let loading = false;
+  let requester: {
+    resolve: (result: boolean) => void
+  } | undefined = undefined;
+  function onClose() {
+    requester?.resolve(false);
+    requester = undefined;
+  }
+
+  function confirm() {
+    requester?.resolve(true);
+    requester = undefined;
+    open = false;
+  }
+
+  function cancel() {
+    requester?.resolve(false);
+    requester = undefined;
+    open = false;
+  }
+
+  export function prompt(promptSubject: string): Promise<boolean> {
+    if (requester) throw new Error('already prompting for a delete');
+    return new Promise((resolve) => {
+      requester = { resolve };
+      subject = promptSubject;
+      open = true;
+    });
+  }
+</script>
+<Dialog {open} on:close={onClose} {loading} persistent={loading}>
+  <div slot="title">Delete {subject}</div>
+  <div class="m-6">
+    <p>Are you sure you want to delete {subject}?</p>
+  </div>
+  <div slot="actions">
+    <Button on:click={() => cancel()}>Don't delete</Button>
+    <Button variant="fill-light" color="success" on:click={_ => confirm()}>Delete {subject}</Button>
+  </div>
+</Dialog>
---
+++ frontend/viewer/src/lib/entry-editor/dialog-service.ts
@@ -0,0 +1,28 @@
+﻿import type DeleteDialog from '$lib/entry-editor/DeleteDialog.svelte';
+import {getContext, setContext} from 'svelte';
+
+export function initDialogService(rootDialog: () => DeleteDialog | undefined): DialogService {
+  const dialogService = new DialogService(rootDialog);
+  setContext('DialogService', dialogService);
+  return dialogService;
+}
+
+export function useDialogService(): DialogService {
+  const rootDialog = getContext('DialogService');
+  if (!rootDialog) {
+    throw new Error('DialogService not found');
+  }
+  return rootDialog as DialogService;
+}
+
+export class DialogService {
+
+  constructor(private deleteDialog: () => DeleteDialog | undefined) {
+  }
+
+  promptDelete(subject: string): Promise<boolean> {
+    const deleteDialog = this.deleteDialog();
+    if (!deleteDialog) throw new Error('no deleted dialog found');
+    return deleteDialog.prompt(subject);
+  }
+}
