ec86c190538c0140cbd0330dcd5cff69d8e1dc81
Kevin Hahn <kevin_hahn@sil.org>
2025-01-14 10:12:39 +0700

    workaround autofocus bug in svelte-ux

--- frontend/viewer/src/lib/entry-editor/inputs/CrdtTextField.svelte
+++ frontend/viewer/src/lib/entry-editor/inputs/CrdtTextField.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import type { ComponentProps } from 'svelte';
   import CrdtField from './CrdtField.svelte';
-  import { TextField } from 'svelte-ux';
+  import { TextField, autoFocus as autoFocusFunc } from 'svelte-ux';
 
   export let value: string | number | null | undefined;
   export let unsavedChanges = false;
@@ -18,15 +18,18 @@
     if (label && labelElem) labelElem.setAttribute('title', label);
   }
 </script>
-
 <CrdtField on:change bind:value bind:unsavedChanges let:editorValue let:save let:onEditorValueChange viewMergeButtonPortal={append}>
   <TextField
     on:change={(e) => onEditorValueChange(e.detail.inputValue)}
     on:blur={(e) => {if (e.target) save()}}
-    actions={(el) => { addTitleToLabel(el); return []; }}
+    actions={(el) => {
+      addTitleToLabel(el);
+      // autofocus requires a delay otherwise it won't work in a dialog
+      // also we can't use the autofocus attribute because of https://github.com/techniq/svelte-ux/issues/532
+      return autofocus ? [autoFocusFunc(el, {delay: 5})] : [];
+    }}
     value={editorValue}
     disabled={readonly}
-    autofocus={autofocus}
     class="ws-field gap-2 text-right"
     classes={{ root: `${editorValue ? '' : 'empty'} ${readonly ? 'readonly' : ''}`, input: 'field-input', container: 'field-container' }}
     {label}
