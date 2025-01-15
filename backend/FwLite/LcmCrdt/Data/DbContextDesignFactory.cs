b06d58333ddfb74ea1eaddbc1021736435ab0170
Kevin Hahn <kevin_hahn@sil.org>
2025-01-15 16:24:05 +0700

    switch to using migrations with LcmCrdt, auto migrate all projects on startup

---
+++ backend/FwLite/LcmCrdt/Data/DbContextDesignFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LcmCrdt.Data;
+
+[Obsolete("this class is only here to facilitate migrations, it's not used in the application", true)]
+public class DbContextDesignFactory: IDesignTimeDbContextFactory<LcmCrdtDbContext>
+{
+    public LcmCrdtDbContext CreateDbContext(string[] args)
+    {
+        var servicesRoot = new ServiceCollection()
+            .AddSingleton<IConfiguration>(new ConfigurationRoot([]))
+            .AddLcmCrdtClient()
+            .BuildServiceProvider();
+        var scope = servicesRoot.CreateScope();
+        scope.ServiceProvider.GetRequiredService<CurrentProjectService>().SetupProjectContextForNewDb(new ("DesignDb", "design.sqlite"));
+        return scope.ServiceProvider.GetRequiredService<LcmCrdtDbContext>();
+    }
+}
