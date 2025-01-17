8d4cdd56cd57d8fcfae426c29174b5a200f6863d
Kevin Hahn <kevin_hahn@sil.org>
2025-01-17 14:11:50 +0700

    fix Toc links taking you home

--- frontend/viewer/src/lib/layout/Toc.svelte
+++ frontend/viewer/src/lib/layout/Toc.svelte
@@ -11,13 +11,13 @@
 {#if entry}
   {@const _headword = headword(entry) ?? ''}
   <div class="border flex flex-col rounded-md overflow-auto">
-    <a class="toc-item px-2" href="#entry" title={_headword}><span class="opacity-80 mr-1">{fieldName({id: 'entry'}, $currentView.i18nKey)}:</span>{_headword || '—'}</a>
+    <a class="toc-item px-2" href="{location}/#entry" title={_headword}><span class="opacity-80 mr-1">{fieldName({id: 'entry'}, $currentView.i18nKey)}:</span>{_headword || '—'}</a>
     {#each entry.senses as sense, i (sense.id)}
       {@const _sense = firstDefOrGlossVal(sense) ?? ''}
-      <a class="toc-item px-2 pl-4" href="#sense{i + 1}" title={_sense}><span class="opacity-80 mr-1">{fieldName({id: 'sense'}, $currentView.i18nKey)}:</span>{_sense || '—'}</a>
+      <a class="toc-item px-2 pl-4" href="{location}/#sense{i + 1}" title={_sense}><span class="opacity-80 mr-1">{fieldName({id: 'sense'}, $currentView.i18nKey)}:</span>{_sense || '—'}</a>
       {#each sense.exampleSentences as example, j (example.id)}
         {@const _example = firstSentenceOrTranslationVal(example) ?? ''}
-        <a class="toc-item px-2 pl-6 text-sm" href="#example{i + 1}-{j + 1}" title={_example}><span class="opacity-80 mr-1">Example:</span>{_example || '—'}</a>
+        <a class="toc-item px-2 pl-6 text-sm" href="{location}/#example{i + 1}-{j + 1}" title={_example}><span class="opacity-80 mr-1">Example:</span>{_example || '—'}</a>
       {/each}
     {/each}
   </div>
