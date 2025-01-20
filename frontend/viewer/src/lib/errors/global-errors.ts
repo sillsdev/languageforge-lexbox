ac11183bcbf295a55fd5bb0f45851ae0a52850d5
Tim Haasdyk <tim_haasdyk@sil.org>
2025-01-20 12:06:49 +0100

    Supress 'Perhaps the DotNetObjectReference instance was already disposed' error notifications

--- frontend/viewer/src/lib/errors/global-errors.ts
+++ frontend/viewer/src/lib/errors/global-errors.ts
@@ -1,11 +1,31 @@
 import {AppNotification} from '$lib/notifications/notifications';
 
+function getPromiseRejectionMessage(event: PromiseRejectionEvent): string {
+  if (event.reason instanceof Error) {
+    return event.reason.message;
+  } else if (typeof event.reason === 'string') {
+    return event.reason;
+  } else {
+    return 'Unknown error';
+  }
+}
+
+function suppressPromiseRejectionNotification(message: string): boolean {
+  if (message.includes('Perhaps the DotNetObjectReference instance was already disposed')) return true;
+  return false;
+}
+
 export function setupGlobalErrorHandlers() {
   window.addEventListener('error', (event: ErrorEvent) => {
+    console.error('Global error', event);
     AppNotification.display(event.message, 'error', undefined);
   });
 
   window.addEventListener('unhandledrejection', (event: PromiseRejectionEvent) => {
-    AppNotification.display(event.reason as string, 'error', undefined);
+    const message = getPromiseRejectionMessage(event);
+    console.error('Global unhandled rejection', message, event);
+
+    if (suppressPromiseRejectionNotification(message)) return;
+    AppNotification.display(message, 'error', undefined);
   });
 }
