50afd44ff75ac9b6a720deb47d3b143d07b2e39f
Kevin Hahn <kevin_hahn@sil.org>
2025-01-28 14:59:34 +0700

    fix json deserialize errors (#1420)
    * write deserialization tests to ensure we can always deserialize changes, even after they are modified
    
    * add snapshot regression testing
    
    * add a custom converter to Sense.PoS we could probably just drop this, but it's a good example of working around a problem, so we can leave it for now.

---
+++ backend/FwLite/LcmCrdt.Tests/Data/SnapshotDeserializationTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text.Json;
+using SIL.Harmony.Entities;
+
+namespace LcmCrdt.Tests.Data;
+
+public class SnapshotDeserializationTests
+{
+
+    private static readonly Lazy<JsonSerializerOptions> LazyOptions = new(() =>
+    {
+        var config = new CrdtConfig();
+        LcmCrdtKernel.ConfigureCrdt(config);
+        config.JsonSerializerOptions.ReadCommentHandling = JsonCommentHandling.Skip;
+        return config.JsonSerializerOptions;
+    });
+
+    private static readonly JsonSerializerOptions Options = LazyOptions.Value;
+    [Fact]
+    public void CanDeserializeRegressionData()
+    {
+        //this file represents projects which already have changes applied, we want to ensure that we don't break anything.
+        //nothing should ever be removed from this file
+        //if a new property is added then a new json object should be added with that property
+        using var jsonFile = File.OpenRead(GetJsonFilePath("RegressionDeserializationData.json"));
+        var changes = JsonSerializer.Deserialize<List<IObjectBase>>(jsonFile, Options);
+        changes.Should().NotBeNullOrEmpty().And.NotContainNulls();
+    }
+
+    private static string GetJsonFilePath(string name, [CallerFilePath] string sourceFile = "")
+    {
+        return Path.Combine(
+            Path.GetDirectoryName(sourceFile) ??
+            throw new InvalidOperationException("Could not get directory of source file"),
+            name);
+    }
+}
