6457bcf06361ecf69d4fa8602284b64ee4da81d5
Tim Haasdyk <tim_haasdyk@sil.org>
2025-02-03 11:49:31 +0100

    Add failing test for 'UNIQUE constraint failed: PartOfSpeech.Id'

---
+++ backend/FwLite/LcmCrdt.Tests/Changes/SenseChangeTests.cs
@@ -0,0 +1,28 @@
+﻿using SIL.Harmony.Changes;
+using LcmCrdt.Changes;
+
+namespace LcmCrdt.Tests.Changes;
+
+public class SenseChangeTests(MiniLcmApiFixture fixture) : IClassFixture<MiniLcmApiFixture>
+{
+    [Fact]
+    public async Task AddSenseAndUpdatePartOfSpeechInOneCommit()
+    {
+        // arrange
+        var entry = await fixture.Api.CreateEntry(new() { LexemeForm = { { "en", "test entry" } }, });
+        var partOfSpeech = await fixture.Api.CreatePartOfSpeech(new() { Name = new() { { "en", "test pos" } } });
+        var sense = new Sense() { Id = Guid.NewGuid(), Gloss = new() { { "en", "test sense" } } };
+
+        var createSenseChange = new CreateSenseChange(sense, entry.Id);
+        var setPartOfSpeechChange = new SetPartOfSpeechChange(sense.Id, partOfSpeech.Id);
+        List<IChange> changes = [createSenseChange, setPartOfSpeechChange];
+
+        // act
+        await fixture.DataModel.AddChanges(Guid.NewGuid(), changes);
+
+        // assert
+        var actualSense = await fixture.Api.GetSense(entry.Id, sense.Id);
+        actualSense.Should().NotBeNull();
+        actualSense.PartOfSpeechId.Should().Be(partOfSpeech.Id);
+    }
+}
