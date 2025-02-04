545863c2c706555a98911c3c8b3c636580346dfa
Kevin Hahn <kevin_hahn@sil.org>
2025-02-04 12:46:34 +0700

    move server into it's own component outside the server list

--- frontend/viewer/src/home/ServersList.svelte
+++ frontend/viewer/src/home/ServersList.svelte
@@ -12,6 +12,8 @@
   import {type Project} from '$lib/services/projects-service';
   import {onMount} from 'svelte';
   import {useAuthService, useProjectsService} from '$lib/services/service-provider';
+  import Server from './Server.svelte';
+
   export let localProjects: Project[];
   export let refreshProjects: () => Promise<void>;
 
@@ -62,84 +64,27 @@
 
   async function refreshProjectsAndServers() {
     await fetchRemoteProjects();
-    serversStatus = await authService.servers();
+    serversPromise = authService.servers();
+    await serversPromise;
   }
 
 
-  let serversStatus: IServerStatus[] = [];
-  onMount(async () => {
-    serversStatus = await authService.servers();
-  });
+  let serversPromise = authService.servers();
 
-  function matchesProject(projects: Project[], project: Project): Project | undefined {
-    if (project.id) {
-      return projects.find(p => p.id == project.id && p.server?.id == project.server?.id);
-    }
-    return undefined;
-  }
 </script>
 
-{#each serversStatus as status}
-  {@const server = status.server}
-  {@const serverProjects = remoteProjects[server.id]?.filter(p => p.crdt) ?? []}
-  <div>
-    <div class="flex flex-row mb-2 items-end mr-2 md:mr-0">
-      <p class="sub-title !my-0">
-        {server.displayName} Server
-      </p>
-      <div class="flex-grow"></div>
-      {#if status.loggedIn}
-        <Button icon={mdiRefresh}
-                title="Refresh Projects"
-                disabled={loadingServerProjects === server.id}
-                on:click={() => refreshServerProjects(server, true)}/>
-        <LoginButton {status} on:status={() => refreshProjectsAndServers()}/>
-      {/if}
-    </div>
-    <div>
-      {#if !serverProjects.length}
-        <p class="text-surface-content/50 text-center elevation-1 md:rounded p-4">
-          {#if status.loggedIn}
-            No projects
-          {:else}
-            <LoginButton {status} on:status={() => refreshProjectsAndServers()}/>
-          {/if}
-        </p>
-      {/if}
-      {#if loadingServerProjects === server.id}
-        <p class="text-surface-content/50 text-center elevation-1 md:rounded p-4">
-          <Icon data={mdiRefresh} class="animate-spin"/>
-          Loading...
-        </p>
-      {:else}
-        {#each serverProjects as project}
-          {@const localProject = matchesProject(localProjects, project)}
-          {#if localProject?.crdt}
-            <AnchorListItem href={`/project/${project.name}`}>
-              <ListItem icon={mdiCloud}
-                        title={project.name}
-                        loading={downloading === project.name}>
-                <div slot="actions" class="pointer-events-none">
-                  <Button disabled icon={mdiBookSyncOutline} class="p-2">
-                    Synced
-                  </Button>
-                </div>
-              </ListItem>
-            </AnchorListItem>
-          {:else}
-            <ListItem icon={mdiCloud}
-                      title={project.name}
-                      on:click={() => void downloadCrdtProject(project, server)}
-                      loading={downloading === project.name}>
-              <div slot="actions" class="pointer-events-none">
-                <Button icon={mdiBookArrowDownOutline} class="p-2">
-                  Download
-                </Button>
-              </div>
-            </ListItem>
-          {/if}
-        {/each}
-      {/if}
-    </div>
-  </div>
-{/each}
+
+{#await serversPromise}
+  <p>loading...</p>
+{:then serversStatus}
+  {#each serversStatus as status}
+    {@const server = status.server}
+    {@const serverProjects = remoteProjects[server.id]?.filter(p => p.crdt) ?? []}
+    <Server {status}
+            projects={serverProjects}
+            {localProjects}
+            loading={loadingServerProjects === server.id}
+            on:refreshProjects={() => refreshServerProjects(server, true)}
+            on:refreshAll={() => refreshProjectsAndServers()}/>
+  {/each}
+{/await}
