4855abe87e447217b74e2ce16aae481e6c939ed5
Kevin Hahn <kevin_hahn@sil.org>
2025-02-10 10:00:19 +0700

    add a refresh button to local projects. Automatically refresh the project list after downloading a project.

--- frontend/viewer/src/home/HomeView.svelte
+++ frontend/viewer/src/home/HomeView.svelte
@@ -5,7 +5,7 @@
     mdiBookPlusOutline,
     mdiChatQuestion,
     mdiChevronRight,
-    mdiFaceAgent,
+    mdiFaceAgent, mdiRefresh,
     mdiTestTube,
   } from '@mdi/js';
   import {AppBar, Button, ListItem} from 'svelte-ux';
@@ -114,7 +114,13 @@
     {:then projects}
       <div class="space-y-4 md:space-y-8">
         <div>
-          <p class="sub-title">Local</p>
+          <div class="flex flex-row">
+            <p class="sub-title">Local</p>
+            <div class="flex-grow"></div>
+            <Button icon={mdiRefresh}
+                    title="Refresh Projects"
+                    on:click={() => refreshProjects()}/>
+          </div>
           <div>
             {#each projects.filter(p => p.crdt) as project, i (project.id ?? i)}
               {@const server = project.server}
