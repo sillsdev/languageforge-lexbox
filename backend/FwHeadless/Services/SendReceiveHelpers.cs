b0acab8f50acda4ab039fe08002ac80569e6fe81
Robin Munn <rmunn@pobox.com>
2025-02-14 09:17:44 +0700

    Improve API to query sync status (#1454)
    * SendReceiveService can now count pending hg commits
    
    This will be useful in the updated sync-status API
    
    * Update sync-status API to include commit dates
    
    Also includes code to check pending Mercurial commits, and report that
    in the sync status as well.
    
    * Add Lexbox API to get sync status and trigger sync
    * Address review comments
    * Document that -1 hg changes means "all of them"
    
    A -1 returned in sync status means "all the hg changes on the server
    will be pulled, but since there's no local clone we can't easily count
    how many there will be".

--- backend/FwHeadless/Services/SendReceiveHelpers.cs
+++ backend/FwHeadless/Services/SendReceiveHelpers.cs
@@ -1,3 +1,4 @@
+using Chorus.VcsDrivers.Mercurial;
 using FwDataMiniLcmBridge;
 using SIL.Progress;
 
@@ -28,25 +29,54 @@ public static class SendReceiveHelpers
         return new LfMergeBridgeResult(lfMergeBridgeOutputForClient, progress == null ? sbProgress.ToString() : "");
     }
 
-    private static Uri BuildSendReceiveUrl(string baseUrl, string projectCode, SendReceiveAuth? auth)
+    private static Uri BuildSendReceiveUrl(string baseUrl, string projectCode, SendReceiveAuth? auth, bool forChorus = true)
     {
         var baseUri = new Uri(baseUrl);
         var projectUri = new Uri(baseUri, projectCode);
         if (auth == null) return projectUri;
-        // Stop Chorus from saving passwords, since we're not a GUI app (and it calls Windows-only APIs anyway)
-        var chorusSettings = new Chorus.Model.ServerSettingsModel
+        if (forChorus)
         {
-            RememberPassword = false,
-            Username = auth.Username,
-            Password = auth.Password
-        };
-        // Chorus relies too much on its global ServerSettingsModel.PasswordForSession variable
-        chorusSettings.SaveUserSettings();
+            // Stop Chorus from saving passwords, since we're not a GUI app (and it calls Windows-only APIs anyway)
+            var chorusSettings = new Chorus.Model.ServerSettingsModel
+            {
+                RememberPassword = false,
+                Username = auth.Username,
+                Password = auth.Password
+            };
+            // Chorus relies too much on its global ServerSettingsModel.PasswordForSession variable
+            chorusSettings.SaveUserSettings();
+        }
         // TODO: Consider a global S/R lock because of Chorus's PasswordForSession behavior
         var builder = new UriBuilder(projectUri);
         builder.UserName = auth.Username;
         builder.Password = auth.Password;
         return builder.Uri;
+    }
+
+    public static async Task<int> PendingMercurialCommits(FwDataProject project, string? projectCode = null, string baseUrl = "http://localhost", SendReceiveAuth? auth = null, IProgress? progress = null)
+    {
+        projectCode ??= project.Name;
+        progress ??= new NullProgress();
+
+        var fwdataInfo = new FileInfo(project.FilePath);
+        if (!fwdataInfo.Exists) return -1; // If there's no local clone then `hg incoming` won't work, so -1 is used to mean "all the commits on the server will be pulled"
+        if (fwdataInfo.Directory is null) throw new InvalidOperationException(
+            $"Not allowed to Send/Receive root-level directories like C:\\, was '{project.FilePath}'");
+
+        var repoUrl = BuildSendReceiveUrl(baseUrl, projectCode, auth, forChorus: false);
+        var hgResult = await Task.Run(() => HgRunner.Run($"hg incoming -T \"node: {{node}}\\n\" {repoUrl}", fwdataInfo.Directory.FullName, 9999, progress));
+        if (hgResult.ExitCode == 1)
+        {
+            // hg incoming exits with 1 if there were no changes
+            return 0;
+        }
+        var output = hgResult.StandardOutput;
+        if (string.IsNullOrEmpty(output))
+        {
+            return 0;
+        }
+        var lines = output.Split('\n').Where(line => line.StartsWith("node"));
+        return lines.Count();
     }
 
     public static async Task<LfMergeBridgeResult> SendReceive(FwDataProject project, string? projectCode = null, string baseUrl = "http://localhost", SendReceiveAuth? auth = null, string fdoDataModelVersion = "7000072", string? commitMessage = null, IProgress? progress = null)
---
+++ backend/LexBoxApi/Controllers/SyncController.cs
@@ -0,0 +1,32 @@
+using LexBoxApi.Services;
+using LexCore.ServiceInterfaces;
+using LexCore.Sync;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LexBoxApi.Controllers;
+
+[ApiController]
+[Route("/api/fw-lite/sync")]
+[ApiExplorerSettings(GroupName = LexBoxKernel.OpenApiPublicDocumentName)]
+public class SyncController(
+    IPermissionService permissionService,
+    FwHeadlessClient fwHeadlessClient) : ControllerBase
+{
+    [HttpGet("status/{projectId}")]
+    public async Task<ActionResult<ProjectSyncStatus>> GetSyncStatus(Guid projectId)
+    {
+        if (!await permissionService.CanViewProject(projectId)) return Forbid();
+        var result = await fwHeadlessClient.CrdtSyncStatus(projectId);
+        if (result is null) return NotFound();
+        return Ok(result);
+    }
+
+    [HttpPost("trigger/{projectId}")]
+    public async Task<ActionResult<SyncResult>> TriggerSync(Guid projectId)
+    {
+        if (!await permissionService.CanSyncProject(projectId)) return Forbid();
+        var result = await fwHeadlessClient.CrdtSync(projectId);
+        if (result is null) return StatusCode(500); // Apparently there's no InternalServerError()? Weird.
+        return Ok(result);
+    }
+}
