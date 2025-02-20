4d295adda91e2574156cf9895bca22835f419928
Kevin Hahn <kevin_hahn@sil.org>
2025-02-20 14:47:06 +0700

    remove aspire references

--- backend/WebServiceDefaults/Extensions.cs
+++ backend/WebServiceDefaults/Extensions.cs
@@ -11,7 +11,7 @@ using OpenTelemetry.Trace;
 
 namespace WebServiceDefaults;
 
-// Adds common .NET Aspire services: service discovery, resilience, health checks, and OpenTelemetry.
+// Adds common .NET services: service discovery, resilience, health checks, and OpenTelemetry.
 // This project should be referenced by each service project in your solution.
 // To learn more about using this project, see https://aka.ms/dotnet/aspire/service-defaults
 public static class Extensions
