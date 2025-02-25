1145c28373ad8a0a4ca2ae0507191c2113151797
Kevin Hahn <kevin_hahn@sil.org>
2025-02-25 16:13:40 +0700

    fix deserialization issue due to property names not matching constructor parameters

--- backend/FwLite/LcmCrdt/Changes/CreatePublicationChange.cs
+++ backend/FwLite/LcmCrdt/Changes/CreatePublicationChange.cs
@@ -5,9 +5,9 @@ using SIL.Harmony.Entities;
 
 namespace LcmCrdt.Changes;
 
-public class CreatePublicationChange(Guid id, MultiString pubName) : CreateChange<Publication>(id), ISelfNamedType<CreatePublicationChange>
+public class CreatePublicationChange(Guid entityId, MultiString name) : CreateChange<Publication>(entityId), ISelfNamedType<CreatePublicationChange>
 {
-    public MultiString Name { get; } = pubName;
+    public MultiString Name { get; } = name;
     public override ValueTask<Publication> NewEntity(Commit commit, IChangeContext context)
     {
         return ValueTask.FromResult(new Publication { Id = EntityId, Name = Name});
