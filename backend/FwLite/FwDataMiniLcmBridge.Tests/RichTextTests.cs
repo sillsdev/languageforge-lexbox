4d55bc7f345333d749b8b29e6f8fdd186c30efff
Kevin Hahn <kevin_hahn@sil.org>
2025-03-13 16:00:16 +0700

    being writing tests for all prop types and ensure that they are all tested

--- backend/FwLite/FwDataMiniLcmBridge.Tests/RichTextTests.cs
+++ backend/FwLite/FwDataMiniLcmBridge.Tests/RichTextTests.cs
@@ -59,7 +59,97 @@ public class RichTextTests(ITestOutputHelper output)
         textProps.GetStrPropValue((int)FwTextPropType.ktptNamedStyle).Should().Be("Strong");
         textProps.GetIntPropValues((int)FwTextPropType.ktptWs, out _).Should().Be(FakeWsHandleEn);
         textProps.GetIntPropValues((int)FwTextPropType.ktptBaseWs, out _).Should().Be(FakeWsHandleEn);
+    }
 
+    public static IEnumerable<object[]> IntPropTypeIsMappedCorrectlyData()
+    {
+        IEnumerable<(FwTextPropType propType, int value, int variation, Action<ITsTextProps, RichSpan> assert)> GetData()
+        {
+            yield return (FwTextPropType.ktptWs, FakeWsHandleEn, 0, (props, span) => span.Ws.Should().Be((WritingSystemId)"en"));
+            yield return (FwTextPropType.ktptBaseWs, FakeWsHandleEn, 0, (props, span) => span.WsBase.Should().Be((WritingSystemId)"en"));
+            yield return (FwTextPropType.ktptItalic, (int)FwTextToggleVal.kttvOff, 0, (props, span) => span.Italic.Should().Be(RichTextToggle.Off));
+            yield return (FwTextPropType.ktptItalic, (int)FwTextToggleVal.kttvForceOn, 0, (props, span) => span.Italic.Should().Be(RichTextToggle.On));
+            yield return (FwTextPropType.ktptItalic, (int)FwTextToggleVal.kttvInvert, 0, (props, span) => span.Italic.Should().Be(RichTextToggle.Invert));
+        }
+        return GetData().Select(x => new object[] { x.propType, x.value, x.variation, x.assert });
+    }
+
+    [Theory]
+    [MemberData(nameof(IntPropTypeIsMappedCorrectlyData))]
+    public void IntPropTypeIsMappedCorrectly(FwTextPropType propType, int value, int variation, Action<ITsTextProps, RichSpan> assert)
+    {
+        var span = new RichSpan() { Text = "test" };
+        var builder = TsStringUtils.MakePropsBldr();
+        builder.SetIntPropValues((int)propType, variation, value);
+        var textProps = builder.GetTextProps();
+
+        RichTextMapping.WriteToSpan(span, textProps, WsIdLookup);
+        assert(textProps, span);
+    }
+    public static IEnumerable<object[]> StringPropTypeIsMappedCorrectlyData()
+    {
+        IEnumerable<(FwTextPropType propType, string value, Action<ITsTextProps, RichSpan> assert)> GetData()
+        {
+            yield return (FwTextPropType.ktptNamedStyle, "Strong", (props, span) => span.NamedStyle.Should().Be("Strong"));
+            yield return (FwTextPropType.ktptCharStyle, "SomeString", (props, span) => span.CharStyle.Should().Be("SomeString"));
+        }
+        return GetData().Select(x => new object[] { x.propType, x.value, x.assert });
+    }
+
+    [Theory]
+    [MemberData(nameof(StringPropTypeIsMappedCorrectlyData))]
+    public void StringPropTypeIsMappedCorrectly(FwTextPropType propType, string value, Action<ITsTextProps, RichSpan> assert)
+    {
+        var span = new RichSpan() { Text = "test" };
+        var builder = TsStringUtils.MakePropsBldr();
+        builder.SetStrPropValue((int)propType, value);
+        var textProps = builder.GetTextProps();
+
+        RichTextMapping.WriteToSpan(span, textProps, WsIdLookup);
+        assert(textProps, span);
+    }
+
+    [Fact]
+    public void AllPropTypesAreTested()
+    {
+        var testedStringTypes = StringPropTypeIsMappedCorrectlyData()
+            .Select(arr => (FwTextPropType)arr[0]).ToArray();
+        var testedIntTypes = IntPropTypeIsMappedCorrectlyData().Select(arr => (FwTextPropType)arr[0]).ToArray();
+        var testedTypes = testedStringTypes
+            .Union(testedIntTypes);
+
+        FwTextPropType[] excludedTypes = [FwTextPropType.ktptMarkItem];
+        var allTypes = Enum.GetValues<FwTextPropType>();
+        var notTested = allTypes
+            .Except(excludedTypes)
+            .Except(testedTypes)
+            .ToArray();
+        if (notTested.Length > 0) output.WriteLine($"Not tested: {string.Join(", ", notTested)}");
+        notTested.Should().BeEmpty();
+
+        //all these types are duplicated so must be in both string and int tested types
+        HashSet<FwTextPropType> duplicatedTypes =
+        [
+            // ReSharper disable DuplicateKeyCollectionInitialization
+            FwTextPropType.ktptWs, FwTextPropType.ktptFontFamily,
+
+            FwTextPropType.ktptItalic, FwTextPropType.ktptCharStyle,
+
+            FwTextPropType.ktptBold, FwTextPropType.ktptParaStyle,
+
+            FwTextPropType.ktptSuperscript, FwTextPropType.ktptTabList,
+
+            FwTextPropType.ktptUnderline, FwTextPropType.ktptTags,
+
+            FwTextPropType.ktptFontSize, FwTextPropType.ktptObjData,
+
+            FwTextPropType.ktptOffset, FwTextPropType.ktptCustomBullet
+            // ReSharper restore DuplicateKeyCollectionInitialization
+        ];
+
+        //ensure that these types are all tested in both string and int tests
+        duplicatedTypes.Except(testedStringTypes).Should().BeEmpty();
+        duplicatedTypes.Except(testedIntTypes).Should().BeEmpty();
     }
 
     private WritingSystemId? WsIdLookup(int? handle)
