050023ef113b9900dd6ed23dc7cd5c71046bb0b1
Kevin Hahn <kevin_hahn@sil.org>
2025-03-17 11:23:20 +0700

    test null LexemeFormOA

--- backend/FwLite/FwDataMiniLcmBridge.Tests/MiniLcmTests/UpdateEntryTests.cs
+++ backend/FwLite/FwDataMiniLcmBridge.Tests/MiniLcmTests/UpdateEntryTests.cs
@@ -1,4 +1,7 @@
-﻿using FwDataMiniLcmBridge.Tests.Fixtures;
+﻿using FwDataMiniLcmBridge.Api;
+using FwDataMiniLcmBridge.Tests.Fixtures;
+using MiniLcm.Models;
+using SIL.LCModel.Infrastructure;
 
 namespace FwDataMiniLcmBridge.Tests.MiniLcmTests;
 
@@ -11,4 +14,34 @@ public class UpdateEntryTests(ProjectLoaderFixture fixture) : UpdateEntryTestsBa
     }
 
     protected override bool ApiUsesImplicitOrdering => true;
+
+    [Fact]
+    public async Task UpdateEntry_WithNullLexemeFormOA_CreatesNewLexemeForm()
+    {
+        // Arrange
+        var entry = await Api.CreateEntry(new Entry
+        {
+            LexemeForm = new MultiString { { "en", "test" } }
+        });
+
+        var fwApi = (FwDataMiniLcmApi)Api;
+        var lexEntry = fwApi.EntriesRepository.GetObject(entry.Id);
+        UndoableUnitOfWorkHelper.DoUsingNewOrCurrentUOW("Set LexemeFormOA to null",
+            "Restore LexemeFormOA",
+            fwApi.Cache.ServiceLocator.ActionHandler,
+            () =>
+            {
+                lexEntry.LexemeFormOA = null;
+            });
+
+        // Act
+        var updatedEntry = await Api.UpdateEntry(entry, entry with
+        {
+            LexemeForm = new MultiString { { "en", "updated test" } }
+        });
+
+        // Assert
+        updatedEntry.LexemeForm.Should().NotBeNull();
+        updatedEntry.LexemeForm["en"].Should().Be("updated test");
+    }
 }
