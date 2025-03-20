44af3a5d4e5065671bcd0294a6ded322df3db3b1
Kevin Hahn <kevin_hahn@sil.org>
2025-03-20 11:00:44 +0700

    add collapsable section to search filter

--- frontend/viewer/src/project/browse/SearchFilter.svelte
+++ frontend/viewer/src/project/browse/SearchFilter.svelte
@@ -1,17 +1,22 @@
 <script lang="ts">
+  import {buttonVariants} from '$lib/components/ui/button';
+  import * as Collapsible from '$lib/components/ui/collapsible';
   import { Icon } from '$lib/components/ui/icon';
   import { Input } from '$lib/components/ui/input';
   import { t } from 'svelte-i18n-lingui';
 
-  let {search = $bindable()}: {search: string} = $props();
+  let { search = $bindable() }: { search: string } = $props();
 </script>
 
-<div class="mb-4 p-2 pr-4 relative">
-  <Icon icon="i-mdi-magnify" class="absolute top-1/2 -translate-y-1/2 left-5 size-5" />
-  <Input
-    bind:value={search}
-    placeholder={$t`Filter`}
-    class="pl-12 pr-10"
-  />
-  <Icon icon="i-mdi-filter" class="absolute top-1/2 -translate-y-1/2 right-6 size-5" />
-</div>
+<Collapsible.Root>
+  <div class="relative">
+    <Icon icon="i-mdi-magnify" class="absolute top-1/2 -translate-y-1/2 left-3 size-5" />
+    <Input bind:value={search} placeholder={$t`Filter`} class="pl-10 pr-10" />
+    <Collapsible.Trigger class={buttonVariants({ variant: 'ghost', size: 'sm', class: 'absolute top-1/2 -translate-y-1/2 right-1' })}>
+      <Icon icon="i-mdi-filter" class="size-5" />
+    </Collapsible.Trigger>
+  </div>
+  <Collapsible.Content>
+    more fancy stuff here...
+  </Collapsible.Content>
+</Collapsible.Root>
