627471c539a9207e08577c392a0063a1693ecb41
Kevin Hahn <kevin_hahn@sil.org>
2025-03-21 13:47:53 +0700

    cleanup project data path duplication

--- backend/FwLite/FwDataMiniLcmBridge/FwDataProject.cs
+++ backend/FwLite/FwDataMiniLcmBridge/FwDataProject.cs
@@ -12,14 +12,16 @@ namespace FwDataMiniLcmBridge;
 /// </param>
 public class FwDataProject(string name, string projectsPath) : IProjectIdentifier
 {
-    public string Name { get; } = name;
-    public string FileName { get; } = name + ".fwdata";
-    public string FilePath { get; } = Path.Combine(projectsPath, name, name + ".fwdata");
+    public string Name => name;
+    public string FileName => name + ".fwdata";
+    public string FilePath => Path.Combine(ProjectFolder, FileName);
     public string ProjectFolder => Path.Combine(projectsPath, name);
+
     /// <summary>
     /// A path to the projects folder, there must be a folder with the same name as the project name and a fwdata file in it.
     /// eg: Project name : "MyProject", path: "/data/projects/" is the value of this property, then the fwdata file must be in /data/projects/MyProject/MyProject.fwdata
     /// </summary>
-    public string ProjectsPath { get; } = projectsPath;
+    public string ProjectsPath => projectsPath;
+
     public ProjectDataFormat DataFormat => ProjectDataFormat.FwData;
 }
