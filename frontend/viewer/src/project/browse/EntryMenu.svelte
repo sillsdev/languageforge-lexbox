fa294db491228c94632405f81b7de370d171fc30
Kevin Hahn <kevin_hahn@sil.org>
2025-03-21 15:17:16 +0700

    make the entry menu responsive

--- frontend/viewer/src/project/browse/EntryMenu.svelte
+++ frontend/viewer/src/project/browse/EntryMenu.svelte
@@ -1,21 +1,70 @@
 <script lang="ts">
   import { Icon } from '$lib/components/ui/icon';
+  import { type IconClass } from '$lib/icon-class';
   import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
+  import * as Drawer from '$lib/components/ui/drawer';
+  import { IsMobile } from '$lib/hooks/is-mobile.svelte';
+  import { t } from 'svelte-i18n-lingui';
   import { buttonVariants } from '$lib/components/ui/button';
+  import Button from '$lib/components/ui/button/button.svelte';
 
   let { onDelete } = $props<{
     onDelete?: () => void;
   }>();
+
+  const isMobile = new IsMobile();
+  let open = $state(false);
+  const triggerVariant = buttonVariants({ variant: 'ghost', size: 'sm', class: 'float-right' });
 </script>
 
-<DropdownMenu.Root>
-  <DropdownMenu.Trigger class={buttonVariants({ variant: 'ghost', size: 'sm', class: 'float-right' })}>
-    <Icon icon="i-mdi-dots-vertical" class="cursor-pointer" />
-  </DropdownMenu.Trigger>
-  <DropdownMenu.Content>
-    <DropdownMenu.Item class="cursor-pointer" onSelect={onDelete}>
-      <Icon icon="i-mdi-delete" class="mr-2" />
-      Delete Entry
+{#snippet items()}
+  {@render menuItem('i-mdi-delete', $t`Delete Entry`, onDelete)}
+  {@render menuItem('i-mdi-history', $t`History`, () => {})}
+{/snippet}
+
+{#snippet menuItem(icon: IconClass, label: string, onSelect: () => void)}
+  {#if !isMobile.current}
+    <DropdownMenu.Item class="cursor-pointer" {onSelect}>
+      <Icon {icon} class="mr-2" />
+      {label}
     </DropdownMenu.Item>
-  </DropdownMenu.Content>
-</DropdownMenu.Root>
+  {:else}
+    <Button
+      onclick={onSelect}
+      variant="ghost"
+      class="w-full justify-start"
+      {icon}
+    >
+      {label}
+    </Button>
+  {/if}
+{/snippet}
+
+{#if !isMobile.current}
+  <DropdownMenu.Root bind:open>
+    <DropdownMenu.Trigger class={triggerVariant}>
+      <Icon icon="i-mdi-dots-vertical" class="cursor-pointer" />
+    </DropdownMenu.Trigger>
+    <DropdownMenu.Content align="end">
+      {@render items()}
+    </DropdownMenu.Content>
+  </DropdownMenu.Root>
+{:else}
+  <Drawer.Root bind:open>
+    <Drawer.Trigger class={triggerVariant}>
+      <Icon icon="i-mdi-dots-vertical" class="cursor-pointer" />
+    </Drawer.Trigger>
+    <Drawer.Content>
+      <div class="mx-auto w-full max-w-sm p-4">
+        <Drawer.Header class="justify-items-end">
+          <Drawer.Close class={buttonVariants({ variant: 'ghost', size: 'icon' })}><Icon icon="i-mdi-close" /></Drawer.Close>
+        </Drawer.Header>
+        <div class="space-y-2 flex flex-col gap-2">
+          {@render items()}
+        </div>
+        <Drawer.Footer>
+        </Drawer.Footer>
+      </div>
+    </Drawer.Content>
+  </Drawer.Root>
+{/if}
