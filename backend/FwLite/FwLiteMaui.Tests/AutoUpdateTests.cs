6fde1f1daa9e069f9c41525f2f5bb8ecdb95b67b
Tim Haasdyk <tim_haasdyk@sil.org>
2025-03-24 12:23:40 +0100

    Try to add test project for FwLiteMaui

---
+++ backend/FwLite/FwLiteMaui.Tests/AutoUpdateTests.cs
@@ -0,0 +1,29 @@
+﻿namespace FwLiteMaui.Tests;
+
+using FwLiteMaui;
+
+public class AutoUpdateTests
+{
+    private readonly AppUpdateService _appUpdateService;
+
+    public AutoUpdateTests()
+    {
+        var services = new ServiceCollection()
+            .AddSingleton<AppUpdateService>()
+            .AddHttpClient()
+            //...
+        _appUpdateService = services.GetRequiredService<AppUpdateService>();
+    }
+
+    [Theory]
+    [InlineData(1, false)]
+    [InlineData(15, false)]
+    [InlineData(22, true)]
+    [InlineData(26, true)]
+    [InlineData(36, true)]
+    [InlineData(46, true)]
+    public void ShouldCheckForUpdateAfterThreshold(int hoursInThefuture, bool shouldCheckForUpdate)
+    {
+
+    }
+}
