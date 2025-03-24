fc6654a6ec8d457cc946bc6359a6bd3d19ff5f51
Tim Haasdyk <tim_haasdyk@sil.org>
2025-03-24 12:19:43 +0100

    Use TotalHours instead of Hours or UpdateChecking

--- backend/FwLite/FwLiteMaui/Platforms/Windows/AppUpdateService.cs
+++ backend/FwLite/FwLiteMaui/Platforms/Windows/AppUpdateService.cs
@@ -209,13 +209,13 @@ public class AppUpdateService(
         var lastChecked = preferences.Get(LastUpdateCheck, DateTime.MinValue);
         var timeSinceLastCheck = DateTime.UtcNow - lastChecked;
         //if last checked is in the future (should never happen), then we want to reset the time and check again
-        if (timeSinceLastCheck.Hours < -1)
+        if (timeSinceLastCheck.TotalHours < -1)
         {
             preferences.Set(LastUpdateCheck, DateTime.UtcNow);
             logger.LogInformation("Should check for update, because last check was in the future: {LastCheck}", lastChecked);
             return true;
         }
-        if (timeSinceLastCheck.Hours < 20)
+        if (timeSinceLastCheck.TotalHours < 20)
         {
             logger.LogInformation("Should not check for update, because last check was too recent: {LastCheck}", lastChecked);
             return false;
