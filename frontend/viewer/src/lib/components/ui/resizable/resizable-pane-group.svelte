eedd5614e758f6104f7c5051bc6c86e785034a5c
Tim Haasdyk <tim_haasdyk@sil.org>
2025-03-25 17:21:20 +0100

    Add more resize features

--- frontend/viewer/src/lib/components/ui/resizable/resizable-pane-group.svelte
+++ frontend/viewer/src/lib/components/ui/resizable/resizable-pane-group.svelte
@@ -3,16 +3,22 @@
   import * as ResizablePrimitive from 'paneforge';
 
   let {
+    anyColumnIsCollapsed = $bindable(false),
     ref = $bindable(null),
     this: paneGroup = $bindable(),
     class: className,
     ...restProps
   }: ResizablePrimitive.PaneGroupProps & {
     this?: ResizablePrimitive.PaneGroup;
+    anyColumnIsCollapsed?: boolean;
   } = $props();
 </script>
 
 <ResizablePrimitive.PaneGroup
+  onLayoutChange={(layout) => {
+    anyColumnIsCollapsed = layout.some((column) => column === 0);
+    restProps.onLayoutChange?.(layout);
+  }}
   bind:this={paneGroup}
   class={cn('flex h-full w-full data-[direction=vertical]:flex-col', className)}
   {...restProps}
