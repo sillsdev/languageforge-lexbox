00e71953e22a5cee003371f339e096722b84a653
Kevin Hahn <kevin_hahn@sil.org>
2025-03-27 14:22:37 +0700

    remove unused const

--- frontend/viewer/src/lib/hooks/is-mobile.svelte.ts
+++ frontend/viewer/src/lib/hooks/is-mobile.svelte.ts
@@ -1,8 +1,6 @@
 import {MOBILE_BREAKPOINT} from '../../css-breakpoints';
 import {MediaQuery} from 'svelte/reactivity';
 
-const isMobileMediaQuery = new MediaQuery(`max-width: ${MOBILE_BREAKPOINT - 1}px`);
-
 export class IsMobile extends MediaQuery {
   private constructor() {
     super(`max-width: ${MOBILE_BREAKPOINT - 1}px`);
