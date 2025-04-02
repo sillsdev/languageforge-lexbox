7ef8a35f66f62b2672e800f33bb1fca59193f0cd
Tim Haasdyk <tim_haasdyk@sil.org>
2025-04-02 13:36:19 +0200

    Retry maildev api calls (#1566)

--- frontend/tests/email/maildev-mailbox.ts
+++ frontend/tests/email/maildev-mailbox.ts
@@ -1,6 +1,8 @@
 import type {APIRequestContext} from '@playwright/test';
 import {type EmailSubjects} from './email-page';
 import {Mailbox, type Email} from './mailbox';
+import {delay} from '$lib/util/time';
+import {getErrorMessage} from '$lib/error/utils';
 
 interface MaildevEmail {
   subject: string;
@@ -21,9 +23,23 @@ export class MaildevMailbox extends Mailbox {
   }
 
   async fetchMyEmails(): Promise<MaildevEmail[]> {
-    // Maildev REST API docs: https://github.com/maildev/maildev/blob/master/docs/rest.md
-    const response = await this.api.get(`http://localhost:1080/email`);
-    const emails = await response.json() as MaildevEmail[];
-    return emails.filter(email => email.to.some(to => to.address === this.email));
+    const MAX_TRIES = 3;
+
+    for (let tries = 1; tries <= MAX_TRIES; tries++) {
+      try {
+        // Maildev REST API docs: https://github.com/maildev/maildev/blob/master/docs/rest.md
+        const response = await this.api.get(`http://localhost:1080/email`);
+        const emails = await response.json() as MaildevEmail[];
+        return emails.filter(email => email.to.some(to => to.address === this.email));
+      } catch (error) {
+        const message = getErrorMessage(error);
+        console.error(`Error fetching emails, attempt ${tries}/${MAX_TRIES}.`, message);
+        if (tries >= MAX_TRIES) throw error;
+        await delay(1000);
+      }
+    }
+
+    // This line should never be reached but TypeScript requires a return statement
+    throw new Error('Reached unreachable code');
   }
 }
