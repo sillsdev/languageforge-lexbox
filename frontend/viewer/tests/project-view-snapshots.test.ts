ad933d37131621e4b4b6eb189666f8a0246963dd
Tim Haasdyk <tim_haasdyk@sil.org>
2025-04-09 13:36:24 +0200

    Stabilize and add dark mode screenshots

--- frontend/viewer/tests/project-view-snapshots.test.ts
+++ frontend/viewer/tests/project-view-snapshots.test.ts
@@ -1,26 +1,38 @@
-﻿import {test, expect} from '@playwright/test';
-
+﻿import {Page, expect, test} from '@playwright/test';
 
 const viewports = [
   {height: 720, width: 1280, name: 'medium'},
   {height: 812, width: 375, name: 'iphone-11'},
 ];
-for (let viewport of viewports) {
-  test(`ui snapshot selected entry, view port:${viewport.name}`, async ({page}) => {
-    await page.setViewportSize(viewport);
-    await page.goto('/testing/project-view');
-    await page.getByRole('row', {name: 'ambuka to cross a body of'}).click();
-    await expect(page.getByRole('heading', {name: 'ambuka'})).toBeVisible();
-    await expect(page.locator('.i-mdi-dots-vertical')).toBeVisible();
-    await expect(page.locator('.i-mdi-loading')).not.toBeVisible();
-    await expect(page).toHaveScreenshot();
-  });
-  test(`ui snapshot default, view port:${viewport.name}`, async ({page}) => {
-    await page.setViewportSize(viewport);
-    await page.goto('/testing/project-view');
-    await expect(page.locator('.i-mdi-loading')).not.toBeVisible();
-    await expect(page.getByRole('textbox', {name: 'Filter'})).toBeVisible();
-    await expect(page).toHaveScreenshot();
-  });
+
+for (const viewport of viewports) {
+  for (const colorScheme of ['light', 'dark'] as const) {
+    test(`ui snapshot selected entry (viewport: ${viewport.name} - ${colorScheme})`, async ({page}) => {
+      await page.emulateMedia({colorScheme});
+      await page.setViewportSize(viewport);
+      await page.goto('/testing/project-view');
+      await waitForProjectViewReady(page);
+      await page.getByRole('row', {name: 'ambuka to cross a body of'}).click();
+      await expect(page.getByRole('heading', {name: 'ambuka'})).toBeVisible();
+      await expect(page.locator('.i-mdi-dots-vertical')).toBeVisible();
+      await expect(page.locator('.i-mdi-loading')).not.toBeVisible();
+      await expect(page).toHaveScreenshot();
+    });
+    test(`ui snapshot default (viewport: ${viewport.name} - ${colorScheme})`, async ({page}) => {
+      await page.emulateMedia({colorScheme});
+      await page.setViewportSize(viewport);
+      await page.goto('/testing/project-view');
+      await waitForProjectViewReady(page);
+      await expect(page.locator('.animate-pulse')).not.toBeVisible();
+      await expect(page.locator('.i-mdi-loading')).not.toBeVisible();
+      await expect(page.getByRole('textbox', {name: 'Filter'})).toBeVisible();
+      await expect(page).toHaveScreenshot({});
+    });
+  }
 }
 
+async function waitForProjectViewReady(page: Page) {
+  await expect(page.locator('.i-mdi-loading')).not.toBeVisible();
+  await page.waitForFunction(() => document.fonts.ready);
+  await expect(page.locator('.animate-pulse')).not.toBeVisible();
+}
