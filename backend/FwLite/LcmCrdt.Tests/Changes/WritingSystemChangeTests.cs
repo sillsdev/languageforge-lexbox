8044d9fa47a02b6bb2eabf02aee2376d635bc514
Kevin Hahn <kevin_hahn@sil.org>
2025-04-09 14:49:30 +0700

    add WritingSystemChangeTests to exhibit creating the same writing system twice

---
+++ backend/FwLite/LcmCrdt.Tests/Changes/WritingSystemChangeTests.cs
@@ -0,0 +1,27 @@
+﻿using LcmCrdt.Changes;
+
+namespace LcmCrdt.Tests.Changes;
+
+public class WritingSystemChangeTests(MiniLcmApiFixture fixture) : IClassFixture<MiniLcmApiFixture>
+{
+    [Fact]
+    public async Task CreatingTheSameWritingSystemShouldWork()
+    {
+        var writingSystem = new WritingSystem()
+        {
+            Id = Guid.NewGuid(),
+            WsId = "de",
+            Abbreviation = "De",
+            Font = "test",
+            Name = "German",
+            Type = WritingSystemType.Analysis
+        };
+        await fixture.Api.CreateWritingSystem(WritingSystemType.Analysis, writingSystem);
+
+        //added via sync
+        await fixture.DataModel.AddChange(Guid.NewGuid(),
+            new CreateWritingSystemChange(writingSystem, WritingSystemType.Analysis, Guid.NewGuid(), 2));
+        var writingSystems = await fixture.Api.GetWritingSystems();
+        writingSystems.Analysis.Should().Contain(ws => ws.WsId == "de");
+    }
+}
