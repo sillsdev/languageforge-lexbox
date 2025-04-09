502af5de807001a06b7d0371e566d43747bacb96
Kevin Hahn <kevin_hahn@sil.org>
2025-04-09 16:11:01 +0700

    setup screenshot testing using playwright

---
+++ frontend/viewer/playwright.config.ts
@@ -0,0 +1,53 @@
+﻿import {defineConfig, devices} from '@playwright/test';
+import * as testEnv from '../tests/envVars';
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 1 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : 2,
+  outputDir: 'test-results',
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: process.env.CI
+    ? [['github'], ['list'], ['junit', {outputFile: 'test-results/results.xml'}]]
+    // Putting the HTML report in a subdirectory of the main output directory results in a warning log
+    // stating that it will "lead to artifact loss" but the warning in this case is not accurate
+    : [['list'], ['html', {outputFolder: 'html-test-results', open: 'never'}]],
+
+  use: {
+    baseURL: 'http://localhost:5137',
+    /* Local storage to be populated for every test */
+    storageState: {
+      cookies: [],
+      origins: [
+        {
+          origin: testEnv.serverHostname,
+          localStorage: [
+            {
+              name: 'isPlaywright',
+              value: 'true',
+            },
+            {
+              name: 'shadcnMode',
+              value: 'true'
+
+            }
+          ],
+        },
+      ],
+    },
+    viewport: {
+      height: 720,
+      width: 1280,
+    }
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: {...devices['Desktop Chrome'], userAgent: 'Playwright Chrome'},
+    },
+  ]
+});
---
+++ frontend/viewer/tests/project-view-snapshots.test.ts
@@ -0,0 +1,26 @@
+﻿import {test, expect} from '@playwright/test';
+
+
+const viewports = [
+  {height: 720, width: 1280, name: 'medium'},
+  {height: 812, width: 375, name: 'iphone-11'},
+];
+for (let viewport of viewports) {
+  test(`ui snapshot selected entry, view port:${viewport.name}`, async ({page}) => {
+    await page.setViewportSize(viewport);
+    await page.goto('/testing/project-view');
+    await page.getByRole('row', {name: 'ambuka to cross a body of'}).click();
+    await expect(page.getByRole('heading', {name: 'ambuka'})).toBeVisible();
+    await expect(page.locator('.i-mdi-dots-vertical')).toBeVisible();
+    await expect(page.locator('.i-mdi-loading')).not.toBeVisible();
+    await expect(page).toHaveScreenshot();
+  });
+  test(`ui snapshot default, view port:${viewport.name}`, async ({page}) => {
+    await page.setViewportSize(viewport);
+    await page.goto('/testing/project-view');
+    await expect(page.locator('.i-mdi-loading')).not.toBeVisible();
+    await expect(page.getByRole('textbox', {name: 'Filter'})).toBeVisible();
+    await expect(page).toHaveScreenshot();
+  });
+}
+
