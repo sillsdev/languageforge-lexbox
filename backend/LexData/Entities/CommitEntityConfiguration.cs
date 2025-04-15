8e5ab3a6ae9ade544e1155e500b07467e59b3067
Kevin Hahn <kevin_hahn@sil.org>
2025-04-15 13:07:59 +0700

    create a converter and use in EF to deserialize ChangeEntries which stored json in json (#1602)

--- backend/LexData/Entities/CommitEntityConfiguration.cs
+++ backend/LexData/Entities/CommitEntityConfiguration.cs
@@ -11,6 +11,10 @@ namespace LexData.Entities;
 
 public class CommitEntityConfiguration : IEntityTypeConfiguration<ServerCommit>
 {
+    private static JsonSerializerOptions Options = new JsonSerializerOptions()
+    {
+        Converters = { new ServerJsonChangeConverter() }
+    };
     public void Configure(EntityTypeBuilder<ServerCommit> builder)
     {
         builder.ToTable("CrdtCommits");
@@ -24,12 +28,37 @@ public class CommitEntityConfiguration : IEntityTypeConfiguration<ServerCommit>
             json => JsonSerializer.Deserialize<CommitMetadata>(json, (JsonSerializerOptions?)null) ?? new()
         );
         builder.Property(c => c.ChangeEntities).HasConversion(
-            c => JsonSerializer.Serialize(c, (JsonSerializerOptions?)null),
-            json => JsonSerializer.Deserialize<List<ChangeEntity<ServerJsonChange>>>(json, (JsonSerializerOptions?)null) ?? new()
+            c => JsonSerializer.Serialize(c, Options),
+            json => JsonSerializer.Deserialize<List<ChangeEntity<ServerJsonChange>>>(json, Options) ?? new()
         ).HasColumnType("jsonb").IsRequired(false);
     }
 
     private static ServerJsonChange Deserialize(string s) => JsonSerializer.Deserialize<ServerJsonChange>(s)!;
 
     private static string Serialize(ServerJsonChange c) => JsonSerializer.Serialize(c);
+
+    private class ServerJsonChangeConverter: JsonConverter<ServerJsonChange>
+    {
+        public override ServerJsonChange? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions _)
+        {
+            //ignoring JsonSerializerOptions, otherwise we would stack overflow recursively using this converter
+            if (typeToConvert != typeof(ServerJsonChange))
+                throw new SerializationException("type not supported " + typeToConvert.FullName);
+            //special case, this object type used to be encoded as a string in json like this: {"Change": "{\"Prop\": 1}"}
+            if (reader.TokenType == JsonTokenType.String)
+            {
+                var json = reader.GetString();
+                if (json is null) return null;
+                return JsonSerializer.Deserialize<ServerJsonChange>(json);
+            }
+
+            return JsonSerializer.Deserialize<ServerJsonChange>(ref reader);
+        }
+
+        public override void Write(Utf8JsonWriter writer, ServerJsonChange value, JsonSerializerOptions options)
+        {
+            //not passing in options otherwise it would just use this converter again
+            JsonSerializer.Serialize(writer, value);
+        }
+    }
 }
