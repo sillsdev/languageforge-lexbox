98747ea2aefefe6204ac431d0eb24c9bee57f41d
Kevin Hahn <kevin_hahn@sil.org>
2025-04-15 13:38:30 +0700

    add migration for EntityId index

---
+++ backend/FwLite/LcmCrdt/Migrations/20250415063756_AddHarmonyUpdate.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LcmCrdt.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHarmonyUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Snapshots_EntityId",
+                table: "Snapshots",
+                column: "EntityId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Snapshots_EntityId",
+                table: "Snapshots");
+        }
+    }
+}
