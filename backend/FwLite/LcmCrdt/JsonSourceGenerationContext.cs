c4eb0dfed9287e65d9cb8cff151ff96435e1142f
Kevin Hahn <kevin_hahn@sil.org>
2025-04-16 16:37:14 +0700

    add change types so json source generator

--- backend/FwLite/LcmCrdt/JsonSourceGenerationContext.cs
+++ backend/FwLite/LcmCrdt/JsonSourceGenerationContext.cs
@@ -1,6 +1,9 @@
 ﻿using System.Text.Json.Serialization;
+using LcmCrdt.Changes;
+using LcmCrdt.Changes.Entries;
 using LcmCrdt.Objects;
 using SIL.Harmony;
+using SIL.Harmony.Changes;
 using SIL.Harmony.Core;
 using SIL.Harmony.Db;
 
@@ -18,6 +21,47 @@ namespace LcmCrdt;
 [JsonSerializable(typeof(List<SemanticDomain>))]
 [JsonSerializable(typeof(IList<SemanticDomain>))]
 [JsonSerializable(typeof(List<ComplexFormType>))]
+[JsonSerializable(typeof(MultiString))]
+[JsonSerializable(typeof(IDictionary<WritingSystemId, string>))]
+[JsonSerializable(typeof(Dictionary<WritingSystemId, string>))]
+[JsonSerializable(typeof(CreateEntryChange))]
+[JsonSerializable(typeof(CreateWritingSystemChange))]
+[JsonSerializable(typeof(JsonPatchChange<Entry>))]
+[JsonSerializable(typeof(JsonPatchChange<Sense>))]
+[JsonSerializable(typeof(JsonPatchChange<ExampleSentence>))]
+[JsonSerializable(typeof(JsonPatchChange<WritingSystem>))]
+[JsonSerializable(typeof(JsonPatchChange<PartOfSpeech>))]
+[JsonSerializable(typeof(JsonPatchChange<SemanticDomain>))]
+[JsonSerializable(typeof(JsonPatchChange<ComplexFormType>))]
+[JsonSerializable(typeof(JsonPatchChange<Publication>))]
+[JsonSerializable(typeof(DeleteChange<Entry>))]
+[JsonSerializable(typeof(DeleteChange<Sense>))]
+[JsonSerializable(typeof(DeleteChange<ExampleSentence>))]
+[JsonSerializable(typeof(DeleteChange<WritingSystem>))]
+[JsonSerializable(typeof(DeleteChange<PartOfSpeech>))]
+[JsonSerializable(typeof(DeleteChange<SemanticDomain>))]
+[JsonSerializable(typeof(DeleteChange<ComplexFormType>))]
+[JsonSerializable(typeof(DeleteChange<ComplexFormComponent>))]
+[JsonSerializable(typeof(DeleteChange<Publication>))]
+[JsonSerializable(typeof(SetPartOfSpeechChange))]
+[JsonSerializable(typeof(AddSemanticDomainChange))]
+[JsonSerializable(typeof(RemoveSemanticDomainChange))]
+[JsonSerializable(typeof(ReplaceSemanticDomainChange))]
+[JsonSerializable(typeof(CreateEntryChange))]
+[JsonSerializable(typeof(CreateSenseChange))]
+[JsonSerializable(typeof(CreateExampleSentenceChange))]
+[JsonSerializable(typeof(CreatePartOfSpeechChange))]
+[JsonSerializable(typeof(CreateSemanticDomainChange))]
+[JsonSerializable(typeof(CreateWritingSystemChange))]
+[JsonSerializable(typeof(CreatePublicationChange))]
+[JsonSerializable(typeof(AddComplexFormTypeChange))]
+[JsonSerializable(typeof(AddEntryComponentChange))]
+[JsonSerializable(typeof(RemoveComplexFormTypeChange))]
+[JsonSerializable(typeof(SetComplexFormComponentChange))]
+[JsonSerializable(typeof(CreateComplexFormType))]
+[JsonSerializable(typeof(Changes.SetOrderChange<Sense>))]
+[JsonSerializable(typeof(Changes.SetOrderChange<ExampleSentence>))]
+[JsonSerializable(typeof(Changes.SetOrderChange<ComplexFormComponent>))]
 public partial class JsonSourceGenerationContext: JsonSerializerContext
 {
 
