8b17395c6c23a67517fe8566bc1f4297f1c2bbdd
Kevin Hahn <kevin_hahn@sil.org>
2025-04-18 15:43:40 +0700

    update to ef 9.0.4

--- backend/FwLite/LcmDebugger/Program.cs
+++ backend/FwLite/LcmDebugger/Program.cs
@@ -7,6 +7,7 @@ using LcmCrdt;
 using LexCore.Exceptions;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Refit;
 using SIL.Harmony;
