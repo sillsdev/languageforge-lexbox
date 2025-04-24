17089d7f9e89387640a588741ca3454fda18b31d
Tim Haasdyk <tim_haasdyk@sil.org>
2025-04-24 16:05:47 +0200

    Fix wrong class name in commented-out code

--- backend/FwLite/LcmDebugger/Program.cs
+++ backend/FwLite/LcmDebugger/Program.cs
@@ -47,6 +47,6 @@ var fwDataFactory = app.Services.GetRequiredService<FwDataFactory>();
 // var miniLcmApi = fwDataFactory.GetFwDataMiniLcmApi(fwDataProject, false);
 // var entries = await miniLcmApi.GetEntries().ToArrayAsync();
 // var entries = miniLcmApi.GetEntries(new(Filter: new() { GridifyFilter = "LexemeForm[sbe]=ta" })).ToArrayAsync();
-// var entry = Tools.GetLexEntry(app.Services, fwDataProject, new Guid("{018d71a9-12c2-4129-be8a-35fe246afda2}"));
+// var entry = Utils.GetLexEntry(app.Services, fwDataProject, new Guid("{018d71a9-12c2-4129-be8a-35fe246afda2}"));
 
 fwDataFactory.Dispose();
