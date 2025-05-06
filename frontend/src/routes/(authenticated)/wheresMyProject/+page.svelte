97d96b23b89f697f3761a53b23aa7b7c51900424
Robin Munn <rmunn@pobox.com>
2025-05-06 16:30:07 +0700

    Add "Where's my project?" page to Lexbox
    Page content is placeholder for now

---
+++ frontend/src/routes/(authenticated)/wheresMyProject/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { TitlePage, HomeBreadcrumb } from '$lib/layout';
+  import t from '$lib/i18n';
+  import Markdown from 'svelte-exmarkdown';
+  import FeatureFlagAlternateContent from '$lib/layout/FeatureFlagAlternateContent.svelte';
+</script>
+
+<HomeBreadcrumb />
+<TitlePage title={$t('where_is_my_project.title')}>
+  <div class="prose text-lg">
+    <FeatureFlagAlternateContent flag="FwLiteBeta">
+      {#snippet hasFlagContent()}
+        <Markdown md={$t('where_is_my_project.body')} />
+      {/snippet}
+      {#snippet missingFlagContent()}
+        <Markdown md={$t('where_is_my_project.user_not_in_beta')} />
+      {/snippet}
+    </FeatureFlagAlternateContent>
+  </div>
+</TitlePage>
+
+<style>
+</style>
