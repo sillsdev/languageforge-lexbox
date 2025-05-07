ec415b39a922d9a5bae52302c37491b8d0f94748
Tim Haasdyk <tim_haasdyk@sil.org>
2025-05-07 08:56:16 +0200

    Make FwLiteBetaRequestEmail mutation happy

--- frontend/src/routes/(authenticated)/wheresMyProject/+page.ts
+++ frontend/src/routes/(authenticated)/wheresMyProject/+page.ts
@@ -1,31 +1,16 @@
-import { getClient, graphql } from '$lib/gql';
+import type {$OpResult, SendFwLiteBetaRequestEmailMutation} from '$lib/gql/types';
+import {getClient, graphql} from '$lib/gql';
 
-// import type {PageLoadEvent} from './$types';
-// import {type LexAuthUser} from '$lib/user';
-// import {redirect} from '@sveltejs/kit';
-// import {getBoolSearchParam, getSearchParam} from '$lib/util/query-params';
-// import {isGuid} from '$lib/util/guid';
-import type {
-  $OpResult,
-  SendFwLiteBetaRequestEmailPayload,
-  // SendFwLiteBetaRequestEmailMutation, // TODO Why isn't this one being created?
-} from '$lib/gql/types';
-// import type {LoadAdminDashboardProjectsQuery, LoadAdminDashboardUsersQuery} from '$lib/gql/types';
-// import type {ProjectFilters} from '$lib/components/Projects';
-// import {DEFAULT_PAGE_SIZE} from '$lib/components/Paging';
-// import type {AdminTabId} from './AdminTabs.svelte';
-// import {derived, readable} from 'svelte/store';
-// import type {UserType} from '$lib/components/Users/UserFilter.svelte';
-import type { UUID } from 'crypto';
+import type {UUID} from 'crypto';
 
-export async function _sendFWLiteBetaRequestEmail(userId: UUID, name: string): $OpResult<unknown> { // SendFwLiteBetaRequestEmailPayload
+export async function _sendFWLiteBetaRequestEmail(userId: UUID, name: string): $OpResult<SendFwLiteBetaRequestEmailMutation> { // SendFwLiteBetaRequestEmailPayload
   //language=GraphQL
   const result = await getClient()
     .mutation(
       graphql(`
         mutation SendFWLiteBetaRequestEmail($input: SendFWLiteBetaRequestEmailInput!) {
-          sendFWLiteBetaRequestEmail(input: $input)
-            user {
+          sendFWLiteBetaRequestEmail(input: $input) {
+            meDto {
               id
               email
             }
@@ -38,7 +23,7 @@ export async function _sendFWLiteBetaRequestEmail(userId: UUID, name: string): $
           }
         }
       `),
-      { input: { userId, name } }
+      {input: {userId, name}}
     )
   return result;
 }
