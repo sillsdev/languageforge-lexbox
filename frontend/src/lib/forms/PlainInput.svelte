e9db06c465b9f3d3f01d61252ca2d3cc8b652134
Robin Munn <rmunn@pobox.com>
2025-05-09 14:28:08 +0700

    Make PlainInput plain again

--- frontend/src/lib/forms/PlainInput.svelte
+++ frontend/src/lib/forms/PlainInput.svelte
@@ -1,13 +1,10 @@
 <script lang="ts">
   import { randomFormId } from './utils';
-  import { DEFAULT_DEBOUNCE_TIME } from '$lib/util/time';
-  import { Debounced } from 'runed';
 
   let input: HTMLInputElement | undefined = $state();
 
   export function clear(): void {
-    debouncer.setImmediately(undefined);
-    inputValue = undefined;
+    value = undefined;
   }
 
   export function focus(): void {
@@ -17,7 +14,6 @@
   interface Props {
     id?: string;
     value?: string | null;
-    onInput?: () => void;
     type?: 'text' | 'email' | 'password';
     autofocus?: true;
     readonly?: boolean;
@@ -26,47 +22,20 @@
     // Despite the compatibility table, 'new-password' seems to work well in Chrome, Edge & Firefox
     // https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/autocomplete#browser_compatibility
     autocomplete?: 'new-password' | 'current-password' | 'off';
-    debounce?: number | boolean;
-    debouncing?: boolean;
-    undebouncedValue?: string | null;
     style?: string;
-    keydownHandler?: (event: KeyboardEvent) => void;
   }
 
   let {
     id = randomFormId(),
     value = $bindable(),
-    onInput,
     type = 'text',
     autofocus,
     readonly = false,
     error,
     placeholder = '',
     autocomplete,
-    debounce = false,
-    debouncing = $bindable(false),
-    undebouncedValue = $bindable(),
     style,
-    keydownHandler,
   }: Props = $props();
-
-  const debounceTime: number = $derived(typeof debounce === 'boolean' ? DEFAULT_DEBOUNCE_TIME : debounce);
-
-  // TODO: Now that we've switched to DebouncedInput, get rid of debouncer here and verify that everything still works
-  let inputValue = $state(value);
-  let debouncer = new Debounced(
-    () => inputValue,
-    () => debounceTime,
-  );
-  $effect(() => {
-    undebouncedValue = inputValue;
-    // The !! part below is so that undefined/null and '' will be considered equal for this purpose
-    debouncing = (!!inputValue || !!value) && inputValue != value;
-  });
-  $effect(() => {
-    value = debouncer.current;
-    debouncing = false;
-  });
 </script>
 
 <!-- https://daisyui.com/components/input -->
@@ -75,13 +44,11 @@
   bind:this={input}
   {id}
   {type}
-  bind:value={inputValue}
+  bind:value
   class:input-error={error && error.length}
-  oninput={onInput}
   {placeholder}
   class="input input-bordered {style ?? ''}"
   {readonly}
   {autofocus}
   {autocomplete}
-  onkeydown={keydownHandler}
 />
