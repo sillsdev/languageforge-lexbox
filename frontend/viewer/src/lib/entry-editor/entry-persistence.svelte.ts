6084d2e1a46e218c3f379bbb38436afd448567d1
Kevin Hahn <kevin_hahn@sil.org>
2025-05-16 14:05:42 +0700

    make entry persistence service to attach to entry editor

---
+++ frontend/viewer/src/lib/entry-editor/entry-persistence.svelte.ts
@@ -0,0 +1,54 @@
+import { type Props } from './object-editors/EntryEditor.svelte';
+import { useLexboxApi } from '$lib/services/service-provider';
+import { useSaveHandler } from '$lib/services/save-event-service.svelte';
+import type { Getter } from 'runed';
+import type { IEntry } from '$lib/dotnet-types';
+
+export class EntryPersistence {
+  lexboxApi = useLexboxApi();
+  saveHandler = useSaveHandler();
+  initialEntry: IEntry | undefined = undefined;
+  constructor(private entryGetter: Getter<IEntry | undefined>, private onRefresh: () => void = () => { }) {
+    $effect(() => {
+      if (this.initialEntry) return;
+      if (entryGetter()) this.updateInitialEntry();
+    });
+  }
+
+  private get entry(): IEntry {
+    const entry = this.entryGetter();
+    if (!entry) throw new Error('Entry not found');
+    return entry;
+  }
+
+  get entryEditorProps(): Partial<Props> {
+    return {
+      onchange: async changed => {
+        await this.updateEntry(changed.entry);
+        this.onRefresh();
+        this.updateInitialEntry();
+      },
+      ondelete: async (e) => {
+        if (e.example !== undefined && e.sense !== undefined) {
+          await this.saveHandler.handleSave(() => this.lexboxApi.deleteExampleSentence(e.entry.id, e.sense!.id, e.example!.id));
+        } else if (e.sense !== undefined) {
+          await this.saveHandler.handleSave(() => this.lexboxApi.deleteSense(e.entry.id, e.sense!.id));
+        } else {
+          await this.saveHandler.handleSave(() => this.lexboxApi.deleteEntry(e.entry.id));
+          this.onRefresh();
+          return;
+        }
+        this.updateInitialEntry();
+      }
+    };
+  }
+
+  async updateEntry(updatedEntry: IEntry) {
+    if (this.initialEntry.id != updatedEntry.id) throw new Error('Entry id mismatch');
+    await this.saveHandler.handleSave(() => this.lexboxApi.updateEntry(this.initialEntry, updatedEntry));
+  }
+
+  updateInitialEntry() {
+    this.initialEntry = JSON.parse(JSON.stringify(this.entry)) as IEntry;
+  }
+}
