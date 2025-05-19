7a4b9f5c9c6259feb09b922e9bec895729596f6a
Tim Haasdyk <tim_haasdyk@sil.org>
2025-05-19 09:42:07 +0200

    Finalize shadcn buttons (#1670)
    * Fix delete dialog close detection
    
    * Wire up sidebar and hide unimplemented content
    
    * Migrate home-page app-bar buttons to shadcn
    
    * Use function instead of prop to open troubleshooting dialog
    
    * Fix broken enter animation due to troubleshooting dialog resize after open.
    
    * Introduce responsive-menu for entry-menu
    
    * Add Open in FieldWorks button
    
    * Fix logo doesn't respond to dark-mode
    
    * Condense home-view app-bar
    
    * Make radio buttons bigger on mobile
    
    * only show Open In Flex button when supported
    
    ---------
    
    Co-authored-by: Kevin Hahn <kevin_hahn@sil.org>

--- frontend/viewer/src/project/browse/EntryMenu.svelte
+++ frontend/viewer/src/project/browse/EntryMenu.svelte
@@ -1,13 +1,5 @@
 <script lang="ts">
-  import { Icon } from '$lib/components/ui/icon';
-  import { type IconClass } from '$lib/icon-class';
-  import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
-  import * as Drawer from '$lib/components/ui/drawer';
-  import * as ContextMenu from '$lib/components/ui/context-menu';
-  import { IsMobile } from '$lib/hooks/is-mobile.svelte';
   import { t } from 'svelte-i18n-lingui';
-  import { buttonVariants } from '$lib/components/ui/button';
-  import Button from '$lib/components/ui/button/button.svelte';
   import {useMultiWindowService} from '$lib/services/multi-window-service';
   import type {IEntry} from '$lib/dotnet-types';
   import type {Snippet} from 'svelte';
@@ -19,6 +11,10 @@
   import {useCurrentView} from '$lib/views/view-service';
   import {useFeatures} from '$lib/services/feature-service';
   import HistoryView from '$lib/history/HistoryView.svelte';
+  import * as ResponsiveMenu from '$lib/components/responsive-menu';
+  import {useAppLauncherService} from '$lib/services/app-launcher-service';
+  import {IsMobile} from '$lib/hooks/is-mobile.svelte';
+  import OpenInFieldWorksButton from '$lib/OpenInFieldWorksButton.svelte';
 
   const multiWindowService = useMultiWindowService();
   const dialogsService = useDialogsService();
@@ -36,7 +32,6 @@
   const headword = $derived((entry && writingSystemService.headword(entry)) || $t`Untitled`);
 
   let open = $state(false);
-  const triggerVariant = buttonVariants({ variant: 'ghost', size: 'icon' });
 
   async function onDelete() {
     if (!await dialogsService.promptDelete($t`Entry`, headword)) return;
@@ -45,77 +40,35 @@
   }
 
   const features = useFeatures();
-  const showHistoryView = $state(false);
+  let showHistoryView = $state(false);
+  const appLauncher = useAppLauncherService();
 </script>
 {#if features.history}
   <HistoryView bind:open={showHistoryView} id={entry.id}/>
 {/if}
-{#snippet items()}
-  {@render menuItem('i-mdi-delete', pt($t`Delete Entry`, $t`Delete Word`, $currentView), () => void onDelete())}
-  {#if features.history}
-    {@render menuItem('i-mdi-history', $t`History`, () => showHistoryView = true)}
-  {/if}
-  {#if multiWindowService}
-    {@render menuItem('i-mdi-open-in-new', $t`Open in new Window`, () => void multiWindowService.openEntryInNewWindow(entry.id))}
-  {/if}
-{/snippet}
 
-{#snippet menuItem(icon: IconClass, label: string, onSelect: () => void)}
-  {#if contextMenu}
-    <ContextMenu.Item class="cursor-pointer" onclick={onSelect}>
-      <Icon icon={icon} class="mr-2"/>
-      {label}
-    </ContextMenu.Item>
-  {:else if !IsMobile.value}
-    <DropdownMenu.Item class="cursor-pointer" {onSelect}>
-      <Icon {icon} class="mr-2" />
-      {label}
-    </DropdownMenu.Item>
-  {:else}
-    <Button
-      onclick={onSelect}
-      variant="ghost"
-      class="w-full justify-start"
-      {icon}
-    >
-      {label}
-    </Button>
-  {/if}
-{/snippet}
-{#if contextMenu}
-  <ContextMenu.Root>
-    <ContextMenu.Trigger>
-      {@render children?.()}
-    </ContextMenu.Trigger>
-    <ContextMenu.Content>
-      {@render items()}
-    </ContextMenu.Content>
-  </ContextMenu.Root>
-{:else if !IsMobile.value}
-  <DropdownMenu.Root bind:open>
-    <DropdownMenu.Trigger class={triggerVariant}>
-      <Icon icon="i-mdi-dots-vertical" />
-    </DropdownMenu.Trigger>
-    <DropdownMenu.Content align="end">
-      {@render items()}
-    </DropdownMenu.Content>
-  </DropdownMenu.Root>
-{:else}
-  <Drawer.Root bind:open>
-    <Drawer.Trigger class={triggerVariant}>
-      <Icon icon="i-mdi-dots-vertical" />
-    </Drawer.Trigger>
-    <Drawer.Content>
-      <div class="mx-auto w-full max-w-sm p-4">
-        <Drawer.Header class="justify-items-end">
-          <Drawer.Close class={buttonVariants({ variant: 'ghost', size: 'icon' })}><Icon icon="i-mdi-close" /></Drawer.Close>
-        </Drawer.Header>
-        <div class="space-y-2 flex flex-col gap-2">
-          {@render items()}
-        </div>
-        <Drawer.Footer>
-        </Drawer.Footer>
-      </div>
-    </Drawer.Content>
-  </Drawer.Root>
-{/if}
+<ResponsiveMenu.Root {contextMenu} bind:open>
+  <ResponsiveMenu.Trigger {children} />
+  <ResponsiveMenu.Content>
+    <ResponsiveMenu.Item icon="i-mdi-delete" onSelect={onDelete}>
+      {pt($t`Delete Entry`, $t`Delete Word`, $currentView)}
+    </ResponsiveMenu.Item>
+    {#if features.history}
+      <ResponsiveMenu.Item icon="i-mdi-history" onSelect={() => showHistoryView = true}>
+        {$t`History`}
+      </ResponsiveMenu.Item>
+    {/if}
+    {#if multiWindowService}
+      <ResponsiveMenu.Item icon="i-mdi-open-in-new" onSelect={() => void multiWindowService.openEntryInNewWindow(entry.id)}>
+        {$t`Open in new Window`}
+      </ResponsiveMenu.Item>
+    {/if}
+    {#if features.openWithFlex && (appLauncher || !IsMobile.value)}
+      <ResponsiveMenu.Item>
+        {#snippet child({props})}
+          <OpenInFieldWorksButton {entry} {...props} />
+        {/snippet}
+      </ResponsiveMenu.Item>
+    {/if}
+  </ResponsiveMenu.Content>
+</ResponsiveMenu.Root>
