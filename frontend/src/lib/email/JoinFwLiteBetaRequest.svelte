5f5eb579909a4d9caf29309f6d43e680e328b8cf
Tim Haasdyk <tim_haasdyk@sil.org>
2025-05-19 15:13:30 +0200

    Fix up request email

--- frontend/src/lib/email/JoinFwLiteBetaRequest.svelte
+++ frontend/src/lib/email/JoinFwLiteBetaRequest.svelte
@@ -8,7 +8,7 @@
   let approveUrl = new URL(`/admin/?userSearch=${encodeURIComponent(email)}`, baseUrl);
 </script>
 
-<Email subject={$t('emails.join_fw_lite_beta_request_email.subject')} {name}>
+<Email subject={$t('emails.join_fw_lite_beta_request_email.subject', { name })}>
   <mj-text>{$t('emails.join_fw_lite_beta_request_email.body', { name })}</mj-text>
   <mj-button href={approveUrl}>{$t('emails.join_fw_lite_beta_request_email.approve_button')}</mj-button>
 </Email>
