582b3746d37b51b2f1300ba660724aa7634a0528
Kevin Hahn <kevin_hahn@sil.org>
2025-05-28 10:27:01 +0700

    add ExampleSentenceTestsBase.cs to catch issues creating examples without references

---
+++ backend/FwLite/MiniLcm.Tests/ExampleSentenceTestsBase.cs
@@ -0,0 +1,42 @@
+namespace MiniLcm.Tests;
+
+public abstract class ExampleSentenceTestsBase : MiniLcmTestBase
+{
+    private readonly Guid _entryId = Guid.NewGuid();
+    private readonly Guid _senseId = Guid.NewGuid();
+
+    public override async Task InitializeAsync()
+    {
+        await base.InitializeAsync();
+        await Api.CreateEntry(new Entry()
+        {
+            Id = _entryId,
+            LexemeForm = { { "en", "new-lexeme-form" } },
+            Senses =
+            [
+                new Sense() { Id = _senseId, Gloss = { { "en", "new-sense-gloss" } } }
+            ]
+        });
+    }
+
+    [Fact]
+    public async Task CanCreateExampleSentence()
+    {
+        var expectedExampleSentence = new ExampleSentence()
+        {
+            Reference = new RichString("This is a reference", "en"),
+            Sentence = { { "en", new RichString("test", "en") } },
+            Translation = { { "en", new RichString("test", "en") } }
+        };
+        var actualSentence = await Api.CreateExampleSentence(_entryId, _senseId, expectedExampleSentence);
+        actualSentence.Should().BeEquivalentTo(expectedExampleSentence);
+    }
+
+    [Fact]
+    public async Task CanCreateEmptyExampleSentence()
+    {
+        var expectedExampleSentence = new ExampleSentence();
+        var actualSentence = await Api.CreateExampleSentence(_entryId, _senseId, expectedExampleSentence);
+        actualSentence.Should().BeEquivalentTo(expectedExampleSentence);
+    }
+}
