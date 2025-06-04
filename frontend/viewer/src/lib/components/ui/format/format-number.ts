3c7797c06cc05eb05c67deb2a7c4a6f3307989f8
Tim Haasdyk <tim_haasdyk@sil.org>
2025-06-04 12:00:58 +0200

    Format numbers with lingui/core

--- frontend/viewer/src/lib/components/ui/format/format-number.ts
+++ frontend/viewer/src/lib/components/ui/format/format-number.ts
@@ -1,14 +1,7 @@
-import {fromStore} from 'svelte/store';
-import {locale} from 'svelte-i18n-lingui';
-
-const stateLocale = fromStore(locale);
+import {i18n} from '@lingui/core';
 
 export function formatNumber(value: number | undefined, options?: Intl.NumberFormatOptions, defaultValue = ''): string {
   if (value === undefined) return defaultValue;
 
-  return new Intl.NumberFormat(stateLocale.current, {
-    style: 'decimal',
-    useGrouping: true,
-    ...options,
-  }).format(value);
+  return i18n.number(value, options);
 }
