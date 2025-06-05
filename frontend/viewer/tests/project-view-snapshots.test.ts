0fbf8eb44ef2def4455668d81ceaaa1b888898a6
Kevin Hahn <kevin_hahn@sil.org>
2025-06-05 12:21:21 +0700

    add helpers for marketing screenshots

--- frontend/viewer/tests/project-view-snapshots.test.ts
+++ frontend/viewer/tests/project-view-snapshots.test.ts
@@ -1,32 +1,27 @@
 ﻿import {type Page, expect, test} from '@playwright/test';
 import { assertScreenshot } from './snapshot';
 
-const viewports = [
-  {height: 720, width: 1280, name: 'medium'},
-  {height: 812, width: 375, name: 'iphone-11'},
-];
+for (const colorScheme of ['light', 'dark'] as const) {
+  test(`ui snapshot selected entry (color: ${colorScheme})`, async ({page}) => {
+    await page.emulateMedia({colorScheme});
+    await page.goto('/testing/project-view');
+    await waitForProjectViewReady(page);
+    await page.getByRole('row', {name: 'ambuka to cross a body of'}).click();
+    await expect(page.getByRole('heading', {name: 'ambuka'})).toBeVisible();
+    await expect(page.locator('.i-mdi-dots-vertical')).toBeVisible();
+    await expect(page.locator('.i-mdi-loading')).toHaveCount(0);
+    await assertScreenshot(page, 'project-view-entry-selected-' + colorScheme);
+  });
 
-  for (const colorScheme of ['light', 'dark'] as const) {
-    test(`ui snapshot selected entry (color: ${colorScheme})`, async ({page}) => {
-      await page.emulateMedia({colorScheme});
-      await page.goto('/testing/project-view');
-      await waitForProjectViewReady(page);
-      await page.getByRole('row', {name: 'ambuka to cross a body of'}).click();
-      await expect(page.getByRole('heading', {name: 'ambuka'})).toBeVisible();
-      await expect(page.locator('.i-mdi-dots-vertical')).toBeVisible();
-      await expect(page.locator('.i-mdi-loading')).toHaveCount(0);
-      await assertScreenshot(page, 'project-view-entry-selected-' + colorScheme);
-    });
-
-    test(`ui snapshot default (color: ${colorScheme})`, async ({page}) => {
-      await page.emulateMedia({colorScheme});
-      await page.goto('/testing/project-view');
-      await waitForProjectViewReady(page);
-      await expect(page.getByRole('textbox', {name: 'Filter'})).toBeVisible();
-      await page.getByRole('row', { name: 'ambuka to cross a body of' }).scrollIntoViewIfNeeded();
-      await assertScreenshot(page, 'project-view-default-' + colorScheme);
-    });
-  }
+  test(`ui snapshot default (color: ${colorScheme})`, async ({page}) => {
+    await page.emulateMedia({colorScheme});
+    await page.goto('/testing/project-view');
+    await waitForProjectViewReady(page);
+    await expect(page.getByRole('textbox', {name: 'Filter'})).toBeVisible();
+    await page.getByRole('row', { name: 'ambuka to cross a body of' }).scrollIntoViewIfNeeded();
+    await assertScreenshot(page, 'project-view-default-' + colorScheme);
+  });
+}
 
 
 async function waitForProjectViewReady(page: Page) {
