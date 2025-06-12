7e6c59c268690cf83a37230de60afa48d4688e8c
Tim Haasdyk <tim_haasdyk@sil.org>
2025-06-12 21:24:32 +0200

    Migrate writing-system-dialog (rough and untested)

--- frontend/viewer/src/lib/writing-system/WritingSystemDialog.svelte
+++ frontend/viewer/src/lib/writing-system/WritingSystemDialog.svelte
@@ -1,24 +1,30 @@
 ﻿<script lang="ts">
-  import {Button, Dialog, SelectField} from 'svelte-ux';
   import WritingSystemEditor from '$lib/writing-system/WritingSystemEditor.svelte';
   import {useWritingSystemService} from '$lib/writing-system-service.svelte';
   import {type IWritingSystem, WritingSystemType} from '$lib/dotnet-types';
   import {defaultWritingSystem} from '$lib/utils';
+  import {Button} from '$lib/components/ui/button';
+  import * as Dialog from '$lib/components/ui/dialog';
+  import Select from '$lib/components/field-editors/select.svelte';
+  import {Label} from '$lib/components/ui/label';
 
-  export let open = false;
-  let loading = false;
+  interface Props {
+    open?: boolean;
+  }
+
+  let { open = $bindable(false) }: Props = $props();
+
   const writingSystemService = useWritingSystemService();
   let vernacular = writingSystemService.vernacular;
   let analysis = writingSystemService.analysis;
-  let options: Array<{ value: IWritingSystem, label: string, group: 'Vernacular' | 'Analysis' }> = [];
   let newVernacular = defaultWritingSystem(WritingSystemType.Vernacular);
   let newAnalysis = defaultWritingSystem(WritingSystemType.Analysis);
-  $: options = [
-    ...vernacular.map(ws => mapOption(ws)),
-    {value: newVernacular, label: 'New Vernacular Writing System', group: 'Vernacular'},
-    ...analysis.map(ws => mapOption(ws)),
-    {value: newAnalysis, label: 'New Analysis Writing System', group: 'Analysis'},
-  ];
+  let options: Array<{ value: IWritingSystem, label: string, group: 'Vernacular' | 'Analysis' }> = $derived([
+      ...vernacular.map(ws => mapOption(ws)),
+      {value: newVernacular, label: 'New Vernacular Writing System', group: 'Vernacular'},
+      ...analysis.map(ws => mapOption(ws)),
+      {value: newAnalysis, label: 'New Analysis Writing System', group: 'Analysis'},
+    ]);
 
   function mapOption(ws: IWritingSystem): {
     value: IWritingSystem,
@@ -28,7 +34,7 @@
     return {value: ws, label: `${ws.name} (${ws.wsId})`, group: ws.type === WritingSystemType.Vernacular ? 'Vernacular' : 'Analysis'};
   }
 
-  let selectedOption: IWritingSystem = vernacular[0] ?? analysis[0];
+  let selectedOption: IWritingSystem = $state(vernacular[0] ?? analysis[0]);
 
   function onCreate(_writingSystem: IWritingSystem) {
     //todo need a better way to invalidate the writing system service
@@ -40,15 +46,24 @@
   }
 </script>
 
-<Dialog bind:open={open} {loading} persistent={loading} style="height: auto">
-  <div slot="title">Edit Writing Systems</div>
-  <SelectField label="Writing System" bind:value={selectedOption} {options} clearable={false}/>
-  <WritingSystemEditor
-    writingSystem={selectedOption}
-    newWs={selectedOption === newVernacular || selectedOption === newAnalysis}
-    on:create={(e) => onCreate(e.detail.writingSystem)}
-  on:change={(e) => onChange(e.detail.writingSystem)}/>
-  <div slot="actions">
-    <Button on:click={() => open = false}>Close</Button>
-  </div>
-</Dialog>
+<Dialog.Root bind:open={open}>
+  <Dialog.Content>
+    <Dialog.Header>
+      <Dialog.Title>Edit Writing Systems</Dialog.Title>
+    </Dialog.Header>
+
+    <Label>
+      Writing System
+      <Select bind:value={selectedOption} idSelector="id" labelSelector={(ws) => `${ws.wsId} - ${ws.name} (${ws.type})`} options={options.map(option => option.value)} />
+    </Label>
+    <WritingSystemEditor
+      writingSystem={selectedOption}
+      newWs={selectedOption === newVernacular || selectedOption === newAnalysis}
+      oncreate={onCreate}
+      onchange={onChange}
+    />
+    <Dialog.Footer>
+      <Button onclick={() => open = false}>Close</Button>
+    </Dialog.Footer>
+  </Dialog.Content>
+</Dialog.Root>
