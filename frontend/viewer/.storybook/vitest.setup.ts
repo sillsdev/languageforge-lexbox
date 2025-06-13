d0474f22a847a4a3c6d2b73d3a4eb14073c1d1b7
Tim Haasdyk <tim_haasdyk@sil.org>
2025-06-13 11:11:03 +0200

    Restore orignal console.warn

--- frontend/viewer/.storybook/vitest.setup.ts
+++ frontend/viewer/.storybook/vitest.setup.ts
@@ -1,6 +1,6 @@
 import * as previewAnnotations from './preview';
 
-import {beforeAll} from 'vitest';
+import {afterAll, beforeAll} from 'vitest';
 import {setProjectAnnotations} from '@storybook/svelte-vite';
 
 const annotations = setProjectAnnotations([previewAnnotations]);
@@ -15,11 +15,18 @@ const ignoredWarnings = [
 ];
 
 const originalWarn = console.warn.bind(console);
-console.warn = (...args: any[]) => {
-  const isIgnoredWarning = args.some(arg => typeof arg === 'string' && ignoredWarnings.some(ignored => arg.includes(ignored)));
-  if (isIgnoredWarning) {
-    const outOfOurControl = args.some(arg => typeof arg === 'string' && arg.includes('node_modules'));
-    if (outOfOurControl) return; // swallow the noise
-  }
-  originalWarn(...args);
-};
+
+beforeAll(() => {
+  console.warn = (...args: any[]) => {
+    const isIgnoredWarning = args.some(arg => typeof arg === 'string' && ignoredWarnings.some(ignored => arg.includes(ignored)));
+    if (isIgnoredWarning) {
+      const outOfOurControl = args.some(arg => typeof arg === 'string' && arg.includes('node_modules'));
+      if (outOfOurControl) return; // swallow the noise
+    }
+    originalWarn(...args);
+  };
+});
+
+afterAll(() => {
+  console.warn = originalWarn; // Restore the original console.warn
+});
