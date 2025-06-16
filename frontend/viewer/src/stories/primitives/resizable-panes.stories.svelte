4b2fae2f2c3e28f63b0399d1206a56ecea9a09c8
Tim Haasdyk <tim_haasdyk@sil.org>
2025-06-16 14:16:50 +0200

    Fix panes story args

--- frontend/viewer/src/stories/primitives/resizable-panes.stories.svelte
+++ frontend/viewer/src/stories/primitives/resizable-panes.stories.svelte
@@ -10,21 +10,26 @@
       // eslint-disable-next-line @typescript-eslint/naming-convention
       'Pane': Resizable.Pane,
     },
+    args: {
+      direction: 'horizontal',
+    }
   });
 </script>
 
-<Story name="Resizable Panes" asChild>
-  <Resizable.PaneGroup direction="horizontal" class="h-[200px] border rounded-lg">
-    <Resizable.Pane defaultSize={20} class="bg-muted p-4">
-      Left Pane
-    </Resizable.Pane>
-    <Resizable.Handle withHandle />
-    <Resizable.Pane defaultSize={30} class="bg-muted p-4">
-      Middle Pane
-    </Resizable.Pane>
-    <Resizable.Handle withHandle />
-    <Resizable.Pane class="bg-muted p-4">
-      Right Pane
-    </Resizable.Pane>
-  </Resizable.PaneGroup>
+<Story name="Resizable Panes">
+  {#snippet template(args)}
+    <Resizable.PaneGroup {...args} class="h-[200px] border rounded-lg min-h-80">
+      <Resizable.Pane defaultSize={20} class="bg-muted p-4">
+        Left Pane
+      </Resizable.Pane>
+      <Resizable.Handle withHandle />
+      <Resizable.Pane defaultSize={30} class="bg-muted p-4">
+        Middle Pane
+      </Resizable.Pane>
+      <Resizable.Handle withHandle />
+      <Resizable.Pane class="bg-muted p-4">
+        Right Pane
+      </Resizable.Pane>
+    </Resizable.PaneGroup>
+  {/snippet}
 </Story>
