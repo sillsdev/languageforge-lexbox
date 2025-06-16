e0e65f316d3a2ab33cf461b8fcea15c0edb70d42
Tim Haasdyk <tim_haasdyk@sil.org>
2025-06-16 16:05:46 +0200

    Revert sandbox changes

--- frontend/viewer/src/lib/sandbox/Sandbox.svelte
+++ frontend/viewer/src/lib/sandbox/Sandbox.svelte
@@ -1,26 +1,38 @@
 <script lang="ts">
-  import DialogsProvider from '$lib/DialogsProvider.svelte';
   import OverrideFields from '$lib/OverrideFields.svelte';
-  import ThemePicker from '$lib/ThemePicker.svelte';
-  import {EditorGrid} from '$lib/components/editor';
-  import IfOnce from '$lib/components/if-once/if-once.svelte';
   import {Button, buttonVariants} from '$lib/components/ui/button';
   import {Checkbox} from '$lib/components/ui/checkbox';
-  import * as Dialog from '$lib/components/ui/dialog';
-  import {Switch} from '$lib/components/ui/switch';
-  import {DotnetService, type ISense} from '$lib/dotnet-types';
+  import {DotnetService, type IEntry, type ISense} from '$lib/dotnet-types';
   import type {FieldIds} from '$lib/entry-editor/field-data';
   import SenseEditorPrimitive from '$lib/entry-editor/object-editors/SenseEditorPrimitive.svelte';
   import {InMemoryApiService} from '$lib/in-memory-api-service';
   import {AppNotification} from '$lib/notifications/notifications';
   import {tryUseService} from '$lib/services/service-provider';
-  import {useBackHandler} from '$lib/utils/back-handler.svelte';
   import {delay} from '$lib/utils/time';
-  import {QueryParamState} from '$lib/utils/url.svelte';
   import {initView, initViewSettings} from '$lib/views/view-service';
   import {dndzone} from 'svelte-dnd-action';
-  import {T} from 'svelte-i18n-lingui';
+  import * as Resizable from '$lib/components/ui/resizable';
+  import LcmRichTextEditor from '$lib/components/lcm-rich-text-editor/lcm-rich-text-editor.svelte';
+  import {lineSeparator} from '$lib/components/lcm-rich-text-editor/lcm-rich-text-editor.svelte';
+  import type {IRichString} from '$lib/dotnet-types/generated-types/MiniLcm/Models/IRichString';
+  import ThemePicker from '$lib/ThemePicker.svelte';
+  import {EditorGrid} from '$lib/components/editor';
+  import EditorSandbox from './EditorSandbox.svelte';
+  import EntryOrSensePicker, {type EntrySenseSelection} from '$lib/entry-editor/EntryOrSensePicker.svelte';
+  import {useWritingSystemService} from '$lib/writing-system-service.svelte';
+  import DialogsProvider from '$lib/DialogsProvider.svelte';
+  import {TabsList, TabsTrigger, Tabs} from '$lib/components/ui/tabs';
+  import {Reorderer} from '$lib/components/reorderer/index.js';
+  import {Switch} from '$lib/components/ui/switch';
+  import {QueryParamState} from '$lib/utils/url.svelte';
   import {Link} from 'svelte-routing';
+  import {useBackHandler} from '$lib/utils/back-handler.svelte';
+  import * as Dialog from '$lib/components/ui/dialog';
+  import {T} from 'svelte-i18n-lingui';
+  import IfOnce from '$lib/components/if-once/if-once.svelte';
+  import LocalizationPicker from '$lib/i18n/LocalizationPicker.svelte';
+  import {formatDate, FormatDate, formatNumber} from '$lib/components/ui/format';
+  import {SvelteDate} from 'svelte/reactivity';
 
 
   const testingService = tryUseService(DotnetService.TestingService);
@@ -37,6 +49,7 @@
   InMemoryApiService.setup();
   initView();
   initViewSettings();
+  const writingSystemService = useWritingSystemService();
 
   function makeSense(s: ISense) {
     return s;
@@ -56,7 +69,36 @@
     loading = false;
   }
 
+  const originalRichString: IRichString = {
+    spans: [{text: 'Hello', ws: 'en'}, {text: ' World', ws: 'js'}, {text: ` type ${lineSeparator}script`, ws: 'ts'}],
+  };
+  let richString: IRichString | undefined = $state(originalRichString);
+  let readonly = $state(false);
+  let selectedEntryHistory: EntrySenseSelection[] = $state([]);
+  let openPicker = $state(false);
+  let pickerMode: 'entries-and-senses' | 'only-entries' = $state('only-entries');
+
+  function disableEntry(entry: IEntry): false | { reason: string, disableSenses?: true } {
+    const selected = selectedEntryHistory.some(e => e.entry.id === entry.id);
+    if (!selected) return false;
+    return {
+      reason: 'You cannot select an entry that you have already selected',
+      disableSenses: true
+    };
+  }
+
   //reorderer
+  let direction: 'vertical' | 'horizontal' = $state('vertical');
+  let items: string[] = $state(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']);
+  let currentItem = $state('1');
+  let display: 'x100' | 'x1' = $state('x1');
+  function displayFunc(item: string) {
+    return (Number(item) * (display === 'x100' ? 100 : 1)).toString();
+  }
+  function randomItems() {
+    items = Array.from({length: 10}, () => (Math.random() * 100).toFixed(0));
+    currentItem = items[0];
+  }
 
 
 
@@ -68,9 +110,21 @@
   let dialogOpen = $state(false);
   useBackHandler({addToStack: () => dialogOpen, onBack: () => dialogOpen = false});
 
+  const variants = Object.keys(buttonVariants.variants.variant) as unknown as (keyof typeof buttonVariants.variants.variant)[];
+  const sizes = Object.keys(buttonVariants.variants.size) as unknown as (keyof typeof buttonVariants.variants.size)[];
+
+  let buttonsLoading = $state(false);
+  function testLoading() {
+    buttonsLoading = true;
+    setTimeout(() => {
+      buttonsLoading = false;
+    }, 1000);
+  }
+
   let show = $state(false);
   let reseter = $state(0);
 
+  let currentDate = new SvelteDate();
 </script>
 <DialogsProvider/>
 <div class="p-6 shadcn-root">
@@ -91,11 +145,86 @@
       </label>
     </div>
   </div>
+  <EditorSandbox />
+  <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+    <div>
+      <Button onclick={() => richString = {spans: [{text: 'test', ws: 'en'}]}}>Replace Rich Text</Button>
+      <Button onclick={() => richString = undefined}>Set undefined</Button>
+      <Button onclick={() => richString = originalRichString}>Reset</Button>
+      <label>
+        <Checkbox bind:checked={readonly}/> Readonly
+      </label>
+    </div>
+    <LcmRichTextEditor label="Test Rich Text Editor" bind:value={richString} {readonly}
+       onchange={() => richString = JSON.parse(JSON.stringify($state.snapshot(richString)))} />
+    <pre>{JSON.stringify(richString, null, 2)?.replaceAll(lineSeparator, '\n') ?? 'undefined'}</pre>
+  </div>
+  <div class="flex flex-col gap-2 border p-4 justify-between">
+    <h3 class="font-medium">Resizable Example</h3>
+    <Resizable.PaneGroup direction="horizontal" class="h-[200px] border rounded-lg">
+      <Resizable.Pane defaultSize={20} class="bg-muted p-4">
+        Left Pane
+      </Resizable.Pane>
+      <Resizable.Handle withHandle />
+      <Resizable.Pane defaultSize={30} class="bg-muted p-4">
+        Middle Pane
+      </Resizable.Pane>
+      <Resizable.Handle withHandle />
+      <Resizable.Pane class="bg-muted p-4">
+        Right Pane
+      </Resizable.Pane>
+    </Resizable.PaneGroup>
+  </div>
+  <div class="flex flex-col gap-2 border p-4 justify-between">
+    <h3 class="font-medium">Entry picker example</h3>
+
+    <Tabs bind:value={pickerMode} class="mb-1">
+      <TabsList>
+        <TabsTrigger value="only-entries">Entry only</TabsTrigger>
+        <TabsTrigger value="entries-and-senses">Entry or Sense</TabsTrigger>
+      </TabsList>
+    </Tabs>
+    <Button onclick={() => openPicker = true}>Open picker</Button>
+    <EntryOrSensePicker title="Test selecting something"
+                        bind:open={openPicker}
+                        disableEntry={disableEntry}
+                        mode={pickerMode}
+                        pick={(e) => selectedEntryHistory.push(e)}/>
+    <div>
+      {#each selectedEntryHistory as selected}
+        <p>
+          Entry: {writingSystemService.headword(selected.entry)}
+          {#if selected.sense}
+            Sense: {writingSystemService.firstGloss(selected.sense)}
+          {/if}
+        </p>
+      {/each}
+    </div>
+  </div>
   <div class="flex flex-col gap-2 border p-4 justify-between">
     <h3 class="font-medium">Reorder example</h3>
-
+    <Tabs bind:value={direction} class="mb-1">
+      <TabsList>
+        <TabsTrigger value="vertical">Vertical</TabsTrigger>
+        <TabsTrigger value="horizontal">Horizontal</TabsTrigger>
+      </TabsList>
+    </Tabs>
+    <Tabs bind:value={display} class="mb-1">
+      <TabsList>
+        <TabsTrigger value="x1">Times 1</TabsTrigger>
+        <TabsTrigger value="x100">Times 100</TabsTrigger>
+      </TabsList>
+    </Tabs>
+    <Button onclick={randomItems}>Randomize items</Button>
     <div>
-
+      <p>Current item: {currentItem}</p>
+      <Reorderer {direction}
+                 item={currentItem}
+                 bind:items
+                 getDisplayName={displayFunc}/>
+    </div>
+    <div>
+      <code>{JSON.stringify(items)}</code>
     </div>
   </div>
   <div class="flex flex-col gap-2 border p-4 justify-between">
@@ -185,6 +314,43 @@
           Error opening override fields {error}
         {/snippet}
       </svelte:boundary>
+    </div>
+
+    <div class="flex flex-col gap-2 border p-4 justify-between">
+      <div>
+        <h3>Buttons</h3>
+      </div>
+      <div><Button onclick={testLoading}>Test loading</Button></div>
+      <div class="flex gap-2 flex-wrap">
+        {#each variants as variant}
+          <Button loading={buttonsLoading} {variant}>{variant} button</Button>
+        {/each}
+        {#each variants as variant}
+          <Button loading={buttonsLoading} {variant} icon="i-mdi-auto-fix"></Button>
+        {/each}
+        {#each sizes as size}
+          <Button loading={buttonsLoading} {size}>Size: {size}</Button>
+        {/each}
+        {#each sizes as size}
+          <Button loading={buttonsLoading} {size} icon="i-mdi-auto-fix"/>
+        {/each}
+      </div>
+    </div>
+    <div class="flex flex-col gap-2 border p-4 justify-between">
+      <div>
+        <h3>Formatters</h3>
+      </div>
+      <div>
+        <LocalizationPicker/>
+      </div>
+      <div class="grid grid-cols-2 gap-x-4 gap-y-2 items-center">
+        <div class="font-medium">Date component:</div>
+        <div><FormatDate date={currentDate} options={{timeStyle: 'medium'}}/></div>
+        <div class="font-medium">Date function:</div>
+        <div>{formatDate(currentDate, {timeStyle: 'medium'})}</div>
+        <div class="font-medium">Number function:</div>
+        <div>{formatNumber(currentDate.getTime())}</div>
+      </div>
     </div>
   </div>
 </div>
