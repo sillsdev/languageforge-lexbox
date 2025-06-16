f2a0010923dd1ebdbb92631f65c1493caf977c6c
Kevin Hahn <kevin_hahn@sil.org>
2025-06-16 13:27:53 +0700

    update OpenIddict to 6.4.0

--- backend/LexBoxApi/Controllers/OauthController.cs
+++ backend/LexBoxApi/Controllers/OauthController.cs
@@ -56,12 +56,12 @@ public class OauthController(
         var result = await HttpContext.AuthenticateAsync();
         if (!result.Succeeded ||
             result.Principal.Identity?.IsAuthenticated is not true ||
-            request.HasPrompt(OpenIddictConstants.Prompts.Login) ||
+            request.HasPromptValue(OpenIddictConstants.PromptValues.Login) ||
             IsExpired(request, result))
         {
             // If the client application requested promptless authentication,
             // return an error indicating that the user is not logged in.
-            if (request.HasPrompt(OpenIddictConstants.Prompts.None))
+            if (request.HasPromptValue(OpenIddictConstants.PromptValues.None))
             {
                 return Forbid(
                     authenticationSchemes: OpenIddictServerAspNetCoreDefaults.AuthenticationScheme,
@@ -76,7 +76,7 @@ public class OauthController(
 
             // To avoid endless login -> authorization redirects, the prompt=login flag
             // is removed from the authorization request payload before redirecting the user.
-            var prompt = string.Join(" ", request.GetPrompts().Remove(OpenIddictConstants.Prompts.Login));
+            var prompt = string.Join(" ", request.GetPromptValues().Remove(OpenIddictConstants.PromptValues.Login));
 
             var parameters = Request.HasFormContentType
                 ? Request.Form.Where(parameter => parameter.Key != OpenIddictConstants.Parameters.Prompt).ToList()
@@ -117,7 +117,7 @@ public class OauthController(
             // return an authorization response without displaying the consent form.
             case OpenIddictConstants.ConsentTypes.Implicit:
             case OpenIddictConstants.ConsentTypes.External when authorizations.Count is not 0:
-            case OpenIddictConstants.ConsentTypes.Explicit when authorizations.Count is not 0 && !request.HasPrompt(OpenIddictConstants.Prompts.Consent):
+            case OpenIddictConstants.ConsentTypes.Explicit when authorizations.Count is not 0 && !request.HasPromptValue(OpenIddictConstants.PromptValues.Consent):
 
                 return await FinishSignIn(user, request, applicationId, authorizations);
 
@@ -135,8 +135,8 @@ public class OauthController(
 
             // At this point, no authorization was found in the database and an error must be returned
             // if the client application specified prompt=none in the authorization request.
-            case OpenIddictConstants.ConsentTypes.Explicit when request.HasPrompt(OpenIddictConstants.Prompts.None):
-            case OpenIddictConstants.ConsentTypes.Systematic when request.HasPrompt(OpenIddictConstants.Prompts.None):
+            case OpenIddictConstants.ConsentTypes.Explicit when request.HasPromptValue(OpenIddictConstants.PromptValues.None):
+            case OpenIddictConstants.ConsentTypes.Systematic when request.HasPromptValue(OpenIddictConstants.PromptValues.None):
                 return Forbid(
                     authenticationSchemes: OpenIddictServerAspNetCoreDefaults.AuthenticationScheme,
                     properties: new AuthenticationProperties(new Dictionary<string, string?>
