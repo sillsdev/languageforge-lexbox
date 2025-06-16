362c377a201fd706f80187dc85ac93b6e048cfd9
Robin Munn <rmunn@pobox.com>
2025-06-16 17:13:28 +0700

    Tweak upload test's use of JWT auth
    Setting DefaultRequestHeaders has a side-effect: the cleanup code wasn't
    cleaning up projects correctly, because the API was returning 401
    Unauthorized when the "Authorization: Bearer (JWT)" header was present.

--- backend/Testing/FwHeadless/MediaFileTestFixture.cs
+++ backend/Testing/FwHeadless/MediaFileTestFixture.cs
@@ -17,7 +17,6 @@ public class MediaFileTestFixture : ApiTestBase, IAsyncLifetime
     public async Task InitializeAsync()
     {
         AdminJwt = await LoginAs("admin");
-        // HttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", AdminJwt);
         ProjectCode = Utils.NewProjectCode();
         ProjectId = await FwHeadlessTestHelpers.CopyProjectToNewProject(HttpClient, ProjectCode, "sena-3");
         // Project folder needs to exist in order for file uploads to be allowed, so trigger first sync including S/R
@@ -27,14 +26,23 @@ public class MediaFileTestFixture : ApiTestBase, IAsyncLifetime
 
     public async Task DisposeAsync()
     {
-        await HttpClient.DeleteAsync($"api/project/{ProjectId}");
+        // Must ensure we're logged in as admin, since some tests might have changed that
+        await LoginAs("admin");
+        HttpClient.DefaultRequestHeaders.Authorization = null; // OAuth headers will result in 401 Unauthorized response from API
+        var result = await HttpClient.DeleteAsync($"api/project/{ProjectId}");
+        // result.EnsureSuccessStatusCode(); // Don't cause test failure if deleting project failed somehow
     }
 
-    public async Task<Guid> PostFile(string localPath, FileMetadata? metadata = null)
+    public async Task<Guid> PostFile(string localPath, FileMetadata? metadata = null, string? loginAs = null)
     {
         var filename = Path.GetFileName(localPath);
         using (var formData = new MultipartFormDataContent())
         {
+            var jwt = AdminJwt;
+            if (loginAs is not null)
+            {
+                jwt = await LoginAs(loginAs);
+            }
             formData.Add(new StringContent(filename), name: "filename");
             formData.Add(new StringContent(ProjectId.ToString()), name: "projectId");
             if (metadata is not null)
@@ -43,9 +51,10 @@ public class MediaFileTestFixture : ApiTestBase, IAsyncLifetime
             }
             var stream = new StreamContent(File.OpenRead(localPath));
             formData.Add(stream, name: "file", fileName: filename);
-            // formData.Headers.Add("Authorization", $"Bearer {AdminJwt}"); // Can't do this, not allowed (sigh)
-            HttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", AdminJwt); // This sets the default for the entire class, which is not ideal
+            // formData.Headers.Add("Authorization", $"Bearer {jwt}"); // Can't do this, not allowed (sigh)
+            HttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", jwt); // This sets the default for the entire class, which is not ideal
             var result = await HttpClient.PostAsync($"api/media/", formData);
+            result.EnsureSuccessStatusCode();
             var obj = await result.Content.ReadFromJsonAsync<PostFileResult>();
             return obj?.guid ?? Guid.Empty;
         }
