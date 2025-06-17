e4e5c90cd4f0dd7946a934443ae0ffceb1c2e0cc
Robin Munn <rmunn@pobox.com>
2025-06-17 14:55:22 +0700

    PUT, like POST, does not need ASP.NET's anti-XSRF

--- backend/FwHeadless/Routes/MediaFileRoutes.cs
+++ backend/FwHeadless/Routes/MediaFileRoutes.cs
@@ -9,7 +9,7 @@ public static class MediaFileRoutes
         var group = app.MapGroup("/api").WithOpenApi();
         group.MapGet("/list-media/{projectId}", MediaFileController.ListFiles); // TODO: During code review, discuss what URL to assign this API endpoint
         group.MapGet("/media/{fileId}", MediaFileController.GetFile);
-        group.MapPut("/media/{fileId}", MediaFileController.PutFile);
+        group.MapPut("/media/{fileId}", MediaFileController.PutFile).DisableAntiforgery();
         group.MapDelete("/media/{fileId}", MediaFileController.DeleteFile);
         group.MapPost("/media/", MediaFileController.PostFile).DisableAntiforgery();
         group.MapGet("/metadata/{fileId}", MediaFileMetadataController.GetFileMetadata);
