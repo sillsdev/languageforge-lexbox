e3e6b5d6bd59aa3527bcfbe63f2c6f8f2d30cbe0
Robin Munn <rmunn@pobox.com>
2025-06-18 13:05:30 +0700

    No more hardcoded paths for test upload file

--- backend/Testing/FwHeadless/MediaFileTests.cs
+++ backend/Testing/FwHeadless/MediaFileTests.cs
@@ -5,6 +5,7 @@ using FluentAssertions.Collections;
 using LexCore.Sync;
 using SIL.Harmony.Core;
 using Testing.ApiTests;
+using Testing.Fixtures;
 using Testing.Services;
 
 namespace Testing.FwHeadless;
@@ -13,6 +14,9 @@ namespace Testing.FwHeadless;
 public class MediaFileTests : ApiTestBase, IClassFixture<MediaFileTestFixture>
 {
     private MediaFileTestFixture Fixture { get; init; }
+    private FileInfo TestRepoZip = IntegrationFixture.TemplateRepoZip;
+    private string TestRepoZipFilename => TestRepoZip.Name;
+    private string TestRepoZipPath => TestRepoZip.FullName;
 
     public MediaFileTests(MediaFileTestFixture fixture)
     {
@@ -22,39 +26,39 @@ public class MediaFileTests : ApiTestBase, IClassFixture<MediaFileTestFixture>
     [Fact]
     public async Task UploadFile_WorksForAdmins()
     {
-        var guid = await Fixture.PostFile("/home/rmunn/code/lexbox/data/sena-3.zip");
+        var guid = await Fixture.PostFile(TestRepoZipPath);
         guid.Should().NotBe(Guid.Empty);
         var files = await Fixture.ListFiles(Fixture.ProjectId);
         files.Should().NotBeNull();
-        files.Files.Should().Contain("sena-3.zip");
-        await Fixture.PutFile("/home/rmunn/code/lexbox/data/sena-3.zip", guid);
+        files.Files.Should().Contain(TestRepoZipFilename);
+        await Fixture.PutFile(TestRepoZipPath, guid);
         files = await Fixture.ListFiles(Fixture.ProjectId);
         files.Should().NotBeNull();
-        files.Files.Should().Contain("sena-3.zip");
+        files.Files.Should().Contain(TestRepoZipFilename);
     }
 
     [Fact]
     public async Task UploadFile_FailsForNonMembers()
     {
-        var guid = await Fixture.PostFile("/home/rmunn/code/lexbox/data/sena-3.zip", loginAs: "user", expectSuccess: false);
+        var guid = await Fixture.PostFile(TestRepoZipPath, loginAs: "user", expectSuccess: false);
         guid.Should().Be(Guid.Empty);
         var files = await Fixture.ListFiles(Fixture.ProjectId, loginAs: "user");
-        (files?.Files ?? []).Should().NotContain("sena-3.zip");
+        (files?.Files ?? []).Should().NotContain(TestRepoZipFilename);
     }
 
     [Fact]
     public async Task UploadFile_WorksForProjectManagers()
     {
-        await Fixture.PostFile("/home/rmunn/code/lexbox/data/sena-3.zip", loginAs: "manager", expectSuccess: true);
+        await Fixture.PostFile(TestRepoZipPath, loginAs: "manager", expectSuccess: true);
         var files = await Fixture.ListFiles(Fixture.ProjectId, loginAs: "manager");
-        (files?.Files ?? []).Should().Contain("sena-3.zip");
+        (files?.Files ?? []).Should().Contain(TestRepoZipFilename);
     }
 
     [Fact]
     public async Task UploadFile_WorksForProjectEditors()
     {
-        await Fixture.PostFile("/home/rmunn/code/lexbox/data/sena-3.zip", loginAs: "editor", expectSuccess: true);
+        await Fixture.PostFile(TestRepoZipPath, loginAs: "editor", expectSuccess: true);
         var files = await Fixture.ListFiles(Fixture.ProjectId, loginAs: "editor");
-        (files?.Files ?? []).Should().Contain("sena-3.zip");
+        (files?.Files ?? []).Should().Contain(TestRepoZipFilename);
     }
 }
