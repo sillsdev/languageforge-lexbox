2a83fd11dd11764984dd35eb315ff05fee77efbf
Kevin Hahn <kevin_hahn@sil.org>
2025-06-18 15:23:19 +0700

    change paratext extension to work as an iframe in paratext (#1750)
    * change paratext extension to work as an iframe in paratext
    
    * fix issue where FwLiteWeb fails load js files when started with the wrong CWD
    
    * fix up the taskfile to work with the new fw lite
    
    * fix some restart issues along with loading the url at launch
    
    Co-authored-by: Tim Haasdyk <tim_haasdyk@sil.org>

--- backend/FwLite/FwLiteWeb/Program.cs
+++ backend/FwLite/FwLiteWeb/Program.cs
@@ -2,6 +2,8 @@
 using FwLiteWeb;
 using Microsoft.Extensions.Options;
 
+//paratext won't let us change the working directory, and if it's not set correctly then loading js files doesn't work
+Directory.SetCurrentDirectory(Path.GetDirectoryName(typeof(Program).Assembly.Location)!);
 var app = FwLiteWebServer.SetupAppServer(new() {Args = args});
 await using (app)
 {
