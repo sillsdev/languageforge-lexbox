7b3f140583192be475d5a614ee37a6e26d60f852
Kevin Hahn <kevin_hahn@sil.org>
2025-06-18 10:11:46 +0700

    update deps June (#1754)
    * move AddQuartz into LexBoxDbContext.cs to ensure it's always run and avoid Pending model change warnings
    
    * update to ef 9.0.4
    
    * replace EntityFrameworkCore.Projectables with NeinLinq, upgrade packages to 9.0.6
    
    * update OpenIddict to 6.4.0
    
    * update harmony to ef 9

--- backend/LexBoxApi/Controllers/LegacyProjectApiController.cs
+++ backend/LexBoxApi/Controllers/LegacyProjectApiController.cs
@@ -1,6 +1,4 @@
-using System.ComponentModel.DataAnnotations;
 using System.Net.Mime;
-using EntityFrameworkCore.Projectables;
 using LexCore;
 using LexCore.Entities;
 using LexCore.ServiceInterfaces;
@@ -57,7 +55,12 @@ public class LegacyProjectApiController : ControllerBase
                     member.Project.Name,
                     //it seems this is largely ignored by the client as it uses the LF domain instead
                     "http://public.languagedepot.org",
-                    RoleToString(member.Role)))
+                    //instead of using toString which could change if we rename the enum, we only ever want to return these 3 values.
+                            //this needs to be ugly so that projectable will work :(
+                            member.Role == ProjectRole.Manager ? "manager"
+                            : member.Role == ProjectRole.Editor ? "editor"
+                            : "unknown"//fieldworks doesn't know about or support observers
+                            ))
             })
             .FirstOrDefaultAsync();
         if (user == null)
@@ -73,14 +76,6 @@ public class LegacyProjectApiController : ControllerBase
 
         return user.projects.ToArray();
     }
-
-    [Projectable]
-    private string RoleToString(ProjectRole role) =>
-        //instead of using toString which could change if we rename the enum, we only ever want to return these 3 values.
-        //this needs to be ugly so that projectable will work :(
-        role == ProjectRole.Manager ? "manager"
-        : role == ProjectRole.Editor ? "editor"
-        : "unknown";//fieldworks doesn't know about or support observers
 }
 
 public record LegacyApiProject(string Identifier, string Name, string Repository, string Role);
