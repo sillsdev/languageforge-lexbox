58d5b8a9434a8c07685f60ac397eea227b56392f
Kevin Hahn <kevin_hahn@sil.org>
2025-06-19 11:20:20 +0700

    write a test for getting persisting metadata to the db

--- backend/LexData/Entities/MediaFileEntityConfiguration.cs
+++ backend/LexData/Entities/MediaFileEntityConfiguration.cs
@@ -15,19 +15,16 @@ public class MediaFileEntityConfiguration : IEntityTypeConfiguration<MediaFile>
         builder.HasOne<Project>().WithMany()
             .HasPrincipalKey(project => project.Id)
             .HasForeignKey(c => c.ProjectId);
+        var jsonOptions = new JsonSerializerOptions
+        {
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+        };
         builder.Property(u => u.Metadata)
             .IsRequired(false)
-            // .HasColumnType("jsonb")  // TODO: Figure out why this is giving me "column "Metadata" is of type jsonb but expression is of type text" errors when EF Core saves changes
-            // .HasDefaultValueSql("'{}'::jsonb")
             .HasDefaultValueSql("'{}'")
             .HasConversion(
-                v => JsonSerializer.Serialize(v, new JsonSerializerOptions
-                {
-                    DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
-                }),
-                v => JsonSerializer.Deserialize<FileMetadata>(v, new JsonSerializerOptions
-                {
-                    DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
-                }));
+                v => JsonSerializer.Serialize(v, jsonOptions),
+                v => JsonSerializer.Deserialize<FileMetadata>(v, jsonOptions)
+            );
     }
 }
