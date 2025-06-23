78f1ff1d7a3778aa9388d899ca989fcdf56c4dc5
Robin Munn <rmunn@pobox.com>
2025-06-23 11:42:14 +0700

    Remove unused class

--- backend/FwHeadless/Models/FileUploadRequest.cs
+++ backend/FwHeadless/Models/FileUploadRequest.cs
@@ -2,11 +2,6 @@ using System.Text.Json.Serialization;
 
 namespace FwHeadless.Models;
 
-public record FileUploadRequest(
-    string Filename,
-    Guid ProjectId
-);
-
 public record PostFileResult(Guid guid);
 
 [JsonConverter(typeof(JsonStringEnumConverter))]
