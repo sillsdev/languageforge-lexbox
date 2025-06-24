f4085ca99dbe5b5241ee9bf3e598cc71d22d54fe
Tim Haasdyk <tim_haasdyk@sil.org>
2025-06-24 16:09:08 +0200

    Use Android URL in update notification on Android

--- frontend/viewer/src/lib/notifications/NotificationOutlet.svelte
+++ frontend/viewer/src/lib/notifications/NotificationOutlet.svelte
@@ -8,15 +8,24 @@
   import {FwEventType} from '$lib/dotnet-types/generated-types/FwLiteShared/Events/FwEventType';
   import {UpdateResult} from '$lib/dotnet-types/generated-types/FwLiteShared/AppUpdate/UpdateResult';
   import {t} from 'svelte-i18n-lingui';
+  import {useFwLiteConfig} from '$lib/services/service-provider';
+  import {FwLitePlatform} from '$lib/dotnet-types/generated-types/FwLiteShared/FwLitePlatform';
 
   const notifications = AppNotification.notifications;
 
   const eventBus = useEventBus();
+  const fwliteConfig = useFwLiteConfig();
+
+  const updateUrls: Partial<Record<FwLitePlatform, string>> = {
+    [FwLitePlatform.Android]: 'https://play.google.com/store/apps/details?id=org.sil.FwLiteMaui',
+  };
+
   eventBus.onEventType<IAppUpdateEvent>(FwEventType.AppUpdate, event => {
     if (event.result == UpdateResult.ManualUpdateRequired) {
       AppNotification.displayAction($t`A new version of FieldWorks lite is available.`, 'info', {
         callback: () => {
-          window.open('https://lexbox.org/fw-lite', '_blank');
+          const url = updateUrls[fwliteConfig.os] ?? 'https://lexbox.org/fw-lite';
+          window.open(url, '_blank');
         },
         label: $t`Download`
       });
