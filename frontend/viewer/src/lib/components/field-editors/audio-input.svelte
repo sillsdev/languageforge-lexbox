956541796bc9d1cc2de624f31012004fb82dd141
Kevin Hahn <kevin_hahn@sil.org>
2025-06-25 17:09:28 +0700

    create a custom audio player

--- frontend/viewer/src/lib/components/field-editors/audio-input.svelte
+++ frontend/viewer/src/lib/components/field-editors/audio-input.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
   import {onDestroy} from 'svelte';
-  import {watch} from 'runed';
+  import {useEventListener, watch} from 'runed';
   import {useProjectContext} from '$lib/project-context.svelte';
   import {AppNotification} from '$lib/notifications/notifications';
+  import {Button} from '$lib/components/ui/button';
+  import {Slider} from '$lib/components/ui/slider';
+  import {createSubscriber} from 'svelte/reactivity';
+  import {on} from 'svelte/events';
+
 
   let {
     loader = defaultLoader,
@@ -29,6 +34,7 @@
 
   async function load() {
     if (!audio || loadedAudioId === audioId) return;
+    playerState = 'loading';
     const stream = await loader(audioId);
     if (!stream) {
       AppNotification.display(`Failed to load audio ${audioId}`, 'error', 'long');
@@ -42,23 +48,79 @@
 
   async function play() {
     await load();
-    audio?.play();
+    void audio?.play();
+    playerState = 'playing';
+  }
+
+  function pause() {
+    audio?.pause();
+    playerState = 'paused';
+  }
+
+  function togglePlay() {
+    if (playerState === 'playing') pause();
+    else void play();
   }
 
   let loadedAudioId: string | undefined;
   let audio: HTMLAudioElement | undefined = $state(undefined);
+  let audioRuned = $derived(audio ? new AudioRuned(audio) : null);
+  useEventListener(() => audio, 'ended', () => playerState = 'paused');
+
   onDestroy(() => {
     if (!audio || !audio.src) return;
     URL.revokeObjectURL(audio.src);
   });
+  let playerState = $state<'loading' | 'playing' | 'paused'>('paused');
+  let loading = $derived(playerState === 'loading');
+  let playIcon: 'i-mdi-play' | 'i-mdi-pause' = $derived(playerState === 'playing'
+    ? 'i-mdi-pause'
+    : 'i-mdi-play');
+  class AudioRuned {
+    #currentTimeSub = createSubscriber(update => {
+      const off = on(this.audio, 'timeupdate', update);
+      return () => off();
+    });
+    #durationSub = createSubscriber(update => on(this.audio, 'durationchange', update));
+    constructor(private audio: HTMLAudioElement) {
+    }
+    get currentTime() {
+      this.#currentTimeSub();
+      return this.audio.currentTime;
+    }
+    set currentTime(value) {
+      this.audio.currentTime = value;
+    }
+    get duration() {
+      this.#durationSub();
+      return this.audio.duration;
+    }
+  }
 </script>
 {#if supportsAudio}
   {#if audioId}
-    <div class="flex gap-2">
-      <audio class="rounded" controls bind:this={audio} onplay={load}>
+    <div class="flex space-x-2 items-center">
+      {#if loading}
+        <!--for some reason using the same button for loading, play and pause doesn't work and pause is never shown-->
+        <Button loading size="icon"></Button>
+      {:else}
+        <Button onclick={togglePlay} icon={playIcon} size="icon"></Button>
+      {/if}
+      {#if audioRuned}
+        <Slider type="single"
+                class="pl-2"
+                value={audioRuned.currentTime}
+                onValueCommit={(v) => audioRuned.currentTime = v}
+                max={audioRuned?.duration}
+                step="0.01"/>
+        {#if !isNaN(audioRuned.duration)}
+          <span class="break-keep text-nowrap pr-2">
+            {audioRuned.currentTime.toFixed(2)} / {audioRuned.duration.toFixed(2)}
+          </span>
+        {/if}
+      {/if}
+      <audio class="rounded"  bind:this={audio} onplay={load}>
       </audio>
-      <!--chrome doesn't enable the play button when there's no src so we need this hack, shouldn't be needed once we implement our own player-->
-      <button onclick={play}>Play Chrome</button>
     </div>
   {:else}
     <div class="text-gray-500">No Audio</div>
