ce4a32cb819a60f001e730d0f9a910838392e358
Kevin Hahn <kevin_hahn@sil.org>
2025-06-30 13:38:50 +0700

    ignore Guid Id for writing system import

--- backend/FwLite/FwLiteProjectSync.Tests/Import/ImportTests.cs
+++ backend/FwLite/FwLiteProjectSync.Tests/Import/ImportTests.cs
@@ -73,7 +73,7 @@ public class ImportTests : IClassFixture<SyncFixture>
         await ImportService.ImportProject(_fixture.CrdtApi, _fixture.FwDataApi, 1);
         var importedWritingSystems = await _fixture.CrdtApi.GetWritingSystems();
         var importedWritingSystem = importedWritingSystems.Vernacular.SingleOrDefault(vws => vws.WsId == ws.WsId);
-        importedWritingSystem.Should().BeEquivalentTo(ws);
+        importedWritingSystem.Should().BeEquivalentTo(ws, c => c.Excluding(w => w.Id));//Id is not mapped from FW to Crdt as FW does not have an ID for writing systems
     }
 
     [Fact]
