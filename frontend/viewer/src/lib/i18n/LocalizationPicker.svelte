b3bc726e5414f0a51287b71bb1eadfd886c45c1a
Kevin Hahn <kevin_hahn@sil.org>
2025-07-08 17:08:57 +0700

    fix localization in prod (#1805)
    * ensure messages are extracted properly
    
    * ensure i18n messages are loaded before mounting the UI

--- frontend/viewer/src/lib/i18n/LocalizationPicker.svelte
+++ frontend/viewer/src/lib/i18n/LocalizationPicker.svelte
@@ -1,34 +1,17 @@
-﻿<script lang="ts" module>
-  const hasSetLang = {value: false};
-</script>
-<script lang="ts">
-import {locale} from 'svelte-i18n-lingui';
-import {onMount} from 'svelte';
-import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
-import {Button} from '$lib/components/ui/button';
-import {Icon} from '$lib/components/ui/icon';
-import {IsMobile} from '$lib/hooks/is-mobile.svelte';
+﻿<script lang="ts">
+  import {locale} from 'svelte-i18n-lingui';
+  import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
+  import {Button} from '$lib/components/ui/button';
+  import {Icon} from '$lib/components/ui/icon';
+  import {IsMobile} from '$lib/hooks/is-mobile.svelte';
+  import {setLanguage} from '$lib/i18n';
 
-const languages: Record<string, string> = {
-  'en': 'English',
-  'fr': 'Français',
-  'es': 'Español'
-};
-const currentLanguage = $derived(languages[$locale] ?? 'Unknown: ' + $locale);
-async function setLanguage(lang: string) {
-  const wasDefault = lang === 'default';
-  if (!lang || wasDefault) lang = localStorage.getItem('locale') ?? 'en';
-  let {messages} = await import(`../../locales/${lang}.json?lingui`);
-  locale.set(lang, messages);
-  //only save when the user changes locale
-  if (!wasDefault) localStorage.setItem('locale', lang);
-}
-onMount(() => {
-  if (!hasSetLang.value) {
-    void setLanguage($locale);
-    hasSetLang.value = true;
-  }
-});
+  const languages: Record<string, string> = {
+    'en': 'English',
+    'fr': 'Français',
+    'es': 'Español'
+  };
+  const currentLanguage = $derived(languages[$locale] ?? 'Unknown: ' + $locale);
 </script>
 <DropdownMenu.Root>
   <DropdownMenu.Trigger>
--- frontend/viewer/src/main.ts
+++ frontend/viewer/src/main.ts
@@ -9,11 +9,16 @@ import {mount} from 'svelte';
 import {setupDotnetServiceProvider} from './lib/services/service-provider-dotnet';
 import {setupServiceProvider} from '$lib/services/service-provider';
 import {useEventBus} from '$lib/services/event-bus';
+import {setLanguage} from '$lib/i18n';
 
 setupServiceProvider();
 setupDotnetServiceProvider();
 useEventBus();
 
-mount(App, {
-  target: document.getElementById('svelte-app')!,
-});
+//don't mount the app until after we've loaded the local
+void setLanguage('default')
+  .then(() => {
+    mount(App, {
+      target: document.getElementById('svelte-app')!,
+    });
+  });
