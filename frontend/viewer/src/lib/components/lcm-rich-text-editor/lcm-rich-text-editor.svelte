6d0307d3b92fc6256a21acd615774429532df6ca
Kevin Hahn <kevin_hahn@sil.org>
2025-07-09 10:45:01 +0700

    resolve merge issues

--- frontend/viewer/src/lib/components/lcm-rich-text-editor/lcm-rich-text-editor.svelte
+++ frontend/viewer/src/lib/components/lcm-rich-text-editor/lcm-rich-text-editor.svelte
@@ -1,55 +1,7 @@
 ﻿<script lang="ts" module>
   import {type Node} from 'prosemirror-model';
   import {cn} from '$lib/utils';
-
-  /*
-  whitespace: pre tells prose-mirror how to parse the dom, not how to render it, that's our job
-  */
-  const textSchema = new Schema({
-    nodes: {
-      text: {
-        whitespace: 'pre',
-      },
-      /**
-       * Note: it seems that our spans likely should have been modeled as "marks" rather than "nodes".
-       * Conceptually our users "mark" up a text.
-       * I assume using marks would make delete and backspace behave more intuitively and automatically solve what our
-       * custom key bindings do.
-       * */
-      span: {
-        selectable: false,
-        content: 'text* br?',
-        // If we remove this Backspace + Delete start removing whole spans
-        inline: true,
-        whitespace: 'pre',
-        toDOM: (node) => {
-          return ['span', {
-            title: gt`Writing system: ${node.attrs.richSpan.ws}`,
-            class: cn(node.attrs.className),
-          }, 0];
-        },
-        parseDOM: [{tag: 'span'}],
-        //richSpan is used to track the original span which was modified
-        //this allows us to update the text property without having to map all the span properties into the schema
-        attrs: {richSpan: {default: {}}, className: {default: ''}}
-      },
-      br: {
-        inline: true,
-        group: 'inline',
-        selectable: false,
-        linebreakReplacement: true,
-        toDOM: () => ['br'],
-        parseDOM: [{tag: 'br'}]
-      },
-      doc: {
-        whitespace: 'pre',
-        // if we remove this Shift + Enter creates new spans and then Backspace starts removing whole spans
-        inline: true,
-        content: 'span*',
-        attrs: {}
-      }
-    }
-  });
+  import {textSchema} from './editor-schema';
 
   //matching the character used in FieldWorks
   //https://unicode-explorer.com/c/2028
