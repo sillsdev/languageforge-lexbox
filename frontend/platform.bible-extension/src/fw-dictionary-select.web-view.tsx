be42b0fadfd9badae9dc2d9e58914d324ee6cc66
Danny Rorabaugh <imnasnainaec@gmail.com>
2025-07-16 12:38:56 -0400

    Update dictionary selection handling

--- frontend/platform.bible-extension/src/fw-dictionary-select.web-view.tsx
+++ frontend/platform.bible-extension/src/fw-dictionary-select.web-view.tsx
@@ -2,16 +2,39 @@ import type { WebViewProps } from '@papi/core';
 import papi from '@papi/frontend';
 import type { IProjectModel } from 'fw-lite-extension';
 import { ComboBox } from 'platform-bible-react';
-import { useState, useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 globalThis.webViewComponent = function fwDictionarySelect(props: WebViewProps) {
   const [fwDictionaries, setFwDictionaries] = useState<IProjectModel[] | undefined>();
   const [selectedDictionaryCode, setSelectedDictionaryCode] = useState('');
+  const [settingSaved, setSettingSaved] = useState(false);
+  const [settingSaving, setSettingSaving] = useState(false);
+
+  const saveSetting = useCallback(() => {
+    if (!selectedDictionaryCode) return;
+    setSettingSaving(true);
+    papi.commands
+      .sendCommand('fwLiteExtension.selectDictionary', props.projectId!, selectedDictionaryCode)
+      .then(() => setSettingSaved(true))
+      .catch((e) => papi.logger.error('Error saving FieldWorks dictionary selection:', e))
+      .finally(() => setSettingSaving(false));
+  }, [selectedDictionaryCode, props.projectId]);
 
   useEffect(() => {
     papi.logger.info(`This web view was opened for project '${props.projectId}'`);
-    papi.commands.sendCommand('fwLiteExtension.fwDictionaries').then(setFwDictionaries);
-  }, []);
+    papi.commands
+      .sendCommand('fwLiteExtension.fwDictionaries')
+      .then(setFwDictionaries)
+      .catch((e) => papi.logger.error('Error fetching FieldWorks dictionaries:', e));
+  }, [props.projectId]);
+
+  if (settingSaving) {
+    return <p>Saving dictionary selection {selectedDictionaryCode}...</p>;
+  }
+
+  if (settingSaved) {
+    return <p>Dictionary selection saved. You can close this window.</p>;
+  }
 
   return (
     <div>
@@ -33,17 +56,8 @@ globalThis.webViewComponent = function fwDictionarySelect(props: WebViewProps) {
       />
       {!!selectedDictionaryCode && (
         <>
-          <button
-            onClick={() =>
-              papi.commands.sendCommand(
-                'fwLiteExtension.selectDictionary',
-                props.projectId!,
-                selectedDictionaryCode,
-              )
-            }
-            type="button"
-          >
-            Open project
+          <button onClick={saveSetting} type="button">
+            Confirm selection
           </button>
           <button onClick={() => setSelectedDictionaryCode('')} type="button">
             Clear selection
