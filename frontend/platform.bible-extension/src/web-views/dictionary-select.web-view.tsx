f2b864a55998470ff5940a154131b8efa175da64
Danny Rorabaugh <imnasnainaec@gmail.com>
2025-07-18 16:02:17 -0400

    Split off component from web view

--- frontend/platform.bible-extension/src/web-views/dictionary-select.web-view.tsx
+++ frontend/platform.bible-extension/src/web-views/dictionary-select.web-view.tsx
@@ -1,69 +1,26 @@
 import type { WebViewProps } from '@papi/core';
-import papi from '@papi/frontend';
+import { commands, logger } from '@papi/frontend';
 import type { IProjectModel } from 'fw-lite-extension';
-import { ComboBox } from 'platform-bible-react';
 import { useCallback, useEffect, useState } from 'react';
+import DictionaryComboBox from '../components/dictionary-combo-box';
 
 globalThis.webViewComponent = function fwDictionarySelect(props: WebViewProps) {
   const [fwDictionaries, setFwDictionaries] = useState<IProjectModel[] | undefined>();
-  const [selectedDictionaryCode, setSelectedDictionaryCode] = useState('');
-  const [settingSaved, setSettingSaved] = useState(false);
-  const [settingSaving, setSettingSaving] = useState(false);
 
-  const saveSetting = useCallback(() => {
-    if (!selectedDictionaryCode) return;
-    setSettingSaving(true);
-    papi.commands
-      .sendCommand('fwLiteExtension.selectDictionary', props.projectId!, selectedDictionaryCode)
-      .then(() => void setSettingSaved(true))
-      .catch((e) => void papi.logger.error('Error saving FieldWorks dictionary selection:', e))
-      .finally(() => void setSettingSaving(false));
-  }, [selectedDictionaryCode, props.projectId]);
+  const selectDictionary = useCallback(
+    async (code: string): Promise<void> => {
+      commands.sendCommand('fwLiteExtension.selectDictionary', props.projectId!, code);
+    },
+    [props.projectId],
+  );
 
   useEffect(() => {
-    papi.logger.info(`This web view was opened for project '${props.projectId}'`);
-    papi.commands
+    logger.info(`This web view was opened for project '${props.projectId}'`);
+    commands
       .sendCommand('fwLiteExtension.fwDictionaries')
       .then(setFwDictionaries)
-      .catch((e) => void papi.logger.error('Error fetching FieldWorks dictionaries:', e));
+      .catch((e) => void logger.error('Error fetching FieldWorks dictionaries:', e));
   }, [props.projectId]);
 
-  if (settingSaving) {
-    return <p>Saving dictionary selection {selectedDictionaryCode}...</p>;
-  }
-
-  if (settingSaved) {
-    return <p>Dictionary selection saved. You can close this window.</p>;
-  }
-
-  return (
-    <div>
-      <ComboBox
-        buttonPlaceholder={
-          !fwDictionaries
-            ? 'Loading dictionaries...'
-            : !fwDictionaries.length
-            ? 'No dictionaries found'
-            : !selectedDictionaryCode
-            ? 'Select a dictionary'
-            : `Selected: ${selectedDictionaryCode}`
-        }
-        commandEmptyMessage="No dictionaries found"
-        isDisabled={!fwDictionaries?.length}
-        onChange={setSelectedDictionaryCode}
-        options={fwDictionaries?.map((p) => p.code)}
-        textPlaceholder="Select a dictionary"
-      />
-      {!!selectedDictionaryCode && (
-        <>
-          <button onClick={saveSetting} type="button">
-            Confirm selection
-          </button>
-          <button onClick={() => void setSelectedDictionaryCode('')} type="button">
-            Clear selection
-          </button>
-        </>
-      )}
-    </div>
-  );
+  return <DictionaryComboBox dictionaries={fwDictionaries} selectDictionary={selectDictionary} />;
 };
