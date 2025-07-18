c7f84b201fed19829430534929e994100389fee1
Danny Rorabaugh <imnasnainaec@gmail.com>
2025-07-18 15:30:33 -0400

    Create add word web view and menu item

---
+++ frontend/platform.bible-extension/src/add-new-entry.tsx
@@ -0,0 +1,101 @@
+import type { IEntry, ISense } from 'fw-lite-extension';
+import { Button, Card, CardContent, CardHeader, Input, Label } from 'platform-bible-react';
+import { useCallback, useEffect, useState } from 'react';
+
+interface AddNewEntryProps {
+  addEntry: (entry: IEntry) => Promise<void>;
+  analysisLang: string;
+  headword?: string;
+  isAdding?: boolean;
+  vernacularLang: string;
+}
+
+export default function AddNewEntry(props: AddNewEntryProps) {
+  const [isAdding, setIsAdding] = useState(props.isAdding);
+  const [isReady, setIsReady] = useState(false);
+
+  const [headword, setHeadword] = useState('');
+  const [gloss, setGloss] = useState('');
+  const [definition, setDefinition] = useState('');
+
+  useEffect(() => setHeadword(props.headword || ''), [props.headword]);
+
+  useEffect(() => {
+    setIsReady(!!(headword.trim() && (gloss.trim() || definition.trim())));
+  }, [definition, gloss, headword]);
+
+  async function addEntry(): Promise<void> {
+    const sense = CreateSense(props.analysisLang, gloss.trim(), definition.trim());
+    const entry = CreateEntry(props.vernacularLang, headword.trim(), sense);
+    await props.addEntry(entry);
+  }
+
+  const clearEntry = useCallback((): void => {
+    setIsAdding(props.isAdding);
+    setHeadword(props.headword || '');
+    setGloss('');
+    setDefinition('');
+  }, [props.headword, props.isAdding]);
+
+  return isAdding ? (
+    <Card>
+      <CardHeader>Adding new entry</CardHeader>
+      <CardContent>
+        <div>
+          <Label htmlFor="newEntryHeadword">Headword:</Label>
+          <Input
+            id="newEntryHeadword"
+            onChange={(e) => setHeadword(e.target.value)}
+            value={headword}
+          />
+        </div>
+        <div>
+          <Label htmlFor="newEntryGloss">Gloss:</Label>
+          <Input id="newEntryGloss" onChange={(e) => setGloss(e.target.value)} value={gloss} />
+        </div>
+        <div>
+          <Label htmlFor="newEntryDefinition">Definition:</Label>
+          <Input
+            id="newEntryDefinition"
+            onChange={(e) => setDefinition(e.target.value)}
+            value={definition}
+          />
+        </div>
+        <div>
+          <Button disabled={!isReady} onClick={() => void addEntry()}>
+            Submit new entry
+          </Button>
+          <Button onClick={clearEntry}>Cancel</Button>
+        </div>
+      </CardContent>
+    </Card>
+  ) : (
+    <Button onClick={() => void setIsAdding(true)}>Add new entry</Button>
+  );
+}
+
+function CreateSense(lang: string, gloss?: string, definition?: string): ISense {
+  return {
+    definition: definition ? { [lang]: definition } : {},
+    entryId: '',
+    exampleSentences: [],
+    gloss: gloss ? { [lang]: gloss } : {},
+    id: '',
+    semanticDomains: [],
+  };
+}
+
+function CreateEntry(lang: string, headword: string, sense: ISense): IEntry {
+  return {
+    citationForm: { [lang]: headword },
+    complexForms: [],
+    complexFormTypes: [],
+    components: [],
+    id: sense.entryId || '',
+    lexemeForm: {},
+    literalMeaning: {},
+    note: {},
+    publishIn: [],
+    senses: [sense],
+  };
+}
---
+++ frontend/platform.bible-extension/src/fw-lite-add-word.web-view.tsx
@@ -0,0 +1,51 @@
+import type { NetworkObject } from '@papi/core';
+import papi, { logger } from '@papi/frontend';
+import type { IEntry, IEntryService, WordWebViewOptions } from 'fw-lite-extension';
+import { useCallback, useEffect, useState } from 'react';
+import AddNewEntry from './add-new-entry';
+
+globalThis.webViewComponent = function fwLiteAddWord({ projectId, word }: WordWebViewOptions) {
+  const [fwLiteNetworkObject, setFwLiteNetworkObject] = useState<
+    NetworkObject<IEntryService> | undefined
+  >();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  useEffect(() => {
+    void papi.networkObjects
+      .get<IEntryService>('fwliteextension.entryService')
+      .then((networkObject) => {
+        logger.info('Got network object:', networkObject);
+        setFwLiteNetworkObject(networkObject);
+      });
+  }, []);
+
+  const addEntry = useCallback(
+    async (entry: IEntry) => {
+      if (!projectId || !fwLiteNetworkObject) {
+        logger.warn(
+          `Missing required parameters: projectId=${projectId}, fwLiteNetworkObject=${fwLiteNetworkObject}`,
+        );
+        return;
+      }
+
+      setIsSubmitting(true);
+      logger.info(`Adding entry: ${JSON.stringify(entry)}`);
+      await fwLiteNetworkObject.addEntry(projectId, entry);
+      setIsSubmitting(false);
+    },
+    [fwLiteNetworkObject, projectId],
+  );
+
+  return (
+    <div>
+      <AddNewEntry
+        addEntry={addEntry}
+        analysisLang="en"
+        headword={word}
+        isAdding
+        vernacularLang="qaa"
+      />
+      {isSubmitting && <p>Adding entry to FieldWorks...</p>}
+    </div>
+  );
+};
