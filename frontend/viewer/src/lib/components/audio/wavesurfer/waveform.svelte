5d4fb1733ea44a035bb49c6ac75e5bd3a4edbcd8
Tim Haasdyk <tim_haasdyk@sil.org>
2025-07-21 06:23:07 +0200

    Add audio recording and file picking (#1817)
    * Add audio recording and file picking
    
    * Add menu for removing and replacing audio
    
    * remove if around dialog as this causes focus errors in close
    
    ---------
    
    Co-authored-by: Kevin Hahn <kevin_hahn@sil.org>

---
+++ frontend/viewer/src/lib/components/audio/wavesurfer/waveform.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+  import {useWaveSurfer, type WaveSurferAudio} from './wavesurfer-utils';
+  import type WaveSurfer from 'wavesurfer.js';
+  import {watch} from 'runed';
+  import type {HTMLAttributes} from 'svelte/elements';
+
+  type AudioUrl = string;
+
+  type Props = HTMLAttributes<HTMLDivElement> & {
+    audio?: WaveSurferAudio;
+    audioApi?: WaveSurfer;
+    playing?: boolean;
+    showTimeline?: boolean;
+    autoplay?: boolean;
+    duration?: number | null;
+  };
+
+  let {
+    audio,
+    audioApi: wavesurfer = $bindable(),
+    playing = $bindable(false),
+    duration = $bindable(null),
+    showTimeline,
+    autoplay,
+    ...rest
+  }: Props = $props();
+
+  let container = $state<HTMLElement>();
+
+  watch(() => container, (newContainer) => {
+
+    wavesurfer?.destroy();
+    wavesurfer = undefined;
+
+    if (newContainer) {
+      wavesurfer = useWaveSurfer({container: newContainer, autoplay, showTimeline});
+      wavesurfer.on('play', () => playing = true);
+      wavesurfer.on('pause', () => playing = false);
+      wavesurfer.on('finish', () => playing = false);
+    }
+  });
+
+  $effect(() => {
+    if (audio && wavesurfer)
+      void loadAudio(audio);
+  });
+
+  async function loadAudio(audio: AudioUrl | Blob) {
+    if (!wavesurfer) throw new Error('WaveSurfer not initialized');
+
+    if (typeof audio === 'string') {
+      await wavesurfer.load(audio);
+    } else if (audio instanceof Blob) {
+      await wavesurfer.loadBlob(audio);
+    } else {
+      throw new Error('Invalid audio type');
+    }
+
+    duration = wavesurfer.getDuration();
+  }
+</script>
+
+<div bind:this={container} {...rest}></div>
