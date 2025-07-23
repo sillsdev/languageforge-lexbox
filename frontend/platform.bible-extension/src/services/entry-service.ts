8fa0d21c37b58256f3d9648508f28684c3f7a2b8
Danny Rorabaugh <imnasnainaec@gmail.com>
2025-07-23 14:18:53 -0400

    Finish refactor

--- frontend/platform.bible-extension/src/services/entry-service.ts
+++ frontend/platform.bible-extension/src/services/entry-service.ts
@@ -1,7 +1,7 @@
 import { logger } from '@papi/backend';
 import type { IEntry, IEntryQuery, IEntryService } from 'fw-lite-extension';
 import { FwLiteApi } from '../utils/fw-lite-api';
-import { WebViewProjectSettings } from '../utils/web-view-project-settings';
+import { ProjectManager } from '../utils/project-managers';
 
 export class EntryService implements IEntryService {
   private fwLiteApi: FwLiteApi;
@@ -14,16 +14,14 @@ export class EntryService implements IEntryService {
       logger.debug('No query!');
       return;
     }
-    const projectSettings = await WebViewProjectSettings.createFromProjectId(projectId);
-    const dictionaryCode = await projectSettings?.getFwDictionaryCode();
+    const dictionaryCode = await new ProjectManager(projectId).getFwDictionaryCode();
     if (!dictionaryCode) return;
     console.log(`About to fetch entries for '${query.surfaceForm}' in '${dictionaryCode}'`);
     return this.fwLiteApi.getEntries(query.surfaceForm, dictionaryCode);
   }
 
   async addEntry(projectId: string, entry: Partial<IEntry>): Promise<IEntry | undefined> {
-    const projectSettings = await WebViewProjectSettings.createFromProjectId(projectId);
-    const dictionaryCode = await projectSettings?.getFwDictionaryCode();
+    const dictionaryCode = await new ProjectManager(projectId).getFwDictionaryCode();
     if (!dictionaryCode) return;
     return await this.fwLiteApi.postNewEntry(entry, dictionaryCode);
   }
@@ -34,8 +32,7 @@ export class EntryService implements IEntryService {
   }
 
   async deleteEntry(projectId: string, id: string): Promise<undefined> {
-    const projectSettings = await WebViewProjectSettings.createFromProjectId(projectId);
-    const dictionaryCode = await projectSettings?.getFwDictionaryCode();
+    const dictionaryCode = await new ProjectManager(projectId).getFwDictionaryCode();
     if (!dictionaryCode) return;
     await this.fwLiteApi.deleteEntry(id, dictionaryCode);
   }
