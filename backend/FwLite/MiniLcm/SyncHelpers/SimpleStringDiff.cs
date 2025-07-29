06c3d0d74a6f0377a4c098c87948ba4ac00b368b
Robin Munn <rmunn@pobox.com>
2025-07-29 16:38:45 +0700

    Fix bug in SimpleStringDiff and IntegerDiff
    The "add" operation, if we had ever used it, would have added nothing
    because the value to be added wasn't being passed in. Fixed now.

--- backend/FwLite/MiniLcm/SyncHelpers/SimpleStringDiff.cs
+++ backend/FwLite/MiniLcm/SyncHelpers/SimpleStringDiff.cs
@@ -10,7 +10,7 @@ public static class SimpleStringDiff
     {
         if (before == after) yield break;
         if (after is null) yield return new Operation<T>("remove", $"/{path}", null);
-        else if (before is null) yield return new Operation<T>("add", $"/{path}", null);
+        else if (before is null) yield return new Operation<T>("add", $"/{path}", null, after);
         else yield return new Operation<T>("replace", $"/{path}", null, after);
     }
 }
