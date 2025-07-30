36baee1ebb8b3c6ae9760b7eb14706e3e8a0cbf3
Robin Munn <rmunn@pobox.com>
2025-07-30 14:03:53 +0700

    Hardcode idle timeout in idle-service.ts
    If we want to change it, we'll change it in code, not as a parameter to
    the service.

--- frontend/viewer/src/lib/services/idle-service.ts
+++ frontend/viewer/src/lib/services/idle-service.ts
@@ -2,19 +2,18 @@ import {IsIdle} from 'runed';
 import {getContext, hasContext, setContext} from 'svelte';
 
 const symbol = Symbol.for('fw-lite-idle-service');
-const DEFAULT_IDLE_TIMEOUT_MS = 5 * 60 * 1000;
+const IDLE_TIMEOUT_MS = 5 * 60 * 1000;
 
-export function useIdleService(timeout?: number): IdleService {
+export function useIdleService(): IdleService {
   if (hasContext(symbol)) return getContext(symbol);
-  // Note that only the first call to useIdleService() can set the timeout.
-  const service = new IdleService(timeout);
+  const service = new IdleService();
   setContext(symbol, service);
   return service;
 }
 
 export class IdleService {
-  constructor(timeoutMs?: number) {
-    this.#isIdle = new IsIdle({ timeout: timeoutMs ?? DEFAULT_IDLE_TIMEOUT_MS });
+  constructor() {
+    this.#isIdle = new IsIdle({ timeout: IDLE_TIMEOUT_MS });
   }
 
   #isIdle: IsIdle;
