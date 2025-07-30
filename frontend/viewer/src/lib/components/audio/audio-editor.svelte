bd54b0a07fd72d64cf57041f007a0720824a6123
Kevin Hahn <kevin_hahn@sil.org>
2025-07-30 16:44:55 +0700

    setup audio pipeline for the audio editor to process and modify audio

--- frontend/viewer/src/lib/components/audio/audio-editor.svelte
+++ frontend/viewer/src/lib/components/audio/audio-editor.svelte
@@ -1,61 +1,88 @@
 <script lang="ts">
-  import { t } from 'svelte-i18n-lingui';
+  import {t} from 'svelte-i18n-lingui';
   import Button from '../ui/button/button.svelte';
   import Waveform from './wavesurfer/waveform.svelte';
   import type WaveSurfer from 'wavesurfer.js';
   import {formatDigitalDuration} from '../ui/format/format-duration';
   import DevContent from '$lib/layout/DevContent.svelte';
   import {Label} from '../ui/label';
-  import {convertToWav, loadFFmpeg} from './ffmpeg';
+  import {FFmpegApi, type FFmpegFile} from './ffmpeg';
+  import Loading from '$lib/components/Loading.svelte';
+  import {resource, watch} from 'runed';
 
   type Props = {
-    audio: Blob;
-    name: string
+    audio: File;
+    finalAudio: File | undefined;
     onDiscard: () => void;
   };
 
-  let { audio, name, onDiscard }: Props = $props();
+  let {
+    audio,
+    finalAudio = $bindable(undefined),
+    onDiscard
+  }: Props = $props();
 
   let audioApi = $state<WaveSurfer>();
   let playing = $state(false);
   let duration = $state<number | null>(null);
-  const mb = $derived((audio.size / 1024 / 1024).toFixed(2));
-  const formatedDuration = $derived(duration ? formatDigitalDuration({ seconds: duration }) : 'unknown');
+  const mb = $derived(!finalAudio ? '0' : (finalAudio.size / 1024 / 1024).toFixed(2));
+  const formatedDuration = $derived(duration ? formatDigitalDuration({seconds: duration}) : 'unknown');
+  let ffmpegApi: FFmpegApi | undefined;
 
-  void loadFFmpeg().then(async (ffmpeg) => {
-    if (!audio) return;
-    audio = await convertToWav(ffmpeg, audio);
+  let ffmpegFile = resource(() => audio, async (audio) => {
+    ffmpegApi ??= await FFmpegApi.create();
+    return await ffmpegApi.toFFmpegFile(audio);
   });
+
+  let flacFile = resource(() => ffmpegFile.current, async (file) => {
+    if (!file) return;
+    ffmpegApi ??= await FFmpegApi.create();
+    return await ffmpegApi.convertToFlac(file);
+  });
+
+  watch(() => [flacFile.current], async ([file]) => {
+    if (!file) return;
+    ffmpegApi ??= await FFmpegApi.create();
+    finalAudio = undefined;
+    finalAudio = await ffmpegApi.readFile(file);
+  });
+
+  const loading = $derived(ffmpegFile.loading || flacFile.loading);
 </script>
 
 <div class="flex flex-col gap-4 items-center justify-center">
+  {@debug finalAudio}
+  {#if loading || !finalAudio}
+    <Loading class="self-center justify-self-center size-16"/>
+  {:else}
   <span class="inline-grid grid-cols-[auto_auto_1rem_auto_auto] gap-2 items-baseline">
     <Label class="justify-self-end">{$t`Length:`}</Label> <span>{$t`${formatedDuration}`}</span>
     <span></span>
     <Label class="justify-self-end">{$t`Size:`}</Label> <span>{$t`${mb} MB`}</span>
-    {#if name}
+    {#if finalAudio.name}
       <Label class="justify-self-end">{$t`File name:`}</Label>
-      <span class="col-span-4">{$t`${name}`}</span>
+      <span class="col-span-4">{$t`${finalAudio.name}`}</span>
     {/if}
     <DevContent>
       <Label class="justify-self-end">{$t`Type:`}</Label>
-      <span class="col-span-4">{$t`${audio.type}`}</span>
+      <span class="col-span-4">{$t`${finalAudio.type}`}</span>
     </DevContent>
   </span>
-  <!-- contain-size prevents wavesurfer from freaking out inside a grid
-  contain-inline-size would improve the height reactivity of the waveform, but
-  results in the waveform sometimes change its height unexpectedly -->
-  <!-- pb-8 ensures the timeline is in the bounds of the container -->
-  <div class="w-full grow max-h-32 pb-3 contain-size border-y">
-    <Waveform {audio} bind:playing bind:audioApi bind:duration showTimeline autoplay class="size-full" />
-  </div>
-  <div class="flex gap-2">
-    <Button variant="secondary" icon="i-mdi-close" onclick={onDiscard} disabled={!audio}>{$t`Discard`}</Button>
-    <Button
-      icon={playing ? 'i-mdi-pause' : 'i-mdi-play'}
-      onclick={() => (playing ? audioApi?.pause() : audioApi?.play())}
-      disabled={!audioApi}
-      size="icon"
-    />
-  </div>
+    <!-- contain-size prevents wavesurfer from freaking out inside a grid
+    contain-inline-size would improve the height reactivity of the waveform, but
+    results in the waveform sometimes change its height unexpectedly -->
+    <!-- pb-8 ensures the timeline is in the bounds of the container -->
+    <div class="w-full grow max-h-32 pb-3 contain-size border-y">
+      <Waveform audio={finalAudio} bind:playing bind:audioApi bind:duration showTimeline autoplay class="size-full"/>
+    </div>
+    <div class="flex gap-2">
+      <Button variant="secondary" icon="i-mdi-close" onclick={onDiscard} disabled={!audio}>{$t`Discard`}</Button>
+      <Button
+        icon={playing ? 'i-mdi-pause' : 'i-mdi-play'}
+        onclick={() => (playing ? audioApi?.pause() : audioApi?.play())}
+        disabled={!audioApi}
+        size="icon"
+      />
+    </div>
+  {/if}
 </div>
--- frontend/viewer/src/lib/components/audio/ffmpeg/ffmpeg-api.ts
+++ frontend/viewer/src/lib/components/audio/ffmpeg/ffmpeg-api.ts
@@ -3,26 +3,29 @@ import coreURL from '@ffmpeg/core?url';
 import {fetchFile} from '@ffmpeg/util';
 import inlineDataUrlWorker from './bundled-ffmpeg-worker.js?url&inline';
 import wasmUrl from '@ffmpeg/core/wasm?url';
+import {randomId} from '$lib/utils';
 
 let loadingPromise: Promise<FFmpeg> | null = null;
 
 export async function loadFFmpeg(): Promise<FFmpeg> {
   if (loadingPromise) {
-    return loadingPromise;
+    return await loadingPromise;
   }
 
   try {
     loadingPromise = loadFFmpegInternal();
+    return await loadingPromise;
   } catch (error) {
     loadingPromise = null;
     throw error;
   }
-
-  return loadingPromise;
 }
 
 async function loadFFmpegInternal(): Promise<FFmpeg> {
   const ffmpeg = new FFmpeg();
+  ffmpeg.on('log', (msg) => {
+    console.log('FFmpeg log:', msg);
+  });
 
   await ffmpeg.load({
     coreURL: coreURL,
@@ -55,3 +58,99 @@ export async function convertToWav(ffmpeg: FFmpeg, blob: Blob): Promise<Blob> {
   return new Blob([data], {type: 'audio/wav'});
 }
 
+export class FFmpegFile {
+  readonly filename: string;
+  private id: string = randomId();
+  private prefix: string;
+
+  constructor(mimeType: string, filename: string, prefix: string) {
+    this.mimeType = mimeType;
+    this.filename = filename;
+    this.prefix = prefix;
+  }
+
+  get internalFilePath() {
+    return `${this.internalFileDir}/${this.filename}`;
+  }
+  get internalFileDir() {
+    return `/${this.prefix}/${this.id}`;
+  }
+  readonly mimeType: string;
+
+  changeExtension(newExtension: string, mimeType?: string, prefix?: string): FFmpegFile {
+    return new FFmpegFile(mimeType ?? this.mimeType, this.filename.replace(/\.[^.]+$/, `.${newExtension}`), prefix ?? this.prefix);
+  }
+}
+
+export class FFmpegApi {
+  public static async create(): Promise<FFmpegApi> {
+    console.log('Loading FFmpeg...');
+    const ffmpeg = await loadFFmpeg();
+    console.log('FFmpeg loaded:', ffmpeg);
+    return new FFmpegApi(ffmpeg);
+  }
+  private constructor(private ffmpeg: FFmpeg) {
+  }
+
+  private async createDir(path: string) {
+    console.log('Creating dir:', path);
+    const paths = path.split('/').filter(v => !!v);
+    let newDir = '';
+    for (const dir of paths) {
+      console.log('Checking dir:', newDir, dir);
+      const dirExists = (await this.ffmpeg.listDir(newDir || '/')).some(f => f.name === dir);
+      newDir += `/${dir}`;
+      if (dirExists) {
+        continue;
+      }
+      console.log('Creating dir:', newDir);
+      //create dir never returns if the dir already exists or if you're trying to create a sub directory of a directory which doesn't exist
+      await this.ffmpeg.createDir(newDir);
+    }
+  }
+  async toFFmpegFile(file: File): Promise<FFmpegFile> {
+    console.log('Writing file to ffmpeg FS:', file.name);
+    const ffmpegFile = new FFmpegFile(file.type, file.name, 'input');
+    await this.createDir(ffmpegFile.internalFileDir);
+    await this.ffmpeg.writeFile(ffmpegFile.internalFilePath, await fetchFile(file));
+    return ffmpegFile;
+  }
+
+  async readFile(file: FFmpegFile): Promise<File> {
+    console.log('Reading file from ffmpeg FS:', file.filename);
+    const data = await this.ffmpeg.readFile(file.internalFilePath) as Uint8Array;
+    return new File([data], file.filename, {type: file.mimeType});
+  }
+
+  async convertToWav(file: FFmpegFile): Promise<FFmpegFile> {
+    const convertedFile = file.changeExtension('wav', 'audio/wav', 'convert');
+    await this.createDir(convertedFile.internalFileDir);
+    await this.ffmpeg.exec(
+      [
+        '-i', file.internalFilePath,
+        '-af', 'loudnorm',
+        '-ar', '44100',
+        '-ac', '2',
+        '-codec:a', 'pcm_s16le',
+        convertedFile.internalFilePath
+      ]
+    );
+    return convertedFile;
+  }
+
+  async convertToFlac(file: FFmpegFile): Promise<FFmpegFile> {
+    console.log('Converting to FLAC:', file.filename);
+    const convertedFile = file.changeExtension('flac', 'audio/flac', 'convert');
+    await this.createDir(convertedFile.internalFileDir);
+    await this.ffmpeg.exec(
+      [
+        '-i', file.internalFilePath,
+        '-af', 'loudnorm',
+        '-ar', '44100',
+        '-codec:a', 'flac',
+        convertedFile.internalFilePath
+      ]
+    );
+    return convertedFile;
+  }
+}
