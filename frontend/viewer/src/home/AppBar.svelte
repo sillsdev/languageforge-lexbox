5f4a9c5b97d1008fd48258d5d942ebaeed8bbcda
Kevin Hahn <kevin_hahn@sil.org>
2025-08-04 14:58:57 +0700

    make app bar not partly transparent

--- frontend/viewer/src/home/AppBar.svelte
+++ frontend/viewer/src/home/AppBar.svelte
@@ -12,10 +12,22 @@
     if (tabTitle) {
       document.title = tabTitle;
     }
-  })
+  });
 </script>
-<header class="flex items-center relative z-50 gap-2 justify-between px-4 max-md:px-1 min-h-12 bg-primary/50 dark:bg-primary/70 shadow-md m-3 rounded sticky top-3">
+<header
+  class="AppBar flex items-center z-50 gap-2 justify-between px-4 max-md:px-1 min-h-12 shadow-md m-3 rounded sticky top-3">
   {@render title()}
   <div class="grow-0"></div>
   {@render actions()}
 </header>
+<style>
+  .AppBar {
+    --shading: 1.5;
+    background-color: hsl(from hsl(var(--primary)) h s calc(l * var(--shading)));
+  }
+
+  :global(.dark) .AppBar {
+    --shading: 0.5;
+  }
+
+</style>
