2b02568f15fd9217f53036d04ed57edd39bec8ac
Kevin Hahn <kevin_hahn@sil.org>
2025-08-07 15:35:44 +0700

    Fix permission checks on windows (#1885)
    * disable update check when running local builds
    
    * manually enable permissions on windows web view

--- backend/FwLite/FwLiteMaui/MainPage.xaml.Windows.cs
+++ backend/FwLite/FwLiteMaui/MainPage.xaml.Windows.cs
@@ -1,6 +1,7 @@
-using Microsoft.AspNetCore.Components.WebView;
-
 #if WINDOWS
+using Microsoft.AspNetCore.Components.WebView;
+using Microsoft.Web.WebView2.Core;
+
 namespace FwLiteMaui;
 
 public partial class MainPage
@@ -11,7 +12,34 @@ public partial class MainPage
 
     private partial void BlazorWebViewInitialized(object? sender, BlazorWebViewInitializedEventArgs e)
     {
-        e.WebView.CoreWebView2.PermissionRequested += new SilentPermissionRequestHandler().OnPermissionRequested;
+        //we only need microphone for now, but we'll just set them all so we don't have to worry about it later
+        Span<CoreWebView2PermissionKind> permissions =
+        [
+            CoreWebView2PermissionKind.Microphone,
+            CoreWebView2PermissionKind.Autoplay,
+            CoreWebView2PermissionKind.Camera,
+            CoreWebView2PermissionKind.Geolocation,
+            CoreWebView2PermissionKind.Notifications,
+            CoreWebView2PermissionKind.ClipboardRead,
+            CoreWebView2PermissionKind.FileReadWrite,
+            CoreWebView2PermissionKind.LocalFonts,
+            CoreWebView2PermissionKind.MultipleAutomaticDownloads,
+            CoreWebView2PermissionKind.OtherSensors,
+            CoreWebView2PermissionKind.WindowManagement,
+        ];
+#pragma warning disable VSTHRD110
+        foreach (var permission in permissions)
+        {
+            //either of these ip addresses may be used, so just do both
+            e.WebView.CoreWebView2.Profile.SetPermissionStateAsync(permission,
+                "https://0.0.0.1",
+                CoreWebView2PermissionState.Allow);
+            e.WebView.CoreWebView2.Profile.SetPermissionStateAsync(permission,
+                "https://0.0.0.0",
+                CoreWebView2PermissionState.Allow);
+        }
+#pragma warning restore VSTHRD110
+        e.WebView.CoreWebView2.Settings.IsGeneralAutofillEnabled = false;
     }
 }
 
