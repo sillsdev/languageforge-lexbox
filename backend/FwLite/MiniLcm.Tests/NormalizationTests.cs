464d4a09b6be83c3f76ca7c8f63e59d045acd4c1
Robin Munn <rmunn@pobox.com>
2025-08-08 13:32:28 +0700

    Add tests for NormalizationWrapper
    Also hardcode normalization form to NFD, since we don't plan on ever
    changing it.

---
+++ backend/FwLite/MiniLcm.Tests/NormalizationTests.cs
@@ -0,0 +1,87 @@
+using MiniLcm.Validators;
+using Moq;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace MiniLcm.Tests;
+
+public class NormalizationTests
+{
+    public IMiniLcmApi MockApi { get; init; }
+    public IMiniLcmApi NormalizingApi { get; init; }
+
+    public const string NFCString = "naïve"; // U+00EF LATIN SMALL LETTER I WITH DIAERESIS
+    public const string NFDString = "naïve"; // U+0069 LATIN SMALL LETTER I + U+0308 COMBINING DIAERESIS
+
+    public FilterQueryOptions NFCOptions = new()
+    {
+        Exemplar = new ExemplarOptions(NFCString, "en"),
+        Filter = new Filtering.EntryFilter { GridifyFilter = NFCString },
+    };
+
+    public QueryOptions NFCQueryOptions = new()
+    {
+        Exemplar = new ExemplarOptions(NFCString, "en"),
+        Filter = new Filtering.EntryFilter { GridifyFilter = NFCString },
+    };
+
+    public FilterQueryOptions NFDOptions = new()
+    {
+        Exemplar = new ExemplarOptions(NFDString, "en"),
+        Filter = new Filtering.EntryFilter { GridifyFilter = NFDString },
+    };
+
+    public NormalizationTests()
+    {
+        MockApi = Mock.Of<IMiniLcmApi>();
+        // Mock.Get(MockApi).Setup(api => api.SearchEntries(It.IsAny<string>(), null)).Returns(new List<Entry>().ToAsyncEnumerable());
+        var factory = new MiniLcmApiStringNormalizationWrapperFactory();
+        NormalizingApi = factory.Create(MockApi);
+    }
+
+    [Fact]
+    public void SearchEntriesIsNormalized()
+    {
+        NormalizingApi.Should().BeOfType<MiniLcmApiStringNormalizationWrapper>();
+        var results = NormalizingApi.SearchEntries(NFCString, null);
+        Mock.Get(MockApi).Verify(api => api.SearchEntries(NFDString, null));
+    }
+
+    [Fact]
+    public void SearchEntriesWithQueryOptionsAreNormalized()
+    {
+        NormalizingApi.Should().BeOfType<MiniLcmApiStringNormalizationWrapper>();
+        var results = NormalizingApi.SearchEntries(NFCString, NFCQueryOptions);
+        Mock.Get(MockApi).Verify(api => api.SearchEntries(NFDString, It.Is<QueryOptions>(
+            opt => opt.Exemplar!.Value == NFDOptions.Exemplar!.Value &&
+                   opt.Filter!.GridifyFilter == NFDOptions.Filter!.GridifyFilter)));
+    }
+
+    [Fact]
+    public void CountEntriesIsNormalized()
+    {
+        NormalizingApi.Should().BeOfType<MiniLcmApiStringNormalizationWrapper>();
+        var results = NormalizingApi.CountEntries(NFCString, null);
+        Mock.Get(MockApi).Verify(api => api.CountEntries(NFDString, null));
+    }
+
+    [Fact]
+    public void CountEntriesWithFilterQueryOptionsIsNormalized()
+    {
+        NormalizingApi.Should().BeOfType<MiniLcmApiStringNormalizationWrapper>();
+        var results = NormalizingApi.CountEntries(NFCString, NFCOptions);
+        Mock.Get(MockApi).Verify(api => api.CountEntries(NFDString, It.Is<FilterQueryOptions>(
+            opt => opt.Exemplar!.Value == NFDOptions.Exemplar!.Value &&
+                   opt.Filter!.GridifyFilter == NFDOptions.Filter!.GridifyFilter)));
+    }
+
+    [Fact]
+    public void GetEntriesIsNormalized()
+    {
+        NormalizingApi.Should().BeOfType<MiniLcmApiStringNormalizationWrapper>();
+        var results = NormalizingApi.GetEntries(NFCQueryOptions);
+        Mock.Get(MockApi).Verify(api => api.GetEntries(It.Is<QueryOptions>(
+            opt => opt.Exemplar!.Value == NFDOptions.Exemplar!.Value &&
+                   opt.Filter!.GridifyFilter == NFDOptions.Filter!.GridifyFilter)));
+    }
+}
--- backend/FwLite/MiniLcm/Normalization/MiniLcmApiStringNormalizationWrapper.cs
+++ backend/FwLite/MiniLcm/Normalization/MiniLcmApiStringNormalizationWrapper.cs
@@ -6,38 +6,39 @@ using MiniLcm.Wrappers;
 
 namespace MiniLcm.Validators;
 
-public class MiniLcmApiStringNormalizationWrapperFactory(NormalizationForm normalization = NormalizationForm.FormD) : IMiniLcmWrapperFactory
+public class MiniLcmApiStringNormalizationWrapperFactory() : IMiniLcmWrapperFactory
 {
     public IMiniLcmApi Create(IMiniLcmApi api, IProjectIdentifier _unused) => Create(api);
 
     public IMiniLcmApi Create(IMiniLcmApi api)
     {
-        return new MiniLcmApiStringNormalizationWrapper(api, normalization);
+        return new MiniLcmApiStringNormalizationWrapper(api);
     }
 }
 
 public partial class MiniLcmApiStringNormalizationWrapper(
-    IMiniLcmApi api,
-    NormalizationForm form) : IMiniLcmApi
+    IMiniLcmApi api) : IMiniLcmApi
 {
-    [BeaKona.AutoInterface(IncludeBaseInterfaces = true)]
+    public const NormalizationForm Form = NormalizationForm.FormD;
+
+    [BeaKona.AutoInterface(IncludeBaseInterfaces = true, MemberMatch = BeaKona.MemberMatchTypes.Any)]
     private readonly IMiniLcmApi _api = api;
 
     // ********** Overrides go here **********
 
     public IAsyncEnumerable<Entry> SearchEntries(string query, QueryOptions? options = null)
     {
-        return _api.SearchEntries(query.Normalize(form), options?.Normalized(form));
+        return _api.SearchEntries(query.Normalize(Form), options?.Normalized(Form));
     }
 
     public Task<int> CountEntries(string? query = null, FilterQueryOptions? options = null)
     {
-        return _api.CountEntries(query?.Normalize(form), options?.Normalized(form));
+        return _api.CountEntries(query?.Normalize(Form), options?.Normalized(Form));
     }
 
     public IAsyncEnumerable<Entry> GetEntries(QueryOptions? options = null)
     {
-        return _api.GetEntries(options?.Normalized(form));
+        return _api.GetEntries(options?.Normalized(Form));
     }
 
     void IDisposable.Dispose()
