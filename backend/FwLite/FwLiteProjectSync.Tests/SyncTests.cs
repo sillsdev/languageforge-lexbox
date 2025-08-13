80204f2e5d4bc17d26665ee9443a6ab501cc2a00
Kevin Hahn <kevin_hahn@sil.org>
2025-08-13 11:06:20 +0700

    only exclude properties which are `.Id` not `.ComplexFormId` for example

--- backend/FwLite/FwLiteProjectSync.Tests/SyncTests.cs
+++ backend/FwLite/FwLiteProjectSync.Tests/SyncTests.cs
@@ -93,9 +93,9 @@ public class SyncTests : IClassFixture<SyncFixture>, IAsyncLifetime
                 options
                     //DO NOT exclude things which are just not yet supported, otherwise you will run into a similar issue as seen in https://github.com/sillsdev/languageforge-lexbox/issues/1912
                     .Using<double>(Exclude)
-                    .When(info => info.RuntimeType == typeof(double) && info.Path.EndsWith("Order") && excludeOrderTypes.Contains(info.ParentType))
+                    .When(info => info.RuntimeType == typeof(double) && info.Path.EndsWith(".Order") && excludeOrderTypes.Contains(info.ParentType))
                     .Using<Guid>(Exclude)
-                    .When(info => info.RuntimeType == typeof(Guid) && info.Path.EndsWith("Id") && excludeIds.Contains(info.ParentType))
+                    .When(info => info.RuntimeType == typeof(Guid) && info.Path.EndsWith(".Id") && excludeIds.Contains(info.ParentType))
         );
 
         void Exclude<T>(IAssertionContext<T> context)
