2c83ae06ac996d14a315b1a0fd32944b34e96bb2
Kevin Hahn <kevin_hahn@sil.org>
2025-08-14 09:54:53 +0700

    tweak styles, change urls

--- frontend/viewer/src/lib/about/FeedbackDialog.svelte
+++ frontend/viewer/src/lib/about/FeedbackDialog.svelte
@@ -1,46 +1,54 @@
 <script lang="ts">
   import * as Dialog from '$lib/components/ui/dialog';
-  import { Button } from '$lib/components/ui/button';
-  import { t } from 'svelte-i18n-lingui';
-  import { Icon } from '$lib/components/ui/icon';
-  import { useFwLiteConfig } from '$lib/services/service-provider';
+  import {t} from 'svelte-i18n-lingui';
+  import {Icon} from '$lib/components/ui/icon';
+  import {useFwLiteConfig} from '$lib/services/service-provider';
 
-  let { open } = $props();
+  let {open = $bindable()}: { open: boolean } = $props();
   const config = useFwLiteConfig();
 
   const appVersion = config.appVersion;
+  const mailtoUrl = `mailto:lexbox_support@groups.sil.org?subject=${encodeURIComponent('FW Lite Feedback')}&body=${encodeURIComponent(`App Version: ${appVersion} on ${config.os.toString()}`)}`;
 </script>
 
 <Dialog.Root bind:open>
-  <Dialog.Content>
+  <Dialog.Content class="!max-h-none !min-h-0">
     <Dialog.Header>
       <Dialog.Title>{$t`Feedback`}</Dialog.Title>
     </Dialog.Header>
     <div class="flex flex-col gap-4">
-      <a href="https://userjo.com/1" target="_blank" class="flex items-center gap-4 p-4 rounded-lg hover:bg-muted">
-        <Icon icon="i-mdi-lightbulb-on-outline" class="text-2xl" />
+      <a href="https://lexbox.org/fw-lite/request-features" target="_blank" class="flex items-center gap-4 p-4 rounded-lg hover:bg-muted">
+        <Icon icon="i-mdi-lightbulb-on-outline" class="size-10"/>
         <div>
-          <div class="font-semibold">{$t`Suggest your ideas`}</div>
-          <div class="text-sm text-muted-foreground">{$t`Share your suggestions for new features or improvements.`}</div>
+          <div class="font-semibold underline">{$t`Suggest your ideas`}</div>
+          <div class="text-sm text-muted-foreground">
+            {$t`Share your suggestions for new features or improvements.`}
+          </div>
         </div>
       </a>
       <a href={config.feedbackUrl} target="_blank" class="flex items-center gap-4 p-4 rounded-lg hover:bg-muted">
-        <Icon icon="i-mdi-bug-outline" class="text-2xl" />
+        <Icon icon="i-mdi-bug-outline" class="size-10"/>
         <div>
-          <div class="font-semibold">{$t`Report a technical problem`}</div>
-          <div class="text-sm text-muted-foreground">{$t`Let us know about any bugs or technical issues you encounter.`}</div>
+          <div class="font-semibold underline">
+            {$t`Report a technical problem`}
+          </div>
+          <div
+            class="text-sm text-muted-foreground">
+            {$t`Let us know about any bugs or technical issues you encounter.`}
+          </div>
         </div>
       </a>
-      <a href={`mailto:lexbox_support@groups.sil.org?subject=FW%20Lite%20Feedback&body=App%20Version:%20${appVersion}`} class="flex items-center gap-4 p-4 rounded-lg hover:bg-muted">
-        <Icon icon="i-mdi-email-outline" class="text-2xl" />
+      <a href={mailtoUrl} class="flex items-center gap-4 p-4 rounded-lg hover:bg-muted">
+        <Icon icon="i-mdi-email-outline" class="size-10"/>
         <div>
-          <div class="font-semibold">{$t`Send us a message`}</div>
-          <div class="text-sm text-muted-foreground">{$t`For any other inquiries, feel free to send us an email.`}</div>
+          <div class="font-semibold underline">
+            {$t`Send us a message`}
+          </div>
+          <div class="text-sm text-muted-foreground">
+            {$t`For any other inquiries, feel free to send us an email.`}
+          </div>
         </div>
       </a>
     </div>
-    <Dialog.Footer>
-      <Button onclick={() => open = false}>{$t`Close`}</Button>
-    </Dialog.Footer>
   </Dialog.Content>
 </Dialog.Root>
