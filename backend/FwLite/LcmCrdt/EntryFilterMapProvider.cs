24db777941356c78e22b242620b9d1d0641c3e70
Kevin Hahn <kevin_hahn@sil.org>
2025-08-29 17:41:36 +0700

    enable filtering on rich text with gridify

--- backend/FwLite/LcmCrdt/EntryFilterMapProvider.cs
+++ backend/FwLite/LcmCrdt/EntryFilterMapProvider.cs
@@ -15,18 +15,18 @@ public class EntryFilterMapProvider : EntryFilterMapProvider<Entry>
         EntryFilter.NormalizeEmptyToNull<SemanticDomain>;
     public override Expression<Func<Entry, object?>> EntrySensesExampleSentences => e => e.Senses.Select(s => s.ExampleSentences);
     public override Expression<Func<Entry, string, object?>> EntrySensesExampleSentencesSentence =>
-        (e, ws) => e.Senses.SelectMany(s => s.ExampleSentences).Select(example => Json.Value(example.Sentence, ms => ms[ws]));
+        (e, ws) => e.Senses.SelectMany(s => s.ExampleSentences).Select(example => Json.Value(example.Sentence, ms => ms[ws])!.GetPlainText());
     public override Expression<Func<Entry, object?>> EntrySensesPartOfSpeechId => e => e.Senses.Select(s => s.PartOfSpeechId);
     public override Expression<Func<Entry, object?>> EntrySenses => e => e.Senses;
 
     public override Expression<Func<Entry, string, object?>> EntrySensesGloss =>
         (entry, ws) => entry.Senses.Select(s => Json.Value(s.Gloss, ms => ms[ws]));
     public override Expression<Func<Entry, string, object?>> EntrySensesDefinition =>
-        (entry, ws) => entry.Senses.Select(s => Json.Value(s.Definition, ms => ms[ws]));
-    public override Expression<Func<Entry, string, object?>> EntryNote => (entry, ws) => Json.Value(entry.Note, ms => ms[ws]);
+        (entry, ws) => entry.Senses.Select(s => Json.Value(s.Definition, ms => ms[ws])!.GetPlainText());
+    public override Expression<Func<Entry, string, object?>> EntryNote => (entry, ws) => Json.Value(entry.Note, ms => ms[ws])!.GetPlainText();
     public override Expression<Func<Entry, string, object?>> EntryLexemeForm => (entry, ws) => Json.Value(entry.LexemeForm, ms => ms[ws]);
     public override Expression<Func<Entry, string, object?>> EntryCitationForm => (entry, ws) => Json.Value(entry.CitationForm, ms => ms[ws]);
-    public override Expression<Func<Entry, string, object?>> EntryLiteralMeaning => (entry, ws) => Json.Value(entry.LiteralMeaning, ms => ms[ws]);
+    public override Expression<Func<Entry, string, object?>> EntryLiteralMeaning => (entry, ws) => Json.Value(entry.LiteralMeaning, ms => ms[ws])!.GetPlainText();
     public override Expression<Func<Entry, object?>> EntryComplexFormTypes => e => e.ComplexFormTypes;
     public override Func<string, object>? EntryComplexFormTypesConverter => EntryFilter.NormalizeEmptyToEmptyList<ComplexFormType>;
 }
