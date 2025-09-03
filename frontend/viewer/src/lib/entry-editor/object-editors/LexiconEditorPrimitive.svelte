bcb638ca5f3f7905cf42e7bf92273330bb9159f7
Kevin Hahn <kevin_hahn@sil.org>
2025-09-03 13:56:25 +0700

    create a lexicon editor primitive which takes an object of `Entry` `Sense` or `Example` and choses the related editor

---
+++ frontend/viewer/src/lib/entry-editor/object-editors/LexiconEditorPrimitive.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import type {IEntry, IExampleSentence, ISense} from '$lib/dotnet-types';
+  import {isEntry, isExample, isSense} from '$lib/utils';
+  import EntryEditorPrimitive from '$lib/entry-editor/object-editors/EntryEditorPrimitive.svelte';
+  import SenseEditorPrimitive from '$lib/entry-editor/object-editors/SenseEditorPrimitive.svelte';
+  import ExampleEditorPrimitive from '$lib/entry-editor/object-editors/ExampleEditorPrimitive.svelte';
+
+  let {
+    object
+  }: {
+    object: IEntry | ISense | IExampleSentence | undefined
+  } = $props();
+</script>
+{#if isEntry(object)}
+  <EntryEditorPrimitive entry={object} readonly />
+{:else if isSense(object)}
+  <SenseEditorPrimitive sense={object} readonly />
+{:else if isExample(object)}
+  <ExampleEditorPrimitive example={object} readonly />
+{/if}
