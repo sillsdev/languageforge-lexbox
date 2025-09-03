00c66bdd9fc29a834ae8e4687622d2bd3a4d0208
Kevin Hahn <kevin_hahn@sil.org>
2025-09-03 14:02:17 +0700

    pass the writing system abbreviation to the audio input for display in the audio dialog

--- frontend/viewer/src/lib/components/field-editors/multi-ws-input.svelte
+++ frontend/viewer/src/lib/components/field-editors/multi-ws-input.svelte
@@ -52,6 +52,7 @@
       {:else}
         <AudioInput
           bind:audioId={value[ws.wsId]} onchange={() => onchange?.(ws.wsId, value[ws.wsId], value)}
+          wsLabel={ws.abbreviation}
           {readonly} />
       {/if}
     </div>
--- frontend/viewer/src/lib/components/field-editors/rich-multi-ws-input.svelte
+++ frontend/viewer/src/lib/components/field-editors/rich-multi-ws-input.svelte
@@ -77,6 +77,7 @@
       {:else}
         <AudioInput
           bind:audioId={() => getAudioId(value[ws.wsId]), audioId => setAudioId(audioId, ws.wsId)}
+          wsLabel={ws.abbreviation}
           {readonly} />
       {/if}
     </div>
