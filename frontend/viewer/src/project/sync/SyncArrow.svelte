f49ead94cd24f7a07b10de6fc8628e73243bcb94
Kevin Hahn <kevin_hahn@sil.org>
2025-09-04 13:13:49 +0700

    fix sync arrow size when rotated

--- frontend/viewer/src/project/sync/SyncArrow.svelte
+++ frontend/viewer/src/project/sync/SyncArrow.svelte
@@ -3,23 +3,31 @@
 
   const {dir, tailLength = 20, size = 1, class: className}: {
     dir: 'up' | 'down' | 'left' | 'right',
-    tailLength: number,
-    size: number
+    tailLength?: number,
+    size?: number
     class?: string
   } = $props();
-  const height = $derived(tailLength);
+  const isVertical = $derived(dir === 'up' || dir === 'down');
+  const isHorizontal = $derived(dir === 'left' || dir === 'right');
+  const height = $derived(isVertical ? tailLength : 20);
+  const width = $derived(isHorizontal ? tailLength : 20);
+  const boxHeight = $derived((size * 20) + height - 20);
+  const boxWidth = $derived((size * 20) + width - 20);
 </script>
 <div class={cn(
     'block',
-    dir === 'right' && 'rotate-0',
-    dir === 'down' && 'rotate-180',
-    dir === 'left' && 'rotate-180',
-    dir === 'up' && 'rotate-0',
+    (dir === 'down' || dir === 'right') && 'rotate-180',
     className,
 )}>
-  <svg xmlns="http://www.w3.org/2000/svg" width="{size * 20}px" height="{(size * 20) + height - 20}px" viewBox="0 0 20 {height}">
-    <rect width="20" height={height} fill="none"/>
-    <path fill="currentColor" d="M 9 4 l 5.5 5.5 l 1.42 -1.42 L 8 0.16 L 0.08 8.08 L 1.5 9.5 L 7 4 V {tailLength} h 2 Z"/>
+  <svg xmlns="http://www.w3.org/2000/svg" width="{boxWidth}px" height="{boxHeight}px" viewBox="0 0 {width} {height}">
+    <rect width={width} height={height} fill="none"/>
+    {#if isVertical}
+      <path fill="currentColor"
+            d="M 9 4 l 5.5 5.5 l 1.42 -1.42 L 8 0.16 L 0.08 8.08 L 1.5 9.5 L 7 4 V {tailLength} h 2 Z"/>
+    {:else}
+      <path fill="currentColor"
+            d="M 4 7 l 5.5 -5.5 l -1.42 -1.42 L 0.16 8 L 8.08 15.92 L 9.5 14.5 L 4 9 H {tailLength} v -2 Z"/>
+    {/if}
   </svg>
 </div>
 <style>
