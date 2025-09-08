61066d396a7a7efcbead84946b511a3b00756f7a
Danny Rorabaugh <imnasnainaec@gmail.com>
2025-09-08 16:37:34 -0400

    Start matching styles of dictionary extension

---
+++ platform.bible-extension/src/types/localized-string-keys.ts
@@ -0,0 +1,10 @@
+import { LocalizeKey } from 'platform-bible-utils';
+
+export const LOCALIZED_STRING_KEYS: LocalizeKey[] = [
+  '%fwLiteExtension_error_failedToAddEntry%',
+  '%fwLiteExtension_error_gettingNetworkObject%',
+  '%fwLiteExtension_error_missingParam%',
+  '%fwLiteExtension_findWord_loading%',
+  '%fwLiteExtension_findWord_noMatchingEntries%',
+  '%fwLiteExtension_findWord_textField%',
+];
---
+++ platform.bible-extension/src/utils/use-is-wide-screen.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react';
+
+export function useIsWideScreen() {
+  const [isWide, setIsWide] = useState(() => window.innerWidth >= 1024);
+
+  useEffect(() => {
+    // Matches Tailwind css lg breakpoint
+    const mediaQuery = window.matchMedia('(min-width: 1024px)');
+
+    const handler = (e: MediaQueryListEvent) => setIsWide(e.matches);
+    mediaQuery.addEventListener('change', handler);
+
+    // Set initial state
+    setIsWide(mediaQuery.matches);
+
+    return () => mediaQuery.removeEventListener('change', handler);
+  }, []);
+
+  return isWide;
+}
