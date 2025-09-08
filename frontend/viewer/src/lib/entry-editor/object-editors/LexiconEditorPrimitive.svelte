814973bcb9588351180ef541d46ccb1cb0c4161f
Tim Haasdyk <tim_haasdyk@sil.org>
2025-09-08 15:28:42 +0200

    Add editor grid and root to editor primitive

--- frontend/viewer/src/lib/entry-editor/object-editors/LexiconEditorPrimitive.svelte
+++ frontend/viewer/src/lib/entry-editor/object-editors/LexiconEditorPrimitive.svelte
@@ -4,6 +4,7 @@
   import EntryEditorPrimitive from '$lib/entry-editor/object-editors/EntryEditorPrimitive.svelte';
   import SenseEditorPrimitive from '$lib/entry-editor/object-editors/SenseEditorPrimitive.svelte';
   import ExampleEditorPrimitive from '$lib/entry-editor/object-editors/ExampleEditorPrimitive.svelte';
+  import * as Editor from '$lib/components/editor';
 
   let {
     object
@@ -11,10 +12,15 @@
     object: IEntry | ISense | IExampleSentence | undefined
   } = $props();
 </script>
-{#if isEntry(object)}
-  <EntryEditorPrimitive entry={object} readonly />
-{:else if isSense(object)}
-  <SenseEditorPrimitive sense={object} readonly />
-{:else if isExample(object)}
-  <ExampleEditorPrimitive example={object} readonly />
-{/if}
+
+<Editor.Root>
+  <Editor.Grid>
+    {#if isEntry(object)}
+      <EntryEditorPrimitive entry={object} readonly />
+    {:else if isSense(object)}
+      <SenseEditorPrimitive sense={object} readonly />
+    {:else if isExample(object)}
+      <ExampleEditorPrimitive example={object} readonly />
+    {/if}
+  </Editor.Grid>
+</Editor.Root>
