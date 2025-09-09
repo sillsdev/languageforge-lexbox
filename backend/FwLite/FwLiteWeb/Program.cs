85fd9ff61a1e76d4e3e40e1afe9f26458c696178
Kevin Hahn <kevin_hahn@sil.org>
2025-09-09 13:18:12 +0700

    Handle null checks in `Directory.SetCurrentDirectory` to avoid potential errors on startup.

--- backend/FwLite/FwLiteWeb/Program.cs
+++ backend/FwLite/FwLiteWeb/Program.cs
@@ -3,7 +3,8 @@ using FwLiteWeb;
 using Microsoft.Extensions.Options;
 
 //paratext won't let us change the working directory, and if it's not set correctly then loading js files doesn't work
-Directory.SetCurrentDirectory(Path.GetDirectoryName(typeof(Program).Assembly.Location)!);
+if (Path.GetDirectoryName(typeof(Program).Assembly.Location) is {} directoryName)
+    Directory.SetCurrentDirectory(directoryName);
 var app = FwLiteWebServer.SetupAppServer(new() {Args = args});
 await using (app)
 {
