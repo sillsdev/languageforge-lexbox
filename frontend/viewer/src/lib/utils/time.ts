1573b35babfd5eb38e7909d5b6951dcd96edbe70
Tim Haasdyk <tim_haasdyk@sil.org>
2025-09-09 08:46:04 +0200

    Remove more unused time util code

--- frontend/viewer/src/lib/utils/time.ts
+++ frontend/viewer/src/lib/utils/time.ts
@@ -1,10 +1,4 @@
-export const enum Duration {
-  Default = 5000,
-  Medium = 10000,
-  Long = 15000,
-}
-
-export async function delay<T>(ms: Duration | number = Duration.Default): Promise<T> {
+export async function delay<T>(ms: number): Promise<T> {
   return new Promise<T>(resolve => setTimeout(resolve, ms));
 }
 
