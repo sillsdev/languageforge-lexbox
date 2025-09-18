df23b7c7c6d9549831e581fc8ab7b07f8ab32cf9
Tim Haasdyk <tim_haasdyk@sil.org>
2025-09-18 14:02:55 +0200

    Make generated regression test rich-spans less noisy

--- backend/FwLite/MiniLcm.Tests/AutoFakerHelpers/AutoFakerDefault.cs
+++ backend/FwLite/MiniLcm.Tests/AutoFakerHelpers/AutoFakerDefault.cs
@@ -8,7 +8,7 @@ public static class AutoFakerDefault
 {
     public static readonly AutoFakerConfig Config = MakeConfig();
 
-    public static AutoFakerConfig MakeConfig(string[]? validWs = null, int repeatCount = 5)
+    public static AutoFakerConfig MakeConfig(string[]? validWs = null, int repeatCount = 5, bool minimalRichSpans = false)
     {
         return new AutoFakerConfig()
         {
@@ -17,6 +17,7 @@ public static class AutoFakerDefault
             [
                 new MultiStringOverride(validWs),
                 new RichMultiStringOverride(validWs),
+                new RichSpanOverride(minimalRichSpans),
                 new WritingSystemIdOverride(validWs),
                 new ObjectWithIdOverride(),
                 new OrderableOverride(),
