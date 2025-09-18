2d19212f5e302e7dc2350335e8aaa6e0c2aae79c
Tim Haasdyk <tim_haasdyk@sil.org>
2025-09-18 11:29:35 +0200

    Add RegressionDataUpToDate to keep change regression data up-to-date

--- backend/FwLite/LcmCrdt.Tests/Changes/ChangeSerializationTests.cs
+++ backend/FwLite/LcmCrdt.Tests/Changes/ChangeSerializationTests.cs
@@ -1,12 +1,14 @@
+using System.Buffers;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Text;
 using System.Text.Json;
+using System.Text.Json.Nodes;
 using FluentAssertions.Execution;
 using LcmCrdt.Changes;
 using LcmCrdt.Changes.Entries;
 using MiniLcm.Tests.AutoFakerHelpers;
 using SIL.Harmony.Changes;
-using SIL.WritingSystems;
 using Soenneker.Utils.AutoBogus;
 using SystemTextJsonPatch;
 
@@ -22,42 +24,56 @@ public class ChangeSerializationTests
         return config.JsonSerializerOptions;
     });
     private static readonly JsonSerializerOptions Options = LazyOptions.Value;
+    private static readonly JsonSerializerOptions OptionsIndented = new(Options)
+    {
+        WriteIndented = true,
+    };
     private static readonly AutoFaker Faker = new()
     {
         Config = AutoFakerDefault.MakeConfig(repeatCount: 1)
     };
 
+    private static IEnumerable<IChange> GeneratedChangesForType(Type type)
+    {
+        //can't generate this type because there's no public constructor
+        if (type == typeof(SetComplexFormComponentChange))
+        {
+            yield return SetComplexFormComponentChange.NewComplexForm(Guid.NewGuid(), Guid.NewGuid());
+            yield return SetComplexFormComponentChange.NewComponent(Guid.NewGuid(), Guid.NewGuid());
+            yield return SetComplexFormComponentChange.NewComponentSense(Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid());
+            yield break;
+        }
+
+        object change;
+        if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(JsonPatchChange<>))
+        {
+            change = PatchMethod.MakeGenericMethod(type.GenericTypeArguments[0]).Invoke(null, null)!;
+        }
+        else
+        {
+            try
+            {
+                change = Faker.Generate(type);
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Failed to generate change of type {type.Name}", e);
+            }
+        }
+
+        change.Should().NotBeNull($"change type {type.Name} should have been generated").And.BeAssignableTo<IChange>();
+        yield return (IChange)change;
+    }
+
     private static IEnumerable<IChange> GeneratedChanges()
     {
         foreach (var type in LcmCrdtKernel.AllChangeTypes())
         {
-            //can't generate this type because there's no public constructor, so its specified below
-            if (type == typeof(SetComplexFormComponentChange)) continue;
-
-            object change;
-            if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(JsonPatchChange<>))
+            foreach (var change in GeneratedChangesForType(type))
             {
-                change = PatchMethod.MakeGenericMethod(type.GenericTypeArguments[0]).Invoke(null, null)!;
+                yield return change;
             }
-            else
-            {
-                try
-                {
-                    change = Faker.Generate(type);
-                }
-                catch (Exception e)
-                {
-                    throw new Exception($"Failed to generate change of type {type.Name}", e);
-                }
-            }
-
-            change.Should().NotBeNull($"change type {type.Name} should have been generated").And.BeAssignableTo<IChange>();
-            yield return (IChange) change;
         }
-
-        yield return SetComplexFormComponentChange.NewComplexForm(Guid.NewGuid(), Guid.NewGuid());
-        yield return SetComplexFormComponentChange.NewComponent(Guid.NewGuid(), Guid.NewGuid());
-        yield return SetComplexFormComponentChange.NewComponentSense(Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid());
     }
 
     public static IEnumerable<object[]> Changes()
@@ -103,12 +119,13 @@ public class ChangeSerializationTests
     }
 
     [Fact]
-    public void CanDeserializeRegressionData()
+    public void CanDeserializeLatestRegressionData()
     {
-        //this file represents projects which already have changes applied, we want to ensure that we don't break anything.
-        //nothing should ever be removed from this file
-        //if a new property is added then a new json object should be added with that property
-        using var jsonFile = File.OpenRead(GetJsonFilePath("RegressionDeserializationData.json"));
+        //nothing should ever be removed from this file!
+        //it represents changes that could be out in the wild and we need to support
+        //changes are updated and appended by RegressionDataUpToDate() whenever it finds a "latest" change that doesn't stably round-trip
+        //or when it finds a change type that isn't represented
+        using var jsonFile = File.OpenRead(GetJsonFilePath("RegressionDeserializationData.latest.verified.txt"));
         var changes = JsonSerializer.Deserialize<List<IChange>>(jsonFile, Options);
         changes.Should().NotBeNullOrEmpty().And.NotContainNulls();
 
@@ -123,12 +140,86 @@ public class ChangeSerializationTests
         }
     }
 
+    [Fact]
+    public void CanDeserializeLegacyRegressionData()
+    {
+        //nothing should ever be removed from this file!
+        //it represents changes that could be out in the wild and we need to support
+        //changes are moved here from RegressionDeserializationData.latest.verified.txt
+        //when RegressionDataUpToDate() detects that it doesn't stably round-trip
+        using var jsonFile = File.OpenRead(GetJsonFilePath("RegressionDeserializationData.legacy.verified.txt"));
+        var changes = JsonSerializer.Deserialize<List<IChange>>(jsonFile, Options);
+        changes.Should().NotBeNullOrEmpty().And.NotContainNulls();
+    }
+
+    [Fact]
+    public async Task RegressionDataUpToDate()
+    {
+        var legacyJsonArray = ReadJsonArrayFromFile(GetJsonFilePath("RegressionDeserializationData.legacy.verified.txt"));
+        var latestJsonArray = ReadJsonArrayFromFile(GetJsonFilePath("RegressionDeserializationData.latest.verified.txt"));
+        var newLatestJsonArray = new JsonArray();
+
+        var seenChangeTypes = new HashSet<Type>();
+        foreach (var latestJsonNode in latestJsonArray)
+        {
+            latestJsonNode.Should().NotBeNull();
+            var latestJson = ToNormalizedIndentedJsonString(latestJsonNode);
+            latestJson.Should().NotBeNullOrWhiteSpace();
+            var change = JsonSerializer.Deserialize<IChange>(latestJsonNode, Options);
+            change.Should().NotBeNull();
+            seenChangeTypes.Add(change.GetType());
+            var newLatestJson = JsonSerializer.Serialize(change, OptionsIndented);
+
+            if (latestJson != newLatestJson)
+            {
+                // The current "latest" json doesn't match it's reserialized form.
+                // I.e. it's no longer the latest. It's now legacy
+                legacyJsonArray.Add(latestJsonNode.DeepClone());
+                newLatestJsonArray.Add(JsonNode.Parse(newLatestJson));
+                // additionally we re-add generated changes, because it's much easier for a dev to remove unwanted changes than
+                // to manually generate and insert them. We can remove this if it's too noisy
+                foreach (var generatedChange in GeneratedChangesForType(change.GetType()))
+                {
+                    var serialized = JsonSerializer.Serialize(generatedChange, OptionsIndented);
+                    newLatestJsonArray.Add(JsonNode.Parse(serialized));
+                }
+            }
+            else
+            {
+                // it's still the latest
+                newLatestJsonArray.Add(latestJsonNode.DeepClone());
+            }
+        }
+
+        await Task.WhenAll(
+            Verify(SerializeRegressionData(legacyJsonArray))
+                .UseStrictJson()
+                .UseFileName("RegressionDeserializationData.legacy"),
+            Verify(SerializeRegressionData(newLatestJsonArray))
+                .UseStrictJson()
+                .UseFileName("RegressionDeserializationData.latest")
+        );
+    }
+
+    private static string SerializeRegressionData(JsonArray jsonArray)
+    {
+        return JsonSerializer.Serialize(jsonArray, new JsonSerializerOptions
+        {
+            WriteIndented = true,
+            Encoder = Options.Encoder,
+        })
+        // The "+" in DateTimeOffsets does not get escaped by our standard crdt serializer,
+        // but it does here. Presumably, because it's reading it as a string and not a DateTimeOffset
+        .Replace("\\u002B", "+");
+    }
+
     //helper method, can be called manually to regenerate the json file
+    //Note: RegressionDataUpToDate() should generate new changes as necessary
     [Fact(Skip = "Only run manually")]
     public static void GenerateNewJsonFile()
     {
         using var jsonFile = File.Open(GetJsonFilePath("NewJson.json"), FileMode.Create);
-        JsonSerializer.Serialize(jsonFile, GeneratedChanges(), Options);
+        JsonSerializer.Serialize(jsonFile, GeneratedChanges(), OptionsIndented);
     }
 
     private static string GetJsonFilePath(string name, [CallerFilePath] string sourceFile = "")
@@ -136,5 +227,35 @@ public class ChangeSerializationTests
         return Path.Combine(
             Path.GetDirectoryName(sourceFile) ?? throw new InvalidOperationException("Could not get directory of source file"),
             name);
+    }
+
+    private static readonly JsonWriterOptions GenericJsonWriterOptions = new()
+    {
+        Indented = true,
+        Encoder = Options.Encoder,
+    };
+
+    private static string ToNormalizedIndentedJsonString(JsonNode element)
+    {
+        var buffer = new ArrayBufferWriter<byte>();
+        using (var writer = new Utf8JsonWriter(buffer, GenericJsonWriterOptions))
+        {
+            element.WriteTo(writer);
+        }
+        return Encoding.UTF8.GetString(buffer.WrittenSpan)
+        // The "+" in DateTimeOffsets does not get escaped by our standard crdt serializer,
+        // but it does here. Presumably, because it's reading it as a string and not a DateTimeOffset
+        .Replace("\\u002B", "+");
+    }
+
+    private static JsonArray ReadJsonArrayFromFile(string path)
+    {
+        using var stream = File.OpenRead(path);
+        var node = JsonNode.Parse(stream, null, new()
+        {
+            AllowTrailingCommas = true,
+            CommentHandling = JsonCommentHandling.Skip
+        }) ?? throw new InvalidOperationException("Could not parse json array");
+        return node.AsArray();
     }
 }
