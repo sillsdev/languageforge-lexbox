ac771741ef7e3566827c0b6a5d102ed3b17a9922
Tim Haasdyk <tim_haasdyk@sil.org>
2025-10-21 17:52:50 +0200

    Add LcmDebugger method for syncing downloaded fw-headless project

--- backend/FwLite/LcmDebugger/Program.cs
+++ backend/FwLite/LcmDebugger/Program.cs
@@ -1,6 +1,5 @@
 // See https://aka.ms/new-console-template for more information
 
-using System.Text.Json;
 using FwDataMiniLcmBridge;
 using FwLiteProjectSync;
 using LcmCrdt;
@@ -8,11 +7,8 @@ using LcmDebugger;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using MiniLcm.Exceptions;
-using Refit;
-using SIL.Harmony;
-using SIL.Harmony.Core;
+using MiniLcm.Project;
+using Moq;
 
 var builder = Host.CreateApplicationBuilder();
 //slows down import to log all sql.
@@ -20,9 +16,12 @@ builder.Logging.AddFilter("Microsoft.EntityFrameworkCore", LogLevel.Warning);
 builder.Services.AddFwDataBridge();
 builder.Services.AddLcmCrdtClient();
 builder.Services.AddFwLiteProjectSync();
+builder.Services.AddScoped((_services) => new Mock<IServerHttpClientProvider>().Object);
 
 using var app = builder.Build();
 
 await using var scope = app.Services.CreateAsyncScope();
+var services = scope.ServiceProvider;
 
-await scope.ServiceProvider.PrintAllEntries("sena-3");
+// await services.SyncDownloadedProject("uzb-flex-20251021141736/uzb-flex-64ae5ccc-97fb-4e0a-ba8c-7140b51393d8", dryRun: false);
+// await services.PrintAllEntries("sena-3");
