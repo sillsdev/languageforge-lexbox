99970e1011c249251b7880e81edae049c66f85ab
Tim Haasdyk <tim_haasdyk@sil.org>
2025-11-25 16:13:41 +0100

    Add download page link and download update as install fallback

--- frontend/viewer/src/lib/updates/utils.ts
+++ frontend/viewer/src/lib/updates/utils.ts
@@ -7,8 +7,33 @@ const updateUrls: Partial<Record<FwLitePlatform, string>> = {
   [FwLitePlatform.Android]: 'https://play.google.com/store/apps/details?id=org.sil.FwLiteMaui',
 };
 
+const downloadPageUrls: Partial<Record<FwLitePlatform, string>> = {
+  [FwLitePlatform.Android]: 'https://play.google.com/store/apps/details?id=org.sil.FwLiteMaui',
+};
+
+
+export const releaseNotesUrl = 'https://community.software.sil.org/t/10807';
+
+/**
+ * will start download directly or open the appropriate store page
+ */
 export async function openReleaseUrl(release: IFwLiteRelease) {
-  const fwliteConfig = useFwLiteConfig();
-  const url = updateUrls[fwliteConfig.os] ?? release.url;
+  const url = getReleaseUrl(release);
   await openUrl(url);
+}
+
+/**
+ * returns a url directly to the update package if possible, otherwise the release page
+ */
+export function getReleaseUrl(release: IFwLiteRelease): string {
+  const fwliteConfig = useFwLiteConfig();
+  return updateUrls[fwliteConfig.os] ?? release.url;
+}
+
+/**
+ * returns a url to the download page for the current platform
+ */
+export function getDownloadPageUrl(): string {
+  const fwliteConfig = useFwLiteConfig();
+  return downloadPageUrls[fwliteConfig.os] ?? 'https://lexbox.org/fw-lite';
 }
