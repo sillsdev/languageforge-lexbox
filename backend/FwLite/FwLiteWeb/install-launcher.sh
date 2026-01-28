bf8440be9ab00145366f6bc6b0f8b375e12b6ceb
Robin Munn <rmunn@pobox.com>
2026-01-28 14:47:40 +0700

    Give manual install instructions if install fails
    It's probably not possible for the `cd` to fail, but if it ever does,
    the rest of the script would not run correctly. Rather than a generic
    error message if that happens, let's include manual instructions if the
    script fails so that users can get up and running anyway.

--- backend/FwLite/FwLiteWeb/install-launcher.sh
+++ backend/FwLite/FwLiteWeb/install-launcher.sh
@@ -4,7 +4,15 @@
 APPDIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
 # Make it the current directory so rest of script will work
-cd "$APPDIR"
+cd "$APPDIR" || {
+        echo "Automatic install failed: couldn't load launcher template from $APPDIR"
+        echo "You can create the FwLite launcher yourself by doing the following:"
+        echo "  1. Copy $APPDIR/fwlite.desktop.template to your home folder"
+        echo "  2. Edit it to replace the word APPDIR with ${APPDIR}"
+        echo "  3. Rename it to fwlite.desktop and run chmod +x fwlite.desktop"
+        echo "  4. Copy it into ${HOME}/.local/share/applications"
+        exit 1
+}
 
 # Replace APPDIR in the template and create the actual .desktop file
 sed "s|APPDIR|$APPDIR|g" fwlite.desktop.template > fwlite.desktop
