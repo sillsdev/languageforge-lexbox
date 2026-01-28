b293dbc48d94ca7c56d48d3c5064269c5d984aee
Robin Munn <rmunn@pobox.com>
2026-01-28 16:30:27 +0700

    Find fwlite.desktop.template in correct directory (#2133)
    If install-lancher.sh script is run from a different working directory,
    then it can fail to find the fwlite.desktop.template file. We can fix
    that by finding the script's directory and cd'ing into it at the outset.
    This will only affect the working directory of the subshell running the
    script, and will not affect the user's working directory, so it's safe.

--- backend/FwLite/FwLiteWeb/install-launcher.sh
+++ backend/FwLite/FwLiteWeb/install-launcher.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-# Get the full path to the current directory
-APPDIR="$(pwd)"
+# Get the full path to the script directory
+APPDIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+
+# Make it the current directory so rest of script will work
+cd "$APPDIR" || {
+        echo "Automatic install failed: couldn't load launcher template from $APPDIR"
+        echo "You can create the FwLite launcher yourself by doing the following:"
+        echo "  1. Copy $APPDIR/fwlite.desktop.template to your home folder"
+        echo "  2. Edit it to replace the word APPDIR with ${APPDIR}"
+        echo "  3. Rename it to fwlite.desktop and run chmod +x fwlite.desktop"
+        echo "  4. Copy it into ${HOME}/.local/share/applications"
+        exit 1
+}
 
 # Replace APPDIR in the template and create the actual .desktop file
 sed "s|APPDIR|$APPDIR|g" fwlite.desktop.template > fwlite.desktop
