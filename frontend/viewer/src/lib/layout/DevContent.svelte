1934dab935e12ae1c00233bc8311457a5a4198db
Tim Haasdyk <tim_haasdyk@sil.org>
2026-02-06 14:19:38 +0100

    Add dev toggle to project view

--- frontend/viewer/src/lib/layout/DevContent.svelte
+++ frontend/viewer/src/lib/layout/DevContent.svelte
@@ -1,5 +1,6 @@
 <script context="module" lang="ts">
-  import { writable } from 'svelte/store';
+  import {multiClick} from '$lib/attachments/multiClick';
+  import {get, writable} from 'svelte/store';
 
   //indicates that the user is a developer, show them features that are not ready for production, etc.
   //does not indicate this is at development time
@@ -11,6 +12,11 @@
     enable ? localStorage.setItem('devMode', 'true') : localStorage.removeItem('devMode');
   };
   isDev.set(localStorage.getItem('devMode') === 'true');
+  export const devModeToggle = multiClick({
+    count: 3,
+    timeoutMs: 500,
+    onTrigger: () => globalThis.enableDevMode(!get(isDev)),
+  });
 </script>
 
 <script lang="ts">
--- frontend/viewer/src/project/ProjectSidebar.svelte
+++ frontend/viewer/src/project/ProjectSidebar.svelte
@@ -13,7 +13,7 @@
   import {navigate, useRouter} from 'svelte-routing';
   import type {IProjectModel} from '$lib/dotnet-types';
   import {usePrimaryAction} from './SidebarPrimaryAction.svelte';
-  import DevContent from '$lib/layout/DevContent.svelte';
+  import DevContent, {devModeToggle} from '$lib/layout/DevContent.svelte';
   import TroubleshootDialog from '$lib/troubleshoot/TroubleshootDialog.svelte';
   import FeedbackDialog from '$lib/about/FeedbackDialog.svelte';
   import SyncDialog from './SyncDialog.svelte';
@@ -212,7 +212,7 @@
   <Sidebar.Footer>
       <div class="text-xs text-muted-foreground py-2 m-auto">
         <div>{$t`Version ${config.appVersion}`}</div>
-        <div>{$t`Made with ❤️ from 🇦🇹 🇹🇭 🇺🇸`}</div>
+        <div {@attach devModeToggle}>{$t`Made with ❤️ from 🇦🇹 🇹🇭 🇺🇸`}</div>
       </div>
   </Sidebar.Footer>
   <Sidebar.Rail></Sidebar.Rail>
