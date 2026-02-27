1483e0a4e81a77ddb1bbf890d3e07b4356fbfdde
Robin Munn <rmunn@pobox.com>
2026-02-27 16:32:04 +0700

    Test for parsing query params in CGI
    If this is correct, we'll use this approach to implement the regexcount
    command (which will pass one regex to Mercurial for filenames, and a
    second regex to grep for finding patterns in those files).

--- hgweb/command-runner.sh
+++ hgweb/command-runner.sh
@@ -8,6 +8,24 @@ IFS='/' read -ra PATH_SEGMENTS <<< "$PATH_INFO"
 project_code="${PATH_SEGMENTS[1]}"
 command_name="${PATH_SEGMENTS[2]}"
 
+# Get the query string from the URL (used in regexcount command)
+IFS='&=' read -ra QUERY_PARAMS <<< "$QUERY_STRING"
+# Temp debugging: echo the params count, followed by each param
+if [[ ${#QUERY_PARAMS[@]} -gt 0 ]]; then
+    echo "lexbox-version: $APP_VERSION"
+    echo "Status: 200 OK"
+    echo "Content-type: text/plain"
+    echo ""
+    for i in "${!QUERY_PARAMS[@]}"; do
+        j=$((i + 1))
+        key=${QUERY_PARAMS[$i]}
+        value=${QUERY_PARAMS[$j]}
+        echo "Param $((i / 2)): $key = $value"
+    done
+    exit 0
+fi
+
+
 # Ensure the project code and command name are safe to use in a shell command
 if [[ ! "$project_code" =~ ^[a-z0-9][a-z0-9-]*$ ]] || [[ ! "$command_name" =~ ^[a-zA-Z0-9]+$ ]]; then
     echo "Content-type: text/plain"
