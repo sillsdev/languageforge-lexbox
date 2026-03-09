39a78774605e9f07080e073508555b70f822bb09
D. Ror. <imnasnainaec@gmail.com>
2026-03-09 10:42:16 -0400

    Disable file-logging in Platform.Bible extension (#2196)
    Co-authored-by: Tim Haasdyk <tim_haasdyk@sil.org>

--- backend/FwLite/FwLiteWeb/FwLiteWebConfig.cs
+++ backend/FwLite/FwLiteWeb/FwLiteWebConfig.cs
@@ -5,4 +5,5 @@ public class FwLiteWebConfig
     public bool CorsAllowAny { get; init; } = false;
     public bool OpenBrowser { get; init; } = true;
     public string? LogFileName { get; init; } = null;
+    public bool EnableFileLogging { get; init; } = true;
 }
