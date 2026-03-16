1e5af4905694fa19452ecb25781c72fca70ff391
Robin Munn <rmunn@pobox.com>
2026-03-16 13:43:26 +0700

    Tell C# CreateMorphTypeChange constructor is okay
    The CreateMorphTypeChange main constructor sets all required properties,
    but C# needs a compiler hint to tell it so.

--- backend/FwLite/LcmCrdt/Changes/CreateMorphTypeChange.cs
+++ backend/FwLite/LcmCrdt/Changes/CreateMorphTypeChange.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.CodeAnalysis;
 using System.Text.Json.Serialization;
 using LcmCrdt.Utils;
 using SIL.Harmony;
@@ -9,6 +10,7 @@ namespace LcmCrdt.Changes;
 
 public class CreateMorphTypeChange : CreateChange<MorphType>, ISelfNamedType<CreateMorphTypeChange>
 {
+    [SetsRequiredMembers]
     public CreateMorphTypeChange(MorphType morphType) : base(morphType.Id == Guid.Empty ? Guid.NewGuid() : morphType.Id)
     {
         morphType.Id = EntityId;
