a3cb7c5c9a88d633ea6e6a47326984da1f65b892
Tim Haasdyk <tim_haasdyk@sil.org>
2026-03-27 12:25:59 +0100

    Add shadcn button-group component

--- frontend/viewer/src/lib/components/ui/separator/separator.svelte
+++ frontend/viewer/src/lib/components/ui/separator/separator.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import {cn} from '$lib/utils.js';
   import {Separator as SeparatorPrimitive} from 'bits-ui';
+  import {cn} from '$lib/utils.js';
 
   let {
     ref = $bindable(null),
@@ -14,7 +14,9 @@
   bind:ref
   data-slot={dataSlot}
   class={cn(
-    'bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:min-h-full data-[orientation=vertical]:w-px',
+    'bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:w-px',
+    // this is different in shadcn/ui but self-stretch breaks things for us
+    'data-[orientation=vertical]:h-full',
     className,
   )}
   {...restProps}
