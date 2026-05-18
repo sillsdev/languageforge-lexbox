eb4e34cca741270ba463357f098581eca616a5a4
Tim Haasdyk <tim_haasdyk@sil.org>
2026-05-18 15:12:59 +0200

    Show all root fields in Update-entry-dialog

---
+++ frontend/viewer/src/lib/views/RootFields.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import {initViewService, useViewService} from '$lib/views/view-service.svelte';
+  import type {Snippet} from 'svelte';
+
+  let {children}: {children?: Snippet} = $props();
+
+  const viewService = useViewService();
+  const overrideService = initViewService({persist: false});
+
+  $effect.pre(() => {
+    // use the root view of what is potentially a custom-view
+    overrideService.overrideView(viewService.rootView);
+  });
+</script>
+
+{@render children?.()}
