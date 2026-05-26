6265405560d64c25d608dbeefb5787e33598a8ed
Tim Haasdyk <tim_haasdyk@sil.org>
2026-05-26 13:53:14 +0200

    Fix: don't drop className

--- frontend/viewer/src/lib/components/ui/radio-group/radio-group-option.svelte
+++ frontend/viewer/src/lib/components/ui/radio-group/radio-group-option.svelte
@@ -12,7 +12,7 @@
   let {ref = $bindable(null), class: className, children, ...restProps}: Props = $props();
 </script>
 
-<Label class={cn('flex items-center gap-4 md:gap-2 max-md:py-3', restProps.disabled || 'cursor-pointer')}>
+<Label class={cn('flex items-center gap-4 md:gap-2 max-md:py-3', restProps.disabled || 'cursor-pointer', className)}>
   <Item bind:ref {...restProps} />
   {@render children?.()}
 </Label>
