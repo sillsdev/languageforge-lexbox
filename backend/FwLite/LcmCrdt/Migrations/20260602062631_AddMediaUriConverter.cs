2d2754320b817ac73ef2a36bcac626e2605e11c3
Robin Munn <rmunn@pobox.com>
2026-06-04 15:52:15 +0700

    Bump Refit package to 10.2.0 (10.1.6 rerelease)

---
+++ backend/FwLite/LcmCrdt/Migrations/20260602062631_AddMediaUriConverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LcmCrdt.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMediaUriConverter : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Picture",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Order = table.Column<double>(type: "REAL", nullable: false),
+                    MediaUri = table.Column<string>(type: "TEXT", nullable: false),
+                    Caption = table.Column<string>(type: "jsonb", nullable: false),
+                    DeletedAt = table.Column<DateTimeOffset>(type: "TEXT", nullable: true),
+                    SenseId = table.Column<Guid>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Picture", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Picture_Sense_SenseId",
+                        column: x => x.SenseId,
+                        principalTable: "Sense",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Picture_SenseId",
+                table: "Picture",
+                column: "SenseId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Picture");
+        }
+    }
+}
