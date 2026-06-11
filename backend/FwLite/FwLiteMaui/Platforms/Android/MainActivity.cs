e9d99a474608651554cf03e0f9748fb02e8aea20
Kevin Hahn <kevin_hahn@sil.org>
2026-06-11 15:53:51 +0700

    mark the android activity as resizable to allow resizing on chromebooks

--- backend/FwLite/FwLiteMaui/Platforms/Android/MainActivity.cs
+++ backend/FwLite/FwLiteMaui/Platforms/Android/MainActivity.cs
@@ -12,6 +12,7 @@ namespace FwLiteMaui;
 [Activity(Theme = "@style/Maui.SplashTheme",
     MainLauncher = true,
     LaunchMode = LaunchMode.SingleTop,
+    ResizeableActivity = true,
     ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode |
                            ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 [IntentFilter([Platform.Intent.ActionAppAction],
