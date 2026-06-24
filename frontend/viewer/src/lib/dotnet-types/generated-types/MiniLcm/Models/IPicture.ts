5246d0343cf697cc66045a1b51ab7656a29234d3
Robin Munn <rmunn@pobox.com>
2026-06-24 14:07:20 +0700

    Add TypeScript conversion of Picture
    When we changed the interface that Picture implements, we didn't commit
    the change to the generated TypeScript file.

--- frontend/viewer/src/lib/dotnet-types/generated-types/MiniLcm/Models/IPicture.ts
+++ frontend/viewer/src/lib/dotnet-types/generated-types/MiniLcm/Models/IPicture.ts
@@ -3,10 +3,9 @@
 //     Changes to this file may cause incorrect behavior and will be lost if
 //     the code is regenerated.
 
-import type {IObjectWithId} from './IObjectWithId';
 import type {IRichMultiString} from '$lib/dotnet-types/i-multi-string';
 
-export interface IPicture extends IObjectWithId
+export interface IPicture
 {
 	id: string;
 	order: number;
