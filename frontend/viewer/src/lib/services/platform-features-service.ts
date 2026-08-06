2009c02b944d530de29ca5a7fc6a7221546ca8b7
Kevin Hahn <kevin_hahn@sil.org>
2026-08-06 14:10:45 +0700

    Handle missing platform features service with fallback implementation

--- frontend/viewer/src/lib/services/platform-features-service.ts
+++ frontend/viewer/src/lib/services/platform-features-service.ts
@@ -2,8 +2,9 @@ import type {
   IPlatformFeaturesService
 } from '$lib/dotnet-types/generated-types/FwLiteShared/Services/IPlatformFeaturesService';
 import {DotnetService} from '$lib/dotnet-types';
-import {useService} from '$lib/services/service-provider';
+import {tryUseService} from '$lib/services/service-provider';
 import {SvelteMap} from 'svelte/reactivity';
+import type {ICameraResult} from '$lib/dotnet-types/generated-types/FwLiteShared/Services/ICameraResult';
 
 const cache = new SvelteMap<string, boolean>();
 const features = ['supportsImageCapture'] as const satisfies (keyof Pick<IPlatformFeaturesService, {
@@ -11,7 +12,22 @@ const features = ['supportsImageCapture'] as const satisfies (keyof Pick<IPlatfo
 }[keyof IPlatformFeaturesService]>)[];
 type Features = typeof features[number];
 export function usePlatformFeaturesService(): {service: IPlatformFeaturesService, features: Record<Features, boolean>} {
-  const service = useService(DotnetService.PlatformFeaturesService);
+  const service = tryUseService(DotnetService.PlatformFeaturesService);
+
+  if (!service) {
+    return {
+      service: {
+        captureImage(): Promise<ICameraResult> {
+          //type gen isn't working correctly, this should be Promise<ICameraResult | null>
+          return Promise.resolve(null) as unknown as Promise<ICameraResult>;
+        },
+        supportsImageCapture(): Promise<boolean> {
+          return Promise.resolve(false);
+        }
+      },
+      features: Object.fromEntries(features.map((feature) => [feature, false])) as Record<Features, boolean>
+    };
+  }
   const featuresObj = {} as Record<Features, boolean>;
   for (const feature of features) {
     if (!cache.has(feature)) {
