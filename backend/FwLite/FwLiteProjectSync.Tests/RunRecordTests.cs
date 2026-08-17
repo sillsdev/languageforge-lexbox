c20f825368c8291e1a9a539e503baf6f12facdf6
Tim Haasdyk <myieye@gmail.com>
2026-08-17 11:19:24 +0200

    Reduce test cases

--- backend/FwLite/FwLiteProjectSync.Tests/RunRecordTests.cs
+++ backend/FwLite/FwLiteProjectSync.Tests/RunRecordTests.cs
@@ -6,10 +6,6 @@ public class RunRecordTests
     [InlineData("\n")]
     [InlineData("\r")]
     [InlineData("\r\n")]
-    [InlineData("\f")]
-    [InlineData("\u0085")]
-    [InlineData("\u2028")]
-    [InlineData("\u2029")]
     public void DescriptionStaysOnOneLine(string lineEnding)
     {
         var record = new RecordingMiniLcmApi.RunRecord("CreateEntry", $"first{lineEnding}second");
